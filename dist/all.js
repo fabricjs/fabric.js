39184f5ac4b6c639766e5150b46bd9cf34509143
kangax <kangax@gmail.com>
2011-05-10 16:27:00 -0400

    Version 0.2 --> 0.2.1

--- dist/all.js
+++ dist/all.js
@@ -1,6 +1,6 @@
 /*! Fabric.js Copyright 2008-2011, Bitsonnet (Juriy Zaytsev, Maxim Chernyak) */
 
-var fabric = fabric || { version: 0.2 };
+var fabric = fabric || { version: "0.2.1" };
 
 /**
  * Wrapper around `console.log` (when available)
