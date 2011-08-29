709348f96f262c616aab2ef6fed5bee19edd8ad7
kangax <kangax@gmail.com>
2011-08-28 21:28:46 -0400

    More compliant `Array.prototype.indexOf`.

--- src/util/lang_array.js
+++ src/util/lang_array.js
@@ -1,22 +1,37 @@
-(function(){
+(function() {
   
   var slice = Array.prototype.slice;
   
   if (!Array.prototype.indexOf) {
-    Array.prototype.indexOf = function(value, from) {
-      var len = this.length >>> 0;
-      from = Number(from) || 0;
-      from = Math[from < 0 ? 'ceil' : 'floor'](from);
-      if (from < 0) {
-        from += len;
+    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+      if (this === void 0 || this === null) {
+        throw new TypeError();
       }
-      for (; from < len; from++) {
-        if (from in this && this[from] === value) {
-          return from;
+      var t = Object(this), len = t.length >>> 0;
+      if (len === 0) {
+        return -1;
+      }
+      var n = 0;
+      if (arguments.length > 0) {
+        n = Number(arguments[1]);
+        if (n !== n) { // shortcut for verifying if it's NaN
+          n = 0;
+        }
+        else if (n !== 0 && n !== (1 / 0) && n !== -(1 / 0)) {
+          n = (n > 0 || -1) * Math.floor(Math.abs(n));
+        }
+      }
+      if (n >= len) {
+        return -1;
+      }
+      var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+      for (; k < len; k++) {
+        if (k in t && t[k] === searchElement) {
+          return k;
         }
       }
       return -1;
-    };
+    }
   }
 
   if (!Array.prototype.forEach) {
