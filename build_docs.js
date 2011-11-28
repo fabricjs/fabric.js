17926a17e8e5b6af73de96aba1eb347b496da909
kangax <kangax@gmail.com>
2011-11-27 23:59:43 -0500

    Add build_docs.js script.

---
+++ build_docs.js
@@ -0,0 +1,10 @@
+var exec = require('child_process').exec;
+
+var command = 'java -jar lib/jsdoc-toolkit/jsrun.jar \
+                         lib/jsdoc-toolkit/app/run.js -a \
+                         -t=lib/jsdoc-toolkit/templates/jsdoc \
+                         -d=site/docs HEADER.js src/ src/util/';
+
+exec(command, function (error, output) { 
+  console.log(output)
+});
