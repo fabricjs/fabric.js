c39053456cf06b8b17da696f7bcc00994d23c79a
kangax <kangax@gmail.com>
2012-01-05 22:39:30 -0500

    Fix `fabric.Text#textShadow` to actually use specified color.

--- HEADER.js
+++ HEADER.js
@@ -1,6 +1,6 @@
 /*! Fabric.js Copyright 2008-2011, Bitsonnet (Juriy Zaytsev, Maxim Chernyak) */
 
-var fabric = fabric || { version: "0.7.7" };
+var fabric = fabric || { version: "0.7.8" };
 
 if (typeof exports != 'undefined') {
   exports.fabric = fabric;
