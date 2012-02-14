2ab05fdeb3466ec0cf3edfab182fba76bddfade9
kangax <kangax@gmail.com>
2012-02-14 12:50:11 -0500

    Add bunch of stuff to .npmignore

--- dist/all.js
+++ dist/all.js
@@ -1,6 +1,6 @@
 /*! Fabric.js Copyright 2008-2012, Bitsonnet (Juriy Zaytsev, Maxim Chernyak) */
 
-var fabric = fabric || { version: "0.7.22" };
+var fabric = fabric || { version: "0.7.23" };
 
 if (typeof exports != 'undefined') {
   exports.fabric = fabric;
