1214ded0a80b2791a6136f8ebbce73264a8ce77b
Oliver Caldwell <olliec87@gmail.com>
2012-07-06 15:37:42 +0100

    Added text stroking and fixed the size / height being ignored.

--- src/text.class.js
+++ src/text.class.js
@@ -225,19 +225,27 @@
       var font = [
         this.fontStyle,
         this.fontWeight,
-        this.fontSize + 'px',
-        this.lineHeight,
+        this.fontSize + 'px/' + this.lineHeight,
         this.fontFamily
       ];
 
       ctx.fillStyle = this.fill;
+      ctx.strokeStyle = this.strokeStyle;
+      ctx.lineWidth = this.strokeWidth;
+
       ctx.font = font.join(' ');
       ctx.textAlign = this.textAlign;
 
       this.transform(ctx);
       this.width = ctx.measureText(this.text).width;
       this.height = this.fontSize;
+
       ctx.fillText(this.text, -this.width / 2, 0);
+
+      if(this.strokeStyle) {
+        ctx.strokeText(this.text, -this.width / 2, 0);
+      }
+
       this.setCoords();
     },
 
