a7638431dff0fdf14c1787baffb552651b81cfb1
Oliver Caldwell <olliec87@gmail.com>
2012-07-06 15:20:05 +0100

    Added more font settings.

--- src/text.class.js
+++ src/text.class.js
@@ -221,8 +221,19 @@
      * @param {CanvasRenderingContext2D} ctx Context to render on
      */
     _render_native: function(ctx) {
+      // Create the font string
+      var font = [
+        this.fontStyle,
+        this.fontWeight,
+        this.fontSize + 'px',
+        this.lineHeight,
+        this.fontFamily
+      ];
+
       ctx.fillStyle = this.fill;
-      ctx.font = this.fontSize + 'px ' + this.fontFamily;
+      ctx.font = font.join(' ');
+      ctx.textAlign = this.textAlign;
+
       this.transform(ctx);
       this.width = ctx.measureText(this.text).width;
       this.height = this.fontSize;
