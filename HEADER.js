0371d7ba7c3aa2d4b50fd301540303ff61558b49
kangax <kangax@gmail.com>
2012-07-27 00:35:09 +0200

    Bump version to 0.8.4 (now with native text support).

--- HEADER.js
+++ HEADER.js
@@ -1,6 +1,6 @@
 /*! Fabric.js Copyright 2008-2012, Printio (Juriy Zaytsev, Maxim Chernyak) */
 
-var fabric = fabric || { version: "0.8.35" };
+var fabric = fabric || { version: "0.8.4" };
 
 if (typeof exports != 'undefined') {
   exports.fabric = fabric;
