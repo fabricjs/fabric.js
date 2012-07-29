7aaba5c227b1f01618206a41744ce842e2290777
kangax <kangax@gmail.com>
2012-07-29 22:48:34 +0200

    Make `npm test` work via node-qunit. Add .travis.yml.

---
+++ test.js
@@ -0,0 +1,29 @@
+var testrunner = require('qunit');
+
+testrunner.run({
+    code: "./dist/all.js",
+    tests: [
+      './site/test/unit/rect.js',
+      './site/test/unit/ellipse.js',
+      './site/test/unit/color.js',
+      './site/test/unit/circle.js',
+      './site/test/unit/line.js',
+      './site/test/unit/polyline.js',
+      './site/test/unit/polygon.js',
+      './site/test/unit/path.js',
+      './site/test/unit/path_group.js',
+      './site/test/unit/observable.js',
+      './site/test/unit/object.js',
+      './site/test/unit/text.js',
+      './site/test/unit/util.js',
+      './site/test/unit/image.js',
+      './site/test/unit/group.js',
+      './site/test/unit/parser.js',
+      './site/test/unit/canvas.js',
+      './site/test/unit/canvas_static.js'
+    ]
+}, function(err, report) {
+  if (err) {
+    console.log(err);
+  }
+});
