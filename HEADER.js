f99ee7e2e76522e3534d781048cdd055ea993adb
kangax <kangax@gmail.com>
2012-08-08 00:04:44 +0200

    Build distribution, bump version.

--- HEADER.js
+++ HEADER.js
@@ -1,6 +1,6 @@
 /*! Fabric.js Copyright 2008-2012, Printio (Juriy Zaytsev, Maxim Chernyak) */
 
-var fabric = fabric || { version: "0.8.43" };
+var fabric = fabric || { version: "0.8.44" };
 
 if (typeof exports != 'undefined') {
   exports.fabric = fabric;
