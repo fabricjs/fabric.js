d941e6ffe90554a053fad2c0233ba604a71ef426
Steve Pemberton <sjpemberton09@gmail.com>
2012-10-20 22:31:43 +0100

    Cleaned up code
    Created a private method on the canvas object to reset a targets
    transform.
    Reseting of _theta now done through setAngle() method to ensure
    consistency.

--- src/canvas.class.js
+++ src/canvas.class.js
@@ -649,10 +649,7 @@
       if (activeGroup) {
         if (activeGroup.contains(target)) {
           activeGroup.removeWithUpdate(target);
-          //Reset transform!
-          activeGroup.scaleX = 1;
-          activeGroup.scaleY = 1;
-          activeGroup._theta = 0;
+          this._resetObjectTransform(activeGroup);
           target.setActive(false);
           if (activeGroup.size() === 1) {
             // remove group alltogether if after removal it only contains 1 object
@@ -661,10 +658,7 @@
         }
         else {
           activeGroup.addWithUpdate(target);
-          //Reset transform!
-          activeGroup.scaleX = 1;
-          activeGroup.scaleY = 1;
-          activeGroup._theta = 0;
+          this._resetObjectTransform(activeGroup);
         }
         this.fire('selection:created', { target: activeGroup, e: e });
         activeGroup.setActive(true);
@@ -842,7 +836,17 @@
      */
     _setCursor: function (value) {
       this.upperCanvasEl.style.cursor = value;
-    },
+  },
+
+  /**
+  * @private
+  * @method _setupCurrentTransform
+  */
+  _resetObjectTransform: function (target) {
+      target.scaleX = 1;
+      target.scaleY = 1;
+      target.setAngle(0);
+  },
 
     /**
      * Sets the cursor depending on where the canvas is being hovered.
