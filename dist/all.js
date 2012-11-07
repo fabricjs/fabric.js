78f91d10bc420b9603d0d7fc621f17e016f162e3
Frank Leue <info@frankleue.de>
2012-11-07 02:04:41 +0100

    Adjusted text positioning to provide same results for svg / object

--- dist/all.js
+++ dist/all.js
@@ -13740,11 +13740,6 @@ fabric.Image.filters.Pixelate.fromObject = function(object) {
 
       this._renderTextBackground(ctx, textLines);
 
-      if (this.textAlign !== 'left') {
-        ctx.save();
-        ctx.translate(this.textAlign === 'center' ? (this.width / 2) : this.width, 0);
-      }
-
       this._setTextShadow(ctx);
       this._renderTextFill(ctx, textLines);
       this.textShadow && ctx.restore();
@@ -13857,11 +13852,11 @@ fabric.Image.filters.Pixelate.fromObject = function(object) {
     _renderTextFill: function(ctx, textLines) {
       this._boundaries = [ ];
       for (var i = 0, len = textLines.length; i < len; i++) {
-        ctx.fillText(
-          textLines[i],
-          -this.width / 2,
-          (-this.height / 2) + (i * this.fontSize * this.lineHeight) + this.fontSize
-        );
+          ctx.fillText(
+              textLines[i],
+              0,
+              i * this.fontSize * this.lineHeight
+          );
       }
     },
 
@@ -13874,8 +13869,8 @@ fabric.Image.filters.Pixelate.fromObject = function(object) {
         for (var i = 0, len = textLines.length; i < len; i++) {
           ctx.strokeText(
             textLines[i],
-            -this.width / 2,
-            (-this.height / 2) + (i * this.fontSize * this.lineHeight) + this.fontSize
+            0,
+            i * this.fontSize * this.lineHeight
           );
         }
       }
@@ -13896,10 +13891,10 @@ fabric.Image.filters.Pixelate.fromObject = function(object) {
           var lineLeftOffset = this._getLineLeftOffset(lineWidth);
 
           ctx.fillRect(
-            (-this.width / 2) + lineLeftOffset,
-            (-this.height / 2) + (i * this.fontSize * this.lineHeight),
+            0 + lineLeftOffset,
+            (i-1) * this.fontSize * this.lineHeight,
             lineWidth,
-            this.fontSize
+            this.fontSize * this.lineHeight
           );
         }
         ctx.restore();
@@ -14235,7 +14230,7 @@ fabric.Image.filters.Pixelate.fromObject = function(object) {
    */
   fabric.Text.ATTRIBUTE_NAMES =
     ('x y fill fill-opacity opacity stroke stroke-width transform ' +
-     'font-family font-style font-weight font-size text-decoration').split(' ');
+     'font-family font-style font-weight font-size text-decoration text-anchor').split(' ');
 
   /**
    * Returns fabric.Text instance from an object representation
@@ -14265,19 +14260,14 @@ fabric.Image.filters.Pixelate.fromObject = function(object) {
     var parsedAttributes = fabric.parseAttributes(element, fabric.Text.ATTRIBUTE_NAMES);
     options = fabric.util.object.extend((options ? fabric.util.object.clone(options) : { }), parsedAttributes);
 
+    // convert svg alignment to canvas alignment
+    var map = {start:'left', middle:'center', end:'right'};
+    if(options['text-anchor']){
+        options.textAlign = map[options['text-anchor']];
+        delete options['text-anchor']
+    }
+
     var text = new fabric.Text(element.textContent, options);
-
-    /*
-      Adjust positioning:
-        x/y attributes in SVG correspond to the bottom-left corner of text bounding box
-        top/left properties in Fabric correspond to center point of text bounding box
-    */
-
-    text.set({
-      left: text.getLeft() + text.getWidth() / 2,
-      top: text.getTop() - text.getHeight() / 2
-    });
-
     return text;
   };
 
