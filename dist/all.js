ec469b6419fe77c5ba24cafe6a0149eb1de5f510
kangax <kangax@gmail.com>
2013-01-15 00:08:05 +0100

    Version 1.0.1

--- dist/all.js
+++ dist/all.js
@@ -1,7 +1,7 @@
 /* build: `node build.js modules=ALL exclude=gestures` */
 /*! Fabric.js Copyright 2008-2012, Printio (Juriy Zaytsev, Maxim Chernyak) */
 
-var fabric = fabric || { version: "1.0.0" };
+var fabric = fabric || { version: "1.0.1" };
 
 if (typeof exports !== 'undefined') {
   exports.fabric = fabric;
