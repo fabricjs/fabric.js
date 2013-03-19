da9cda6c9031bd12dde260fe7c649775e44efd9f
kangax <kangax@gmail.com>
2013-03-19 16:05:13 +0100

    [BACK_INCOMPAT] Version 1.1.2.
    canvas.freeDrawingColor -> canvas.freeDrawingBrush.color;
    canvas.freeDrawingLineWidth -> canvas.freeDrawingBrush.width;

--- HEADER.js
+++ HEADER.js
@@ -1,6 +1,6 @@
 /*! Fabric.js Copyright 2008-2013, Printio (Juriy Zaytsev, Maxim Chernyak) */
 
-var fabric = fabric || { version: "1.1.1" };
+var fabric = fabric || { version: "1.1.2" };
 
 if (typeof exports !== 'undefined') {
   exports.fabric = fabric;
--- dist/all.js
+++ dist/all.js
@@ -1,7 +1,7 @@
 /* build: `node build.js modules=ALL exclude=gestures` */
 /*! Fabric.js Copyright 2008-2013, Printio (Juriy Zaytsev, Maxim Chernyak) */
 
-var fabric = fabric || { version: "1.1.1" };
+var fabric = fabric || { version: "1.1.2" };
 
 if (typeof exports !== 'undefined') {
   exports.fabric = fabric;
