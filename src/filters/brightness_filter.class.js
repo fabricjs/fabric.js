86211daa9acac14ac5e82350cd14833fdf9b8f70
Kienz <stefankienzle.de@gmail.com>
2013-05-28 21:44:10 +0200

    Fix indentation (2 whitespaces) Add opaque to fabric.Image.filters.Convolute.toJSON Update docs

--- src/filters/brightness_filter.class.js
+++ src/filters/brightness_filter.class.js
@@ -4,66 +4,67 @@
  */
 fabric.Image.filters = fabric.Image.filters || { };
 
-
 /**
  * Brightness filter class
  * @class fabric.Image.filters.Brightness
  * @memberOf fabric.Image.filters
  */
-fabric.Image.filters.Brightness = fabric.util.createClass( /** @lends fabric.Image.filters.Brightness.prototype */ {
+fabric.Image.filters.Brightness = fabric.util.createClass(/** @lends fabric.Image.filters.Brightness.prototype */ {
 
-    /**
-     * Filter type
-     * @param {String} type
-     */
-    type: "Brightness",
+  /**
+   * Filter type
+   * @param {String} type
+   * @default
+   */
+  type: 'Brightness',
 
-    /**
-     * Constructor
-     * @memberOf fabric.Image.filters.Brightness.prototype
-     * @param {Object} [options] Options object
-     */
-    initialize: function(options) {
-        options || (options = { });
-        this.brightness = options.brightness || 100;
-    },
+  /**
+   * Constructor
+   * @memberOf fabric.Image.filters.Brightness.prototype
+   * @param {Object} [options] Options object
+   */
+  initialize: function(options) {
+    options = options || { };
+    this.brightness = options.brightness || 100;
+  },
 
-    /**
-     * Applies filter to canvas element
-     * @param {Object} canvasEl Canvas element to apply filter to
-     */
-    applyTo: function(canvasEl) {
-        var context = canvasEl.getContext('2d'),
-            imageData = context.getImageData(0, 0, canvasEl.width, canvasEl.height),
-            data = imageData.data,
-            brightness = this.brightness;
+  /**
+   * Applies filter to canvas element
+   * @param {Object} canvasEl Canvas element to apply filter to
+   */
+  applyTo: function(canvasEl) {
+    var context = canvasEl.getContext('2d'),
+        imageData = context.getImageData(0, 0, canvasEl.width, canvasEl.height),
+        data = imageData.data,
+        brightness = this.brightness;
 
-        for (var i = 0, len = data.length; i < len; i += 4) {
-            data[i] += brightness;
-            data[i + 1] += brightness;
-            data[i + 2] += brightness;
-        }
-
-        context.putImageData(imageData, 0, 0);
-    },
-
-    /**
-     * Returns json representation of filter
-     * @return {String} json representation of filter
-     */
-    toJSON: function() {
-        return {
-            type: this.type,
-            brightness: this.brightness
-        };
+    for (var i = 0, len = data.length; i < len; i += 4) {
+      data[i] += brightness;
+      data[i + 1] += brightness;
+      data[i + 2] += brightness;
     }
+
+    context.putImageData(imageData, 0, 0);
+  },
+
+  /**
+   * Returns json representation of filter
+   * @return {String} json representation of filter
+   */
+  toJSON: function() {
+    return {
+      type: this.type,
+      brightness: this.brightness
+    };
+  }
 });
 
 /**
  * Returns filter instance from an object representation
  * @static
- * @return {fabric.Image.filters.Brightness}
+ * @param {Object} object Object to create an instance from
+ * @return {fabric.Image.filters.Brightness} Instance of fabric.Image.filters.Brightness
  */
 fabric.Image.filters.Brightness.fromObject = function(object) {
-    return new fabric.Image.filters.Brightness(object);
+  return new fabric.Image.filters.Brightness(object);
 };
--- src/filters/noise_filter.class.js
+++ src/filters/noise_filter.class.js
@@ -4,69 +4,70 @@
  */
 fabric.Image.filters = fabric.Image.filters || { };
 
-
 /**
  * Noise filter class
  * @class fabric.Image.filters.Noise
  * @memberOf fabric.Image.filters
  */
-fabric.Image.filters.Noise = fabric.util.createClass( /** @lends fabric.Image.filters.Noise.prototype */ {
+fabric.Image.filters.Noise = fabric.util.createClass(/** @lends fabric.Image.filters.Noise.prototype */ {
 
-    /**
-     * Filter type
-     * @param {String} type
-     */
-    type: "Noise",
+  /**
+   * Filter type
+   * @param {String} type
+   * @default
+   */
+  type: 'Noise',
 
-    /**
-     * Constructor
-     * @memberOf fabric.Image.filters.Noise.prototype
-     * @param {Object} [options] Options object
-     */
-    initialize: function(options) {
-        options || (options = { });
-        this.noise = options.noise || 100;
-    },
+  /**
+   * Constructor
+   * @memberOf fabric.Image.filters.Noise.prototype
+   * @param {Object} [options] Options object
+   */
+  initialize: function(options) {
+    options = options || { };
+    this.noise = options.noise || 100;
+  },
 
-    /**
-     * Applies filter to canvas element
-     * @param {Object} canvasEl Canvas element to apply filter to
-     */
-    applyTo: function(canvasEl) {
-        var context = canvasEl.getContext('2d'),
-            imageData = context.getImageData(0, 0, canvasEl.width, canvasEl.height),
-            data = imageData.data,
-            noise = this.noise, rand;
+  /**
+   * Applies filter to canvas element
+   * @param {Object} canvasEl Canvas element to apply filter to
+   */
+  applyTo: function(canvasEl) {
+    var context = canvasEl.getContext('2d'),
+        imageData = context.getImageData(0, 0, canvasEl.width, canvasEl.height),
+        data = imageData.data,
+        noise = this.noise, rand;
 
-        for (var i = 0, len = data.length; i < len; i += 4) {
+    for (var i = 0, len = data.length; i < len; i += 4) {
 
-            rand = (0.5 - Math.random()) * noise;
+      rand = (0.5 - Math.random()) * noise;
 
-            data[i] += rand;
-            data[i + 1] += rand;
-            data[i + 2] += rand;
-        }
-
-        context.putImageData(imageData, 0, 0);
-    },
-
-    /**
-     * Returns json representation of filter
-     * @return {String} json representation of filter
-     */
-    toJSON: function() {
-        return {
-            type: this.type,
-            noise: this.noise
-        };
+      data[i] += rand;
+      data[i + 1] += rand;
+      data[i + 2] += rand;
     }
+
+    context.putImageData(imageData, 0, 0);
+  },
+
+  /**
+   * Returns json representation of filter
+   * @return {String} json representation of filter
+   */
+  toJSON: function() {
+    return {
+      type: this.type,
+      noise: this.noise
+    };
+  }
 });
 
 /**
  * Returns filter instance from an object representation
  * @static
- * @return {fabric.Image.filters.Noise}
+ * @param {Object} object Object to create an instance from
+ * @return {fabric.Image.filters.Noise} Instance of fabric.Image.filters.Noise
  */
 fabric.Image.filters.Noise.fromObject = function(object) {
-    return new fabric.Image.filters.Noise(object);
+  return new fabric.Image.filters.Noise(object);
 };
