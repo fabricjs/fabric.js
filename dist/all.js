3042d0d46e0ec1a6b4bac3e5958b653b0cff750c
kangax <kangax@gmail.com>
2013-06-01 16:36:57 +0200

    Update distribution

--- dist/all.js
+++ dist/all.js
@@ -10095,35 +10095,35 @@ fabric.util.object.extend(fabric.StaticCanvas.prototype, /** @lends fabric.Stati
     angle:                    0,
 
     /**
-     * Size of object's corners (in pixels)
+     * Size of object's controlling corners (in pixels)
      * @type Number
      * @default
      */
     cornerSize:               12,
 
     /**
-     * When true, object's corners are rendered as transparent inside (i.e. stroke instead of fill)
+     * When true, object's controlling corners are rendered as transparent inside (i.e. stroke instead of fill)
      * @type Boolean
      * @default
      */
     transparentCorners:       true,
 
     /**
-     * Padding between object and its borders (in pixels)
+     * Padding between object and its controlling borders (in pixels)
      * @type Number
      * @default
      */
     padding:                  0,
 
     /**
-     * Border color of an object (when it's active)
+     * Color of controlling borders of an object (when it's active)
      * @type String
      * @default
      */
     borderColor:              'rgba(102,153,255,0.75)',
 
     /**
-     * Corner color of an object (when it's active)
+     * Color of controlling corners of an object (when it's active)
      * @type String
      * @default
      */
@@ -10198,14 +10198,14 @@ fabric.util.object.extend(fabric.StaticCanvas.prototype, /** @lends fabric.Stati
     shadow:                   null,
 
     /**
-     * Border opacity when object is active and moving
+     * Opacity of object's controlling borders when object is active and moving
      * @type Number
      * @default
      */
     borderOpacityWhenMoving:  0.4,
 
     /**
-     * Border scale factor
+     * Scale factor of object's controlling borders
      * @type Number
      * @default
      */
@@ -10246,21 +10246,21 @@ fabric.util.object.extend(fabric.StaticCanvas.prototype, /** @lends fabric.Stati
     hasControls:              true,
 
     /**
-     * When set to `false`, object's borders are not rendered
+     * When set to `false`, object's controlling borders are not rendered
      * @type Boolean
      * @default
      */
     hasBorders:               true,
 
     /**
-     * When set to `false`, object's rotating point will not be visible or selectable
+     * When set to `false`, object's controlling rotating point will not be visible or selectable
      * @type Boolean
      * @default
      */
     hasRotatingPoint:         true,
 
     /**
-     * Offset for object's rotating point (when enabled via `hasRotatingPoint`)
+     * Offset for object's controlling rotating point (when enabled via `hasRotatingPoint`)
      * @type Number
      * @default
      */
