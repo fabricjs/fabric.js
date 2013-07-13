35243428e57bb4dc6b955c889fe808938902bec2
kangax <kangax@gmail.com>
2013-07-13 18:57:12 +0200

    Build distribution. Version 1.2.3

--- dist/all.js
+++ dist/all.js
@@ -1,7 +1,7 @@
 /* build: `node build.js modules=ALL exclude=gestures` */
 /*! Fabric.js Copyright 2008-2013, Printio (Juriy Zaytsev, Maxim Chernyak) */
 
-var fabric = fabric || { version: "1.2.2" };
+var fabric = fabric || { version: "1.2.3" };
 
 if (typeof exports !== 'undefined') {
   exports.fabric = fabric;
