86eaff99ed286c1bf517dc9d3b5168c5972c7880
kangax <kangax@gmail.com>
2013-07-13 19:59:31 +0200

    Bring jsdom back to 0.5.6

--- HEADER.js
+++ HEADER.js
@@ -1,6 +1,6 @@
 /*! Fabric.js Copyright 2008-2013, Printio (Juriy Zaytsev, Maxim Chernyak) */
 
-var fabric = fabric || { version: "1.2.3" };
+var fabric = fabric || { version: "1.2.4" };
 
 if (typeof exports !== 'undefined') {
   exports.fabric = fabric;
--- dist/all.js
+++ dist/all.js
@@ -1,7 +1,7 @@
 /* build: `node build.js modules=ALL exclude=gestures` */
 /*! Fabric.js Copyright 2008-2013, Printio (Juriy Zaytsev, Maxim Chernyak) */
 
-var fabric = fabric || { version: "1.2.3" };
+var fabric = fabric || { version: "1.2.4" };
 
 if (typeof exports !== 'undefined') {
   exports.fabric = fabric;
