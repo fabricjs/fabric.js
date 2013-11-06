dcf1e1e98025f88cd69aeaadd5fd3b488e5f2a7d
kangax <kangax@gmail.com>
2013-11-06 21:16:44 +0100

    Add data-url crossOrigin workaround

--- src/util/misc.js
+++ src/util/misc.js
@@ -159,7 +159,13 @@
         callback && callback.call(context, null, true);
         img = img.onload = img.onerror = null;
       };
-      img.crossOrigin = crossOrigin || '';
+      // data-urls appear to be buggy with crossOrigin
+      // https://github.com/kangax/fabric.js/commit/d0abb90f1cd5c5ef9d2a94d3fb21a22330da3e0a#commitcomment-4513767
+      // see https://code.google.com/p/chromium/issues/detail?id=315152
+      //     https://bugzilla.mozilla.org/show_bug.cgi?id=935069
+      if (url.indexOf('data') !== 0) {
+        img.crossOrigin = crossOrigin || '';
+      }
       img.src = url;
     }
     else {
