6026dc62ba789792781cc17933f6436141bef563
kangax <kangax@gmail.com>
2013-11-27 22:53:21 +0100

    Version 1.4.0

--- dist/all.js
+++ dist/all.js
@@ -1,7 +1,7 @@
 /* build: `node build.js modules=ALL exclude=gestures,cufon,json minifier=uglifyjs` */
 /*! Fabric.js Copyright 2008-2013, Printio (Juriy Zaytsev, Maxim Chernyak) */
 
-var fabric = fabric || { version: "1.3.12" };
+var fabric = fabric || { version: "1.4.0" };
 if (typeof exports !== 'undefined') {
   exports.fabric = fabric;
 }
