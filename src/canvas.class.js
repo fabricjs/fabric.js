88157a810786d78e1a464aa1b625a7313a603d00
GordoRank <gordon@ripplesoup.com>
2014-01-24 09:44:08 +0000

    Optimize searchPossibleTargets

--- src/canvas.class.js
+++ src/canvas.class.js
@@ -770,6 +770,23 @@
       var possibleTargets = [],
           target,
           pointer = this.getPointer(e);
+          
+          
+      // Check active object first and short-circuit if possible    
+      if (this._activeObject &&
+          this._activeObject.visible &&
+          this._activeObject.evented &&
+          this.containsPoint(e, this._activeObject)){
+        if ((this.perPixelTargetFind || this._activeObject.perPixelTargetFind) && !this._activeObject.isEditing) {
+          var isTransparent = this.isTargetTransparent(this._activeObject, pointer.x, pointer.y);
+            if (!isTransparent) {
+              return this.relatedTarget;
+            }
+        }
+        else {
+          return this._activeObject;
+        }
+      }
 
       for (var i = this._objects.length; i--; ) {
         if (this._objects[i] &&
