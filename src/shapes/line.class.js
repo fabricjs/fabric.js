6b1e144c44b26ee4cef663033331a365ed3b77ce
Maciej Filip Szkodziński <maciej.szkodzinski@gmail.com>
2014-02-05 15:42:52 +0100

    Line coordinates are correct
    fabric.Line._setWidthHeight was assigning left and top as if both
    origins were 'center'.
    
    It now uses private helper methods to calculate the distances from
    left and top edges of canvas to the line origins.
    
    The data for existing Line.toObject test is updated with origin-relative
    coordinates.
    
    Rendering of line inside a path-group was assuming a 'center' origin
    for both coordinates.
    
    Context translation done before rendering for lines inside path-groups
    now uses private helper methods to calculate distances from the center
    of path-group to center of line.

--- src/shapes/line.class.js
+++ src/shapes/line.class.js
@@ -90,11 +90,11 @@
 
       this.left = 'left' in options
         ? options.left
-        : (Math.min(this.x1, this.x2) + this.width / 2);
+        : this._getLeftToOriginX();
 
       this.top = 'top' in options
         ? options.top
-        : (Math.min(this.y1, this.y2) + this.height / 2);
+        : this._getTopToOriginY();
     },
 
     /**
@@ -112,6 +112,77 @@
 
     /**
      * @private
+     * @return {Number} leftToOriginX Distance from left edge of canvas to originX of Line.
+     */
+    _getLeftToOriginX: makeEdgeToOriginGetter(
+      { // property names
+        origin: 'originX',
+        axis1: 'x1',
+        axis2: 'x2',
+        dimension: 'width',
+      },
+      { // possible values of origin
+        nearest: 'left',
+        center: 'center',
+        farthest: 'right',
+      }
+    ),
+
+    /**
+     * @private
+     * @return {Number} topToOriginY Distance from top edge of canvas to originY of Line.
+     */
+    _getTopToOriginY: makeEdgeToOriginGetter(
+      { // property names
+        origin: 'originY',
+        axis1: 'y1',
+        axis2: 'y2',
+        dimension: 'height',
+      },
+      { // possible values of origin
+        nearest: 'top',
+        center: 'center',
+        farthest: 'bottom',
+      }
+    ),
+
+    /**
+     * @private
+     * @return {Number} centerToCenterX Distance from center of path group to horizontal center of Line.
+     */
+    _getCenterToCenterX: makeCenterToCenterGetter(
+      { // property names
+        origin: 'originX',
+        coordinate: 'left',
+        dimension: 'width',
+      },
+      { // possible values of origin
+        nearest: 'left',
+        center: 'center',
+        farthest: 'right',
+      }
+    ),
+
+    /**
+     * @private
+     * @return {Number} centerToOriginY Distance from center of path group to vertical center of Line.
+     */
+    _getCenterToCenterY: makeCenterToCenterGetter(
+      { // property names
+        origin: 'originY',
+        coordinate: 'top',
+        dimension: 'height',
+      },
+      { // possible values of origin
+        nearest: 'top',
+        center: 'center',
+        farthest: 'bottom',
+      }
+    ),
+
+
+    /**
+     * @private
      * @param {CanvasRenderingContext2D} ctx Context to render on
      */
     _render: function(ctx) {
@@ -119,7 +190,10 @@
 
       var isInPathGroup = this.group && this.group.type === 'path-group';
       if (isInPathGroup && !this.transformMatrix) {
-        ctx.translate(-this.group.width/2 + this.left, -this.group.height / 2 + this.top);
+        ctx.translate(
+          this._getCenterToCenterX(),
+          this._getCenterToCenterY()
+        );
       }
 
       if (!this.strokeDashArray || this.strokeDashArray && supportsLineDash) {
@@ -252,5 +326,63 @@
     var points = [object.x1, object.y1, object.x2, object.y2];
     return new fabric.Line(points, object);
   };
+
+  /**
+   * Produces a function that calculates distance from canvas edge to Line origin.
+   */
+  function makeEdgeToOriginGetter(propertyNames, originValues) {
+    var origin = propertyNames.origin,
+        axis1 = propertyNames.axis1,
+        axis2 = propertyNames.axis2,
+        dimension = propertyNames.dimension,
+        nearest = originValues.nearest,
+        center = originValues.center,
+        farthest = originValues.farthest;
+
+    return function() {
+      switch (this.get(origin)) {
+      case nearest:
+        return Math.min(this.get(axis1), this.get(axis2));
+      case center:
+        return Math.min(this.get(axis1), this.get(axis2)) + (0.5 * this.get(dimension));
+      case farthest:
+        return Math.max(this.get(axis1), this.get(axis2));
+      }
+    };
+
+  }
+
+  /**
+   * Produces a function that calculates distance from path group center to center of Line dimension.
+   *
+   *  The context starts off at center-center of path-group, while line coords
+   *  are relative to left-top of canvas. Additionally, rendering assumes contex
+   *  will start at center-center of line.
+   *  To get coords of line's center-center relative to center-center of
+   *  path-group we subtract the distance from center of PG to it's edge. 
+   *  To get the center of line, we add the distance from it's origin to it's
+   *  center.
+   * 
+   */
+  function makeCenterToCenterGetter(propertyNames, originValues) {
+    var origin = propertyNames.origin,
+        coordinate = propertyNames.coordinate,
+        dimension = propertyNames.dimension,
+        nearest = originValues.nearest,
+        center = originValues.center,
+        farthest = originValues.farthest;
+
+    return function() {
+      pathGroupCenterToEdge = (-0.5 * this.group.get(dimension))
+      switch (this.get(origin)) {
+        case nearest:
+          return pathGroupCenterToEdge + this.get(coordinate) + (0.5 * this.get(dimension));
+        case center:
+          return pathGroupCenterToEdge + this.get(coordinate);
+        case farthest:
+          return pathGroupCenterToEdge + this.get(coordinate) - (0.5 * this.get(dimension));
+      }
+    };
+  }
 
 })(typeof exports !== 'undefined' ? exports : this);
