20567e6123bc15d0b03548583e41e99cd1db69f6
kangax <kangax@gmail.com>
2014-03-08 18:36:14 -0500

    2013 -> 2014

--- HEADER.js
+++ HEADER.js
@@ -1,4 +1,4 @@
-/*! Fabric.js Copyright 2008-2013, Printio (Juriy Zaytsev, Maxim Chernyak) */
+/*! Fabric.js Copyright 2008-2014, Printio (Juriy Zaytsev, Maxim Chernyak) */
 
 var fabric = fabric || { version: "1.4.4" };
 if (typeof exports !== 'undefined') {
