80e9d47b5e0430d34f26aa374238ddbb39a5f5d0
Timo <timo.roskaposti@live.fi>
2014-03-09 18:22:05 +0200

    test/unit/path.js 6

--- test/unit/path.js
+++ test/unit/path.js
@@ -205,16 +205,16 @@
     var el = getPathElement('M100 100 l 200 200 300 300 400 -50 z');
     fabric.Path.fromElement(el, function(obj) {
 
-      deepEqual(obj.path[0], ['M', 100, 100]);
-      deepEqual(obj.path[1], ['l', 200, 200]);
-      deepEqual(obj.path[2], ['l', 300, 300]);
-      deepEqual(obj.path[3], ['l', 400, -50]);
+      deepEqual(obj.path[0], ['M', 0, 0]);
+      deepEqual(obj.path[1], ['L', 200, 200]);
+      deepEqual(obj.path[2], ['L', 500, 500]);
+      deepEqual(obj.path[3], ['L', 900, 450]);
 
       el = getPathElement('c 0,-53.25604 43.17254,-96.42858 96.42857,-96.42857 53.25603,0 96.42857,43.17254 96.42857,96.42857');
       fabric.Path.fromElement(el, function(obj) {
 
-        deepEqual(obj.path[0], ['c', 0, -53.25604, 43.17254, -96.42858, 96.42857, -96.42857]);
-        deepEqual(obj.path[1], ['c', 53.25603, 0, 96.42857, 43.17254, 96.42857, 96.42857]);
+        deepEqual(obj.path[0], ['C', 0, 43.172529999999995, 43.17254, -0.000010000000003174137, 96.42857, 0]);
+        deepEqual(obj.path[1], ['C', 149.6846, 0, 192.85714, 43.17254, 192.85714, 96.42857]);
         start();
       });
     });
@@ -224,10 +224,10 @@
     var el = getPathElement('M56.224 84.12c-.047.132-.138.221-.322.215.046-.131.137-.221.322-.215z');
     fabric.Path.fromElement(el, function(obj) {
 
-      deepEqual(obj.path[0], ['M', 56.224, 84.12]);
-      deepEqual(obj.path[1], ['c', -0.047, 0.132, -0.138, 0.221, -0.322, 0.215]);
-      deepEqual(obj.path[2], ['c', 0.046, -0.131, 0.137, -0.221, 0.322, -0.215]);
-      deepEqual(obj.path[3], ['z']);
+      deepEqual(obj.path[0], ['M', 0.32200000000000273, 0.0002829514169349068]);
+      deepEqual(obj.path[1], ['C', 0.2750000000000057, 0.1322829514169399, 0.1840000000000046, 0.22128295141693854, 0, 0.21528295141693832]);
+      deepEqual(obj.path[2], ['C', 0.045999999999999375, 0.08428295141693809, 0.13700000000000045, -0.005717048583065321, 0.32200000000000273, 0.0002829514169349068]);
+      deepEqual(obj.path[3], ['Z']);
       start();
     });
   });
@@ -236,11 +236,11 @@
     var el = getPathElement('M56.224e2 84.12E-2c-.047.132-.138.221-.322.215.046-.131.137-.221.322-.215m-.050 -20.100z');
     fabric.Path.fromElement(el, function(obj) {
 
-      deepEqual(obj.path[0], ['M', 5622.4, 0.8412]);
-      deepEqual(obj.path[1], ['c', -0.047, 0.132, -0.138, 0.221, -0.322, 0.215]);
-      deepEqual(obj.path[2], ['c', 0.046, -0.131, 0.137, -0.221, 0.322, -0.215]);
-      deepEqual(obj.path[3], ['m', -0.05, -20.100]);
-      deepEqual(obj.path[4], ['z']);
+      deepEqual(obj.path[0], ['M', 0.3220000000001164, 20.1]);
+      deepEqual(obj.path[1], ['C', 0.2750000000005457, 20.232, 0.18400000000019645, 20.321, 0, 20.315]);
+      deepEqual(obj.path[2], ['C', 0.046000000000276486, 20.184, 0.13699999999971624, 20.094, 0.3220000000001164, 20.1]);
+      deepEqual(obj.path[3], ['M', 0.2719999999999345, 0]);
+      deepEqual(obj.path[4], ['Z']);
       start();
     });
   });
