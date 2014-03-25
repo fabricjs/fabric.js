ae6649feeaadb039813caf521bb4ad9fd3849b61
Kureev Alexey <kureev-mail@ya.ru>
2014-03-25 11:36:00 +0400

    isuue #1235 - TypeError: objects is undefined

--- src/mixins/canvas_serialization.mixin.js
+++ src/mixins/canvas_serialization.mixin.js
@@ -46,6 +46,10 @@ fabric.util.object.extend(fabric.StaticCanvas.prototype, /** @lends fabric.Stati
       ? JSON.parse(json)
       : json;
 
+    if (typeof serialized.objects === 'undefined' || !(serialized.objects instanceof Array)) {
+      throw Error('JSON should contain an objects array');
+    }
+
     this.clear();
 
     var _this = this;
