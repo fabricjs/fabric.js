2e41799eac9f2adac09063e7dc4eb13192c9bdd7
Andrea Bogazzi <andreabogazzi79@gmail.com>
2014-07-24 12:07:34 +0200

    Update object.svg_export.js
    Ok this look weird, but in this way i can save some redundant translating that create me troubles later.

--- src/mixins/object.svg_export.js
+++ src/mixins/object.svg_export.js
@@ -71,9 +71,9 @@ fabric.util.object.extend(fabric.Object.prototype, /** @lends fabric.Object.prot
             toFixed(this.scaleY * vpt[3], NUM_FRACTION_DIGITS) +
           ')'),
 
-        flipXPart = this.flipX ? ' matrix(-1 0 0 1 ' + (this.width * vpt[0]) + ' 0) ' : '',
+        flipXPart = this.flipX ? ' matrix(-1 0 0 1 ' + (this.type === 'path-group' ? this.width * vpt[0] : 0) + ' 0) ' : '',
 
-        flipYPart = this.flipY ? ' matrix(1 0 0 -1 0 ' + (this.height * vpt[3]) + ')' : '';
+        flipYPart = this.flipY ? ' matrix(1 0 0 -1 0 ' + (this.type === 'path-group' ? this.height * vpt[3] : 0) + ')' : '';
 
     return [
       translatePart, anglePart, scalePart, flipXPart, flipYPart
