91f4ed636a774b2dfe733228118b074f25e6237f
Andrea Bogazzi <andreabogazzi79@gmail.com>
2014-07-25 10:38:16 +0200

    Update static_canvas.class.js

--- src/static_canvas.class.js
+++ src/static_canvas.class.js
@@ -1131,7 +1131,7 @@
         markup.push(
           '<?xml version="1.0" encoding="', (options.encoding || 'UTF-8'), '" standalone="no" ?>',
             '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" ',
-              '"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">'
+              '"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n'
         );
       }
     },
