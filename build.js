541393b9911670a6272274da38e00c8f2dd81bd5
Marc Galang <marchenrygalang@gmail.com>
2014-07-30 09:50:52 +0300

    Add blend filter. Implements several image blend modes (can add more later)

--- build.js
+++ build.js
@@ -234,6 +234,7 @@ var filesToInclude = [
   ifSpecifiedInclude('image_filters', 'src/filters/sepia2_filter.class.js'),
   ifSpecifiedInclude('image_filters', 'src/filters/tint_filter.class.js'),
   ifSpecifiedInclude('image_filters', 'src/filters/multiply_filter.class.js'),
+  ifSpecifiedInclude('image_filters', 'src/filters/blend_filter.class.js'),
 
   ifSpecifiedInclude('text', 'src/shapes/text.class.js'),
   ifSpecifiedInclude('cufon', 'src/shapes/text.cufon.js'),
