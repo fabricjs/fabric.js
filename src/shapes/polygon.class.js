856716478bed24ea5e7021b06e180810579ee904
Andrea Bogazzi <andreabogazzi79@gmail.com>
2014-08-04 00:41:46 +0200

    Update polygon.class.js

--- src/shapes/polygon.class.js
+++ src/shapes/polygon.class.js
@@ -111,7 +111,8 @@
           'points="', points.join(''),
           '" style="', this.getSvgStyles(),
           '" transform="', this.getSvgTransform(),
-        '"/>'
+          ' ', this.getSvgTransformMatrix(),
+        '"/>\n'
       );
 
       return reviver ? reviver(markup.join('')) : markup.join('');
@@ -125,7 +126,6 @@
     _render: function(ctx) {
       var point;
       ctx.beginPath();
-      ctx.globalAlpha = this.group ? (ctx.globalAlpha * this.opacity) : this.opacity;
       ctx.moveTo(this.points[0].x, this.points[0].y);
       for (var i = 0, len = this.points.length; i < len; i++) {
         point = this.points[i];
@@ -189,9 +189,6 @@
     var points = fabric.parsePointsAttribute(element.getAttribute('points')),
         parsedAttributes = fabric.parseAttributes(element, fabric.Polygon.ATTRIBUTE_NAMES);
 
-    if (!('transformMatrix' in parsedAttributes)) {
-      fabric.util.normalizePoints(points, options);
-    }
     return new fabric.Polygon(points, extend(parsedAttributes, options), true);
   };
   /* _FROM_SVG_END_ */
