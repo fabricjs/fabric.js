81782afb1ee1181c8a42ed314257053a9059f7fb
Andrea Bogazzi <andreabogazzi79@gmail.com>
2014-08-04 23:38:26 +0200

    Update object.svg_export.js

--- src/mixins/object.svg_export.js
+++ src/mixins/object.svg_export.js
@@ -70,10 +70,10 @@ fabric.util.object.extend(fabric.Object.prototype, /** @lends fabric.Object.prot
             ' ' +
             toFixed(this.scaleY * vpt[3], NUM_FRACTION_DIGITS) +
           ')'),
-
-        flipXPart = this.flipX ? ' matrix(-1 0 0 1 ' + (this.type === 'path-group' ? this.width * vpt[0] : 0) + ' 0) ' : '',
-
-        flipYPart = this.flipY ? ' matrix(1 0 0 -1 0 ' + (this.type === 'path-group' ? this.height * vpt[3] : 0) + ')' : '';
+        addTranslateX = this.type === 'path-group' ? this.width * vpt[0] : 0,
+        flipXPart = this.flipX ? ' matrix(-1 0 0 1 ' + addTranslateX + ' 0) ' : '',
+        addTranslateY = (this.type === 'path-group' ? this.height * vpt[3] : 0),
+        flipYPart = this.flipY ? ' matrix(1 0 0 -1 0 ' + addTranslateY + ')' : '';
 
     return [
       translatePart, anglePart, scalePart, flipXPart, flipYPart
