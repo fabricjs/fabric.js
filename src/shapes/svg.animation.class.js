9b03b9b25c65f5facaab9cf3624e8e3aa2fa0310
Dmitri <DRU@popesco.com>
2014-08-10 14:40:24 +0300

    New future, FabricJs Svg animation class implemented

--- src/shapes/svg.animation.class.js
+++ src/shapes/svg.animation.class.js
@@ -10,7 +10,7 @@
 		parentToObject = fabric.Object.prototype.toObject;
 
 	if (fabric.SvgAnimation) {
-		fabric.warn('fabric.PathGroup is already defined');
+		fabric.warn('fabric.SvgAnimation is already defined');
 		return;
 	}
 
