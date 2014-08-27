4c4ba5bfe0b9ef40b18f722d1e7c7c954c1e7dc5
Andrea Bogazzi <andreabogazzi79@gmail.com>
2014-08-27 23:09:31 +0200

    Update parser.js
    Added comment for future reference

--- src/parser.js
+++ src/parser.js
@@ -508,7 +508,10 @@
       var startTime = new Date();
 
       parseUseDirectives(doc);
-
+      /* http://www.w3.org/TR/SVG/struct.html#SVGElementWidthAttribute
+      *  as per specs, width and height attributes are to be considered
+      *  100% if no value is specified.
+      */
       var viewBoxAttr = doc.getAttribute('viewBox'),
           widthAttr = parseUnit(doc.getAttribute('width') || '100%'),
           heightAttr = parseUnit(doc.getAttribute('height') || '100%'),
