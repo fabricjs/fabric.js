328bf8a1b65b6a2a3ec75d80c7aabac49d70ef76
Jaffer Haider <jafferhaider@gmail.com>
2014-10-23 16:15:14 +0500

    Deleting files that shouldn't be in pull request

--- build_postermywall.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-node build.js modules=animation,interaction,gestures,image_filters,text,itext,textbox,shadow
-
-cp ./dist/fabric.js /Applications/MAMP/htdocs/pmw/assets/javascript/vendor/fabric.js
-
-echo 'Copied file to PosterMyWall web.'
