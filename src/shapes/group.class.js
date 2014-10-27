2339030ecdf47eb0e304ef6035dc6291b695020a
Jaffer Haider <jafferhaider@gmail.com>
2014-10-27 17:12:25 +0500

    Textbox support for Groups.

--- src/shapes/group.class.js
+++ src/shapes/group.class.js
@@ -202,10 +202,26 @@
         this[key] = value;
         while (i--) {
           this._objects[i].set(key, value);
-        }
-      }
+            }
+          }
       else {
         this[key] = value;
+        
+        /*
+         * Reverse the group's scaling on Textboxs and change their width
+         * instead. Size of text in a Textbox must be controlled by fontSize.
+         */
+        if(key === 'scaleX') {
+          var i = this._objects.length, o;
+          while (i--) {
+            o = this._objects[i];
+            if(o.type === 'textbox') {
+              o.set(key, 1/value);
+              o.set('width', (o.get('width') * value) / (typeof o.__oldScaleX === 'undefined' ? 1 : o.__oldScaleX));
+              o.__oldScaleX = value;
+            }
+          }
+        }
       }
     },
 
