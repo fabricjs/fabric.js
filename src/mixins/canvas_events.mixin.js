4adee053e1f6a579e045541c8a26d67113701747
Jaffer Haider <jafferhaider@gmail.com>
2014-11-05 17:07:56 +0500

    Fixed outdated multi-touch gestures code. Added support for longpress.

--- src/mixins/canvas_events.mixin.js
+++ src/mixins/canvas_events.mixin.js
@@ -54,6 +54,7 @@
         Event.add(this.upperCanvasEl, 'drag', this._onDrag);
         Event.add(this.upperCanvasEl, 'orientation', this._onOrientationChange);
         Event.add(this.upperCanvasEl, 'shake', this._onShake);
+        Event.add(this.upperCanvasEl, 'longpress', this._onLongPress);
       }
     },
 
@@ -68,6 +69,7 @@
       this._onGesture = this._onGesture.bind(this);
       this._onDrag = this._onDrag.bind(this);
       this._onShake = this._onShake.bind(this);
+      this._onLongPress = this._onLongPress.bind(this);
       this._onOrientationChange = this._onOrientationChange.bind(this);
       this._onMouseWheel = this._onMouseWheel.bind(this);
     },
@@ -90,6 +92,7 @@
         Event.remove(this.upperCanvasEl, 'drag', this._onDrag);
         Event.remove(this.upperCanvasEl, 'orientation', this._onOrientationChange);
         Event.remove(this.upperCanvasEl, 'shake', this._onShake);
+        Event.remove(this.upperCanvasEl, 'longpress', this._onLongPress);
       }
     },
 
@@ -136,6 +139,15 @@
      */
     _onShake: function(e, self) {
       this.__onShake && this.__onShake(e,self);
+    },
+    
+    /**
+     * @private
+     * @param {Event} [e] Event object fired on Event.js shake
+     * @param {Event} [self] Inner Event object
+     */
+    _onLongPress: function(e, self) {
+        this.__onLongPress && this.__onLongPress(e,self);
     },
 
     /**
@@ -514,6 +526,10 @@
 
       if (this.isDrawingMode) {
         this._onMouseMoveInDrawingMode(e);
+        return;
+      }
+      
+      if (typeof e.touches !== 'undefined' && e.touches.length > 1) {
         return;
       }
 
