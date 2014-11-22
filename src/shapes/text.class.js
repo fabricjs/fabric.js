cff556a10f955f7e67d42ce145a53ae241875c4c
Andrea Bogazzi <andreabogazzi79@gmail.com>
2014-11-22 15:14:11 +0100

    Update text.class.js

--- src/shapes/text.class.js
+++ src/shapes/text.class.js
@@ -1112,7 +1112,7 @@
     if (!options.originX) {
       options.originX = 'left';
     }
-    options.top += options.fontSize / this._fontSizeFraction: 4,;
+    options.top += options.fontSize / this._fontSizeFraction;
     var text = new fabric.Text(element.textContent, options),
         /*
           Adjust positioning:
