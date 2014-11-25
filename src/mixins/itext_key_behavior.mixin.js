b716a56e2cbbaa3c53fab9a3ed4fa9459b93045e
Andrea Bogazzi <andreabogazzi79@gmail.com>
2014-11-25 12:33:21 +0100

    Update style for hidden textbox. Fix page jump

--- src/mixins/itext_key_behavior.mixin.js
+++ src/mixins/itext_key_behavior.mixin.js
@@ -7,8 +7,8 @@ fabric.util.object.extend(fabric.IText.prototype, /** @lends fabric.IText.protot
     this.hiddenTextarea = fabric.document.createElement('textarea');
 
     this.hiddenTextarea.setAttribute('autocapitalize', 'off');
-    this.hiddenTextarea.style.cssText = 'position: absolute; top: 0; left: -9999px';
-
+    this.hiddenTextarea.style.cssText = 'position: fixed; bottom: 20px; left: 0px; opacity: 0;'
+                                        + ' width: 0px; height: 0px; z-index: -999;';
     fabric.document.body.appendChild(this.hiddenTextarea);
 
     fabric.util.addListener(this.hiddenTextarea, 'keydown', this.onKeyDown.bind(this));
