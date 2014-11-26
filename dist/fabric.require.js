b249c15d3ffed275d8e856aeb7ba0fc9e031d2cf
Fin Chen <finfin@gmail.com>
2014-11-26 11:21:16 +0800

    Update fabric.require.js

--- dist/fabric.require.js
+++ dist/fabric.require.js
@@ -23397,11 +23397,7 @@ fabric.util.object.extend(fabric.IText.prototype, /** @lends fabric.IText.protot
 
 window.fabric = fabric;
 
-// make sure exports.fabric is always defined when used as 'global' later scopes
-var exports = exports || {};
-exports.fabric = fabric;
-
-if (typeof define === 'function' && define.amd) {
+f (typeof define === 'function' && define.amd) {
   define([], function() { return fabric });
 }
 
