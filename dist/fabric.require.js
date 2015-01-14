36d947f9e8b96d03dc4614ffa67ca4dd89bb059b
kangax <kangax@gmail.com>
2015-01-14 12:35:00 +0100

    Fix docs

--- dist/fabric.require.js
+++ dist/fabric.require.js
@@ -13849,8 +13849,8 @@ fabric.util.object.extend(fabric.Object.prototype, /** @lends fabric.Object.prot
     },
 
     /**
+     * Recalculates line points given width and height
      * @private
-     * Recalculate line points from width and height.
      */
     calcLinePoints: function() {
       var xMult = this.x1 <= this.x2 ? -1 : 1,
