7f2609dd4b347ac365ef22e944f5fbbeac9e28a2
Timothy Kendall <timkendall18@gmail.com>
2015-06-05 13:13:03 -0700

    Rebuilt dist

--- dist/fabric.js
+++ dist/fabric.js
@@ -15209,10 +15209,7 @@ fabric.util.object.extend(fabric.Object.prototype, /** @lends fabric.Object.prot
       this.setOptions(options);
 
       if (!path) {
-        /*
-         * If there is no path make a blank one otherwise Fabric won't render anything
-         */
-        //fabric.warn('`path` argument is required');
+        // If there is no path make a blank one otherwise Fabric won't render anything
         path = 'M0';
       }
 
