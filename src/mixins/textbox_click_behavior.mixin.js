4c17f358e560d73ee88d83bb865f84d3269e4faa
Hussein Jafferjee <hussein@jafferjee.ca>
2015-06-15 13:57:40 -0700

    Properly detect where a click happens inside a textbox

--- src/mixins/textbox_click_behavior.mixin.js
+++ src/mixins/textbox_click_behavior.mixin.js
@@ -12,19 +12,26 @@
    * @returns {Number}
    */
   fabric.IText.prototype._getNewSelectionStartFromOffset = function (mouseOffset, prevWidth, width, index, jlen) {
-    // start by getting the iText cursor location, and then figure out how many actual new lines there are
-    var cursor = this.get2DCursorLocation(index),
-      newLines = this.text.substring(0, index).split(this._reNewline).length - 1;
+    index = override.call(this, mouseOffset, prevWidth, width, index, jlen);
 
-    // adjust index by actual new lines
-    index -= cursor.lineIndex - newLines;
+    // the index passed into the function is padded by the amount of lines from _textLines (to account for \n)
+    // we need to remove this padding, and pad it by actual lines, and / or spaces that are meant to be there
+    var tmp = 0;
+    var removed = 0;
 
-    // were we at an end of a line that is wrapping?
-    var line = this._textLines[cursor.lineIndex];
-    if (cursor.charIndex === line.length - 1 && index !== this.text.length - 1 && this.text[index + 1] !== '\n') {
-      index--;
+    // account for removed characters
+    for (var i = 0; i < this._textLines.length; i++) {
+        tmp += this._textLines[i].length;
+
+        if (tmp + removed >= index) {
+            break;
+        }
+
+        if (this.text[tmp + removed] === '\n' || this.text[tmp + removed] === ' ') {
+            removed++;
+        }
     }
 
-    return override.call(this, mouseOffset, prevWidth, width, index, jlen);
+    return index - i + removed;
   };
 })();
