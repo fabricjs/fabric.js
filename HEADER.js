fb22168f488830878cb55f6006ab2611f2d157b8
Juriy Zaytsev <kangax@gmail.com>
2015-07-10 11:13:51 -0400

    Add support for hidpi rendering on retina screens Closes #316

--- HEADER.js
+++ HEADER.js
@@ -57,3 +57,13 @@ fabric.SHARED_ATTRIBUTES = [
  */
 fabric.DPI = 96;
 fabric.reNum = '(?:[-+]?(?:\\d+|\\d*\\.\\d+)(?:e[-+]?\\d+)?)';
+
+
+/**
+ * Device Pixel Ratio
+ * @see https://developer.apple.com/library/safari/documentation/AudioVideo/Conceptual/HTML-canvas-guide/SettingUptheCanvas/SettingUptheCanvas.html
+ */
+fabric.devicePixelRatio = window.devicePixelRatio || 
+                          window.webkitDevicePixelRatio || 
+                          window.mozDevicePixelRatio || 
+                          1;
