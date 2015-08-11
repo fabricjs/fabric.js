8b491480b4020cdabfd2dc1f750feb71573fa3b5
Andrea Bogazzi <andreabogazzi79@gmail.com>
2015-08-11 03:12:38 +0200

    Optimize _wrapText's use of _measureText

--- src/shapes/textbox.class.js
+++ src/shapes/textbox.class.js
@@ -259,51 +259,45 @@
      * to.
      */
     _wrapLine: function(ctx, text, lineIndex) {
-      var maxWidth  = this.width,
-          lineWidth = this._measureText(ctx, text, lineIndex, 0);
-
-      // first case: does the whole line fit?
-      if (lineWidth < maxWidth) {
-        // if the current line is only one word, we need to keep track of it if it's a large word
-        if (text.indexOf(' ') === -1 && lineWidth > this.dynamicMinWidth) {
-          this.dynamicMinWidth = lineWidth;
-        }
-
-        return [text];
-      }
-
-      // if the whole line doesn't fit, we break it up into words
-      var lines            = [],
+      var lineWidth        = 0,
+          lines            = [],
           line             = '',
           words            = text.split(' '),
+          word             = '',
           offset           = 0,
-          infix            = '',
+          infix            = ' ',
           wordWidth        = 0,
+          infixWidth       = 0,
           largestWordWidth = 0;
 
-      while (words.length > 0) {
-        infix = line === '' ? '' : ' ';
-        wordWidth = this._measureText(ctx, words[0], lineIndex, line.length + infix.length + offset);
-        lineWidth = line === '' ? wordWidth : this._measureText(ctx, line + infix + words[0], lineIndex, offset);
+      for (var i = 0; i < words.length; i++) {
+        word = words[0];
+        wordWidth = this._measureText(ctx, word, lineIndex, offset);
+        offset += word.length;
 
-        if (lineWidth < maxWidth || (line === '' && wordWidth >= maxWidth)) {
-          line += infix + words.shift();
-        }
-        else {
-          offset += line.length + 1; // add 1 because each word is separated by a space
+        lineWidth += infixWidth + wordWidth;
+
+        if (lineWidth >= this.width && line !== '') {
           lines.push(line);
           line = '';
+          lineWidth = wordWidth;
         }
 
-        if (words.length === 0) {
-          lines.push(line);
+        if (line !== '' || word === '') {
+          line += infix;
         }
+        line += word;
+
+        infixWidth = this._measureText(ctx, infix, lineIndex, offset);
+        offset++;
 
         // keep track of largest word
         if (wordWidth > largestWordWidth) {
           largestWordWidth = wordWidth;
         }
       }
+
+      i && lines.push(line);
 
       if (largestWordWidth > this.dynamicMinWidth) {
         this.dynamicMinWidth = largestWordWidth;
