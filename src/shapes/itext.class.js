dae413f7452fb7dff024be12e0270063f8ab6479
Andrea Bogazzi <andreabogazzi79@gmail.com>
2015-08-12 07:09:05 +0200

    Update itext.class.js

--- src/shapes/itext.class.js
+++ src/shapes/itext.class.js
@@ -1121,16 +1121,16 @@
      * @return {Object} object representation of an instance
      */
     toObject: function(propertiesToInclude) {
-      var newStyle = { }, i, j, row;
+      var clonedStyles = { }, i, j, row;
       for (i in this.styles) {
         row = this.styles[i];
-        newStyle[i] = { };
+        clonedStyles[i] = { };
         for (j in row) {
-          newStyle[i][j] = clone(row[j]);
+          clonedStyles[i][j] = clone(row[j]);
         }
       }
       return fabric.util.object.extend(this.callSuper('toObject', propertiesToInclude), {
-        styles: newStyle
+        styles: clonedStyles
       });
     }
   });
