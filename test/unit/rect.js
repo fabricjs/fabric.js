abf3133c362d96511009e8987f37cb5858a135da
Andrea Bogazzi <andreabogazzi79@gmail.com>
2015-09-02 21:28:57 +0200

    Update rect.js

--- test/unit/rect.js
+++ test/unit/rect.js
@@ -28,8 +28,6 @@
     'fillRule':                 'nonzero',
     'globalCompositeOperation': 'source-over',
     'transformMatrix':          null,
-    'skewX':                    0,
-    'skewY':                    0,
     'rx':                       0,
     'ry':                       0,
   };
@@ -104,7 +102,6 @@
     elRectWithAttrs.setAttribute('stroke-linecap', 'round');
     elRectWithAttrs.setAttribute('stroke-linejoin', 'bevil');
     elRectWithAttrs.setAttribute('stroke-miterlimit', 5);
-    elRectWithAttrs.setAttribute('skewX', 30);
     //elRectWithAttrs.setAttribute('transform', 'translate(-10,-20) scale(2) rotate(45) translate(5,10)');
 
     var rectWithAttrs = fabric.Rect.fromElement(elRectWithAttrs);
@@ -123,7 +120,6 @@
       strokeLineCap:    'round',
       strokeLineJoin:   'bevil',
       strokeMiterLimit: 5,
-      skewX:            30,
       rx:               11,
       ry:               12
     });
