7b3da5008f8360bc5f525d843eec03f4facec7a8
Andrea Bogazzi <andreabogazzi79@gmail.com>
2015-12-16 15:48:23 +0100

    use atan2 instead of atan and manual checks

--- src/util/misc.js
+++ src/util/misc.js
@@ -524,16 +524,13 @@
      * @return {Object} Components of transform
      */
     qrDecompose: function(a) {
-      var angle = atan(a[1] / a[0]) / PiBy180,
+      var angle = atan2(a[1], a[0]),
           denom = pow(a[0], 2) + pow(a[1], 2),
           scaleX = sqrt(denom),
           scaleY = (a[0] * a[3] - a[2] * a [1]) / scaleX,
-          skewX = atan((a[0] * a[2] + a[1] * a [3]) / denom);
-      if (a[0] < 0) {
-        angle += 180;
-      }
+          skewX = atan2(a[0] * a[2] + a[1] * a [3], denom);
       return {
-        angle: angle,
+        angle: angle  / PiBy180,
         scaleX: scaleX,
         scaleY: scaleY,
         skewX: skewX / PiBy180,
