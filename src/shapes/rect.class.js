f298302d35351c740196d15534c15aa27d24e47b
Andrea Bogazzi <andreabogazzi79@gmail.com>
2016-02-19 15:19:25 +0100

    Update rect.class.js

--- src/shapes/rect.class.js
+++ src/shapes/rect.class.js
@@ -221,9 +221,7 @@
 
     parsedAttributes.left = parsedAttributes.left || 0;
     parsedAttributes.top  = parsedAttributes.top  || 0;
-    var rect = new fabric.Rect(extend((options ? fabric.util.object.clone(options) : { }), parsedAttributes));
-    rect.visible = rect.visible && rect.width > 0 && rect.height > 0;
-    return rect;
+    return new fabric.Rect(extend((options ? fabric.util.object.clone(options) : { }), parsedAttributes));
   };
   /* _FROM_SVG_END_ */
 
