049d17ef268a02363c320e959a9cc21e1685620d
Andrea Bogazzi <andreabogazzi79@gmail.com>
2016-04-28 11:45:14 +0200

    Update removewhite_filter.class.js

--- src/filters/removewhite_filter.class.js
+++ src/filters/removewhite_filter.class.js
@@ -11,7 +11,7 @@
    * @memberOf fabric.Image.filters
    * @extends fabric.Image.filters.BaseFilter
    * @see {@link fabric.Image.filters.RemoveWhite#initialize} for constructor definition
-   * @see {@link http://fabricjs.com/image-filters/|ImageFilters demo}
+   * @see {@link http://fabricjs.com/image-filters|ImageFilters demo}
    * @example
    * var filter = new fabric.Image.filters.RemoveWhite({
    *   threshold: 40,
