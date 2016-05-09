0e91a3c490f16b0b036733c925f2efb1570afc74
Andrea Bogazzi <andreabogazzi79@gmail.com>
2016-05-09 11:21:05 +0200

    Update rect.js

--- test/unit/rect.js
+++ test/unit/rect.js
@@ -147,6 +147,18 @@
     equal(svg, '<rect x="-50" y="-50" rx="20" ry="30" width="100" height="100" style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform="translate(50 50)"/>\n');
   });
 
+  test('toSVG with alpha colors fill', function() {
+    var rect = new fabric.Rect({ width: 100, height: 100, fill: 'rgba(255, 0, 0, 0.5)' });
+    var svg = rect.toSVG();
+    equal(svg, '<rect x="-50" y="-50" width="100" height="100" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,0,0); fill-opacity: 0.5; fill-rule: nonzero; opacity: 1;" transform="translate(50 50)"/>\n');
+  });
+
+  test('toSVG with alpha colors stroke', function() {
+    var rect = new fabric.Rect({ width: 100, height: 100, fill: '', stroke: 'rgba(255, 0, 0, 0.5)' });
+    var svg = rect.toSVG();
+    equal(svg, '<rect x="-50" y="-50" width="100" height="100" style="stroke: rgb(255, 0, 0); stroke-opacity: 0.5; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(50 50)"/>\n');
+  });
+
   test('toObject without default values', function() {
     var options = { type: 'rect', width: 69, height: 50, left: 10, top: 20 };
     var rect = new fabric.Rect(options);
