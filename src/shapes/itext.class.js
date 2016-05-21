fabricjs/fabric.js

commit dfb5e4e9033057bbe0bf0d6eb638601c1aa7d1c2

Clear only the itext area top context when animating cursor or selection (#2996)

src/shapes/itext.class.js
@@ -493,13 +493,12 @@
 
       ctx.fillStyle = this.getCurrentCharColor(lineIndex, charIndex);
       ctx.globalAlpha = this.__isMousedown ? 1 : this._currentCursorOpacity;
-
+      ctx.clearRect(-this.width / 2, -this.height / 2, this.width, this.height);
       ctx.fillRect(
         boundaries.left + leftOffset,
         boundaries.top + boundaries.topOffset,
         this.cursorWidth / this.scaleX,
         charHeight);
-
     },
 
     /**
@@ -540,7 +539,7 @@
             boxWidth += this._getWidthOfChar(ctx, line[j2], i, j2);
           }
         }
-
+        ctx.clearRect(-this.width / 2, -this.height / 2, this.width, this.height);
         ctx.fillRect(
           boundaries.left + lineOffset,
           boundaries.top + boundaries.topOffset,

commit dd0b9f63d3ecc1719c53ab985147a5cc4f99815a

fix multi line selection regression introduced with #2996

src/shapes/itext.class.js
@@ -515,7 +515,7 @@
           end = this.get2DCursorLocation(this.selectionEnd),
           startLine = start.lineIndex,
           endLine = end.lineIndex;
-
+      ctx.clearRect(-this.width / 2, -this.height / 2, this.width, this.height);
       for (var i = startLine; i <= endLine; i++) {
         var lineOffset = this._getLineLeftOffset(this._getLineWidth(ctx, i)) || 0,
             lineHeight = this._getHeightOfLine(this.ctx, i),
@@ -539,7 +539,6 @@
             boxWidth += this._getWidthOfChar(ctx, line[j2], i, j2);
           }
         }
-        ctx.clearRect(-this.width / 2, -this.height / 2, this.width, this.height);
         ctx.fillRect(
           boundaries.left + lineOffset,
           boundaries.top + boundaries.topOffset,