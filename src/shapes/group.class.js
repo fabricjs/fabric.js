419435c796d5dcfa0e85dcc1d765cfd5c7808772
Samuel Horwitz <sam@kargo.com>
2016-05-26 16:45:20 -0400

    Fixing an oversight with regards to groups nested more than 2 layers deep and how add/remove/insert works on those groups

--- src/shapes/group.class.js
+++ src/shapes/group.class.js
@@ -148,12 +148,13 @@
      * @chainable
      */
     insertWithUpdate: function(object, index, nonSplicing) {
-      if (this.group) {
-        var parentGroup = this.group,
-            index = parentGroup._objects.indexOf(this);
+      var parentGroups = [],
+          parentGroup;
 
-        parentGroup.removeWithUpdate(this);
-      }
+      this.trickleThroughGroups(function(g, child) {
+        parentGroups.push({group: g, child: child, index: g._objects.indexOf(child)});
+        g.removeWithUpdate(child);
+      }, this);
 
       this._restoreObjectsState();
       fabric.util.resetObjectTransform(this);
@@ -177,8 +178,9 @@
       this._calcBounds();
       this._updateObjectsCoords();
 
-      if (parentGroup) {
-        parentGroup.insertWithUpdate(this, index);
+      for (var i = 0; i < parentGroups.length; i++) {
+        parentGroup = parentGroups[i];
+        parentGroup.group.insertWithUpdate(parentGroup.child, parentGroup.index);
       }
 
       return this;
