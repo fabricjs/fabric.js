6d5cf34e4227db05d714df31f922d1c633d8c4bc
Asturur <andreabogazzi79@gmail.com>
2016-10-02 09:30:38 +0200

    fix src to svg

--- src/shapes/image.class.js
+++ src/shapes/image.class.js
@@ -295,7 +295,7 @@
      */
     toSVG: function(reviver) {
       var markup = this._createBaseSVGMarkup(), x = -this.width / 2, y = -this.height / 2,
-          preserveAspectRatio = 'none';
+          preserveAspectRatio = 'none', filtered = true;
       if (this.group && this.group.type === 'path-group') {
         x = this.left;
         y = this.top;
@@ -318,7 +318,7 @@
         this.shadow = null;
       }
       markup.push(
-        '<image ', this.getSvgId(), 'xlink:href="', this.getSvgSrc(),
+        '<image ', this.getSvgId(), 'xlink:href="', this.getSvgSrc(filtered),
           '" x="', x, '" y="', y,
           '" style="', this.getSvgStyles(),
           // we're essentially moving origin of transformation from top/left corner to the center of the shape
@@ -350,10 +350,11 @@
 
     /**
      * Returns source of an image
+     * @param {Boolean} filtered indicates if the src is needed for svg
      * @return {String} Source of an image
      */
-    getSrc: function() {
-      var element = this._originalElement;
+    getSrc: function(filtered) {
+      var element = filtered ? this._element : this._originalElement;
       if (element) {
         return fabric.isLikelyNode ? element._src : element.src;
       }
