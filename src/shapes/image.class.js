89c379dda351a2a7a9c242488425293a058beba8
Asturur <andreabogazzi79@gmail.com>
2017-03-25 18:44:09 +0100

    support svg crossorigin on images

--- src/shapes/image.class.js
+++ src/shapes/image.class.js
@@ -646,7 +646,7 @@
    * @see {@link http://www.w3.org/TR/SVG/struct.html#ImageElement}
    */
   fabric.Image.ATTRIBUTE_NAMES =
-    fabric.SHARED_ATTRIBUTES.concat('x y width height preserveAspectRatio xlink:href'.split(' '));
+    fabric.SHARED_ATTRIBUTES.concat('x y width height preserveAspectRatio xlink:href crossOrigin'.split(' '));
 
   /**
    * Returns {@link fabric.Image} instance from an SVG element
