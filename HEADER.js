42503ea46758a5a9de96b14e4ebbb707e8b854a3
Asturur <andreabogazzi79@gmail.com>
2017-07-22 23:10:16 +0200

    build beta4

--- HEADER.js
+++ HEADER.js
@@ -1,6 +1,6 @@
  /*! Fabric.js Copyright 2008-2015, Printio (Juriy Zaytsev, Maxim Chernyak) */
 
-var fabric = fabric || { version: '2.0.0-beta3' };
+var fabric = fabric || { version: '2.0.0-beta4' };
 if (typeof exports !== 'undefined') {
   exports.fabric = fabric;
 }
