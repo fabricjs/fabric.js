ffc6ddc5bb076e5a6f300d55476c4eaba961fa1a
Asturur <andreabogazzi79@gmail.com>
2017-09-07 12:26:10 +0200

    linting tests

--- test/unit/itext_click_behaviour.js
+++ test/unit/itext_click_behaviour.js
@@ -3,28 +3,28 @@
     var iText = new fabric.IText('test need some word\nsecond line');
     var index = 10;
     var jlen = 20;
-    var selection = iText._getNewSelectionStartFromOffset({ y: 1, x: 1000 }, 500, 520, index, jlen)
-    equal(selection, index, 'index value did not change')
+    var selection = iText._getNewSelectionStartFromOffset({ y: 1, x: 1000 }, 500, 520, index, jlen);
+    equal(selection, index, 'index value did not change');
   });
   test('_getNewSelectionStartFromOffset middle of line', function() {
     var iText = new fabric.IText('test need some word\nsecond line');
     var index = 10;
     var jlen = 20;
-    var selection = iText._getNewSelectionStartFromOffset({ y: 1, x: 519 }, 500, 520, index, jlen)
+    var selection = iText._getNewSelectionStartFromOffset({ y: 1, x: 519 }, 500, 520, index, jlen);
     equal(selection, index + 1, 'index value was moved to next char, since is very near');
   });
   test('_getNewSelectionStartFromOffset middle of line', function() {
     var iText = new fabric.IText('test need some word\nsecond line');
     var index = 10;
     var jlen = 20;
-    var selection = iText._getNewSelectionStartFromOffset({ y: 1, x: 511 }, 500, 520, index, jlen)
+    var selection = iText._getNewSelectionStartFromOffset({ y: 1, x: 511 }, 500, 520, index, jlen);
     equal(selection, index, 'index value was NOT moved to next char, since is very near');
   });
   test('_getNewSelectionStartFromOffset middle of line', function() {
     var iText = new fabric.IText('test need some word\nsecond line');
     var index = 10;
     var jlen = 10;
-    var selection = iText._getNewSelectionStartFromOffset({ y: 1, x: 1000 }, 500, 520, index, jlen)
+    var selection = iText._getNewSelectionStartFromOffset({ y: 1, x: 1000 }, 500, 520, index, jlen);
     equal(selection, index, 'index value was NOT moved to next char, since is already at end of text');
   });
-})
+})();
