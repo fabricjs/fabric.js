ceb1d196082de3e119f72fcc75075950ec77f7eb
Asturur <andreabogazzi79@gmail.com>
2017-09-07 12:19:39 +0200

    added test file

---
+++ test/unit/itext_click_behaviour.js
@@ -0,0 +1,30 @@
+(function(){
+  test('_getNewSelectionStartFromOffset end of line', function() {
+    var iText = new fabric.IText('test need some word\nsecond line');
+    var index = 10;
+    var jlen = 20;
+    var selection = iText._getNewSelectionStartFromOffset({ y: 1, x: 1000 }, 500, 520, index, jlen)
+    equal(selection, index, 'index value did not change')
+  });
+  test('_getNewSelectionStartFromOffset middle of line', function() {
+    var iText = new fabric.IText('test need some word\nsecond line');
+    var index = 10;
+    var jlen = 20;
+    var selection = iText._getNewSelectionStartFromOffset({ y: 1, x: 519 }, 500, 520, index, jlen)
+    equal(selection, index + 1, 'index value was moved to next char, since is very near');
+  });
+  test('_getNewSelectionStartFromOffset middle of line', function() {
+    var iText = new fabric.IText('test need some word\nsecond line');
+    var index = 10;
+    var jlen = 20;
+    var selection = iText._getNewSelectionStartFromOffset({ y: 1, x: 511 }, 500, 520, index, jlen)
+    equal(selection, index, 'index value was NOT moved to next char, since is very near');
+  });
+  test('_getNewSelectionStartFromOffset middle of line', function() {
+    var iText = new fabric.IText('test need some word\nsecond line');
+    var index = 10;
+    var jlen = 10;
+    var selection = iText._getNewSelectionStartFromOffset({ y: 1, x: 1000 }, 500, 520, index, jlen)
+    equal(selection, index, 'index value was NOT moved to next char, since is already at end of text');
+  });
+})
