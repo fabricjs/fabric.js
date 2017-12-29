d0d50bbfa594641b4a201751e43ec9c6a3f32565
Andrea Bogazzi <andreabogazzi79@gmail.com>
2017-12-29 09:34:43 +0100

    Revert "removed this.ctx from Textbox" (#4590)
    * Revert "version 1.7.21 (#4582)"
    
    This reverts commit f555602013a4564eeadf8516f929df95ce5076d3.
    
    * Revert "removed this.ctx from Textbox (#4476)"
    
    This reverts commit e5eef91c2c0b3ba81f26b83b7574c589bc8a39bc.

--- src/shapes/textbox.class.js
+++ src/shapes/textbox.class.js
@@ -78,8 +78,10 @@
      * @return {fabric.Textbox} thisArg
      */
     initialize: function(text, options) {
+
       this.callSuper('initialize', text, options);
       this.setControlsVisibility(fabric.Textbox.getTextboxControlVisibility());
+      this.ctx = this.objectCaching ? this._cacheContext : fabric.util.createCanvasElement().getContext('2d');
     },
 
     /**
