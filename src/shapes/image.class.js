0076a58c7a87931f93ba8297343463b881282a2c
Asturur <andreabogazzi79@gmail.com>
2018-03-16 04:39:20 +0100

    applied to resize filter too

--- src/shapes/image.class.js
+++ src/shapes/image.class.js
@@ -389,6 +389,9 @@
           scaleX = this.scaleX * retinaScaling,
           scaleY = this.scaleY * retinaScaling,
           elementToFilter = this._filteredEl || this._originalElement;
+      if (this.group) {
+        this.set('dirty', true);
+      }
       if (!filter || (scaleX > minimumScale && scaleY > minimumScale)) {
         this._element = elementToFilter;
         this._filterScalingX = 1;
@@ -425,7 +428,7 @@
       filters = filters || this.filters || [];
       filters = filters.filter(function(filter) { return filter; });
       if (this.group) {
-        this.set('dirty');
+        this.set('dirty', true);
       }
       if (filters.length === 0) {
         this._element = this._originalElement;
