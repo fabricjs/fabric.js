a83b022a4e8ff1a505ad9de1c7f9ed14661a31e8
Durga Madhab <durga598@gmail.com>
2018-08-03 12:24:25 +0530

    Added requestRenderAllBound
    As requestRenderAllBound is undeifned in `_initStatic` method. Here is [jsfiddle](https://jsfiddle.net/durga598/utaodqwm/).

--- src/canvas.class.js
+++ src/canvas.class.js
@@ -61,6 +61,7 @@
     initialize: function(el, options) {
       options || (options = { });
       this.renderAndResetBound = this.renderAndReset.bind(this);
+      this.requestRenderAllBound = this.requestRenderAll.bind(this);
       this._initStatic(el, options);
       this._initInteractive();
       this._createCacheCanvas();
