d1b07a045a47b199bc076a9effc02743655d0da2
Andrea Bogazzi <andreabogazzi79@gmail.com>
2018-09-30 20:36:23 +0200

    enable image caching just in case

--- src/shapes/image.class.js
+++ src/shapes/image.class.js
@@ -485,6 +485,23 @@
       this._renderPaintInOrder(ctx);
     },
 
+    /**
+     * Decide if the object should cache or not. Create its own cache level
+     * objectCaching is a global flag, wins over everything
+     * needsItsOwnCache should be used when the object drawing method requires
+     * a cache step. None of the fabric classes requires it.
+     * Generally you do not cache objects in groups because the group outside is cached.
+     * This is the special image version where we would like to avoid caching where possible.
+     * Essentially images do not benefit from caching. They may require caching, and in that
+     * case we do it. Also caching an image usually ends in a loss of details.
+     * A full performance audit should be done.
+     * @return {Boolean}
+     */
+    shouldCache: function() {
+      this.ownCaching = this.objectCaching && this.needsItsOwnCache();
+      return this.ownCaching;
+    },
+
     _renderFill: function(ctx) {
       var w = this.width, h = this.height, sW = w * this._filterScalingX, sH = h * this._filterScalingY,
           x = -w / 2, y = -h / 2, elementToDraw = this._element;
