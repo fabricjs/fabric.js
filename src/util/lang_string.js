c89ad59c14312b64aeb3049d325cfb4608f49baf
sakthikanth <sakthikanth@notionpress.com>
2018-10-01 13:20:51 +0530

    lint_correction

--- src/util/lang_string.js
+++ src/util/lang_string.js
@@ -57,32 +57,31 @@
    * @return {Array} array containing the graphemes with combined consonant characters.
    */
   function languageCharacters(textstring) {
-    var charConfig = {iteration : 4, max_diff : 1.5, fontsize : "10px", fontfamily : 'Arial', letterSpacing : "15px" };
-    var n = 0,max,next,with_next,char1,char2,char3,start,end,char,i,chars = [],text = textstring;
+    var charConfig = { iteration: 4, max_diff: 1.5, fontsize: '10px', fontfamily: 'Arial', letterSpacing: '15px' };
+    var n = 0,max,next,withNext,char1,char2,char3,start,end,char,i,chars = [],text = textstring;
     var canva = fabric.document.getElementById('fabric_canva'),
-      c = canva != null ? canva : fabric.document.createElement("canvas");
+        c = canva !== null ? canva : fabric.document.createElement('canvas');
     c.style.letterSpacing = charConfig.letterSpacing;
     c.style.display = 'none';
-    if(!canva){
-      c.id = "fabric_canva";
+    if (!canva){
+      c.id = 'fabric_canva';
       fabric.document.body.appendChild(c);
     }
-    var ctx = c.getContext("2d");
-    ctx.font = charConfig.fontsize+" "+charConfig.fontfamily;
-    while(n < charConfig.iteration){
+    var ctx = c.getContext('2d');
+    ctx.font = charConfig.fontsize + ' ' + charConfig.fontfamily;
+    while (n < charConfig.iteration){
       chars = [];
-      for( i = 0,max = text.length ;i < max;i++){
-        next = (typeof text[i+1] == 'undefined') ? "" : text[i+1];
-        with_next = (text[i] + next);
+      for ( i = 0,max = text.length ;i < max;i++){
+        next = (typeof text[i + 1] === 'undefined') ? '' : text[ i + 1 ];
+        withNext = (text[i] + next);
         char1 = (ctx.measureText(text[i]).width);
         char2 = (ctx.measureText(next).width);
-        char3 = (ctx.measureText(with_next).width);
+        char3 = (ctx.measureText(withNext).width);
         start = i;
-        i += ( Math.abs(char3 - (char1+char2) ) >= charConfig.max_diff ) ? 1 : 0;
+        i += ( Math.abs(char3 - (char1 + char2) ) >= charConfig.max_diff ) ? 1 : 0;
         end = i;
-        char = start == end ? text[i] : with_next;
-        if(char.length)
-          chars.push(char);
+        char = start === end ? text[i] : withNext;
+        chars.push(char);
       } 
       text = chars;
       n++;
