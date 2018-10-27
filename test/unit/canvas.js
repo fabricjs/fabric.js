edc9179d59e3116c09d386777f308a7082f3cec2
Andrea Bogazzi <andreabogazzi79@gmail.com>
2018-10-27 18:41:59 +0200

    adding wider test

--- test/unit/canvas.js
+++ test/unit/canvas.js
@@ -708,53 +708,79 @@
   });
 
   QUnit.test('findTarget with subTargetCheck and canvas zoom', function(assert) {
-    var rect = makeRect({ left: 0, top: 0, width: 100, height: 100 }),
-        rect2 = makeRect({ left: 100, top:  100, width: 100, height: 100 }), target,
-        group = new fabric.Group([rect, rect2], { top: -150, left: -50 });
+    var rect3 = new fabric.Rect({
+      width: 100,
+      height: 100,
+      fill: 'yellow'
+    });
+    var rect4 = new fabric.Rect({
+      width: 100,
+      height: 100,
+      left: 100,
+      top: 100,
+      fill: 'purple'
+    });
+    var group3 = new fabric.Group(
+      [rect3, rect4],
+      { scaleX: 0.5, scaleY: 0.5, top: 100, left: 0 });
+    group3.subTargetCheck = true;
 
+    var rect1 = new fabric.Rect({
+      width: 100,
+      height: 100,
+      fill: 'red'
+    });
+    var rect2 = new fabric.Rect({
+      width: 100,
+      height: 100,
+      left: 100,
+      top: 100,
+      fill: 'blue'
+    });
+    var g = new fabric.Group([rect1, rect2, group3], { top: -150, left: -50 });
+    g.subTargetCheck = true;
     canvas.viewportTransform = [0.1, 0, 0, 0.1, 100, 200];
-    group.subTargetCheck = true;
-    canvas.add(group);
+    canvas.add(g);
 
-    target = canvas.findTarget({
+    var target = canvas.findTarget({
       clientX: 96, clientY: 186
     }, true);
-    assert.equal(target, group, 'Should return the group 96');
-    assert.equal(canvas.targets[0], rect, 'should find the target rect 96');
+    assert.equal(target, g, 'Should return the group 96');
+    assert.equal(canvas.targets[0], rect1, 'should find the target rect 96');
     canvas.targets = [];
 
     target = canvas.findTarget({
       clientX: 98, clientY: 188
     }, true);
-    assert.equal(target, group, 'Should return the group 98');
-    assert.equal(canvas.targets[0], rect, 'should find the target rect 98');
+    assert.equal(target, g, 'Should return the group 98');
+    assert.equal(canvas.targets[0], rect1, 'should find the target rect1 98');
     canvas.targets = [];
 
     target = canvas.findTarget({
       clientX: 100, clientY: 190
     }, true);
-    assert.equal(target, group, 'Should return the group 100');
-    assert.equal(canvas.targets[0], rect, 'should find the target rect 100');
+    assert.equal(target, g, 'Should return the group 100');
+    assert.equal(canvas.targets[0], rect1, 'should find the target rect1 100');
     canvas.targets = [];
 
     target = canvas.findTarget({
       clientX: 102, clientY: 192
     }, true);
-    assert.equal(target, group, 'Should return the group 102');
-    assert.equal(canvas.targets[0], rect, 'should find the target rect 102');
+    assert.equal(target, g, 'Should return the group 102');
+    assert.equal(canvas.targets[0], rect1, 'should find the target rect 102');
     canvas.targets = [];
 
     target = canvas.findTarget({
       clientX: 104, clientY: 194
     }, true);
-    assert.equal(target, group, 'Should return the group 104');
-    assert.equal(canvas.targets[0], rect, 'should find the target rect 104');
+    assert.equal(target, g, 'Should return the group 104');
+    assert.equal(canvas.targets[0], rect1, 'should find the target rect 104');
     canvas.targets = [];
 
     target = canvas.findTarget({
       clientX: 106, clientY: 196
     }, true);
-    assert.equal(target, group, 'Should return the group 106');
+    assert.equal(target, g, 'Should return the group 106');
     assert.equal(canvas.targets[0], rect2, 'should find the target rect2 106');
     canvas.targets = [];
 
