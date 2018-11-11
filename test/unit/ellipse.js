83d89867e916e72db2a46cb6aa378420a7be3e82
Andrea Bogazzi <andreabogazzi79@gmail.com>
2018-11-12 00:42:51 +0100

    fixed ellipse test

--- test/unit/ellipse.js
+++ test/unit/ellipse.js
@@ -83,18 +83,18 @@
     assert.deepEqual(ellipse.getRx(), ellipse.rx * ellipse.scaleX);
   });
 
-  QUnit.test('render', function(assert) {
+  QUnit.test('isNotVisible', function(assert) {
     var ellipse = new fabric.Ellipse();
     ellipse.set('rx', 0).set('ry', 0);
 
     var wasRenderCalled = false;
 
-    ellipse._render = function(){
-      wasRenderCalled = true;
-    };
-    ellipse.render({});
+    assert.equal(ellipse.isNotVisible(), false, 'isNotVisible false when rx/ry are 0 because strokeWidth is > 0');
 
-    assert.equal(wasRenderCalled, false, 'should not render when rx/ry are 0');
+    ellipse.set('strokeWidth', 0);
+
+    assert.equal(ellipse.isNotVisible(), true, 'should not render anymore with also strokeWidth 0');
+
   });
 
   QUnit.test('toSVG', function(assert) {
