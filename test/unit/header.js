3f0c0deed8fe1c96901ab4182f9e69fd2561821a
Andrea Bogazzi <andreabogazzi79@gmail.com>
2019-02-17 22:55:57 +0100

    more tests

--- test/unit/header.js
+++ test/unit/header.js
@@ -6,7 +6,7 @@
     assert.ok(typeof fabric.window !== 'undefined', 'window is set');
     assert.ok(typeof fabric.isTouchSupported !== 'undefined', 'isTouchSupported is set');
     assert.ok(typeof fabric.isLikelyNode !== 'undefined', 'isLikelyNode is set');
-    assert.equal(fabric.SHARED_ATTRIBUTES.length, 18, 'SHARED_ATTRIBUTES is set');
+    assert.equal(fabric.SHARED_ATTRIBUTES.length, 19, 'SHARED_ATTRIBUTES is set');
   });
 
   QUnit.test('initFilterBackend', function(assert) {
