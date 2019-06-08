d3e0a001c8e6fa0d6beb15845fdbffa3e33c821a
Andrea Bogazzi <andreabogazzi79@gmail.com>
2019-06-08 13:30:22 +0200

    added deprecation warning for Object.transformMatrix (#5747)

--- src/shapes/object.class.js
+++ src/shapes/object.class.js
@@ -372,6 +372,12 @@
 
     /**
      * Transform matrix (similar to SVG's transform matrix)
+     * This property has been depreacted. Since caching and and qrDecompose this
+     * property can be handled with the standard top,left,scaleX,scaleY,angle and skewX.
+     * A documentation example on how to parse and merge a transformMatrix will be provided before
+     * completely removing it in fabric 4.0
+     * If you are starting a project now, DO NOT use it.
+     * @deprecated since 3.2.0
      * @type Array
      */
     transformMatrix:          null,
