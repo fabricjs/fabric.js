50962efe6bbd77174cda2e9bd169c19d301edb7a
0ro <den.bogdanov4@gmail.com>
2019-10-01 16:32:04 +0300

    Fix bug

--- src/mixins/canvas_events.mixin.js
+++ src/mixins/canvas_events.mixin.js
@@ -302,7 +302,7 @@
      * @param {Event} e Event object fired on mousedown
      */
     _onTouchStart: function(e) {
-      e.preventDefault();
+      !this.allowTouchScrolling && e.preventDefault && e.preventDefault();
       if (this.mainTouchId === null) {
         this.mainTouchId = this.getPointerId(e);
       }
