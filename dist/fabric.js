af076fb6e26f2f164c21bcda51668a0a6f429971
Andrea Bogazzi <andreabogazzi79@gmail.com>
2019-12-28 14:55:09 +0100

    header and changelog

--- dist/fabric.js
+++ dist/fabric.js
@@ -1,7 +1,7 @@
 /* build: `node build.js modules=ALL exclude=gestures,accessors requirejs minifier=uglifyjs` */
 /*! Fabric.js Copyright 2008-2015, Printio (Juriy Zaytsev, Maxim Chernyak) */
 
-var fabric = fabric || { version: '3.5.1' };
+var fabric = fabric || { version: '3.6.0' };
 if (typeof exports !== 'undefined') {
   exports.fabric = fabric;
 }
