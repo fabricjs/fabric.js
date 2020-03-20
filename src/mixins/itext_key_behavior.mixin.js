a3e31e81a4e1e92a114fc631fea99335d72b262f
Andrea Bogazzi <andreabogazzi79@gmail.com>
2020-03-20 15:43:35 +0100

    fix(IText) Stop composition events on mousedown to enable cursor position on android keyboards (#6224)

--- src/mixins/itext_key_behavior.mixin.js
+++ src/mixins/itext_key_behavior.mixin.js
@@ -80,11 +80,12 @@ fabric.util.object.extend(fabric.IText.prototype, /** @lends fabric.IText.protot
   },
 
   /**
-   * Handles keyup event
+   * Handles keydown event
+   * only used for arrows and combination of modifier keys.
    * @param {Event} e Event object
    */
   onKeyDown: function(e) {
-    if (!this.isEditing || this.inCompositionMode) {
+    if (!this.isEditing) {
       return;
     }
     if (e.keyCode in this.keysMap) {
@@ -100,6 +101,7 @@ fabric.util.object.extend(fabric.IText.prototype, /** @lends fabric.IText.protot
     e.preventDefault();
     if (e.keyCode >= 33 && e.keyCode <= 40) {
       // if i press an arrow key just update selection
+      this.inCompositionMode = false;
       this.clearContextTop();
       this.renderCursorOrSelection();
     }
