f0152c90fd57f8538955135b698af9e7275cf250
Andrea Bogazzi <andreabogazzi79@gmail.com>
2020-06-27 23:34:07 +0200

    v3.6.4 fix text paste and build new version (#6418)

--- src/shapes/itext.class.js
+++ src/shapes/itext.class.js
@@ -458,6 +458,7 @@
      * High level function to know the height of the cursor.
      * the currentChar is the one that precedes the cursor
      * Returns fontSize of char at the current cursor
+     * Unused from the library, is for the end user
      * @return {Number} Character font size
      */
     getCurrentCharFontSize: function() {
@@ -469,6 +470,7 @@
      * High level function to know the color of the cursor.
      * the currentChar is the one that precedes the cursor
      * Returns color (fill) of char at the current cursor
+     * Unused from the library, is for the end user
      * @return {String} Character color (fill)
      */
     getCurrentCharColor: function() {
