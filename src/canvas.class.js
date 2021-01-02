5377702c59e19eb3a4d8c437df73c13f331e1241
Andrea Bogazzi <andreabogazzi79@gmail.com>
2021-01-02 17:51:29 +0100

    fixed tests

--- src/canvas.class.js
+++ src/canvas.class.js
@@ -1268,7 +1268,7 @@
         layoutProps.forEach(function(prop) {
           originalValues[prop] = instance[prop];
         });
-        this._activeObject.realizeTransform(instance);
+        this._activeObject.realizeTransform(instance, this._activeObject.calcOwnMatrix());
         return originalValues;
       }
       else {
