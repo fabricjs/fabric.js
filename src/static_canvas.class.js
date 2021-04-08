10bbfaee0a1c6939c826280faef3cc1a0ddc7973
ShaMan123 <shacharnen@gmail.com>
2021-04-08 12:17:20 +0300

    Update static_canvas.class.js

--- src/static_canvas.class.js
+++ src/static_canvas.class.js
@@ -195,6 +195,11 @@
     clipPath: undefined,
 
     /**
+     * Indicates whether the background objects of this canvas can be erased by @class fabric.EraserBrush
+     */
+    erasable: false,
+
+    /**
      * @private
      * @param {HTMLElement | String} el &lt;canvas> element to initialize instance on
      * @param {Object} [options] Options object
