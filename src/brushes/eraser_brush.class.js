8f5cc2491c8adb6a37c16a613e997736cdeb8990
ShaMan123 <shacharnen@gmail.com>
2021-04-09 17:42:19 +0300

    Update eraser_brush.class.js

--- src/brushes/eraser_brush.class.js
+++ src/brushes/eraser_brush.class.js
@@ -2,7 +2,7 @@
 
   fabric.util.object.extend(fabric.Canvas.prototype, {
     /**
-     * Sets eraable option on background objects
+     * Sets erasable option on background objects
      * @param {boolean} value 
      * @returns 
      */
