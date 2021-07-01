fc92fad47d435442f01066219d6124a0a1d05254
ShaMan123 <shacharnen@gmail.com>
2021-07-01 13:54:59 +0300

    Update pencil_brush.class.js

--- src/brushes/pencil_brush.class.js
+++ src/brushes/pencil_brush.class.js
@@ -213,7 +213,7 @@
         this.shadow.affectStroke = true;
         path.shadow = new fabric.Shadow(this.shadow);
       }
-      this._addClipPathToResult(group);
+      this._addClipPathToResult(path);
       return path;
     },
 
