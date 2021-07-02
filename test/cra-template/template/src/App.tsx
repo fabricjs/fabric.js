b7659353e24d8a1822ee60688067c3f2dc1b8ade
ShaMan123 <shacharnen@gmail.com>
2021-07-02 11:33:44 +0300

    app

--- test/cra-template/template/src/App.tsx
+++ test/cra-template/template/src/App.tsx
@@ -1,22 +1,18 @@
-import React, { useRef, useEffect } from 'react';
-import { fabric } from './fabric';
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
+import { fabric } from './fabric';
 
 function App() {
   const ref = useRef<HTMLCanvasElement>(null);
   const fc = useRef<fabric.Canvas>();
   useEffect(() => {
-    fc.current = new fabric.Canvas(ref.current!);
+    const canvas = new fabric.Canvas(ref.current!, { backgroundColor: 'white' });
+    fc.current = canvas;
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Fabric React Sandbox
-        </p>
         <a
           className="App-link"
           href="http://fabricjs.com/"
@@ -25,12 +21,29 @@ function App() {
         >
           Fabric Docs
         </a>
+        <h2>
+          Fabric React Sandbox
+        </h2>
+        <a
+          className="App-link"
+          href="https://github.com/fabricjs/fabric.js/blob/master/CONTRIBUTING.md"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contribution Guide
+        </a>
       </header>
-      <canvas
-        width={500}
-        height={500}
-        ref={ref}
-      />
+      <div className="App-canvas">
+        <canvas
+          width={500}
+          height={500}
+          ref={ref}
+        />
+      </div>
+      <footer className="App-footer">
+        <code>fabric</code> is being watched for changes. All you need to do is start coding!<br />
+        To edit this test app open <code>./src/App.tsx</code><br />
+      </footer>
     </div>
   );
 }
