4cfbf97804e681bbcc962c67c75b3d1e3a2bb367
ShaMan123 <shacharnen@gmail.com>
2021-07-02 11:33:53 +0300

    Update run-dev-env.js

--- test/run-dev-env.js
+++ test/run-dev-env.js
@@ -1,21 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
+const templateDir = path.resolve(__dirname, 'cra-template');
 const appDir = path.resolve(__dirname, 'test-fabric');
 const main = path.resolve(__dirname, '..');
 const src = path.resolve(main, 'src');
 const fabricSource = path.resolve(main, 'dist', 'fabric.js');
 const fabricDest = path.resolve(appDir, 'src', 'fabric.js');
 
+function copyBuildToApp() {
+  execSync('node build.js modules=ALL requirejs', { cwd: main });
+  fs.copyFileSync(fabricSource, fabricDest);
+  console.log(`built ${fabricDest}`);
+}
+
 function startDevEnv() {
   if (!fs.existsSync(appDir)) {
-    execSync(`npx create-react-app test/test-fabric --template file:${appDir}`, { stdio: 'inherit' });
+    console.log('dfgsdfgsdfg', templateDir)
+    execSync(`npx create-react-app test/test-fabric --template file:${templateDir}`, { cwd: main, stdio: 'inherit' });
   }
-  fs.watch(src, { recursive: true }, () => {
-    execSync('node build.js modules=ALL requirejs fast', { cwd: main });
-    fs.copyFileSync(fabricSource, fabricDest);
-    console.log(`built ${fabricDest}`);
-  });
+  copyBuildToApp();
+  fs.watch(src, { recursive: true }, copyBuildToApp);
   execSync('npm start', { cwd: appDir, detached: true });
 }
 
