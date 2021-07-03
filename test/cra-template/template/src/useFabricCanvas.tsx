d8519c8895319dd97b297d8754388e96d5fbd14e
ShaMan123 <shacharnen@gmail.com>
2021-07-04 00:28:00 +0300

    Create useFabricCanvas.tsx

---
+++ test/cra-template/template/src/useFabricCanvas.tsx
@@ -0,0 +1,20 @@
+import { useCallback, useRef } from 'react';
+import { fabric } from './fabric';
+
+export function useFabricCanvas() {
+    const fc = useRef<fabric.Canvas | null>(null);
+    const setRef = useCallback((ref: HTMLCanvasElement | null) => {
+        fc.current?.dispose();
+        if (!ref) {
+            fc.current = null;
+            return;
+        }
+        const canvas = new fabric.Canvas(ref, { backgroundColor: 'white' });
+        fc.current = canvas;
+        return () => {
+            fc.current?.dispose();
+            fc.current = null;
+        };
+    }, []);
+    return [fc, setRef] as [typeof fc, typeof setRef];
+}
