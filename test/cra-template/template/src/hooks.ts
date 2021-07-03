7b0db7532595fb5b4f25ca2e077b2dbda354924a
ShaMan123 <shacharnen@gmail.com>
2021-07-04 00:32:33 +0300

    Update hooks.ts

--- test/cra-template/template/src/hooks.ts
+++ test/cra-template/template/src/hooks.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export function useDeployCodeSandbox() {
   const [pending, setPending] = useState(false);
