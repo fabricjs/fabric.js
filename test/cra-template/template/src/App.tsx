73ba4fcde9a82e17797587c073f23251a7d6c0d7
ShaMan123 <shacharnen@gmail.com>
2021-07-04 01:03:54 +0300

    Update App.tsx

--- test/cra-template/template/src/App.tsx
+++ test/cra-template/template/src/App.tsx
@@ -1,13 +1,8 @@
 import React, { useCallback, useEffect, useRef } from 'react';
-import './App.css';
+import './styles.css';
 //  open `./fabric.ts` to change the import source (local build or published module)
 import { fabric } from './fabric';
 import { useCanvas } from './useCanvas';
-
-const SIZE = {
-  width: 500,
-  height: 500
-};
 
 function App() {
   const [fc, setRef] = useCanvas();
@@ -19,8 +14,8 @@ function App() {
 
   return (
     <canvas
-      width={SIZE.width}
-      height={SIZE.height}
+      width={500}
+      height={500}
       ref={setRef}
     />
   );
