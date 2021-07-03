e017bf7ce8285c3af7c771e16efd0dbd0ecfdbf0
ShaMan123 <shacharnen@gmail.com>
2021-07-04 00:28:13 +0300

    extract

---
+++ test/cra-template/template/src/SandboxUI.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { openIDE, useDeployCodeSandbox, useGitInfo, useShowFooter } from './hooks';
+
+function SandboxUI({ children }: React.PropsWithChildren<{}>) {
+  const [pending, deployCodeSandbox] = useDeployCodeSandbox();
+  const gitInfo = useGitInfo();
+  const [showFooter, setShowFooter] = useShowFooter();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="http://fabricjs.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Fabric Docs
+        </a>
+        <div>
+          <h2>
+            Fabric React Sandbox
+          </h2>
+          {
+            gitInfo &&
+            <p>
+              branch: <code>{gitInfo.branch}</code>, tag: <code>{gitInfo.tag}</code><br />
+              {gitInfo.changes.length > 0 && <strong>This branch has uncommitted changes</strong>}
+            </p>
+          }
+          <button
+            onClick={deployCodeSandbox}
+            disabled={pending}
+            style={{ cursor: pending ? 'progress' : 'pointer' }}
+          >
+            <div className="App-deploy">Deploy to codesandbox <img src="https://codesandbox.io/csb-ios.svg" width={24} /></div>
+          </button>
+        </div>
+        <a
+          className="App-link"
+          href="https://github.com/fabricjs/fabric.js/blob/master/CONTRIBUTING.md"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contribution Guide
+        </a>
+      </header>
+      {children}
+      <footer className="App-footer" hidden={showFooter !== 1}>
+        <code>fabric</code> is being watched for changes.<br />
+        All you need to do is start coding!<br />
+        To edit this test app open <button onClick={openIDE}><code>./src/App.tsx</code></button><br />
+        <button className="close" onClick={() => setShowFooter(0)}>X</button>
+      </footer>
+    </div>
+  );
+}
+
+export default SandboxUI;
