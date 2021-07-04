579073dabc8c2e79d2298424368a9133f4bdaa30
ShaMan123 <shacharnen@gmail.com>
2021-07-05 02:00:29 +0300

    Update react-app-env.d.ts

--- test/cra-template/ts/template/src/react-app-env.d.ts
+++ test/cra-template/ts/template/src/react-app-env.d.ts
@@ -3,6 +3,6 @@ declare namespace NodeJS {
     interface ProcessEnv {
         NODE_ENV: 'development' | 'production' | 'test'
         REACT_APP_TEMPLATE: 'js' | 'ts'
-        REACT_APP_SANDBOX_DEPLOYED?: boolean
+        REACT_APP_SANDBOX_DEPLOYED?: 'true' | 'false'
     }
 }
