2d0c146bf43d97dd9eecadc5854f6ca93e14c978
ShaMan123 <shacharnen@gmail.com>
2021-07-12 14:49:37 +0300

    add patching fabric modal

--- test/cra-template/common/template/src/sandbox/SandboxUI.tsx
+++ test/cra-template/common/template/src/sandbox/SandboxUI.tsx
@@ -5,6 +5,15 @@ import { OpenIDE } from './common';
 import githubIcon from './GitHub-Mark-64px.png';
 import { GitInfo, SANDBOX_DEPLOYED, useDeployCodeSandbox, useGitInfo, useShowComments, useShowModal } from './hooks';
 
+const PatchIcon = React.memo((props: ImageProps & React.RefAttributes<HTMLImageElement>) =>
+  <Image
+    src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' class='ionicon' viewBox='0 0 512 512'%3E%3Ctitle%3EBandage%3C/title%3E%3Crect x='-24.43' y='167.88' width='560.87' height='176.25' rx='88.12' ry='88.12' transform='rotate(-45 256 256.002)' fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='32'/%3E%3Crect x='169.41' y='156.59' width='176' height='196' rx='32' ry='32' transform='rotate(45 257.409 254.582)' fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='32'/%3E%3Ccircle cx='256' cy='208' r='16'/%3E%3Ccircle cx='304' cy='256' r='16'/%3E%3Ccircle cx='208' cy='256' r='16'/%3E%3Ccircle cx='256' cy='304' r='16'/%3E%3C/svg%3E"
+    width={24}
+    className="mx-2"
+    {...props}
+  />
+);
+
 const GitInfoDisplay = React.memo(({ user, branch, tag, changes }: GitInfo) => {
   return (
     <Container className="p-2">
@@ -81,37 +90,43 @@ const GitActions = React.memo((info?: Partial<GitInfo> | null) => {
             </code>
           </div>
         </div>
+      </Modal.Body>
+      <Modal.Footer className="justify-content-between">
         {
           info &&
           <div className="my-4">
             <h6>
+              <Image
+                src={githubIcon}
+                width={24}
+                className="mx-2"
+              />
               Github info
             </h6>
             <GitInfoDisplay {...(info as GitInfo)} />
           </div>
         }
-      </Modal.Body>
-      <Modal.Footer>
-        <Button
-          as="a"
-          href={`https://github.com/${user}/fabric.js/tree/${tag || ''}`}
-          target="_blank"
-          rel="noopener noreferrer"
-          variant="outline-primary"
-        >
-          Browse repository
-        </Button>
-        {
-          SANDBOX_DEPLOYED &&
+        <div>
+          <Button
+            as="a"
+            href={`https://github.com/${user}/fabric.js/tree/${tag || ''}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            variant="outline-primary"
+            className="mx-2"
+          >
+            Browse repository
+          </Button>
           <Button
             as="a"
             href="/diff/upstream.diff"
             download="fabric.diff"
             variant="outline-success"
+            className="mx-2"
           >
             Download <code>.diff</code> file
           </Button>
-        }
+        </div>
       </Modal.Footer>
     </>
   );
@@ -127,45 +142,68 @@ function SandboxUI({ children, hidden }: { children: React.ReactNode, hidden?: b
     setShowModal(0);
     showComments === -1 && setShowComments(1);
   }, [showComments, setShowModal, setShowComments]);
-  const [showDownload, setShowDownload] = useState(false);
+  const [showGitActions, setShowGitActions] = useState(false);
+  const [showPatching, setShowPatching] = useState(false);
 
   return hidden ?
     <>{children}</> :
     (
       <div className="App">
         <header className="App-header my-3">
-          <div className="f1">
+          <div className="f1 flex justify-content-evenly">
+            <OverlayTrigger
+              overlay={
+                <Tooltip id="git">
+                  <p className="m-3">
+                    <div><strong>Patching fabric?</strong></div>
+                    Click for details
+                  </p>
+                </Tooltip>
+              }
+              trigger={['focus', 'hover']}
+              flip
+              placement="bottom"
+            >
+              <Button
+                onClick={() => setShowPatching(true)}
+                variant="light"
+              >
+                <PatchIcon width={48} />
+              </Button>
+            </OverlayTrigger>
             {
               gitInfo &&
-              <OverlayTrigger
-                overlay={
-                  <Tooltip id="git">
-                    <p className="m-3">
-                      <strong>Click for more options</strong>
-                    </p>
-                    <GitInfoDisplay {...gitInfo} />
-                  </Tooltip>
-                }
-                trigger={['focus', 'hover']}
-                flip
-                placement="right"
-              >
-                {({ ref, ...props }) =>
-                  <div className="position-relative" {...props}>
-                    <Button
-                      ref={ref}
-                      onClick={() => setShowDownload(true)}
-                      variant="light"
-                    >
-                      <Image
-                        src={githubIcon}
-                        width={48}
-                      />
-                    </Button>
-                    {gitInfo.changes.length > 0 && <Badge className="github" bg="secondary"><small>Uncommitted Changes</small></Badge>}
-                  </div>
-                }
-              </OverlayTrigger>
+              <div className="flex">
+                <OverlayTrigger
+                  overlay={
+                    <Tooltip id="git">
+                      <p className="m-3">
+                        <strong>Click for more options</strong>
+                      </p>
+                      <GitInfoDisplay {...gitInfo} />
+                    </Tooltip>
+                  }
+                  trigger={['focus', 'hover']}
+                  flip
+                  placement="bottom"
+                >
+                  {({ ref, ...props }) =>
+                    <div className="position-relative" {...props}>
+                      <Button
+                        ref={ref}
+                        onClick={() => setShowGitActions(true)}
+                        variant="light"
+                      >
+                        <Image
+                          src={githubIcon}
+                          width={48}
+                        />
+                      </Button>
+                      {gitInfo.changes.length > 0 && <Badge className="github" bg="secondary"><small>Uncommitted Changes</small></Badge>}
+                    </div>
+                  }
+                </OverlayTrigger>
+              </div>
             }
           </div>
           <div className="f1">
@@ -248,13 +286,73 @@ function SandboxUI({ children, hidden }: { children: React.ReactNode, hidden?: b
           </Modal.Footer>
         </Modal>
         <Modal
-          show={showDownload}
-          onHide={() => setShowDownload(false)}
+          show={showGitActions}
+          onHide={() => setShowGitActions(false)}
           backdrop="static"
           centered
           size="lg"
         >
           <GitActions {...gitInfo} />
+        </Modal>
+        <Modal
+          show={showPatching}
+          onHide={() => setShowPatching(false)}
+          backdrop="static"
+          centered
+          size="lg"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>
+              Patching <strong>fabric</strong> with <code>.diff</code> files
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="my-4">
+              <h6>
+                <PatchIcon />
+                Looking to patch <strong>fabric</strong> in an existing project using a package manager?
+              </h6>
+              <div className="m-2">
+                Add a <code>postinstall</code> script to your <code>package.json</code> applying the <code>.diff</code> to <code>dist</code>.<br />
+              </div>
+            </div>
+            <div className="my-4">
+              <h6>
+                <PatchIcon />
+                Using <code>yarn berry</code>?
+              </h6>
+              <div className="m-2">
+                If you're using <code>yarn berry</code> you need to use the <a
+                  href="https://yarnpkg.com/features/protocols#patch"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  patch protocol
+                </a> by changing the depency in your <code>package.json</code> file.<br />
+                <code>fabric: "patch:fabric@<strong>{"\<"}semver_version{"\>"}</strong>#./<strong>{"\<"}path/to/.diff/file{"\>"}</strong>"</code><br />
+                <strong>e.g.</strong><br />
+                <code>patch:fabric@<strong>latest</strong>#./<strong>fabric.diff</strong></code><br />
+                <code>fabric.diff</code> is the generated <code>.diff</code> file located in your app's root.
+              </div>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              onClick={() => setShowGitActions(true)}
+              variant="outline-primary"
+            >
+              Using <code>.diff</code> files
+            </Button>
+            <Button
+              as="a"
+              href="/diff/upstream.diff"
+              download="fabric.diff"
+              variant="outline-success"
+              className="mx-2"
+            >
+              Download <code>.diff</code> file
+            </Button>
+          </Modal.Footer>
         </Modal>
       </div >
     );
