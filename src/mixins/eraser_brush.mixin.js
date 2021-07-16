30d58d3b372a91ddcab5ceb7e246cb60f8ebb556
Christoforos Aslanov <chr314@users.noreply.github.com>
2021-07-16 18:48:59 +0300

    check if fill.render is function

--- src/mixins/eraser_brush.mixin.js
+++ src/mixins/eraser_brush.mixin.js
@@ -112,7 +112,9 @@
         if (needsVpt) {
           ctx.transform(v[0], v[1], v[2], v[3], v[4], v[5]);
         }
-        fill && fill.render(ctx);
+        if (fill && typeof fill.render === 'function') {
+          fill.render(ctx);
+        }
         object && object.render(ctx);
         ctx.restore();
       }
