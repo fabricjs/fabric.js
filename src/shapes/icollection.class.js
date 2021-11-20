3458060775e1c645eb931265ef42b64bc934d40a
ShaMan123 <shacharnen@gmail.com>
2021-11-20 22:24:59 +0200

    Update icollection.class.js

--- src/shapes/icollection.class.js
+++ src/shapes/icollection.class.js
@@ -28,11 +28,11 @@
     fabric.Collection,
     /** @lends fabric.ICollection.prototype */ {
 
-    /**
-     * Type of an object
-     * @type String
-     * @default
-     */
+      /**
+       * Type of an object
+       * @type String
+       * @default
+       */
       type: 'i-collection',
 
       layout: 'auto',
@@ -85,14 +85,14 @@
      */
       _set: function (key, value) {
         if (key === 'subTargetCheck' && this.ownMatrixCache) {
-        //  we want to avoid setting `initialValue` during initializion
+          //  we want to avoid setting `initialValue` during initializion
           var initialValue = this.ownMatrixCache.initialValue;
           if (value && initialValue) {
             this._applyMatrixDiffToObjects(initialValue, this.calcOwnMatrix());
             delete this.ownMatrixCache.initialValue;
           }
           else if (!value && !initialValue) {
-          //  we want to prevent this logic from writing over the exisitng value before it has been applied to objects
+            //  we want to prevent this logic from writing over the exisitng value before it has been applied to objects
             this.ownMatrixCache.initialValue = this.calcOwnMatrix();
           }
         }
@@ -133,7 +133,7 @@
       _applyMatrixDiffToClipPath: function () {
         var clipPath = this.clipPath;
         if (clipPath && !clipPath.absolutePositioned
-        && this.prevMatrixCache && this.ownMatrixCache.key !== this.prevMatrixCache.key) {
+          && this.prevMatrixCache && this.ownMatrixCache.key !== this.prevMatrixCache.key) {
           var from = this.prevMatrixCache.cache, to = this.calcOwnMatrix();
           var transformDiff = multiplyTransformMatrices(invertTransform(to), from);
           applyTransformToObject(clipPath, multiplyTransformMatrices(transformDiff, clipPath.calcTransformMatrix()));
@@ -319,7 +319,7 @@
         var t = this.subTargetCheck ? this.calcTransformMatrix() : this.ownMatrixCache.initialValue;
         ctx.transform.apply(ctx, invertTransform(t));
         this.forEachObject(function (object) {
-        //  do not render the selected object
+          //  do not render the selected object
           object !== this._activeObject && object.render(ctx);
         }, this);
         ctx.restore();
@@ -342,7 +342,7 @@
         context.type !== 'initialization' && this.callSuper('setCoords');
         //  recursive up
         if (this.parent && this.parent._applyLayoutStrategy) {
-        //  append the path recursion to context
+          //  append the path recursion to context
           if (!context.path) {
             context.path = [];
           }
@@ -502,7 +502,7 @@
       getSvgTransform: function (full, additionalTransform) {
         var svgTransform = 'transform="' + fabric.util.matrixToSVG(fabric.iMatrix);
         return svgTransform +
-        (additionalTransform || '') + '" ';
+          (additionalTransform || '') + '" ';
       },
 
       /**
@@ -519,7 +519,7 @@
 
         return this._createBaseClipPathSVGMarkup(svgString, { reviver: reviver });
       },
-    /* _TO_SVG_END_ */
+      /* _TO_SVG_END_ */
     });
 
   /**
