6788ebd1f30d4d8217d70d6231228674ddf3ed84
ShaMan123 <shacharnen@gmail.com>
2021-11-28 01:04:41 +0200

    Update active_selection.class.js

--- src/shapes/active_selection.class.js
+++ src/shapes/active_selection.class.js
@@ -24,51 +24,13 @@
      */
     type: 'activeSelection',
 
-    initialize: function (objects, options) {
-      this.callSuper('initialize', objects, options);
-      this.setCoords();
-    },
-
     /**
      * Returns string representation of a group
      * @return {String}
      */
     toString: function() {
       return '#<fabric.ActiveSelection: (' + this.complexity() + ')>';
-    },
-
-    /**
-     * Decide if the object should cache or not. Create its own cache level
-     * objectCaching is a global flag, wins over everything
-     * needsItsOwnCache should be used when the object drawing method requires
-     * a cache step. None of the fabric classes requires it.
-     * Generally you do not cache objects in groups because the group outside is cached.
-     * @return {Boolean}
-     */
-    shouldCache: function() {
-      return false;
-    },
-
-    /**
-     * Renders controls and borders for the object
-     * @param {CanvasRenderingContext2D} ctx Context to render on
-     * @param {Object} [styleOverride] properties to override the object style
-     * @param {Object} [childrenOverride] properties to override the children overrides
-     */
-    _renderControls: function(ctx, styleOverride, childrenOverride) {
-      ctx.save();
-      ctx.globalAlpha = this.isMoving ? this.borderOpacityWhenMoving : 1;
-      this.callSuper('_renderControls', ctx, styleOverride);
-      childrenOverride = childrenOverride || { };
-      if (typeof childrenOverride.hasControls === 'undefined') {
-        childrenOverride.hasControls = false;
-      }
-      childrenOverride.forActiveSelection = true;
-      for (var i = 0, len = this._objects.length; i < len; i++) {
-        this._objects[i]._renderControls(ctx, childrenOverride);
-      }
-      ctx.restore();
-    },
+    }
   });
 
   /**
