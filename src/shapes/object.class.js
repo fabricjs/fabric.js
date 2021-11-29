2489bdb31513add39a2ef7642a11c8286c3d152f
ShaMan123 <shacharnen@gmail.com>
2021-11-29 07:51:16 +0200

    includeDefaultValues arg

--- src/shapes/object.class.js
+++ src/shapes/object.class.js
@@ -835,9 +835,10 @@
     /**
      * Returns an object representation of an instance
      * @param {Array} [propertiesToInclude] Any properties that you might want to additionally include in the output
+     * @param {boolean} [includeDefaultValues] override instance `includeDefaultValues`
      * @return {Object} Object representation of an instance
      */
-    toObject: function(propertiesToInclude) {
+    toObject: function (propertiesToInclude, includeDefaultValues) {
       var NUM_FRACTION_DIGITS = fabric.Object.NUM_FRACTION_DIGITS,
 
           object = {
@@ -881,7 +882,7 @@
       }
 
       fabric.util.populateWithProperties(this, object, propertiesToInclude);
-      if (!this.includeDefaultValues) {
+      if (typeof includeDefaultValues === 'boolean' ? !includeDefaultValues : !this.includeDefaultValues) {
         object = this._removeDefaultValues(object);
       }
 
