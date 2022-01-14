105159515543750da54d97ba8652a010b03cbcfc
ShaMan123 <shacharnen@gmail.com>
2022-01-14 20:13:11 +0200

    fix(): extract `enlivenObjectEnlivables` to util
    fix image fromObject.
    enables mre flexibilty of nested objects and rehydration without needing to override all the mechanism.

--- src/shapes/group.class.js
+++ src/shapes/group.class.js
@@ -588,14 +588,10 @@
       });
       return;
     }
-    var enlivenProps = fabric.Object.ENLIVEN_PROPS.filter(function (key) { return !!object[key]; });
-    fabric.util.enlivenObjects(objects, function(enlivenedObjects) {
-      fabric.util.enlivenObjects(enlivenProps.map(function (key) { return object[key]; }), function (enlivedProps) {
-        var options = fabric.util.object.clone(object, true);
-        delete options.objects;
-        enlivenProps.forEach(function (key, index) {
-          options[key] = enlivedProps[index];
-        });
+    fabric.util.enlivenObjects(objects, function (enlivenedObjects) {
+      var options = fabric.util.object.clone(object, true);
+      delete options.objects;
+      fabric.util.enlivenObjectEnlivables(object, options, function () {
         callback && callback(new fabric.Group(enlivenedObjects, options, true));
       });
     });
