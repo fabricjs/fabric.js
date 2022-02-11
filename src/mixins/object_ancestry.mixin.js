bb34ecf6eec392a277bc44550d5a82d3533c2593
ShaMan123 <shacharnen@gmail.com>
2022-02-11 17:45:23 +0200

    Update object_ancestry.mixin.js

--- src/mixins/object_ancestry.mixin.js
+++ src/mixins/object_ancestry.mixin.js
@@ -35,4 +35,43 @@ fabric.util.object.extend(fabric.Object.prototype, /** @lends fabric.Object.prot
         return ancestors;
     },
 
+    /**
+     * 
+     * @param {fabric.Object} other 
+     * @returns {{ index: number, otherIndex: number, ancestors: fabric.Object[] }} ancestors may include the passed objects if one is an ancestor of the other resulting in index of -1
+     */
+    findCommonAncestors: function (other) {
+        if (this === other) {
+            return true;
+        }
+        else if (!other) {
+            return false;
+        }
+        var ancestors = this.getAncestors();
+        ancestors.unshift(this);
+        var otherAncestors = other.getAncestors();
+        otherAncestors.unshift(other);
+        for (var i = 0, ancestor; i < ancestors.length; i++) {
+            ancestor = ancestors[i];
+            for (var j = 0; j < otherAncestors.length; j++) {
+                if (ancestor === otherAncestors[j] && !(ancestor instanceof fabric.StaticCanvas)) {
+                    return {
+                        index: i - 1,
+                        otherIndex: j - 1,
+                        ancestors: ancestors.slice(i)
+                    };
+                }
+            }
+        }
+    },
+
+    /**
+     * 
+     * @param {fabric.Object} other 
+     * @returns {boolean}
+     */
+    hasCommonAncestor: function (other) {
+        return !!this.findCommonAncestors(other);
+    }
+
 });
