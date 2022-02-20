06030b7f225c3892c480947058c8b5117fe7879e
ShaMan123 <shacharnen@gmail.com>
2022-02-20 02:52:53 +0200

    Update object.js

--- test/unit/object.js
+++ test/unit/object.js
@@ -495,6 +495,7 @@
     assert.ok(cObj.isType('rect'));
     assert.ok(!cObj.isType('object'));
     assert.ok(cObj.isType('object', 'rect'));
+    assert.ok(!cObj.isType('object', 'circle'));
   });
 
   QUnit.test('toggle', function(assert) {
