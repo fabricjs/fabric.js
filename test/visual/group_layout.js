5221fb2eb5839f5ee212419f0b83c41571f405aa
ShaMan123 <shacharnen@gmail.com>
2022-03-07 11:08:18 +0200

    tests(): layout + origin tests

--- test/visual/group_layout.js
+++ test/visual/group_layout.js
@@ -10,7 +10,7 @@
 
     var tests = [];
 
-    function prepareGroup(text, options) {
+    function createGroupForLayoutTests(text, options) {
         var circle = new fabric.Circle({
             left: 100,
             top: 50,
@@ -34,8 +34,31 @@
         ], options);
     }
 
+    function fixedLayout(canvas, callback) {
+        var g = createGroupForLayoutTests('fixed layout', {
+            backgroundColor: 'azure',
+            layout: 'fixed',
+            width: 50,
+            height: 50,
+            angle: 30
+        });
+        canvas.add(g);
+        canvas.renderAll();
+        callback(canvas.lowerCanvasEl);
+    }
+/*
+    tests.push({
+        test: 'fixed layout with width, height, angle values',
+        code: fixedLayout,
+        golden: 'group-fixed-layout.png',
+        newModule: 'Group Layout',
+        percentage: 0.06,
+        width: 400,
+        height: 300
+    });
+*/
     function fitContentLayout(canvas, callback) {
-        var g = prepareGroup('fit-content layout', {
+        var g = createGroupForLayoutTests('fit-content layout', {
             backgroundColor: 'blue'
         });
         canvas.add(g);
@@ -54,7 +77,7 @@
     });
 
     function fitContentLayoutChange(canvas, callback) {
-        var g = prepareGroup('fit-content layout', {
+        var g = createGroupForLayoutTests('fit-content layout', {
             backgroundColor: 'blue'
         });
         g.item(0).setX(50);
@@ -70,14 +93,13 @@
         test: 'fit-content layout after change',
         code: fitContentLayoutChange,
         golden: 'group-fit-content-layout2.png',
-        newModule: 'Group Layout',
         percentage: 0.06,
         width: 400,
         height: 300
     });
 
     function fitContentLayoutAdd(canvas, callback) {
-        var g = prepareGroup('fit-content layout', {
+        var g = createGroupForLayoutTests('fit-content layout', {
             backgroundColor: 'blue'
         });
         var rect = new fabric.Rect({
@@ -106,7 +128,7 @@
     });
 
     function clipPathLayout(canvas, callback) {
-        var g = prepareGroup('clip path layout', {
+        var g = createGroupForLayoutTests('clip path layout', {
             backgroundColor: 'magenta',
             clipPath: new fabric.Circle({
                 radius: 110,
@@ -130,7 +152,7 @@
     });
 
     function clipPathLayoutWithScale(canvas, callback) {
-        var g = prepareGroup('clip path layout', {
+        var g = createGroupForLayoutTests('clip path layout', {
             backgroundColor: 'magenta',
             clipPath: new fabric.Circle({
                 radius: 110,
@@ -155,7 +177,7 @@
     });
 
     function clipPathLayout2(canvas, callback) {
-        var g = prepareGroup('clip path layout', {
+        var g = createGroupForLayoutTests('clip path layout', {
             backgroundColor: 'magenta',
             clipPath: new fabric.Circle({
                 radius: 110,
@@ -180,7 +202,7 @@
     });
 
     function absClipPathLayout(canvas, callback) {
-        var g = prepareGroup('clip path layout', {
+        var g = createGroupForLayoutTests('clip path layout', {
             backgroundColor: '#0dcaf0',
             clipPath: new fabric.Circle({
                 radius: 110,
@@ -206,6 +228,37 @@
         width: 250,
         height: 250
     });
+
+    function createObjectsForOriginTests(originX, originY, options) {
+        var rect1 = new fabric.Rect({ top: 100, left: 150, width: 30, height: 10, strokeWidth: 0 }),
+            rect2 = new fabric.Rect({ top: 120, left: 200, width: 10, height: 40, strokeWidth: 0 }),
+            controlPoint = new fabric.Circle({ radius: 5, fill: 'blue', left: 150, top: 100, originX: 'center', originY: 'center' });
+        
+        var g = new fabric.Group([rect1, rect2], Object.assign({}, options, {
+            originX, originY, strokeWidth: 1, stroke: 'blue'
+        }));
+        return [controlPoint, g];
+    }
+
+    var originX = ['left', 'center', 'right'];
+    var originY = ['top', 'center', 'bottom'];
+
+    originX.forEach(ox => {
+        originY.forEach(oy => {
+            tests.push({
+                test: 'absolute clip-path layout',
+                code: function (canvas, callback) {
+                    canvas.add(...createObjectsForOriginTests(ox, oy));
+                    canvas.renderAll();
+                    callback(canvas.lowerCanvasEl);
+                },
+                golden: `group-origin-layout-${ox}-${oy}.png`,
+                percentage: 0.06,
+                width: 300,
+                height: 300
+            });
+        })
+    })
 
     tests.forEach(visualTestLoop(QUnit));
 })();
