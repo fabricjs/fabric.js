e4d38829fa9d3a90c6ac05bc92422a9fd5f8d60d
ShaMan123 <shacharnen@gmail.com>
2022-03-24 17:27:41 +0200

    Update canvas_static.js

--- test/unit/canvas_static.js
+++ test/unit/canvas_static.js
@@ -1278,6 +1278,26 @@
     });
   });
 
+  QUnit.test('abort loadFromJSON with image background and color', function (assert) {
+    var done = assert.async();
+    assert.expect(5);
+    var serialized = JSON.parse(PATH_JSON);
+    serialized.background = 'green';
+    serialized.backgroundImage = { "type": "image", "originX": "left", "originY": "top", "left": 13.6, "top": -1.4, "width": 3000, "height": 3351, "fill": "rgb(0,0,0)", "stroke": null, "strokeWidth": 0, "strokeDashArray": null, "strokeLineCap": "butt", "strokeDashOffset": 0, "strokeLineJoin": "miter", "strokeMiterLimit": 4, "scaleX": 0.05, "scaleY": 0.05, "angle": 0, "flipX": false, "flipY": false, "opacity": 1, "shadow": null, "visible": true, "backgroundColor": "", "fillRule": "nonzero", "globalCompositeOperation": "source-over", "skewX": 0, "skewY": 0, "src": IMG_SRC, "filters": [], "crossOrigin": "" };
+    canvas.on('loading:aborted', function (opt) {
+      assert.equal(opt.from, 'json');
+    });
+    canvas.loadFromJSON(serialized).catch(function (err) {
+      assert.equal(err.type, 'abort');
+    });
+    canvas.loadFromJSON(serialized).then(function () {
+      assert.ok(!canvas.isEmpty(), 'canvas is not empty');
+      assert.equal(canvas.backgroundColor, 'green');
+      assert.ok(canvas.backgroundImage instanceof fabric.Image);
+      done();
+    });
+  });
+
   QUnit.test('loadFromJSON custom properties', function(assert) {
     var done = assert.async();
     var rect = new fabric.Rect({ width: 10, height: 20 });
