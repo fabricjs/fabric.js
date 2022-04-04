2708029c5e0c85ada98d1b67c747ccaad2c522b5
ShaMan123 <shacharnen@gmail.com>
2022-04-04 10:28:53 +0300

    patch2

--- src/mixins/object_ancestry.mixin.js
+++ src/mixins/object_ancestry.mixin.js
@@ -1,10 +1,31 @@
 fabric.util.object.extend(fabric.Object.prototype, /** @lends fabric.Object.prototype */ {
 
   /**
-     *
-     * @param {boolean} [strict] returns only ancestors that are objects (without canvas)
-     * @returns {(fabric.Object | fabric.StaticCanvas)[]} ancestors from bottom to top
-     */
+   * Checks if object is decendant of target
+   * Should be used instead of @link {fabric.Collection.contains} for performance reasons
+   * @param {fabric.Object|fabric.StaticCanvas} target
+   * @returns {boolean}
+   */
+  isDescendantOf: function (target) {
+    var parent = this.group || this.canvas;
+    while (parent) {
+      if (target === parent) {
+        return true;
+      }
+      else if (parent instanceof fabric.StaticCanvas) {
+        //  happens after all parents were traversed through without a match
+        return false;
+      }
+      parent = parent.group || parent.canvas;
+    }
+    return false;
+  },
+
+  /**
+   *
+   * @param {boolean} [strict] returns only ancestors that are objects (without canvas)
+   * @returns {(fabric.Object | fabric.StaticCanvas)[]} ancestors from bottom to top
+   */
   getAncestors: function (strict) {
     var ancestors = [];
     var parent = this.group || (!strict ? this.canvas : undefined);
@@ -13,5 +34,44 @@ fabric.util.object.extend(fabric.Object.prototype, /** @lends fabric.Object.prot
       parent = parent.group || (!strict ? parent.canvas : undefined);
     }
     return ancestors;
+  },
+
+  /**
+   *
+   * @param {fabric.Object} other
+   * @returns {{ index: number, otherIndex: number, ancestors: fabric.Object[] }} ancestors may include the passed objects if one is an ancestor of the other resulting in index of -1
+   */
+  findCommonAncestors: function (other) {
+    if (this === other) {
+      return true;
+    }
+    else if (!other) {
+      return false;
+    }
+    var ancestors = this.getAncestors();
+    ancestors.unshift(this);
+    var otherAncestors = other.getAncestors();
+    otherAncestors.unshift(other);
+    for (var i = 0, ancestor; i < ancestors.length; i++) {
+      ancestor = ancestors[i];
+      for (var j = 0; j < otherAncestors.length; j++) {
+        if (ancestor === otherAncestors[j] && !(ancestor instanceof fabric.StaticCanvas)) {
+          return {
+            index: i - 1,
+            otherIndex: j - 1,
+            ancestors: ancestors.slice(i)
+          };
+        }
+      }
+    }
+  },
+
+  /**
+   *
+   * @param {fabric.Object} other
+   * @returns {boolean}
+   */
+  hasCommonAncestors: function (other) {
+    return !!this.findCommonAncestors(other);
   }
 });
