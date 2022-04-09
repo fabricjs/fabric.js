ffafe66f0c0766836c88a503650f72ec6f0c3d1b
Andrea Bogazzi <andreabogazzi79@gmail.com>
2022-04-10 00:27:12 +0200

    more comments

--- src/mixins/object_geometry.mixin.js
+++ src/mixins/object_geometry.mixin.js
@@ -137,7 +137,11 @@
     },
 
     /**
-     * @param {fabric.Point} point position according to object's {@link fabric.Object#originX} {@link fabric.Object#originY} properties in canvas coordinate plane
+     * Set an object position to a particular point, the point is intended in absolute ( canvas ) coordinate.
+     * If you need to pint the object on a particular point, you can specify that using originX and originY,
+     * that otherwise are the object's current values. {@link fabric.Object#originX} {@link fabric.Object#originY}
+     * Example: to set an object bottom left corner to point 5,5, uses setXY(new fabric.Point(5,5), 'left', 'bottom').
+     * @param {fabric.Point} point position in canvas coordinate plane
      * @param {'left'|'center'|'right'|number} [originX] Horizontal origin: 'left', 'center' or 'right'
      * @param {'top'|'center'|'bottom'|number} [originY] Vertical origin: 'top', 'center' or 'bottom'
      */
@@ -159,6 +163,7 @@
     },
 
     /**
+     * As {@link fabric.Object#setXY}, but in current parent's coordinate plane ( the current group if any or the canvas)
      * @param {fabric.Point} point position according to object's {@link fabric.Object#originX} {@link fabric.Object#originY} properties in parent's coordinate plane
      * @param {'left'|'center'|'right'|number} [originX] Horizontal origin: 'left', 'center' or 'right'
      * @param {'top'|'center'|'bottom'|number} [originY] Vertical origin: 'top', 'center' or 'bottom'
