dd416d87144fd8ebcafd456bc081b149942c38ce
ShaMan123 <shacharnen@gmail.com>
2022-04-26 13:40:38 +0300

    fix(): rendering logic of a single selected object

--- src/shapes/group.class.js
+++ src/shapes/group.class.js
@@ -431,7 +431,7 @@
           object.render(ctx, renderingContext);
           ctx.restore();
         }
-        else if ((!filter || forceRendering) && object.group === this) {
+        else if (((!filter && !this._activeObjects.includes(object)) || forceRendering) && object.group === this) {
           object.render(ctx, renderingContext);
         }
       }
