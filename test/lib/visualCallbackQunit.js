4263d5baf91314e76fea86fa26c379dc4398ffbe
Shachar <34343793+ShaMan123@users.noreply.github.com>
2022-05-22 23:55:25 +0300

    test(freedrawing): test enhancement (#7941)

--- test/lib/visualCallbackQunit.js
+++ test/lib/visualCallbackQunit.js
@@ -4,12 +4,7 @@
   }
 
   visualCallback.prototype.addArguments = function(argumentObj) {
-    this.currentArgs = {
-      enabled: true,
-      fabric: argumentObj.fabric,
-      golden: argumentObj.golden,
-      diff: argumentObj.diff,
-    };
+    this.currentArgs = Object.assign({}, argumentObj);
   };
 
   visualCallback.prototype.testDone = function(details) {
@@ -17,6 +12,7 @@
       var fabricCanvas = this.currentArgs.fabric;
       var ouputImageDataRef = this.currentArgs.diff;
       var goldenCanvasRef = this.currentArgs.golden;
+      var goldenName = this.currentArgs.goldenName;
       var id = 'qunit-test-output-' + details.testId;
       var node = document.getElementById(id);
       var fabricCopy = document.createElement('canvas');
@@ -25,11 +21,28 @@
       diff.height = fabricCopy.height = fabricCanvas.height;
       diff.getContext('2d').putImageData(ouputImageDataRef, 0, 0);
       fabricCopy.getContext('2d').drawImage(fabricCanvas, 0, 0);
-      var _div = document.createElement('div');
-      _div.appendChild(goldenCanvasRef);
-      _div.appendChild(fabricCopy);
-      _div.appendChild(diff);
-      node.appendChild(_div);
+
+      var data = {
+        actual: fabricCopy,
+        expected: goldenCanvasRef,
+        diff
+      };
+
+      var template = document.getElementById('error_output');
+      var errorOutput = template.content.cloneNode(true);
+      Object.keys(data).forEach(key => {
+        const canvas = data[key];
+        errorOutput.querySelector(`*[data-canvas-type="${key}"]`).appendChild(canvas);
+        canvas.style.cursor = 'pointer';
+        canvas.onclick = () => {
+          const link = document.createElement('a');
+          link.href = fabric.util.toDataURL(canvas, 'png', 1);
+          link.download = `(${key}) ${goldenName}`;
+          link.click();
+        }
+      });
+      node.appendChild(errorOutput);
+      
       // after one run, disable
       this.currentArgs.enabled = false;
     }
