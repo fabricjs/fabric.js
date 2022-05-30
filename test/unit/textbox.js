a5ad5b16fa915e3e54448327e730ecc501e47e56
ShaMan123 <shacharnen@gmail.com>
2022-05-31 01:47:38 +0300

    Update textbox.js

--- test/unit/textbox.js
+++ test/unit/textbox.js
@@ -74,7 +74,7 @@
   });
 
   QUnit.test('constructor with width too small', function(assert) {
-    var textbox = new fabric.Textbox('test', { width: 5 });
+    var textbox = new fabric.Textbox('test', { width: 5, minWidth: 5 });
     assert.equal(Math.round(textbox.width), 56, 'width is calculated by constructor');
   });
 
@@ -84,6 +84,29 @@
     assert.equal(textbox.type, 'textbox');
     assert.deepEqual(textbox.styles, { });
     assert.ok(textbox.cacheProperties.indexOf('width') > -1, 'width is in cacheProperties');
+  });
+
+  QUnit.test('width properties', function (assert) {
+    var textbox = new fabric.Textbox('test', { minWidth: 10, width: 9, maxWidth: 8 });
+    assert.equal(textbox.minWidth, 10);
+    assert.equal(textbox.width, 10);
+    assert.equal(textbox.maxWidth, 10);
+    textbox.set({ minWidth: 11 });
+    assert.equal(textbox.minWidth, 11);
+    assert.equal(textbox.width, 11);
+    assert.equal(textbox.maxWidth, 11);
+    textbox.set({ maxWidth: 9 });
+    assert.equal(textbox.minWidth, 11);
+    assert.equal(textbox.width, 11);
+    assert.equal(textbox.maxWidth, 11);
+    textbox.set({ width: 9 });
+    assert.equal(textbox.minWidth, 11);
+    assert.equal(textbox.width, 11);
+    assert.equal(textbox.maxWidth, 11);
+    textbox.set({ maxWidth: 12 });
+    assert.equal(textbox.minWidth, 11);
+    assert.equal(textbox.width, 11);
+    assert.equal(textbox.maxWidth, 12);
   });
 
   QUnit.test('toObject', function(assert) {
@@ -451,6 +474,7 @@
     var textbox = new fabric.Textbox(text, {
       styles: { 0: styles },
       width: 5,
+      minWidth: 5
     });
     assert.equal(typeof textbox._deleteStyleDeclaration, 'function', 'function exists');
     textbox._deleteStyleDeclaration(2, 2);
@@ -467,6 +491,7 @@
     var textbox = new fabric.Textbox(text, {
       styles: { 0: styles },
       width: 5,
+      minWidth: 5
     });
     assert.equal(typeof textbox._setStyleDeclaration, 'function', 'function exists');
     var newStyle = { fontSize: 10 };
