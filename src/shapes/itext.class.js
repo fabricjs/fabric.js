1c84b62bdd609f4521d009a4dddd018ed3ddd2cb
ShaMan123 <shacharnen@gmail.com>
2022-06-10 17:33:55 +0300

    cleanup

--- src/shapes/itext.class.js
+++ src/shapes/itext.class.js
@@ -4,7 +4,6 @@
    * prefix when observing canvas.
    * @class fabric.IText
    * @extends fabric.Text
-   * @mixes fabric.Observable
    *
    * @fires changed
    * @fires selection:changed
@@ -45,7 +44,7 @@
    *   Select line:                    triple click
    * </pre>
    */
-  fabric.IText = fabric.util.createClass(fabric.Text, fabric.Observable, /** @lends fabric.IText.prototype */ {
+  fabric.IText = fabric.util.createClass(fabric.Text, /** @lends fabric.IText.prototype */ {
 
     /**
      * Type of an object
