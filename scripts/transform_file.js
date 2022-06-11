7aabd8b8fe9f3a4698158ded2d2da520962b4e7f
ShaMan123 <shacharnen@gmail.com>
2022-06-11 12:01:46 +0300

    Update transform_file.js

--- scripts/transform_file.js
+++ scripts/transform_file.js
@@ -86,13 +86,14 @@ function findClass(raw) {
     const keyWord = getVariableNameOfNS(raw, 'fabric.util.createClass');
     const regex = new RegExp(`(.+)=\\s*${keyWord.replaceAll('.', '\\.')}\\((\.*)\\{`, 'm');
     const result = regex.exec(raw);
-    if (!result) return;
+    if (!result) throw new Error('NOT_FOUND');
     const [match, classNSRaw, superClassRaw] = result;
     const namespace = classNSRaw.trim();
     const name = namespace.slice(namespace.lastIndexOf('.') + 1);
     const superClasses = superClassRaw.trim().split(',').filter(raw => !raw.match(/\/\*+/));
     const rawObject = findObject(raw, '{', '}', result.index);
-    const klass = fabric.util.resolveNamespace(namespace.slice(0, namespace.lastIndexOf('.')))[name];
+    const NS = namespace.slice(0, namespace.lastIndexOf('.'));
+    const klass = fabric.util.resolveNamespace(NS === 'fabric' ? null : NS)[name];
     return {
         name,
         namespace,
@@ -109,6 +110,35 @@ function findClass(raw) {
     };
 }
 
+function transformSuperCall(raw) {
+    const regex = /this.callSuper\((.+)\)/g;
+    const result = regex.exec(raw);
+    if (!result) return raw;
+    const [rawMethodName, ...args] = result[1].split(',');
+    const methodName = rawMethodName.replace(/'|"/g, '');
+    const firstArgIndex = result[1].indexOf(args[0]);
+    const rest = firstArgIndex > -1 ? result[1].slice(firstArgIndex, result[1].length).trim() : '';
+    const transformedCall = `super${methodName === 'initialize' ? '' : `.${methodName}`}(${rest})`;
+    return raw.slice(0, result.index) + transformedCall + raw.slice(result.index + result[0].length);
+}
+
+function transformFile(raw, { namespace, name } = {}) {
+    if (raw.startsWith('(function')) {
+        const wrapper = findObject(raw, '{', '}');
+        raw = wrapper.raw.slice(1, wrapper.raw.length - 1);
+    }
+
+    const annoyingCheck = new RegExp(`if\\s*\\(\\s*(global.)?${namespace.replace(/\./g, '\\.')}\\s*\\)\\s*{`);
+    const result = annoyingCheck.exec(raw);
+    if (result) {
+        const found = findObject(raw, '{', '}', result.index);
+        raw = raw.slice(0, result.index) + raw.slice(found.end+1);
+    }
+    raw = `//@ts-nocheck\n${raw}`;
+    //raw = `${raw}\n/** @todo TODO_JS_MIGRATION remove next line after refactoring build */\n${namespace} = ${name};\n`;
+    return raw;
+}
+
 function transformClass(file) {
     let raw = readFile(file);
     let { prototype, match, name, namespace, superClass, raw: rawClass, end } = findClass(raw);
@@ -121,51 +151,89 @@ function transformClass(file) {
     Object.keys(prototype).forEach((key) => {
         const object = prototype[key];
         if (typeof object === 'function') {
-            const searchPhrase = `${key}\\s*:\\s*function\\s*\\(`;
-            const regex = new RegExp(searchPhrase);
-            const start = regex.exec(rawClass)?.index;
+            const searchPhrase = `^(\\s*)${key}\\s*:\\s*function\\s*\\(`;
+            const regex = new RegExp(searchPhrase, 'm');
+            const result = regex.exec(rawClass);
+            if (!result) return;
+            const whitespace = result[1];
+            const start = result.index + whitespace.length;
             const func = findObject(rawClass, '{', '}', start);
-            func.raw.indexOf('this') === -1 && staticCandidantes.push(key);
+            start && func.raw.indexOf('this') === -1 && staticCandidantes.push(key);
             const indexOfComma = rawClass.indexOf(',', func.end);
             if (indexOfComma > -1) {
                 rawClass = rawClass.slice(0, indexOfComma) + rawClass.slice(indexOfComma + 1);
             }
-            rawClass = rawClass.replace(regex, `${key}(`);
+            rawClass = rawClass.replace(regex, `${whitespace}${key==='initialize'?'constructor':key}(`);
+            if (regex.exec(rawClass)) {
+                throw new Error(`dupliate method found ${name}#${key}`)
+            }
+            
         }
         else {
+           // if (key !=='selectionColor')return
             const start = getPropStart(key);
-            const type = typeof prototype[key];
             switch (typeof prototype[key]) {
+                case 'function':
+                    throw new Error(`DEBUG: ${key}\n${prototype[key].toString()}`);
                 case 'object':
                     const a = findObject('[', ']', start.start);
                     const b = findObject('(', ')', start.start);
                     const c = findObject('{', '}', start.start);
-                    if (!a || !b || !c) {
+                    if (a || b || c) {
+                        const end = Math.max(a?.end || 0, b?.end || 0, c?.end || 0);
+                        const indexOfLineBreak = rawClass.indexOf('\n', end);
+                        const indexOfComma = rawClass.lastIndexOf(',', indexOfLineBreak);
+                        if (indexOfComma > -1) {
+                            rawClass = rawClass.slice(0, indexOfComma) /*+'\n\n\n\nDEBUG\n\n\n\n'*/ + rawClass.slice(indexOfComma + 1);
+                        }
                         break;
                     }
                 default:
-                    const indexOfComma = rawClass.indexOf(',', start.start);
+                    const indexOfLineBreak = rawClass.indexOf('\n', start.start);
+                    const indexOfComma = rawClass.lastIndexOf(',', indexOfLineBreak);
                     if (indexOfComma > -1) {
-                        rawClass = rawClass.slice(0, indexOfComma) + rawClass.slice(indexOfComma + 1);
+                        rawClass = rawClass.slice(0, indexOfComma) /*+'\n\n\n\nDEBUG\n\n\n\n'*/+ rawClass.slice(indexOfComma + 1);
                     }
                     break;
             }
-
+            let transformed = rawClass;
+            do {
+                rawClass = transformed;
+                transformed = transformSuperCall(rawClass);
+            } while (transformed !== rawClass);
+            
             rawClass = rawClass.replace(start.regex, `${key} = `);
         }
     });
 
     const classDirective = `export class ${name}${superClass ? ` extends ${superClass}` : ''} ${rawClass}`;
-    raw = `${raw.slice(0, match.index)}${classDirective}${raw.slice(end+1).replace(/\s*\)\s*;?/,'')}`;
-    if (raw.startsWith('(function')) {
-        const wrapper = findObject(raw, '{', '}');
-        raw = wrapper.raw.slice(1, wrapper.raw.length - 1);
-    }
-    raw = `${raw}\n/** @todo remove next line after refactoring build */\n${namespace} = ${name};\n`;
-    return { raw, staticCandidantes };
+    raw = `${raw.slice(0, match.index)}${classDirective}${raw.slice(end + 1).replace(/\s*\)\s*;?/, '')}`;
+    raw = transformFile(raw, { namespace, name });
+   return { name, raw, staticCandidantes };
 }
 
 //transformFile('src/parser.js')
+const shapes = fs.readdirSync(path.resolve(wd, './src/shapes'));
+shapes.forEach(file => {
+    if (path.parse(file).ext !== '.js') return;
+    const source = path.join('src', 'shapes', file);
+    const dest = path.resolve(wd, source);
+    try {
+        const { name, raw, staticCandidantes } = transformClass(source);
+        if (staticCandidantes.length > 0) {
+            console.log({
+                class: name,
+                origin: file,
+                file: `${name}.ts`,
+                staticCandidantes
+            })
+        }
+        fs.writeFileSync(dest, raw);
+    } catch (e) {
+        console.error(file, e)
+    }
+})
+fs.writeFileSync(path.resolve(wd, './src/Canvas.js'), transformClass('src/canvas.class.js').raw);
 fs.writeFileSync(path.resolve(wd, './src/Canvas.js'), transformClass('src/canvas.class.js').raw);
 //transformFile('src/mixins/canvas_events.mixin.js')
 
