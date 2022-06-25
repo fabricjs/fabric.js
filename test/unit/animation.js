c1c1b9835eb1514e00b12d8fe6908cf9c1e0fcdc
Andrea Bogazzi <andreabogazzi79@gmail.com>
2022-06-25 19:13:15 +0200

    fix test

--- test/unit/animation.js
+++ test/unit/animation.js
@@ -409,7 +409,7 @@
     var object = new fabric.Object({ left: 123, top: 124 });
     var started = false;
     var t = new Date();
-    var abort = object._animate('left', 223, {
+    object._animate('left', 223, {
       onStart: function () {
         started = true;
         assert.gte(new Date() - t, 500, 'animation delay');
@@ -419,7 +419,6 @@
       delay: 500
     });
     assert.ok(started === false);
-    abort();
   });
 
   QUnit.test('animate easing easeInQuad', function(assert) {
