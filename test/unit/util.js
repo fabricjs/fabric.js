ec86828c378bcda2c98cbab836989d6193580998
ShaMan123 <shacharnen@gmail.com>
2022-06-28 12:58:12 +0300

    tests

--- test/unit/util.js
+++ test/unit/util.js
@@ -512,6 +512,16 @@
     });
   });
 
+  QUnit.test('fabric.util.loadImage with AbortController', function (assert) {
+    var done = assert.async();
+    var abortController = new AbortController();
+    fabric.util.loadImage(IMG_URL, { signal: abortController.signal }).catch(function (err) {
+      assert.ok(err instanceof Error, 'callback should be invoked with error set to true');
+      done();
+    });
+    abortController.abort();
+  });
+
   var SVG_WITH_1_ELEMENT = '<?xml version="1.0"?>\
     <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\
     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\
