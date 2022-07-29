e9ee0383c00c3b6dc7aa1ecd2efda3ee9bf8728e
ShaMan123 <shacharnen@gmail.com>
2022-07-29 11:06:55 +0300

    docs

--- src/util/misc.js
+++ src/util/misc.js
@@ -58,10 +58,11 @@
     },
 
     /**
-     * Returns the square root of the sum of squares of its arguments
-     * Chrome implements `Math#hypot` with a calculation that affects percision
+     * Returns the square root of the sum of squares of its arguments\
+     * Chrome implements `Math#hypot` with a calculation that affects percision so we hard code it as a util
      * @see https://stackoverflow.com/questions/62931950/different-results-of-math-hypot-on-chrome-and-firefox
-     *
+     * @static
+     * @memberOf fabric.util
      * @param {...number}
      * @returns {number}
      */
