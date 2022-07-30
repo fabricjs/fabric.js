ffc2a0fb7ae2434b60cd4a28fdad112d67449dde
ShaMan123 <shacharnen@gmail.com>
2022-07-30 16:26:07 +0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,21 @@
 export const halfPI = Math.PI / 2;
+
+export const ALIASING_LIMIT = 2;
+
+/* _FROM_SVG_START_ */
+/**
+ * Attributes parsed from all SVG elements
+ * @type array
+ */
+export const SHARED_ATTRIBUTES = [
+    'display',
+    'transform',
+    'fill', 'fill-opacity', 'fill-rule',
+    'opacity',
+    'stroke', 'stroke-dasharray', 'stroke-linecap', 'stroke-dashoffset',
+    'stroke-linejoin', 'stroke-miterlimit',
+    'stroke-opacity', 'stroke-width',
+    'id', 'paint-order', 'vector-effect',
+    'instantiated_by_use', 'clip-path',
+];
+/* _FROM_SVG_END_ */
