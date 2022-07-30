d37150f4f63a455026e869b9410ccbb4718e07b4
ShaMan123 <shacharnen@gmail.com>
2022-07-31 00:13:29 +0300

    jsdom types

--- context/node_context.ts
+++ context/node_context.ts
@@ -1,9 +1,9 @@
-x
-import jsdom from 'jsdom';
+//@ts-nocheck
+import { JSDOM } from 'jsdom';
 import { implForWrapper } from 'jsdom/lib/jsdom/living/generated/utils';
 import { Canvas } from 'jsdom/lib/jsdom/utils';
 
-const virtualWindow = new jsdom.JSDOM(
+const virtualWindow = new JSDOM(
     decodeURIComponent('%3C!DOCTYPE%20html%3E%3Chtml%3E%3Chead%3E%3C%2Fhead%3E%3Cbody%3E%3C%2Fbody%3E%3C%2Fhtml%3E'),
     {
         features: {
