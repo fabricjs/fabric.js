f7e69b570ff63a83366c28eb7e78c3c50a3d59cd
ShaMan123 <shacharnen@gmail.com>
2022-07-30 11:06:39 +0300

    SharedMethods

---
+++ src/mixins/shared_methods.mixin.ts
@@ -0,0 +1,59 @@
+export class CommonMethods {
+
+  /**
+   * Sets object's properties from options
+   * @deprecated use `set`
+   * @param {Object} [options] Options object
+   */
+  _setOptions(options) {
+    for (var prop in options) {
+      this.set(prop, options[prop]);
+    }
+  }
+
+  /**
+   * Sets property to a given value. When changing position/dimension -related properties (left, top, scale, angle, etc.) `set` does not update position of object's borders/controls. If you need to update those, call `setCoords()`.
+   * @param {String|Object} key Property name or object (if object, iterate over the object properties)
+   * @param {Object|Function} value Property value (if function, the value is passed into it and its return value is used as a new one)
+   * @return {fabric.Object} thisArg
+   * @chainable
+   */
+  set(key, value) {
+    if (typeof key === 'object') {
+      for (var prop in obj) {
+        this._set(prop, obj[prop]);
+      }
+    }
+    else {
+      this._set(key, value);
+    }
+    return this;
+  }
+
+  _set(key, value) {
+    this[key] = value;
+  }
+
+  /**
+   * Toggles specified property from `true` to `false` or from `false` to `true`
+   * @param {String} property Property to toggle
+   * @return {fabric.Object} thisArg
+   * @chainable
+   */
+  toggle(property) {
+    var value = this.get(property);
+    if (typeof value === 'boolean') {
+      this.set(property, !value);
+    }
+    return this;
+  }
+
+  /**
+   * Basic getter
+   * @param {String} property Property name
+   * @return {*} value of a property
+   */
+  get(property) {
+    return this[property];
+  }
+}
