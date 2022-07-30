0825f25e9b25f5b8d5d74d1434fe659b6b9326c3
ShaMan123 <shacharnen@gmail.com>
2022-07-30 22:54:18 +0300

    json import

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,27 @@
 import { terser } from 'rollup-plugin-terser';
 import ts from "rollup-plugin-ts";
+import json from '@rollup/plugin-json';
 
 // rollup.config.js
 export default {
   input: ['./index.js'],
   output: [
     {
-        file: './dist/fabric.js',
-        name: 'fabric',
-        format: 'cjs',
+      file: './dist/fabric.js',
+      name: 'fabric',
+      format: 'cjs',
     },
     {
-        file: './dist/fabric.min.js',
-        format: 'cjs',
-        name: 'fabric',
-        plugins: [terser()],
+      file: './dist/fabric.min.js',
+      format: 'cjs',
+      name: 'fabric',
+      plugins: [terser()],
     },
   ],
   plugins: [
-		ts({
-			/* Plugin options */
-		})
-	]
+    json(),
+    ts({
+      /* Plugin options */
+    })
+  ]
 };
