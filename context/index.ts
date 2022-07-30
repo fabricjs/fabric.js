57e5f591bcc68b992c4b1013f5ccefa395de4555
ShaMan123 <shacharnen@gmail.com>
2022-07-31 00:24:37 +0300

    Update index.ts

--- context/index.ts
+++ context/index.ts
@@ -1,6 +1,10 @@
 import { version } from '../package.json';
 export const VERSION = version;
+
+const a = require.resolve('./browser_context');
+const b = require.resolve('./node_context');
+
 // assume we're running under node.js when document/window are not present
 export const context = typeof document !== 'undefined' && typeof window !== 'undefined' ?
-    require('./browser_context') :
-    require('./node_context')
+    require(a) :
+    require(b);
