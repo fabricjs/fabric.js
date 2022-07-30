15841433a7a6c433684089ced8b71b3856c36a20
ShaMan123 <shacharnen@gmail.com>
2022-07-30 23:49:05 +0300

    Update HEADER.js

--- HEADER.js
+++ HEADER.js
@@ -3,7 +3,7 @@ import { context } from './context';
 import cache from './src/cache';
 import config from './src/config';
 var fabric = fabric || { version: '5.1.0' };
-global.fabric = fabric;
+
 if (typeof exports !== 'undefined') {
   exports.fabric = fabric;
 }
