e79552435e7d0f0492b07116663ccfecec59a25b
ShaMan123 <shacharnen@gmail.com>
2022-07-30 16:25:48 +0300

    mock until exports

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,1 +1,64 @@
+//@ts-nocheck
+const fabric = { util: {} };
 export * from './cos';
+import './animate'; // optional animation
+import './animate_color'; // optional animation
+import './anim_ease'; // optional easing
+import './dom_event'; // optional interaction
+import './dom_misc';
+import './dom_request';
+import './dom_style';
+import './lang_array';
+import './lang_class';
+import './lang_object';
+import './lang_string';
+import './misc';
+import './path';
+
+
+const {
+    getElementOffset,
+    removeFromArray,
+    toFixed,
+    transformPoint,
+    invertTransform,
+    getNodeCanvas,
+    createCanvasElement,
+    toDataURL,
+    multiplyTransformMatrices,
+    applyTransformToObject,
+    degreesToRadians,
+    enlivenObjects,
+    enlivenObjectEnlivables,
+    cleanUpJsdomNode,
+    loadImage,
+    setImageSmoothing,
+    getById,
+    addClass,
+    parsePreserveAspectRatioAttribute,
+    findScaleToFit,
+    findScaleToCover
+} = fabric.util;
+export {
+    getElementOffset,
+    removeFromArray,
+    toFixed,
+    transformPoint,
+    invertTransform,
+    getNodeCanvas,
+    createCanvasElement,
+    toDataURL,
+    multiplyTransformMatrices,
+    applyTransformToObject,
+    degreesToRadians,
+    enlivenObjects,
+    enlivenObjectEnlivables,
+    cleanUpJsdomNode,
+    loadImage,
+    setImageSmoothing,
+    getById,
+    addClass,
+    parsePreserveAspectRatioAttribute,
+    findScaleToFit,
+    findScaleToCover
+};
