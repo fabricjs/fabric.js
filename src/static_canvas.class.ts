9c4e8fd7667ef5544c70c7865c5bb58064328c7f
ShaMan123 <shacharnen@gmail.com>
2022-07-31 11:45:30 +0300

    Update static_canvas.class.ts

--- src/static_canvas.class.ts
+++ src/static_canvas.class.ts
@@ -1,15 +1,14 @@
 //@ts-nocheck
 
-import { VERSION } from '../context';
 import { NUM_FRACTION_DIGITS } from './config';
 import { devicePixelRatio, fontPaths, iMatrix, incrementUID, isLikelyNode } from './constants';
+import { VERSION } from './context';
 import { applyMixins, CanvasSerializationMixinGenerator } from './mixins';
 import { CanvasDataURLExporterMixinGenerator } from "./mixins/canvas_dataurl_exporter.mixin";
 import { CollectionMixinGenerator } from "./mixins/collection.mixin";
 import { CommonMethods } from "./mixins/common_methods.mixin";
 import { NodeCanvasStreamsMixinGenerator } from "./mixins/node_canvas_streams.mixin";
 import { Point } from "./point.class";
-import { FabricObject } from "./shapes/object.class";
 import {
   addClass, cancelAnimFrame, cleanUpJsdomNode, createCanvasElement, getById, getElementOffset, invertTransform, matrixToSVG, populateWithProperties, removeFromArray, requestAnimFrame, setImageSmoothing, toFixed,
   transformPoint
