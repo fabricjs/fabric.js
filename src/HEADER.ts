3c579bb2deb09481b60c84e6d3170493619c6bba
ShaMan123 <shacharnen@gmail.com>
2022-07-31 13:36:40 +0300

    Update HEADER.ts

--- src/HEADER.ts
+++ src/HEADER.ts
@@ -2,6 +2,7 @@
 /*! Fabric.js Copyright 2008-2015, Printio (Juriy Zaytsev, Maxim Chernyak) */
 import cache from './cache';
 import config from './config';
+import constants from './constants';
 import { context } from './context';
 import { fabric } from './main';
 
@@ -13,7 +14,7 @@ else if (typeof define === 'function' && define.amd) {
   define([], function () { return fabric; });
 }
 /* _AMD_END_ */
-Object.assign(fabric, context, config, cache);
+Object.assign(fabric, context, constants, config, cache);
 if (context.DOMParser) {
   global.DOMParser = context.DOMParser;
 }
