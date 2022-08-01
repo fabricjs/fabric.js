701d3c92230adb41038bc408546b29861fd771b2
ShaMan123 <shacharnen@gmail.com>
2022-08-01 22:34:50 +0300

    defaults

--- defaults/index.ts
+++ defaults/index.ts
@@ -1,6 +1,21 @@
+import * as DCircle from './Circle.json';
+import * as DEllipse from './Ellipse.json';
+import * as DLine from './Line.json';
 import * as DObject from './Object.json';
+import * as DPath from './Path.json';
+import * as DPolygon from './Polygon.json';
+import * as DPolyline from './Polyline.json';
 import * as DRect from './Rect.json';
+import * as DTriangle from './Triangle.json';
 
 export const DEFAULTS = {
-    rect: { ...DObject, ...DRect }
+    object: DObject,
+    circle: { ...DObject, ...DCircle },
+    ellipse: { ...DObject, ...DEllipse },
+    line: { ...DObject, ...DLine },
+    path: { ...DObject, ...DPath },
+    polygon: { ...DObject, ...DPolyline, ...DPolygon },
+    polyline: { ...DObject, ...DPolyline },
+    triangle: { ...DObject, ...DTriangle },
+    rect: { ...DObject, ...DRect },
 };
