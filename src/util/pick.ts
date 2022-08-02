1b66a6f0dbbeb294315af6c30ff08167b868c16c
ShaMan123 <shacharnen@gmail.com>
2022-08-02 18:51:36 +0300

    Update pick.ts

--- src/util/pick.ts
+++ src/util/pick.ts
@@ -1,6 +1,6 @@
 //@ts-nocheck
-export function pick(object, keys = []) {
-  return keys.reduce((dest, key) => {
+export function pick(object, keys) {
+  return (keys || []).reduce((dest, key) => {
     key && key in object && (dest[key] = object[key]);
     return dest;
   }, {});
