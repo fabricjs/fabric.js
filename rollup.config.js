9e71568b240613cb09f17b10c448deffed0cf2ea
Shachar <34343793+ShaMan123@users.noreply.github.com>
2022-08-04 01:18:44 +0300

    ci(): adapt build script to rollup (#8114)

--- rollup.config.js
+++ rollup.config.js
@@ -6,13 +6,13 @@ export default {
   input: ['./index.js'],
   output: [
     {
-        file: './dist/fabric.js',
+        file: process.env.BUILD_DESTINATION || './dist/fabric.js',
         name: 'fabric',
         format: 'cjs',
     },
     Number(process.env.MINIFY) ?
       {
-        file: './dist/fabric.min.js',
+        file: process.env.MIN_BUILD_DESTINATION || './dist/fabric.min.js',
         format: 'cjs',
         name: 'fabric',
         plugins: [terser()],
