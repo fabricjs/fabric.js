7c17d24cfbbc31930dc796421f187ec7750e54f3
ShaMan123 <shacharnen@gmail.com>
2022-08-04 18:06:44 +0300

    Update intersection.class.ts

--- src/intersection.class.ts
+++ src/intersection.class.ts
@@ -10,6 +10,8 @@ export class Intersection {
 
   points: Point[]
 
+  status?: IntersectionType
+
   constructor(status?: IntersectionType) {
     this.status = status;
     this.points = [];
@@ -50,8 +52,8 @@ export class Intersection {
    * @return {Intersection}
    */
   static intersectSegmentSegment(a1, a2, b1, b2) {
-    let result,
-      uaT = (b2.x - b1.x) * (a1.y - b1.y) - (b2.y - b1.y) * (a1.x - b1.x),
+    let result;
+    const uaT = (b2.x - b1.x) * (a1.y - b1.y) - (b2.y - b1.y) * (a1.x - b1.x),
       ubT = (a2.x - a1.x) * (a1.y - b1.y) - (a2.y - a1.y) * (a1.x - b1.x),
       uB = (b2.y - b1.y) * (a2.x - a1.x) - (b2.x - b1.x) * (a2.y - a1.y);
     if (uB !== 0) {
