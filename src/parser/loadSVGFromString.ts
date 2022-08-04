85cf000568821c6fdf559385eeeb0451d990e6bb
ShaMan123 <shacharnen@gmail.com>
2022-08-04 19:26:39 +0300

    Update loadSVGFromString.ts

--- src/parser/loadSVGFromString.ts
+++ src/parser/loadSVGFromString.ts
@@ -1,3 +1,7 @@
+//@ts-nocheck
+import { fabric } from "../../HEADER";
+import { parseSVGDocument } from "./parseSVGDocument";
+
 /**
  * Takes string corresponding to an SVG document, and parses it into a set of fabric objects
  * @memberOf fabric
@@ -8,10 +12,9 @@
  * @param {String} [options.crossOrigin] crossOrigin crossOrigin setting to use for external resources
  * @param {AbortSignal} [options.signal] handle aborting, see https://developer.mozilla.org/en-US/docs/Web/API/AbortController/signal
  */
-
 export function loadSVGFromString(string, callback, reviver, options) {
   var parser = new fabric.window.DOMParser(), doc = parser.parseFromString(string.trim(), 'text/xml');
-  fabric.parseSVGDocument(doc.documentElement, function (results, _options, elements, allElements) {
+  parseSVGDocument(doc.documentElement, function (results, _options, elements, allElements) {
     callback(results, _options, elements, allElements);
   }, reviver, options);
 }
