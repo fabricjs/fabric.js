bfc48ad721b95582c2bcf62f223bf750dcd627fd
Andrea Bogazzi <andreabogazzi79@gmail.com>
2022-08-04 11:56:01 +0200

    some es6

--- src/shapes/rect.class.ts
+++ src/shapes/rect.class.ts
@@ -1,6 +1,6 @@
 //@ts-nocheck
 import { fabric } from '../../HEADER';
-
+import { kRect } from '../constants';
 /**
  * Rectangle class
  * @class Rect
@@ -55,11 +55,12 @@ const Rect = fabric.util.createClass(fabric.Object, /** @lends Rect.prototype */
    * @private
    */
   _initRxRy: function() {
-    if (this.rx && !this.ry) {
-      this.ry = this.rx;
+    const { rx, ry } = this;
+    if (rx && !ry) {
+      this.ry = rx;
     }
-    else if (this.ry && !this.rx) {
-      this.rx = this.ry;
+    else if (ry && !rx) {
+      this.rx = ry;
     }
   },
 
@@ -71,31 +72,29 @@ const Rect = fabric.util.createClass(fabric.Object, /** @lends Rect.prototype */
 
     // 1x1 case (used in spray brush) optimization was removed because
     // with caching and higher zoom level this makes more damage than help
+    let { rx, ry } = this;
+    const { width, height } = this;
+    const x = -width / 2;
+    const y = -height / 2;
+    rx = rx ? Math.min(rx, width / 2) : 0;
+    ry = this.ry ? Math.min(ry, height / 2) : 0;
+    const isRounded = rx !== 0 || ry !== 0;
 
-    const rx = this.rx ? Math.min(this.rx, this.width / 2) : 0,
-        ry = this.ry ? Math.min(this.ry, this.height / 2) : 0,
-        w = this.width,
-        h = this.height,
-        x = -this.width / 2,
-        y = -this.height / 2,
-        isRounded = rx !== 0 || ry !== 0,
-        /* "magic number" for bezier approximations of arcs (http://itc.ktu.lt/itc354/Riskus354.pdf) */
-        k = 1 - 0.5522847498;
     ctx.beginPath();
 
     ctx.moveTo(x + rx, y);
 
     ctx.lineTo(x + w - rx, y);
-    isRounded && ctx.bezierCurveTo(x + w - k * rx, y, x + w, y + k * ry, x + w, y + ry);
+    isRounded && ctx.bezierCurveTo(x + w - kRect * rx, y, x + w, y + kRect * ry, x + w, y + ry);
 
     ctx.lineTo(x + w, y + h - ry);
-    isRounded && ctx.bezierCurveTo(x + w, y + h - k * ry, x + w - k * rx, y + h, x + w - rx, y + h);
+    isRounded && ctx.bezierCurveTo(x + w, y + h - kRect * ry, x + w - kRect * rx, y + h, x + w - rx, y + h);
 
     ctx.lineTo(x + rx, y + h);
-    isRounded && ctx.bezierCurveTo(x + k * rx, y + h, x, y + h - k * ry, x, y + h - ry);
+    isRounded && ctx.bezierCurveTo(x + kRect * rx, y + h, x, y + h - kRect * ry, x, y + h - ry);
 
     ctx.lineTo(x, y + ry);
-    isRounded && ctx.bezierCurveTo(x, y + k * ry, x + k * rx, y, x + rx, y);
+    isRounded && ctx.bezierCurveTo(x, y + kRect * ry, x + kRect * rx, y, x + rx, y);
 
     ctx.closePath();
 
@@ -118,12 +117,12 @@ const Rect = fabric.util.createClass(fabric.Object, /** @lends Rect.prototype */
    * of the instance
    */
   _toSVG: function() {
-    const x = -this.width / 2, y = -this.height / 2;
+    const { width, height, rx, ry } = this;
     return [
       '<rect ', 'COMMON_PARTS',
-      'x="', x, '" y="', y,
-      '" rx="', this.rx, '" ry="', this.ry,
-      '" width="', this.width, '" height="', this.height,
+      'x="', -width / 2, '" y="', -height / 2,
+      '" rx="', rx, '" ry="', ry,
+      '" width="', width, '" height="', height,
       '" />\n'
     ];
   },
