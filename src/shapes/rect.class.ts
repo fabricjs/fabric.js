06cbca1a1a807be0e830413da9a6f5c57c3a06bd
Andrea Bogazzi <andreabogazzi79@gmail.com>
2022-08-04 11:45:57 +0200

    ws changes

--- src/shapes/rect.class.ts
+++ src/shapes/rect.class.ts
@@ -1,179 +1,179 @@
 //@ts-nocheck
 import { fabric } from '../../HEADER';
 
+/**
+ * Rectangle class
+ * @class Rect
+ * @extends fabric.Object
+ * @return {Rect} thisArg
+ * @see {@link Rect#initialize} for constructor definition
+ */
+const Rect = fabric.util.createClass(fabric.Object, /** @lends Rect.prototype */ {
+
   /**
-   * Rectangle class
-   * @class Rect
-   * @extends fabric.Object
-   * @return {Rect} thisArg
-   * @see {@link Rect#initialize} for constructor definition
+   * List of properties to consider when checking if state of an object is changed ({@link fabric.Object#hasStateChanged})
+   * as well as for history (undo/redo) purposes
+   * @type Array
    */
-  const Rect = fabric.util.createClass(fabric.Object, /** @lends Rect.prototype */ {
+  stateProperties: fabric.Object.prototype.stateProperties.concat('rx', 'ry'),
 
-    /**
-     * List of properties to consider when checking if state of an object is changed ({@link fabric.Object#hasStateChanged})
-     * as well as for history (undo/redo) purposes
-     * @type Array
-     */
-    stateProperties: fabric.Object.prototype.stateProperties.concat('rx', 'ry'),
-
-    /**
-     * Type of an object
-     * @type String
-     * @default
-     */
-    type: 'rect',
-
-    /**
-     * Horizontal border radius
-     * @type Number
-     * @default
-     */
-    rx:   0,
-
-    /**
-     * Vertical border radius
-     * @type Number
-     * @default
-     */
-    ry:   0,
-
-    cacheProperties: fabric.Object.prototype.cacheProperties.concat('rx', 'ry'),
-
-    /**
-     * Constructor
-     * @param {Object} [options] Options object
-     * @return {Object} thisArg
-     */
-    initialize: function(options) {
-      this.callSuper('initialize', options);
-      this._initRxRy();
-    },
-
-    /**
-     * Initializes rx/ry attributes
-     * @private
-     */
-    _initRxRy: function() {
-      if (this.rx && !this.ry) {
-        this.ry = this.rx;
-      }
-      else if (this.ry && !this.rx) {
-        this.rx = this.ry;
-      }
-    },
-
-    /**
-     * @private
-     * @param {CanvasRenderingContext2D} ctx Context to render on
-     */
-    _render: function(ctx) {
-
-      // 1x1 case (used in spray brush) optimization was removed because
-      // with caching and higher zoom level this makes more damage than help
-
-      const rx = this.rx ? Math.min(this.rx, this.width / 2) : 0,
-          ry = this.ry ? Math.min(this.ry, this.height / 2) : 0,
-          w = this.width,
-          h = this.height,
-          x = -this.width / 2,
-          y = -this.height / 2,
-          isRounded = rx !== 0 || ry !== 0,
-          /* "magic number" for bezier approximations of arcs (http://itc.ktu.lt/itc354/Riskus354.pdf) */
-          k = 1 - 0.5522847498;
-      ctx.beginPath();
-
-      ctx.moveTo(x + rx, y);
-
-      ctx.lineTo(x + w - rx, y);
-      isRounded && ctx.bezierCurveTo(x + w - k * rx, y, x + w, y + k * ry, x + w, y + ry);
-
-      ctx.lineTo(x + w, y + h - ry);
-      isRounded && ctx.bezierCurveTo(x + w, y + h - k * ry, x + w - k * rx, y + h, x + w - rx, y + h);
-
-      ctx.lineTo(x + rx, y + h);
-      isRounded && ctx.bezierCurveTo(x + k * rx, y + h, x, y + h - k * ry, x, y + h - ry);
-
-      ctx.lineTo(x, y + ry);
-      isRounded && ctx.bezierCurveTo(x, y + k * ry, x + k * rx, y, x + rx, y);
-
-      ctx.closePath();
-
-      this._renderPaintInOrder(ctx);
-    },
-
-    /**
-     * Returns object representation of an instance
-     * @param {Array} [propertiesToInclude] Any properties that you might want to additionally include in the output
-     * @return {Object} object representation of an instance
-     */
-    toObject: function(propertiesToInclude) {
-      return this.callSuper('toObject', ['rx', 'ry'].concat(propertiesToInclude));
-    },
-
-    /* _TO_SVG_START_ */
-    /**
-     * Returns svg representation of an instance
-     * @return {Array} an array of strings with the specific svg representation
-     * of the instance
-     */
-    _toSVG: function() {
-      const x = -this.width / 2, y = -this.height / 2;
-      return [
-        '<rect ', 'COMMON_PARTS',
-        'x="', x, '" y="', y,
-        '" rx="', this.rx, '" ry="', this.ry,
-        '" width="', this.width, '" height="', this.height,
-        '" />\n'
-      ];
-    },
-    /* _TO_SVG_END_ */
-  });
-
-  /* _FROM_SVG_START_ */
   /**
-   * List of attribute names to account for when parsing SVG element (used by `Rect.fromElement`)
-   * @static
-   * @memberOf Rect
-   * @see: http://www.w3.org/TR/SVG/shapes.html#RectElement
+   * Type of an object
+   * @type String
+   * @default
    */
-  Rect.ATTRIBUTE_NAMES = fabric.SHARED_ATTRIBUTES.concat('x y rx ry width height'.split(' '));
+  type: 'rect',
 
   /**
-   * Returns {@link Rect} instance from an SVG element
-   * @static
-   * @memberOf Rect
-   * @param {SVGElement} element Element to parse
-   * @param {Function} callback callback function invoked after parsing
+   * Horizontal border radius
+   * @type Number
+   * @default
+   */
+  rx:   0,
+
+  /**
+   * Vertical border radius
+   * @type Number
+   * @default
+   */
+  ry:   0,
+
+  cacheProperties: fabric.Object.prototype.cacheProperties.concat('rx', 'ry'),
+
+  /**
+   * Constructor
    * @param {Object} [options] Options object
+   * @return {Object} thisArg
    */
-  Rect.fromElement = function(element, callback, options) {
-    if (!element) {
-      return callback(null);
-    }
-    options = options || { };
-
-    const parsedAttributes = fabric.parseAttributes(element, Rect.ATTRIBUTE_NAMES);
-    parsedAttributes.left = parsedAttributes.left || 0;
-    parsedAttributes.top  = parsedAttributes.top  || 0;
-    parsedAttributes.height  = parsedAttributes.height || 0;
-    parsedAttributes.width  = parsedAttributes.width || 0;
-    const rect = new Rect(Object.assign({}, options, parsedAttributes));
-    rect.visible = rect.visible && rect.width > 0 && rect.height > 0;
-    callback(rect);
-  };
-  /* _FROM_SVG_END_ */
+  initialize: function(options) {
+    this.callSuper('initialize', options);
+    this._initRxRy();
+  },
 
   /**
-   * Returns {@link Rect} instance from an object representation
-   * @static
-   * @memberOf Rect
-   * @param {Object} object Object to create an instance from
-   * @returns {Promise<Rect>}
+   * Initializes rx/ry attributes
+   * @private
    */
-  Rect.fromObject = function(object) {
-    return fabric.Object._fromObject(Rect, object);
-  };
+  _initRxRy: function() {
+    if (this.rx && !this.ry) {
+      this.ry = this.rx;
+    }
+    else if (this.ry && !this.rx) {
+      this.rx = this.ry;
+    }
+  },
+
+  /**
+   * @private
+   * @param {CanvasRenderingContext2D} ctx Context to render on
+   */
+  _render: function(ctx) {
+
+    // 1x1 case (used in spray brush) optimization was removed because
+    // with caching and higher zoom level this makes more damage than help
+
+    const rx = this.rx ? Math.min(this.rx, this.width / 2) : 0,
+        ry = this.ry ? Math.min(this.ry, this.height / 2) : 0,
+        w = this.width,
+        h = this.height,
+        x = -this.width / 2,
+        y = -this.height / 2,
+        isRounded = rx !== 0 || ry !== 0,
+        /* "magic number" for bezier approximations of arcs (http://itc.ktu.lt/itc354/Riskus354.pdf) */
+        k = 1 - 0.5522847498;
+    ctx.beginPath();
+
+    ctx.moveTo(x + rx, y);
+
+    ctx.lineTo(x + w - rx, y);
+    isRounded && ctx.bezierCurveTo(x + w - k * rx, y, x + w, y + k * ry, x + w, y + ry);
+
+    ctx.lineTo(x + w, y + h - ry);
+    isRounded && ctx.bezierCurveTo(x + w, y + h - k * ry, x + w - k * rx, y + h, x + w - rx, y + h);
+
+    ctx.lineTo(x + rx, y + h);
+    isRounded && ctx.bezierCurveTo(x + k * rx, y + h, x, y + h - k * ry, x, y + h - ry);
+
+    ctx.lineTo(x, y + ry);
+    isRounded && ctx.bezierCurveTo(x, y + k * ry, x + k * rx, y, x + rx, y);
+
+    ctx.closePath();
+
+    this._renderPaintInOrder(ctx);
+  },
+
+  /**
+   * Returns object representation of an instance
+   * @param {Array} [propertiesToInclude] Any properties that you might want to additionally include in the output
+   * @return {Object} object representation of an instance
+   */
+  toObject: function(propertiesToInclude) {
+    return this.callSuper('toObject', ['rx', 'ry'].concat(propertiesToInclude));
+  },
+
+  /* _TO_SVG_START_ */
+  /**
+   * Returns svg representation of an instance
+   * @return {Array} an array of strings with the specific svg representation
+   * of the instance
+   */
+  _toSVG: function() {
+    const x = -this.width / 2, y = -this.height / 2;
+    return [
+      '<rect ', 'COMMON_PARTS',
+      'x="', x, '" y="', y,
+      '" rx="', this.rx, '" ry="', this.ry,
+      '" width="', this.width, '" height="', this.height,
+      '" />\n'
+    ];
+  },
+  /* _TO_SVG_END_ */
+});
+
+/* _FROM_SVG_START_ */
+/**
+ * List of attribute names to account for when parsing SVG element (used by `Rect.fromElement`)
+ * @static
+ * @memberOf Rect
+ * @see: http://www.w3.org/TR/SVG/shapes.html#RectElement
+ */
+Rect.ATTRIBUTE_NAMES = fabric.SHARED_ATTRIBUTES.concat('x y rx ry width height'.split(' '));
+
+/**
+ * Returns {@link Rect} instance from an SVG element
+ * @static
+ * @memberOf Rect
+ * @param {SVGElement} element Element to parse
+ * @param {Function} callback callback function invoked after parsing
+ * @param {Object} [options] Options object
+ */
+Rect.fromElement = function(element, callback, options) {
+  if (!element) {
+    return callback(null);
+  }
+  options = options || { };
+
+  const parsedAttributes = fabric.parseAttributes(element, Rect.ATTRIBUTE_NAMES);
+  parsedAttributes.left = parsedAttributes.left || 0;
+  parsedAttributes.top  = parsedAttributes.top  || 0;
+  parsedAttributes.height  = parsedAttributes.height || 0;
+  parsedAttributes.width  = parsedAttributes.width || 0;
+  const rect = new Rect(Object.assign({}, options, parsedAttributes));
+  rect.visible = rect.visible && rect.width > 0 && rect.height > 0;
+  callback(rect);
+};
+/* _FROM_SVG_END_ */
+
+/**
+ * Returns {@link Rect} instance from an object representation
+ * @static
+ * @memberOf Rect
+ * @param {Object} object Object to create an instance from
+ * @returns {Promise<Rect>}
+ */
+Rect.fromObject = function(object) {
+  return fabric.Object._fromObject(Rect, object);
+};
 
 fabric.Rect = Rect;
 export {
