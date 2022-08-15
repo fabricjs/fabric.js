1f3a26a38a7eaf0c76e5ab9811ad4400c9e8a084
ShaMan123 <shacharnen@gmail.com>
2022-08-15 22:47:30 +0300

    Update index.js

--- .codesandbox/templates/node/src/index.js
+++ .codesandbox/templates/node/src/index.js
@@ -4,8 +4,9 @@ const { fabric } = require('fabric');
 http
   .createServer((req, res) => {
     const canvas = new fabric.Canvas(null, { width: 100, height: 100 });
-    const rect = new fabric.Rect({ width: 20, height: 50, fill: '#ff0000' });
-    canvas.add(rect);
+    const rect = new fabric.Rect({ width: 20, height: 50, fill: "#ff0000" });
+    const text = new fabric.Text("fabric.js", { fill: "blue", fontSize: 24 });
+    canvas.add(rect, text);
     canvas.renderAll();
     canvas.createPNGStream().pipe(res);
     // or send HTML markup
