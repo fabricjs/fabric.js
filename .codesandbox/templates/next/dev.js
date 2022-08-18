6d8dbbad152ecd381aff878780b09b17a21ce4c0
ShaMan123 <shacharnen@gmail.com>
2022-08-19 01:59:14 +0300

    next

--- .codesandbox/templates/next/dev.js
+++ .codesandbox/templates/next/dev.js
@@ -1,3 +1,3 @@
-const { execSync } = require("child_process");
+const { spawnSync } = require("child_process");
 
-execSync('next', { env: { ...process.env, NODE_ENV: 'development' }, stdio: 'inherit' });
+spawnSync('nodemon --config nodemon.config.json server.js 3000', { env: { ...process.env, NODE_ENV: 'development' }, stdio: 'inherit' });
