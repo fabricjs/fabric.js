2bc3dd7f506ff682f558ddd4568ecd5cc74febd5
ShaMan123 <shacharnen@gmail.com>
2022-08-18 13:58:14 +0300

    safeguard from `null`

--- src/parser/percent.ts
+++ src/parser/percent.ts
@@ -10,8 +10,13 @@ export function isPercent(value: string | null) {
 }
 
 export function parsePercent(value: string | number | null, valueIfNaN?: number) {
+    const parsed = typeof value == 'number' ?
+        value :
+        typeof value == 'string' ?
+            parseFloat(value) / (isPercent(value) ? 100 : 1) :
+            NaN;
     return ifNaN(
-        clamp(0, typeof value == 'string' ? parseFloat(value) / (isPercent(value) ? 100 : 1) : value, 1),
+        clamp(0, parsed, 1),
         valueIfNaN
     );
 }
