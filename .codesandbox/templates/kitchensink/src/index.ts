c2726c3c9a8cd526ef33a8a85bd437e28b545b00
ShaMan123 <shacharnen@gmail.com>
2022-08-22 15:40:39 +0300

    byebye

--- .codesandbox/templates/kitchensink/src/index.ts
+++ .codesandbox/templates/kitchensink/src/index.ts
@@ -6,11 +6,11 @@ import './controller';
 import './init';
 // import './paster';
 
-// // https://getbootstrap.com/docs/5.0/components/offcanvas/#via-javascript
-// import { Offcanvas } from 'bootstrap';
-// document.onload = () => {
-//     const offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
-//     offcanvasElementList.map(function (offcanvasEl) {
-//         return new Offcanvas(offcanvasEl, { backdrop: false })
-//     })
-// }
+// https://getbootstrap.com/docs/5.0/components/offcanvas/#via-javascript
+import { Offcanvas } from 'bootstrap';
+document.onload = () => {
+    const offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
+    offcanvasElementList.map(function (offcanvasEl) {
+        return new Offcanvas(offcanvasEl, { backdrop: false })
+    })
+}
