04a5404325d260da52c6fe93509081d3ec0960f7
ShaMan123 <shacharnen@gmail.com>
2022-08-23 00:15:06 +0300

    cleanup

--- .codesandbox/templates/kitchensink/src/index.ts
+++ .codesandbox/templates/kitchensink/src/index.ts
@@ -1,13 +1,12 @@
+import { Offcanvas } from 'bootstrap';
 import "../styles/kitchensink.css";
 import "../styles/master.css";
-import "../styles/prism.css";
 import "../styles/styles.scss";
 import './controller';
 import './init';
 // import './paster';
 
 // https://getbootstrap.com/docs/5.0/components/offcanvas/#via-javascript
-import { Offcanvas } from 'bootstrap';
 document.onload = () => {
     const offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
     offcanvasElementList.map(function (offcanvasEl) {
