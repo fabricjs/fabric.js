9e9090d25d3a0daababb9210d7fdf5682c2b305e
ShaMan123 <shacharnen@gmail.com>
2022-08-22 15:09:50 +0300

    offcanvas

--- .codesandbox/templates/kitchensink/src/index.ts
+++ .codesandbox/templates/kitchensink/src/index.ts
@@ -5,3 +5,12 @@ import "../styles/styles.scss";
 import './controller';
 import './init';
 // import './paster';
+
+// // https://getbootstrap.com/docs/5.0/components/offcanvas/#via-javascript
+// import { Offcanvas } from 'bootstrap';
+// document.onload = () => {
+//     const offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
+//     offcanvasElementList.map(function (offcanvasEl) {
+//         return new Offcanvas(offcanvasEl, { backdrop: false })
+//     })
+// }
