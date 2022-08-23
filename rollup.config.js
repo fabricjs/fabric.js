be4c6cf4227f75826046f1f10b2778728421a663
Shachar <34343793+ShaMan123@users.noreply.github.com>
2022-08-23 23:36:37 +0300

    ci(test): migrate test cmd (#8138)
    Co-authored-by: Andrea Bogazzi <andreabogazzi79@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,20 @@
 import { terser } from 'rollup-plugin-terser';
 import ts from "rollup-plugin-ts";
 
-// rollup.config.js
+// https://rollupjs.org/guide/en/#configuration-files
 export default {
-  input: ['./index.js'],
+  input: process.env.BUILD_INPUT?.split(',') || ['./index.js'],
   output: [
     {
-        file: process.env.BUILD_DESTINATION || './dist/fabric.js',
+        file: process.env.BUILD_OUTPUT || './dist/fabric.js',
         name: 'fabric',
         format: 'cjs',
     },
     Number(process.env.MINIFY) ?
       {
-        file: process.env.MIN_BUILD_DESTINATION || './dist/fabric.min.js',
-        format: 'cjs',
+        file: process.env.BUILD_MIN_OUTPUT || './dist/fabric.min.js',
         name: 'fabric',
+        format: 'cjs',
         plugins: [terser()],
       } : null,
   ],
