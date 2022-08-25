c23bae0a88df50dddaf83c1828bd8cdfa2177b1e
ShaMan123 <shacharnen@gmail.com>
2022-08-25 17:31:05 +0300

    Update fireEvent.ts

--- src/util/fireEvent.ts
+++ src/util/fireEvent.ts
@@ -2,8 +2,7 @@ import { TransformEvent } from "../typedefs";
 
 
 export const fireEvent = <T>(eventName: string, options: TransformEvent<T>) => {
-    const target = options.transform.target,
-        canvas = target.canvas;
-    canvas && canvas.fire('object:' + eventName, Object.assign({}, options, { target: target }));
+    const target = options.transform.target;
+    target.canvas?.fire('object:' + eventName, { ...options, target });
     target.fire(eventName, options);
 }
