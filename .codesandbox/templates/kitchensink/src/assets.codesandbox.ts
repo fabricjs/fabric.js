e4cd5584a00dd5407db1089bbcd8a328ba958467
ShaMan123 <shacharnen@gmail.com>
2022-08-25 11:53:52 +0300

    Update assets.codesandbox.ts

--- .codesandbox/templates/kitchensink/src/assets.codesandbox.ts
+++ .codesandbox/templates/kitchensink/src/assets.codesandbox.ts
@@ -1,6 +1,12 @@
 // This file will be used when deployed to codesandbox for 2 reasons:
-// 1. glob importing isn't supported since parcelrc is not read by codesandbox
+// 1. glob importing isn't supported since parcelrc is not read by codesandbox (at 08/2022)
 // 2. assets are too heavy
 
-export const image = {};
+const BASE_URL ='https://cdn.jsdelivr.net/gh/fabricjs/fabric.js@vkitchensink/.codesandbox/templates/kitchensink/assets/images';
+
+export const images = {
+    ladybug: `${BASE_URL}/ladybug.png`,
+    honey_im_subtle: `${BASE_URL}/honey_im_subtle.png`
+};
+
 export const SVG = {};
