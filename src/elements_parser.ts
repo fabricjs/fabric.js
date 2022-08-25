25a9cbda3722a863fcc997a140ec274a380d370f
ShaMan123 <shacharnen@gmail.com>
2022-08-25 20:16:50 +0300

    Update elements_parser.ts

--- src/elements_parser.ts
+++ src/elements_parser.ts
@@ -39,7 +39,7 @@
           klass.fromElement(el, this.createCallback(index, el), this.options);
         }
         catch (err) {
-          fabric.log(err);
+          console.log(err);
         }
       }
       else {
