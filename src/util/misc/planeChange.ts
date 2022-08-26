d5db60196c949dd7ac7278feea9240d4ded064ba
Andrea Bogazzi <andreabogazzi79@gmail.com>
2022-08-26 16:12:39 +0200

    small fixes

--- src/util/misc/planeChange.ts
+++ src/util/misc/planeChange.ts
@@ -1,32 +1,33 @@
 import type { TMat2D } from "../../typedefs";
-import { invertTransform, multiplyTransformMatrices, transformPoint } from "../matrix";
+import { invertTransform, multiplyTransformMatrices, transformPoint } from "./matrix";
 import { iMatrix } from "../../constants";
 import type { IPoint, Point } from "../../point.class";
+import { applyTransformToObject } from './objectTransforms';
 
 export const enum ObjectRelation {
   sibling = 'sibling',
   child = 'child',
 }
 
-    /**
-     * Sends a point from the source coordinate plane to the destination coordinate plane.\
-     * From the canvas/viewer's perspective the point remains unchanged.
-     *
-     * @example <caption>Send point from canvas plane to group plane</caption>
-     * var obj = new fabric.Rect({ left: 20, top: 20, width: 60, height: 60, strokeWidth: 0 });
-     * var group = new fabric.Group([obj], { strokeWidth: 0 });
-     * var sentPoint1 = fabric.util.sendPointToPlane(new Point(50, 50), null, group.calcTransformMatrix());
-     * var sentPoint2 = fabric.util.sendPointToPlane(new Point(50, 50), fabric.iMatrix, group.calcTransformMatrix());
-     * console.log(sentPoint1, sentPoint2) //  both points print (0,0) which is the center of group
-     *
-     * @static
-     * @memberOf fabric.util
-     * @see {fabric.util.transformPointRelativeToCanvas} for transforming relative to canvas
-     * @param {IPoint} point
-     * @param {TMat2D} [from] plane matrix containing object. Passing `null` is equivalent to passing the identity matrix, which means `point` exists in the canvas coordinate plane.
-     * @param {TMat2D} [to] destination plane matrix to contain object. Passing `null` means `point` should be sent to the canvas coordinate plane.
-     * @returns {Point} transformed point
-     */
+/**
+ * Sends a point from the source coordinate plane to the destination coordinate plane.\
+ * From the canvas/viewer's perspective the point remains unchanged.
+ *
+ * @example <caption>Send point from canvas plane to group plane</caption>
+ * var obj = new fabric.Rect({ left: 20, top: 20, width: 60, height: 60, strokeWidth: 0 });
+ * var group = new fabric.Group([obj], { strokeWidth: 0 });
+ * var sentPoint1 = fabric.util.sendPointToPlane(new Point(50, 50), null, group.calcTransformMatrix());
+ * var sentPoint2 = fabric.util.sendPointToPlane(new Point(50, 50), fabric.iMatrix, group.calcTransformMatrix());
+ * console.log(sentPoint1, sentPoint2) //  both points print (0,0) which is the center of group
+ *
+ * @static
+ * @memberOf fabric.util
+ * @see {fabric.util.transformPointRelativeToCanvas} for transforming relative to canvas
+ * @param {IPoint} point
+ * @param {TMat2D} [from] plane matrix containing object. Passing `null` is equivalent to passing the identity matrix, which means `point` exists in the canvas coordinate plane.
+ * @param {TMat2D} [to] destination plane matrix to contain object. Passing `null` means `point` should be sent to the canvas coordinate plane.
+ * @returns {Point} transformed point
+ */
 export const sendPointToPlane = (point: IPoint, from: TMat2D = iMatrix, to: TMat2D = iMatrix): Point =>
   //  we are actually looking for the transformation from the destination plane to the source plane (which is a linear mapping)
   //  the object will exist on the destination plane and we want it to seem unchanged by it so we reverse the destination matrix (to) and then apply the source matrix (from)
@@ -103,14 +104,13 @@ export const transformPointRelativeToCanvas = (
  * @param {Matrix} [to] destination plane matrix to contain object. Passing `null` means `object` should be sent to the canvas coordinate plane.
  * @returns {Matrix} the transform matrix that was applied to `object`
  */
-export const sendObjectToPlane = (object, from, to) => {
+export const sendObjectToPlane = (object: any, from: TMat2D = iMatrix, to: TMat2D = iMatrix): TMat2D => {
   //  we are actually looking for the transformation from the destination plane to the source plane (which is a linear mapping)
   //  the object will exist on the destination plane and we want it to seem unchanged by it so we reverse the destination matrix (to) and then apply the source matrix (from)
-  var inv = fabric.util.invertTransform(to || fabric.iMatrix);
-  var t = fabric.util.multiplyTransformMatrices(inv, from || fabric.iMatrix);
-  fabric.util.applyTransformToObject(
+  const t = multiplyTransformMatrices(invertTransform(to), from);
+  applyTransformToObject(
     object,
-    fabric.util.multiplyTransformMatrices(t, object.calcOwnMatrix())
+    multiplyTransformMatrices(t, object.calcOwnMatrix())
   );
   return t;
 };
