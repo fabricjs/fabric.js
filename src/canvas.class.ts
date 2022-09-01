d2ac909f5dac2c9b81069193ef99d28309d9bccf
Andrea Bogazzi <andreabogazzi79@gmail.com>
2022-09-01 16:29:55 +0200

    Revert "ok?"
    This reverts commit 77b07271fd359629b1dc294ee3bae859b12dc954.

--- src/canvas.class.ts
+++ src/canvas.class.ts
@@ -1055,7 +1055,7 @@ import { Point } from './point.class';
         upperCanvasEl = this._createCanvasElement();
         this.upperCanvasEl = upperCanvasEl;
       }
-      upperCanvasEl.classList.add('upper-canvas', lowerCanvasClass);
+      upperCanvasEl.classList.add('upper-canvas ' + lowerCanvasClass);
       this.upperCanvasEl.setAttribute('data-fabric', 'top');
       this.wrapperEl.appendChild(upperCanvasEl);
 
