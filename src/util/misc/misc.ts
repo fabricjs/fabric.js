3cd1da1be9549a16662c234f5135456690f14756
Andrea Bogazzi <andreabogazzi79@gmail.com>
2022-09-02 00:23:32 +0200

    moving isTransparent

--- src/util/misc/misc.ts
+++ src/util/misc/misc.ts
@@ -88,6 +88,8 @@ import {
   makeElementUnselectable,
   makeElementSelectable,
 } from '../dom_misc';
+import { isTransparent } from './isTransparent';
+
   /**
    * @namespace fabric.util
    */
@@ -181,51 +183,7 @@ import {
     cleanUpJsdomNode,
     makeElementUnselectable,
     makeElementSelectable,
-
-    /**
-     * Returns true if context has transparent pixel
-     * at specified location (taking tolerance into account)
-     * @param {CanvasRenderingContext2D} ctx context
-     * @param {Number} x x coordinate
-     * @param {Number} y y coordinate
-     * @param {Number} tolerance Tolerance
-     */
-    isTransparent: function(ctx, x, y, tolerance) {
-
-      // If tolerance is > 0 adjust start coords to take into account.
-      // If moves off Canvas fix to 0
-      if (tolerance > 0) {
-        if (x > tolerance) {
-          x -= tolerance;
-        }
-        else {
-          x = 0;
-        }
-        if (y > tolerance) {
-          y -= tolerance;
-        }
-        else {
-          y = 0;
-        }
-      }
-
-      var _isTransparent = true, i, temp,
-          imageData = ctx.getImageData(x, y, (tolerance * 2) || 1, (tolerance * 2) || 1),
-          l = imageData.data.length;
-
-      // Split image data - for tolerance > 1, pixelDataSize = 4;
-      for (i = 3; i < l; i += 4) {
-        temp = imageData.data[i];
-        _isTransparent = temp <= 0;
-        if (_isTransparent === false) {
-          break; // Stop if colour found
-        }
-      }
-
-      imageData = null;
-
-      return _isTransparent;
-    },
+    isTransparent,
 
     /**
      * given a width and height, return the size of the bounding box
