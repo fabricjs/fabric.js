9b9e157c8907927a2b2e26ef884c3fd6a02f22d9
ShaMan123 <shacharnen@gmail.com>
2022-09-02 10:34:30 +0300

    Update node_test_setup.js

--- test/node_test_setup.js
+++ test/node_test_setup.js
@@ -5,7 +5,7 @@ var commander = require('commander');
 
 // TODO remove when node 14 fades out
 // node 14 test polyfill
-if (Number(process.versions.node.split('.')[0]) < 16) {
+if (!AbortController) {
   // polyfill AbortController for node 14
   // remove this dependency when node 14 fades out
   require("abort-controller/polyfill");
