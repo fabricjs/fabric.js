0777f6ead65b169cc046e6ec0c72fe56f8406777
Andrea Bogazzi <andreabogazzi79@gmail.com>
2022-09-03 01:09:07 +0200

    install prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,18 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
   overrides: [
     {
       files: ['**/*.ts'],
-    }
+    },
   ],
   rules: {
     '@typescript-eslint/ban-ts-comment': 1,
-  }
+  },
 };
