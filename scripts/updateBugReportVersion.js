231bd3ddfec5d042a4857a1b2340343fcfb15d5d
ShaMan123 <shacharnen@gmail.com>
2022-09-04 12:24:25 +0300

    pass version to script

--- scripts/updateBugReportVersion.js
+++ scripts/updateBugReportVersion.js
@@ -5,4 +5,4 @@ const { version } = require('../package.json');
 const TAG = '<!-- CI_REPLACE_VERSION -->';
 const file = path.resolve(__dirname, '..', '.github/ISSUE_TEMPLATE/bug_report.md');
 const content = fs.readFileSync(file).toString();
-fs.writeFileSync(file, content.replace(new RegExp(`${TAG}.*${TAG}`), `${TAG}${version}${TAG}`));
+fs.writeFileSync(file, content.replace(new RegExp(`${TAG}.*${TAG}`), `${TAG}${process.env.VERSION || version}${TAG}`));
