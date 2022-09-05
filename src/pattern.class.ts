e70569407a66ee80074058ca6804c8fffc6e3e05
ShaMan123 <shacharnen@gmail.com>
2022-09-05 19:22:46 +0300

    migrate

--- src/pattern.class.ts
+++ src/pattern.class.ts
@@ -1,181 +1,153 @@
-//@ts-nocheck
-
-
-import { pick } from "./util/misc/pick";
+import { fabric } from "../HEADER";
 import { config } from "./config";
+import { TCrossOrigin, TMat2D } from "./typedefs";
+import { loadImage } from "./util/misc/objectEnlive";
+import { pick } from "./util/misc/pick";
+import { toFixed } from "./util/misc/toFixed";
 
+export type TPatternRepeat = 'repeat' | 'repeat-x' | 'repeat-y' | 'no-repeat';
 
-(function(global) {
-  var fabric = global.fabric, toFixed = fabric.util.toFixed;
+/**
+ * Pattern class
+ * @class fabric.Pattern
+ * @see {@link http://fabricjs.com/patterns|Pattern demo}
+ * @see {@link http://fabricjs.com/dynamic-patterns demo}
+ */
+export class Pattern {
 
   /**
-   * Pattern class
-   * @class fabric.Pattern
-   * @see {@link http://fabricjs.com/patterns|Pattern demo}
-   * @see {@link http://fabricjs.com/dynamic-patterns|DynamicPattern demo}
-   * @see {@link fabric.Pattern#initialize} for constructor definition
+   * @type TPatternRepeat
+   * @defaults
    */
+  repeat: TPatternRepeat = 'repeat'
 
+  /**
+   * Pattern horizontal offset from object's left/top corner
+   * @type Number
+   * @default
+   */
+  offsetX = 0
 
-  fabric.Pattern = fabric.util.createClass(/** @lends fabric.Pattern.prototype */ {
+  /**
+   * Pattern vertical offset from object's left/top corner
+   * @type Number
+   * @default
+   */
+  offsetY = 0
 
-    /**
-     * Repeat property of a pattern (one of repeat, repeat-x, repeat-y or no-repeat)
-     * @type String
-     * @default
-     */
-    repeat: 'repeat',
+  /**
+   * @type TCrossOrigin
+   * @default
+   */
+  crossOrigin: TCrossOrigin = ''
 
-    /**
-     * Pattern horizontal offset from object's left/top corner
-     * @type Number
-     * @default
-     */
-    offsetX: 0,
+  /**
+   * transform matrix to change the pattern, imported from svgs.
+   * @type Array
+   * @default
+   */
+  patternTransform: TMat2D | null = null
 
-    /**
-     * Pattern vertical offset from object's left/top corner
-     * @type Number
-     * @default
-     */
-    offsetY: 0,
+  type = 'pattern'
 
-    /**
-     * crossOrigin value (one of "", "anonymous", "use-credentials")
-     * @see https://developer.mozilla.org/en-US/docs/HTML/CORS_settings_attributes
-     * @type String
-     * @default
-     */
-    crossOrigin: '',
+  source!: CanvasImageSource;
 
-    /**
-     * transform matrix to change the pattern, imported from svgs.
-     * @type Array
-     * @default
-     */
-    patternTransform: null,
+  readonly id: number;
 
-    type: 'pattern',
+  /**
+   * Constructor
+   * @param {Object} [options] Options object
+   * @param {option.source} [source] the pattern source, eventually empty or a drawable
+   * @return {fabric.Pattern} thisArg
+   */
+  constructor(options = {}) {
+    this.id = fabric.Object.__uid++;
+    this.setOptions(options);
+  }
 
-    /**
-     * Constructor
-     * @param {Object} [options] Options object
-     * @param {option.source} [source] the pattern source, eventually empty or a drawable
-     * @return {fabric.Pattern} thisArg
-     */
-    initialize: function(options) {
-      options || (options = { });
-      this.id = fabric.Object.__uid++;
-      this.setOptions(options);
-    },
-
-    /**
-     * Returns object representation of a pattern
-     * @param {Array} [propertiesToInclude] Any properties that you might want to additionally include in the output
-     * @return {Object} Object representation of a pattern instance
-     */
-    toObject: function(propertiesToInclude) {
-      var NUM_FRACTION_DIGITS = config.NUM_FRACTION_DIGITS,
-          source, object;
-
+  sourceToString() {
+    return typeof this.source.src === 'string' ?
       // <img> element
-      if (typeof this.source.src === 'string') {
-        source = this.source.src;
-      }
-      // <canvas> element
-      else if (typeof this.source === 'object' && this.source.toDataURL) {
-        source = this.source.toDataURL();
-      }
+      this.source.src :
+      typeof this.source === 'object' && this.source.toDataURL ?
+        // <canvas> element
+        this.source.toDataURL() :
+        '';
+  }
 
-      return {
-        ...pick(this, propertiesToInclude),
-        type: 'pattern',
-        source: source,
-        repeat: this.repeat,
-        crossOrigin: this.crossOrigin,
-        offsetX: toFixed(this.offsetX, NUM_FRACTION_DIGITS),
-        offsetY: toFixed(this.offsetY, NUM_FRACTION_DIGITS),
-        patternTransform: this.patternTransform ? this.patternTransform.concat() : null
-      };
-    },
+  /**
+   * Returns object representation of a pattern
+   * @param {Array} [propertiesToInclude] Any properties that you might want to additionally include in the output
+   * @return {Object} Object representation of a pattern instance
+   */
+  toObject(propertiesToInclude: (keyof this)[]) {
+    return {
+      ...pick(this, propertiesToInclude),
+      type: 'pattern',
+      source: this.sourceToString(),
+      repeat: this.repeat,
+      crossOrigin: this.crossOrigin,
+      offsetX: toFixed(this.offsetX, config.NUM_FRACTION_DIGITS),
+      offsetY: toFixed(this.offsetY, config.NUM_FRACTION_DIGITS),
+      patternTransform: this.patternTransform ? this.patternTransform.concat() : null
+    };
+  }
 
-    /* _TO_SVG_START_ */
-    /**
-     * Returns SVG representation of a pattern
-     * @param {fabric.Object} object
-     * @return {String} SVG representation of a pattern
-     */
-    toSVG: function(object) {
-      var patternSource = typeof this.source === 'function' ? this.source() : this.source,
-          patternWidth = patternSource.width / object.width,
-          patternHeight = patternSource.height / object.height,
-          patternOffsetX = this.offsetX / object.width,
-          patternOffsetY = this.offsetY / object.height,
-          patternImgSrc = '';
-      if (this.repeat === 'repeat-x' || this.repeat === 'no-repeat') {
-        patternHeight = 1;
-        if (patternOffsetY) {
-          patternHeight += Math.abs(patternOffsetY);
-        }
-      }
-      if (this.repeat === 'repeat-y' || this.repeat === 'no-repeat') {
-        patternWidth = 1;
-        if (patternOffsetX) {
-          patternWidth += Math.abs(patternOffsetX);
-        }
-
-      }
-      if (patternSource.src) {
-        patternImgSrc = patternSource.src;
-      }
-      else if (patternSource.toDataURL) {
-        patternImgSrc = patternSource.toDataURL();
-      }
-
-      return '<pattern id="SVGID_' + this.id +
-                    '" x="' + patternOffsetX +
-                    '" y="' + patternOffsetY +
-                    '" width="' + patternWidth +
-                    '" height="' + patternHeight + '">\n' +
-               '<image x="0" y="0"' +
-                      ' width="' + patternSource.width +
-                      '" height="' + patternSource.height +
-                      '" xlink:href="' + patternImgSrc +
-               '"></image>\n' +
-             '</pattern>\n';
-    },
-    /* _TO_SVG_END_ */
-
-    setOptions: function(options) {
-      for (var prop in options) {
-        this[prop] = options[prop];
-      }
-    },
-
-    /**
-     * Returns an instance of CanvasPattern
-     * @param {CanvasRenderingContext2D} ctx Context to create pattern
-     * @return {CanvasPattern}
-     */
-    toLive: function(ctx) {
-      var source = this.source;
-      // if the image failed to load, return, and allow rest to continue loading
-      if (!source) {
-        return '';
-      }
-
-      // if an image
-      if (typeof source.src !== 'undefined') {
-        if (!source.complete) {
-          return '';
-        }
-        if (source.naturalWidth === 0 || source.naturalHeight === 0) {
-          return '';
-        }
-      }
-      return ctx.createPattern(source, this.repeat);
+  /* _TO_SVG_START_ */
+  /**
+   * Returns SVG representation of a pattern
+   * @param {fabric.Object} object
+   * @return {String} SVG representation of a pattern
+   */
+  toSVG(object) {
+    const patternSource = typeof this.source === 'function' ? this.source() : this.source,
+      patternOffsetX = this.offsetX / object.width,
+      patternOffsetY = this.offsetY / object.height,
+      patternImgSrc = this.sourceToString();
+    let patternWidth = patternSource.width / object.width,
+      patternHeight = patternSource.height / object.height;
+    if (this.repeat === 'repeat-x' || this.repeat === 'no-repeat') {
+      patternHeight = 1 + Math.abs(patternOffsetY || 0);
     }
-  });
+    if (this.repeat === 'repeat-y' || this.repeat === 'no-repeat') {
+      patternWidth = 1 + Math.abs(patternOffsetX || 0);
+    }
+
+    return [
+      `<pattern id="SVGID_${this.id}" x="${patternOffsetX}" y="${patternOffsetY}" width="${patternWidth}" height="${patternHeight}">`,
+      `<image x="0" y="0" width="${patternSource.width}" height="${patternSource.height}" xlink:href="${patternImgSrc}"></image>`,
+      `</pattern>`,
+      ''
+    ].join('\n');
+  }
+  /* _TO_SVG_END_ */
+
+  setOptions<K extends keyof this>(options: Record<K, this[K]>) {
+    for (const prop in options) {
+      this[prop] = options[prop];
+    }
+  }
+
+  /**
+   * Returns an instance of CanvasPattern
+   * @param {CanvasRenderingContext2D} ctx Context to create pattern
+   * @return {CanvasPattern}
+   */
+  toLive(ctx: CanvasRenderingContext2D) {
+    const source = this.source;
+
+    if (
+      // if the image failed to load, return, and allow rest to continue loading
+      !source
+      // if an image
+      || (typeof source.src !== 'undefined'
+        && (!source.complete || source.naturalWidth === 0 || source.naturalHeight === 0))
+    ) {
+      return '';
+    }
+
+    return ctx.createPattern(source, this.repeat);
+  }
 
   /**
    *
@@ -184,13 +156,14 @@ import { config } from "./config";
    * @param {AbortSignal} [options.signal] handle aborting, see https://developer.mozilla.org/en-US/docs/Web/API/AbortController/signal
    * @returns
    */
-  fabric.Pattern.fromObject = function(object, options) {
-    var patternOptions = Object.assign({}, object),
-        imageOptions = Object.assign({}, options, { crossOrigin: object.crossOrigin });
-    return fabric.util.loadImage(object.source, imageOptions)
-      .then(function(img) {
-        patternOptions.source = img;
-        return new fabric.Pattern(patternOptions);
-      });
-  };
-})(typeof exports !== 'undefined' ? exports : window);
+  static async fromObject(object: any, options: { signal: AbortSignal }) {
+    const img = await loadImage(object.source, {
+      ...options,
+      crossOrigin: object.crossOrigin
+    })
+    return new Pattern({ ...object, source: img });
+  }
+}
+
+
+fabric.Pattern = Pattern;
