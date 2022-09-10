28284e4bb09d10f322864247a196d0b40807e7bb
ShaMan123 <shacharnen@gmail.com>
2022-09-10 15:52:38 +0300

    m

--- src/gradient/gradient.class.ts
+++ src/gradient/gradient.class.ts
@@ -124,7 +124,7 @@ export class Gradient<S, T extends GradientType = S extends GradientType ? S : '
       const p1 = transformPoint(new Point(coords.x1, coords.y1), transform);
       const p2 = transformPoint(new Point(coords.x2, coords.y2), transform);
       if (p1.eq(p2)) {
-        return null;
+        return 'rgba(0,0,0,0)';
       }
       gradient = ctx.createLinearGradient(
         p1.x, p1.y,
