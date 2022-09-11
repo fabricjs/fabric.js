a28658d66c7740c8b7bb5f2e09f644f327e13ddd
ShaMan123 <shacharnen@gmail.com>
2022-09-11 09:33:00 +0300

    remove optional transform

--- src/util/misc/boundingBoxFromPoints.ts
+++ src/util/misc/boundingBoxFromPoints.ts
@@ -1,26 +1,19 @@
 import { Point } from '../../point.class';
-import { TMat2D } from '../../typedefs';
 
 /**
- * Returns coordinates of points's bounding rectangle (left, top, width, height)
- * This function does not make sense.
- * - it mutates the input in case transform is present
- * - is used in 2 instances of the app one with the transform one without
+ * Calculates bounding box (left, top, width, height) from given `points`
  * @static
  * @memberOf fabric.util
  * @param {Point[]} points
- * @param {TMat2D} [transform] 
  * @return {Object} Object with left, top, width, height properties
  */
-export const makeBoundingBoxFromPoints = (points: Point[], transform: TMat2D) => {
-  const start = transform ? points[0].transform(transform) : points[0];
+export const makeBoundingBoxFromPoints = (points: Point[]) => {
   const { min, max } = points.reduce(({ min, max }, curr) => {
-    const point = transform ? curr.transform(transform) : curr;
     return {
-      min: min.min(point),
-      max: max.max(point)
+      min: min.min(curr),
+      max: max.max(curr)
     }
-  }, { min: start, max: start });
+  }, { min: points[0], max: points[0] });
   const size = max.subtract(min);
 
   return {
