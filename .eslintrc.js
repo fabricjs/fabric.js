2ef70cfa91044c1b874f1ca7bfe6282b2e2c8bcd
Shachar <34343793+ShaMan123@users.noreply.github.com>
2022-09-12 13:47:58 +0300

    chore(lint): disallow `Math.hypot`, `window`, `document` (#8277)

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,27 @@ module.exports = {
   ],
   rules: {
     '@typescript-eslint/ban-ts-comment': 1,
+    'no-restricted-globals': [
+      'error',
+      {
+        name: 'window',
+        message: 'Use fabric window or other utils instead.'
+      },
+      {
+        name: 'document',
+        message: 'Use fabric document or other utils instead.'
+      }
+    ],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: '[callee.object.name="Math"][callee.property.name="hypot"]',
+        message: '`Math.hypot` is not accurate on chrome, import `hypot` from `util` instead.'
+      },
+      {
+        selector: 'VariableDeclarator[init.name="Math"]',
+        message: 'Aliasing or destructing `Math` is not allowed due to restrictions on `Math.hypot` usage.'
+      }
+    ]
   },
 };
