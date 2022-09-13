35c3f706ccc56dcbdc5c733d0891a65c1058edc4
ShaMan123 <shacharnen@gmail.com>
2022-09-13 10:55:54 +0300

    refactor

--- test/visual/gradient.js
+++ test/visual/gradient.js
@@ -1,148 +1,137 @@
+const runner = visualTestLoop(QUnit);
 
-function createBackgroundGradientTest(configureCanvas, gradientOptions = {}) {
-  return (canvas, callback) => {
-    var g = new fabric.Gradient({
-      type: 'linear',
-      gradientTransform: [0.2, -0.4, -0.2, 0.1, -3, -5],
-      coords: {
-        x1: 0,
-        y1: 0,
-        x2: 200,
-        y2: 0
-      },
-      colorStops: [{
-        offset: 0,
-        color: 'green'
-      },
-      {
-        offset: 0.5,
-        color: 'white'
-      },
-      {
-        offset: 1,
-        color: 'blue'
-      }],
-      ...gradientOptions
+QUnit.module('Gradient', hooks => {
+    const options = [
+        {
+            transform: [0, 0, 0, 0, 0, 0],
+            coords: { x1: 0, y1: 0, x2: 1, y2: 1 },
+        },
+        {
+            transform: [1, 0, 0, 1, 0, 0],
+            coords: { x1: 0.5, y1: 1, x2: 0.5, y2: 1 },
+        }
+    ];
+    const gradientMatrices = [
+        [1, 0, 0, 1, 0, 0],
+        [2, 0, 0, 2, 0, 0],
+        [1, 0, 0, 2, 0, 0],
+        [2, 0, 0, 1, 0, 0],
+        [0.5, 0, 0, 0.5, 0, 0],
+        [2, 0, 0, 0.5, 0, 0],
+        [0.5, 0, 0, 2, 0, 0],
+        [1, 0.25, 0, 1, 0, 0],
+        [1, -0.25, 0, 1, 0, 0],
+        [1, 0, 0.25, 1, 0, 0],
+        [1, 0, -0.25, 1, 0, 0],
+        [1, 0.25, 0.25, 1, 0, 0],
+        [1, 0.25, -0.25, 1, 0, 0],
+        [1, -0.25, -0.25, 1, 0, 0],
+        [1, -0.25, 0.25, 1, 0, 0],
+    ];
+    const start = 0;
+    const end = 0.75;
+    [start, end].forEach(x1 => {
+        [start, end].forEach(y1 => {
+            [start, end].forEach(x2 => {
+                [start, end].forEach(y2 => {
+                    [true, false].forEach(percentage => {
+                        [new fabric.Point(), new fabric.Point(-120, 50)].forEach(fakeOffset => {
+                            gradientMatrices.forEach(transform => {
+                                if (x1 === x2 && y1 === y2) return;
+                                options.push({
+                                    coords: { x1, y1, x2, y2 },
+                                    // percentage,
+                                    // fakeOffset,
+                                    transform
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
     });
-    configureCanvas && configureCanvas(canvas);
-    canvas.set({ backgroundColor: g });
-    canvas.renderAll();
-    callback(canvas.lowerCanvasEl);
-  };
-}
 
-[
-  {
-    test: 'canvas gradient background x axis',
-    code: createBackgroundGradientTest(),
-    golden: 'backgroundWithXGradient.png',
-    percentage: 0.09,
-    width: 300,
-    height: 300,
-  },
-  {
-    test: 'canvas gradient background y axis',
-    code: createBackgroundGradientTest(null, {
-      coords: {
-        x1: 0,
-        y1: 0,
-        x2: 0,
-        y2: 200
-      }
-    }),
-    golden: 'backgroundWithYGradient.png',
-    percentage: 0.09,
-    width: 300,
-    height: 300,
-  },
-  {
-    test: 'canvas gradient background',
-    code: createBackgroundGradientTest(null, {
-      coords: {
-        x1: 100,
-        y1: 50,
-        x2: 30,
-        y2: 200
-      },
-      gradientTransform: null
-    }),
-    golden: 'backgroundWithGradient.png',
-    percentage: 0.09,
-    width: 300,
-    height: 300,
-  },
-  {
-    test: 'canvas can have a gradient background and being zoomed',
-    code: createBackgroundGradientTest(canvas => {
-      canvas.setZoom(0.1);
-    }),
-    golden: 'backgroundWithGradientZoom.png',
-    percentage: 0.09,
-    width: 300,
-    height: 300,
-  },
-  {
-    test: 'canvas can have a gradient background with zoom but being unaffected',
-    code: createBackgroundGradientTest(canvas => {
-      canvas.setZoom(0.1);
-      canvas.backgroundVpt = false;
-    }),
-    golden: 'backgroundWithXGradient.png',
-    percentage: 0.09,
-    width: 300,
-    height: 300,
-  },
-  {
-    test: 'canvas can have a percentage gradient background',
-    code: createBackgroundGradientTest(null, {
-      gradientUnits: 'percentage',
-      coords: {
-        x1: 0,
-        y1: 0,
-        x2: 2 / 3,
-        y2: 0
-      }
-    }),
-    golden: 'backgroundWithXGradient.png',
-    percentage: 0.09,
-    width: 300,
-    height: 300,
-  },
-  {
-    test: 'canvas can have a percentage gradient background and being zoomed',
-    code: createBackgroundGradientTest(canvas => {
-      canvas.setZoom(0.1);
-    }, {
-      gradientUnits: 'percentage',
-      coords: {
-        x1: 0,
-        y1: 0,
-        x2: 2 / 3,
-        y2: 0
-      }
-    }),
-    golden: 'backgroundWithGradientZoom.png',
-    percentage: 0.09,
-    width: 300,
-    height: 300,
-  },
-  {
-    test: 'canvas can have a percentage gradient background with zoom but being unaffected',
-    code: createBackgroundGradientTest(canvas => {
-      canvas.setZoom(0.1);
-      canvas.backgroundVpt = false;
-    }, {
-      gradientUnits: 'percentage',
-      coords: {
-        x1: 0,
-        y1: 0,
-        x2: 2 / 3,
-        y2: 0
-      }
-    }),
-    golden: 'backgroundWithXGradient.png',
-    percentage: 0.09,
-    width: 300,
-    height: 300,
-  },
-].forEach(visualTestLoop(QUnit));;
+    function createGradient({
+        transform: [a, b, c, d, e, f] = fabric.iMatrix,
+        offsetX = 0,
+        offsetY = 0,
+        fakeOffset = new fabric.Point(),
+        ...options
+    }) {
+        return new fabric.Gradient({
+            colorStops: [{
+                offset: 0,
+                color: 'green'
+            },
+            {
+                offset: 0.5,
+                color: 'white'
+            },
+            {
+                offset: 1,
+                color: 'blue'
+            }],
+            ...options,
+            type: 'linear',
+            offsetX: offsetX + fakeOffset.x,
+            offsetY: offsetY + fakeOffset.y,
+            gradientTransform: [a, b * Math.PI, c * Math.PI, d, e - fakeOffset.x, f - fakeOffset.y],
+        });
+    }
+
+    options.forEach(({
+        coords: { x1, y1, x2, y2 }, percentage, fakeOffset, transform
+    }) => {
+        const size = {
+            width: 200,
+            height: 200
+        };
+        const coords = {
+            x1: x1 * size.width,
+            y1: y1 * size.height,
+            x2: x2 * size.width,
+            y2: y2 * size.height
+        };
+        const units = percentage ? 'percentage' : 'pixels';
+        const gradient = createGradient({
+            coords: percentage ?
+                { x1, y1, x2, y2 } :
+                coords,
+            transform,
+            fakeOffset,
+            gradientUnit: units
+        });
+        const goldenName = `gradient/coords(${Object.values(coords)})_transform(${transform}).png`;
+        const testName = `gradient coords(${Object.values(coords)}), transform(${transform}), units(${units}), fake offset(${fakeOffset})`;
+        runner({
+            test: testName,
+            code: (canvas, callback) => {
+                const rect = new fabric.Rect({
+                    width: canvas.width,
+                    height: canvas.height,
+                    fill: gradient,
+                    strokeWidth: 0
+                });
+                canvas.add(rect);
+                canvas.renderAll();
+                callback(canvas.lowerCanvasEl);
+            },
+            golden: goldenName,
+            percentage: 0.09,
+            ...size
+        });
+
+        runner({
+            test: `canvas bg ${testName}`,
+            code: (canvas, callback) => {
+                canvas.backgroundColor = gradient;
+                canvas.renderAll();
+                callback(canvas.lowerCanvasEl);
+            },
+            golden: goldenName,
+            percentage: 0.09,
+            ...size
+        });
+    });
+});
