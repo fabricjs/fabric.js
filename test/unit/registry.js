691da68246e78f0a79976d7b0b0ea35ab2a13823
ShaMan123 <shacharnen@gmail.com>
2022-09-13 03:14:30 +0300

    tetsss

--- test/unit/registry.js
+++ test/unit/registry.js
@@ -8,22 +8,28 @@ QUnit.module('IO Registry', hook => {
         assert.ok(registry, 'should exist');
     });
 
-    // QUnit.test('register', assert => {
-    //     registry.register({
-    //          json:sinon
-    //      })
-    //     assert.ok(fabric.registry, 'should exist');
-    //  });
-    
-    // QUnit.test('unregister', assert => {
-    //     assert.ok(fabric.registry, 'should exist');
-    // });
+    QUnit.test('register resolver', assert => {
+        const stubData = {
+            foo: 'bar'
+        };
+        const stubResolver = () => { };
+        registry.registerResolver('json', (a) => {
+            assert.equal(a, stubData);
+            return { handler: stubResolver, key: 'stub' };
+        })
+        assert.ok(registry.resolver.json, 'should add resolver');
+        assert.deepEqual(registry.resolver.json(stubData), { handler: stubResolver, key: 'stub' }, 'should return resolver');
+        assert.ok(registry.assertJSONHandler(stubData), stubResolver, 'should pass back a custom resolver');
+        registry.unregisterResolver('json');
+        assert.ok(!registry.resolver.json, 'should remove resolver');
+        assert.throws(() => registry.assertJSONHandler(stubData), 'no resolver');
+    });
 
-    // QUnit.test('register resolver', assert => {
-    //     assert.ok(fabric.registry, 'should exist');
-    // });
-
-    //  QUnit.test('register resolver', assert => {
-    //     assert.ok(fabric.registry, 'should exist');
-    // });
+    QUnit.test('fail assertion', assert => {
+        const stubData = {
+            foo: 'bar'
+        };
+        assert.throws(() => registry.assertJSONHandler(stubData), 'no resolver');
+        assert.throws(() => registry.assertSVGHandler({key:'NON_EXISTING',element:null}), 'no resolver');
+    });
 });
