814abcbcc6bb9efac4edddf5db50ac4f32232928
ShaMan123 <shacharnen@gmail.com>
2022-09-14 15:56:13 +0300

    feat(ci): `build.lock` file

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,13 @@
+import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
 import ts from 'rollup-plugin-ts';
-import json from '@rollup/plugin-json';
+import * as fs from 'fs';
+import moment from 'moment';
+
+const lockFile = 'build.lock';
+function log(...logs) {
+  return `[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${logs.join('\n')}\n`;
+}
 
 // https://rollupjs.org/guide/en/#configuration-files
 export default {
@@ -22,9 +29,32 @@ export default {
       : null,
   ],
   plugins: [
+    {
+      name: 'rollup-lock-file',
+      buildStart() {
+        fs.writeFileSync(lockFile, log('build start'));
+      },
+      buildEnd(error) {
+        error && fs.appendFileSync(lockFile, log('build error', error));
+      },
+      renderError(error) {
+        fs.appendFileSync(lockFile, log('build error', error));
+      },
+      writeBundle() {
+        try {
+          fs.unlinkSync(lockFile);
+        } catch (error) {}
+      },
+    },
     json(),
     ts({
       /* Plugin options */
+      hook: {
+        diagnostics(diagnostics) {
+          fs.appendFileSync(lockFile, log('ts error'));
+          return diagnostics;
+        },
+      },
     }),
   ],
 };
