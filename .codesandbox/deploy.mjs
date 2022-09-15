72e7e0a738238e7b5cf8932bff974a8d7d8e1fee
ShaMan123 <shacharnen@gmail.com>
2022-09-15 21:31:12 +0300

    Merge kitchensink deploy logic changes

--- .codesandbox/deploy.mjs
+++ .codesandbox/deploy.mjs
@@ -3,6 +3,7 @@ import Axios from 'axios';
 import fs from 'fs-extra';
 import _ from 'lodash';
 import path from 'path';
+import { makeRe } from 'micromatch';
 
 const BINARY_EXT = ['png', 'jpg', 'jpeg'];
 
@@ -10,13 +11,24 @@ function bufferToBase64DataUrl(buffer, mimeType) {
   return 'data:' + mimeType + ';base64,' + buffer.toString('base64');
 }
 
+function globToRegex(glob, opts) {
+  return makeRe(glob, opts);
+}
+
+function parseIgnoreFile(file) {
+  return _.compact(fs.readFileSync(file).toString().split('\n')).map((p) =>
+    globToRegex(p.trim())
+  );
+}
+
 /**
  * https://codesandbox.io/docs/api/#define-api
  */
 export async function createCodeSandbox(appPath) {
-  const { trigger: __, ...packageJSON } = JSON.parse(
-    fs.readFileSync(path.resolve(appPath, 'package.json'))
-  );
+  const { trigger: __, ...packageJSON } = require(path.resolve(
+    appPath,
+    'package.json'
+  ));
   // omit linked package
   if (packageJSON.dependencies.fabric.startsWith('file:')) {
     packageJSON.dependencies.fabric = '*';
@@ -24,11 +36,10 @@ export async function createCodeSandbox(appPath) {
   const files = { 'package.json': JSON.stringify(packageJSON, null, '\t') };
 
   const gitignore = path.resolve(appPath, '.gitignore');
-  const ignore = fs.existsSync(gitignore)
-    ? _.compact(fs.readFileSync(gitignore).toString().split('\n')).map(
-        (p) => new RegExp(p.trim())
-      )
-    : [];
+  const codesandboxignore = path.resolve(appPath, '.codesandboxignore');
+  const ignore = _.flatten(
+    [gitignore, codesandboxignore].filter(fs.existsSync).map(parseIgnoreFile)
+  );
 
   const processFile = (fileName) => {
     const filePath = path.resolve(appPath, fileName);
@@ -38,7 +49,7 @@ export async function createCodeSandbox(appPath) {
       fs.readdirSync(filePath).forEach((file) => {
         processFile(path.join(fileName, file).replace(/\\/g, '/'));
       });
-    } else if (BINARY_EXT.some((x) => x === ext)) {
+    } else if (BINARY_EXT.includes(ext)) {
       files[fileName] = {
         content: bufferToBase64DataUrl(
           fs.readFileSync(filePath),
@@ -47,7 +58,15 @@ export async function createCodeSandbox(appPath) {
         isBinary: true,
       };
     } else {
-      files[fileName] = { content: fs.readFileSync(filePath).toString() };
+      const { name, base, ...rest } = path.parse(filePath);
+      const sandboxVersion = path.format({
+        ...rest,
+        name: `${name}.codesandbox`,
+      });
+      const finalVersion = fs.existsSync(sandboxVersion)
+        ? sandboxVersion
+        : filePath;
+      files[fileName] = { content: fs.readFileSync(finalVersion).toString() };
     }
   };
   fs.readdirSync(appPath).forEach(processFile);
