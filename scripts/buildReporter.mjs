684755deb939f222f2675a7ce951a70f9084eed8
ShaMan123 <shacharnen@gmail.com>
2022-09-15 10:48:16 +0300

    Update buildReporter.mjs

--- scripts/buildReporter.mjs
+++ scripts/buildReporter.mjs
@@ -1,10 +1,14 @@
+/**
+ * This file is used by `./build` to create `cli_output/build-lock.json`
+ */
+
 import { Command } from 'commander';
 import { report } from './buildLock.mjs';
 
 const program = new Command();
 
 program
-  .command('<type> [...args]')
+  .command('<type>')
   .action((type) => {
     report(type);
   })
