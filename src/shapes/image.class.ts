af0df9235874d108c5a330b8c0ae68e24eae1eef
ShaMan123 <shacharnen@gmail.com>
2022-09-16 16:54:37 +0300

    cleanup image caching

--- src/shapes/image.class.ts
+++ src/shapes/image.class.ts
@@ -570,22 +570,7 @@
        */
       drawCacheOnCanvas: function (ctx) {
         ctx.imageSmoothingEnabled = this.imageSmoothing;
-        fabric.Object.prototype.drawCacheOnCanvas.call(this, ctx);
-      },
-
-      /**
-       * Decide if the object should cache or not. Create its own cache level
-       * needsItsOwnCache should be used when the object drawing method requires
-       * a cache step. None of the fabric classes requires it.
-       * Generally you do not cache objects in groups because the group outside is cached.
-       * This is the special image version where we would like to avoid caching where possible.
-       * Essentially images do not benefit from caching. They may require caching, and in that
-       * case we do it. Also caching an image usually ends in a loss of details.
-       * A full performance audit should be done.
-       * @return {Boolean}
-       */
-      shouldCache: function () {
-        return this.needsItsOwnCache();
+        this.callSuper('drawCacheOnCanvas', ctx);
       },
 
       _renderFill: function (ctx) {
