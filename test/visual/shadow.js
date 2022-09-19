ab81bc0399718a8db0366a430c3ac8be9fe231b6
ShaMan123 <shacharnen@gmail.com>
2022-09-19 19:06:53 +0300

    Update shadow.js

--- test/visual/shadow.js
+++ test/visual/shadow.js
@@ -1,11 +1,13 @@
   
-function createClippedNestedShadowedTextTest(a, b, c) {
+function createClippedNestedShadowedTextTest(a, b, c, stroke) {
     return (canvas, callback) => {
       const text = new fabric.Text("Clipped Text\nShadow Test", {
         fontSize: 48,
         fontWeight: 'bold',
         top: 24,
         textAlign: 'center',
+        strokeWidth: stroke ? 5 : 1,
+        stroke: 'rgba(0,0,0,0.5)',
         shadow: new fabric.Shadow({
           color: "red",
           blur: 5,
@@ -20,7 +22,6 @@ function createClippedNestedShadowedTextTest(a, b, c) {
           new fabric.Rect({ width: 150, height: 100, left: 150, fill: "blue" }),
           new fabric.Rect({ width: 150, height: 100, left: 150, top: 100, fill: "magenta" }),
           text,
-
         ],
         {
           clipPath: new fabric.Circle({
@@ -48,20 +49,37 @@ function createClippedNestedShadowedTextTest(a, b, c) {
 }
   
 QUnit.module('Shadow Casting', hooks => {
-    const runner = visualTestLoop(QUnit);
-    angles.forEach(a => {
-        angles.forEach(b => {
-            angles.forEach(c => {
-                let containerDeg = a + b + 360 * -Math.sign(a + b) * Number(Math.abs(a + b) > 180);
-                runner({
-                    test: `clipped group ${b}° with nested shadow ${c}° rotated by ${a}°`,
-                    code: createClippedNestedShadowedTextTest(a, b, c),
-                    golden: `clipping/nested-shadow/${containerDeg},${c}.png`,
-                    percentage: 0.06,
-                    width: 250,
-                    height: 200,
-                });
-            });
+  const runner = visualTestLoop(QUnit);
+  hooks.before(() => {
+    fabric.config.configure({ NUM_FRACTION_DIGITS: 15 });
+  });
+  hooks.after(() => {
+    fabric.config.restoreDefaults();
+  });
+  angles.forEach(a => {
+    angles.forEach(b => {
+      angles.forEach(c => {
+        const containerDeg = a + b + 360 * -Math.sign(a + b) * Number(Math.abs(a + b) > 180);
+        QUnit.module(`clipped group ${b}° with nested shadow ${c}° rotated by ${a}°`, () => {
+          const config = {
+            percentage: 0.06,
+            width: 250,
+            height: 200,
+          };
+          runner({
+            test: 'default stroke',
+            code: createClippedNestedShadowedTextTest(a, b, c, false),
+            golden: `clipping/nested-shadow/${containerDeg},${c}.png`,
+            ...config
+          });
+          runner({
+            test: 'stroke',
+            code: createClippedNestedShadowedTextTest(a, b, c, true),
+            golden: `clipping/nested-shadow/${containerDeg},${c},stroke.png`,
+            ...config
+          });
         });
+      });
     });
+  });
 });
