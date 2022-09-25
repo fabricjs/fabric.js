03accbc1b5c42f1658aba698afa64ea47c495cb0
ShaMan123 <shacharnen@gmail.com>
2022-09-25 22:00:33 +0300

    js-yaml

--- scripts/updateBugReportVersion.mjs
+++ scripts/updateBugReportVersion.mjs
@@ -2,14 +2,13 @@ import fs from 'fs';
 import cp from 'node:child_process';
 import { fileURLToPath } from 'node:url';
 import path from 'path';
-import YAML from 'yaml';
-// const { version } = require('../package.json');
+import YAML from 'js-yaml';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const file = path.resolve(__dirname, '..', '.github/ISSUE_TEMPLATE/bug_report.yml');
 const id = 'version';
-const content = YAML.parse(fs.readFileSync(file).toString());
+const content = YAML.load(fs.readFileSync(file).toString());
 const found = content.body.find(entry => entry.id === id && entry.type === 'dropdown');
 const tags = JSON.parse(cp.execSync('npm view fabric versions --json').toString()).reverse();
 found.attributes.options = tags;
-fs.writeFileSync(file, YAML.stringify(content));
+fs.writeFileSync(file, YAML.dump(content));
