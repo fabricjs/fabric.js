0051b76acc9654fb80dc75a9805d60c9eac1e8a9
ShaMan123 <shacharnen@gmail.com>
2022-09-25 16:57:19 +0300

    Update util.node.mjs

--- test/lib/util.node.mjs
+++ test/lib/util.node.mjs
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'node:url';
 import { getImage } from './common.mjs';
 
 const __filename = fileURLToPath(import.meta.url);
