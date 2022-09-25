118ff559c886ba6db6a0eed2faabf058cd8b41a1
ShaMan123 <shacharnen@gmail.com>
2022-09-25 10:59:59 +0300

    Update rollup.test.config.js

--- rollup.test.config.js
+++ rollup.test.config.js
@@ -1,22 +1,28 @@
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 import ts from 'rollup-plugin-ts';
 
 // https://rollupjs.org/guide/en/#configuration-files
 export default {
-  external: ['qunit', 'fabric', 'pixelmatch'],
-  input: ['test/unit/index.ts', 'test/visual/index.ts', 'test/lib/index.js'],
+  external: ['qunit', 'fabric'],
+  input: ['test/lib/index.js', 'test/visual/index.ts'],
   output: [
     {
       dir: 'cli_output/test',
       format: 'es',
       exports: 'named',
       preserveModules: true, // Keep directory structure and files
+      globals: { qunit: 'QUnit' },
+      entryFileNames: '[name].js'
     },
   ],
   plugins: [
-    ts({
-      /* Plugin options */
-    }),
+    json(),
+    ts(),
+    nodePolyfills({sourceMap:true}),
+    nodeResolve({preferBuiltins: false,browser:true}),
     commonjs(),
   ],
 };
