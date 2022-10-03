4ce9c12cacf8429bacb49cf82c35bbaac479f2a7
Luiz Zappa <luiz.zappa@hotmail.com>
2022-10-03 00:51:17 -0300

    feat(hypot): hard coded hypot calculatoin to deal with precision between browsers

---
+++ src/util/misc/hypot.ts
@@ -0,0 +1,16 @@
+/**
+ * Returns the square root of the sum of squares of its arguments\
+ * Chrome implements `Math#hypot` with a calculation that affects percision so we hard code it as a util
+ * @see https://stackoverflow.com/questions/62931950/different-results-of-math-hypot-on-chrome-and-firefox
+ * @static
+ * @memberOf fabric.util
+ * @param {...number} values
+ * @return {number}
+ */
+export const hypot = (...values: number[]): number => {
+    let sumOfSquares = 0;
+    for (let i = 0; i < values.length; i++) {
+        sumOfSquares += values[i] * values[i];
+    }
+    return Math.sqrt(sumOfSquares);
+}
