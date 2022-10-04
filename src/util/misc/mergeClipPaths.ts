7fe025cf1f0b7ac7e838a226973641f4ed4c0d61
ShaMan123 <shacharnen@gmail.com>
2022-10-04 08:01:59 +0300

    enable merging n clip paths

--- src/util/misc/mergeClipPaths.ts
+++ src/util/misc/mergeClipPaths.ts
@@ -3,13 +3,13 @@ import { TObject } from '../../__types__';
 import { sendObjectToPlane } from './planeChange';
 
 /**
- * Merges 2 clip paths into one visually equal clip path
+ * Merges clip paths into one visually equal clip path
  *
  * **IMPORTANT**:\
- * Does **NOT** clone the arguments, clone them proir if necessary.
+ * Does **NOT** clone the arguments, clone them prior if necessary.
  *
  * Creates a wrapper (group) that contains one clip path and is clipped by the other so content is kept where both overlap.
- * Use this method if both the clip paths may have nested clip paths of their own, so assigning one to the other's clip path property is not possible.
+ * Use this method if 2 or more clip paths may have nested clip paths of their own, so assigning one to the other's clip path property is not possible.
  *
  * In order to handle the `inverted` property we follow logic described in the following cases:\
  * **(1)** both clip paths are inverted - the clip paths pass the inverted prop to the wrapper and loose it themselves.\
@@ -21,21 +21,27 @@ import { sendObjectToPlane } from './planeChange';
  * @param {fabric.Object} c2
  * @returns {fabric.Object} merged clip path
  */
-export const mergeClipPaths = (c1: TObject, c2: TObject) => {
-  let a = c1,
-    b = c2;
-  if (a.inverted && !b.inverted) {
-    //  case (2)
-    a = c2;
-    b = c1;
-  }
-  //  `b` becomes `a`'s clip path so we transform `b` to `a` coordinate plane
-  sendObjectToPlane(b, b.group?.calcTransformMatrix(), a.calcTransformMatrix());
-  //  assign the `inverted` prop to the wrapping group
-  const inverted = a.inverted && b.inverted;
-  if (inverted) {
-    //  case (1)
-    a.inverted = b.inverted = false;
-  }
-  return new fabric.Group([a], { clipPath: b, inverted });
+export const mergeClipPaths = (...[c1, ...clipPaths]: TObject[]) => {
+  return clipPaths.reduce((merged, curr) => {
+    let a = merged,
+      b = curr;
+    if (a.inverted && !b.inverted) {
+      //  case (2)
+      a = curr;
+      b = merged;
+    }
+    //  `b` becomes `a`'s clip path so we transform `b` to `a` coordinate plane
+    sendObjectToPlane(
+      b,
+      b.group?.calcTransformMatrix(),
+      a.calcTransformMatrix()
+    );
+    //  assign the `inverted` prop to the wrapping group
+    const inverted = a.inverted && b.inverted;
+    if (inverted) {
+      //  case (1)
+      a.inverted = b.inverted = false;
+    }
+    return new fabric.Group([a], { clipPath: b, inverted });
+  }, c1);
 };
