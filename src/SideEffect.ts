c0ccf7834748521f3652cf5e8f491df65f02b91b
ShaMan123 <shacharnen@gmail.com>
2022-10-16 02:48:04 +0300

    Update SideEffect.ts

--- src/SideEffect.ts
+++ src/SideEffect.ts
@@ -1,9 +1,13 @@
 export abstract class SideEffect<T, K extends keyof T> {
   abstract name: string;
   abstract keys: K[] | '*';
+  enable = true;
+
   abstract onChange(key: K, value: T[K], prevValue: T[K]): void;
+
   invoke(key: K, value: T[K], prevValue: T[K]) {
-    (this.keys === '*' || this.keys.includes(key)) &&
+    this.enable &&
+      (this.keys === '*' || this.keys.includes(key)) &&
       value !== prevValue &&
       this.onChange(key, value, prevValue);
   }
