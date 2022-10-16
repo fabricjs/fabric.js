a47b2c0a9890c89f3ed6cf4a043221e0a18b8e95
ShaMan123 <shacharnen@gmail.com>
2022-10-16 03:28:47 +0300

    replace stateful

--- src/SideEffect.ts
+++ src/SideEffect.ts
@@ -1,24 +1,45 @@
 export class SideEffect<
   T,
   K extends keyof T,
-  C extends (key: K, value: T[K], prevValue: T[K]) => void
+  P extends Partial<Record<K, T[K]>>,
+  C extends (key: K, value: P[K], prevValue?: P[K]) => void
 > {
   id: string;
   private keys: K[] | '*';
   private callback: C;
   private enabled = true;
+  private persistance: P;
 
-  constructor(id: string, keys: K[] | '*', callback: C) {
+  constructor(id: string, keys: K[] | '*', callback: C, initialValue?: P) {
     this.id = id;
     this.keys = keys;
     this.callback = callback;
+    this.persistance = initialValue || ({} as P);
   }
 
-  invoke(key: K, value: T[K], prevValue: T[K]) {
-    this.enabled &&
+  isEqual(incoming: P) {
+    for (const key in incoming) {
+      if (this.persistance[key] !== incoming[key]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  invoke(key: K, value: P[K]) {
+    const prevValue = this.persistance[key];
+    if (
+      this.enabled &&
       (this.keys === '*' || this.keys.includes(key)) &&
-      value !== prevValue &&
+      value !== prevValue
+    ) {
       this.callback(key, value, prevValue);
+      this.persist(key, value);
+    }
+  }
+
+  persist(key: K, value: P[K]) {
+    this.persistance[key] = value;
   }
 
   isEnabled() {
