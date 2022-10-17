0fedc302161398f2ee4470db13e63efc3bb28a1c
ShaMan123 <shacharnen@gmail.com>
2022-10-17 10:14:30 +0300

    es6

--- test/visual/stroke_projection.js
+++ test/visual/stroke_projection.js
@@ -1,95 +1,102 @@
-const { fabric } = require("../../dist/fabric");
-
-(function() {
-  let getFixture;
-  if (fabric.isLikelyNode) {
-    if (process.env.launcher === 'Firefox') {
-      fabric.browserShadowBlurConstant = 0.9;
-    }
-    if (process.env.launcher === 'Node') {
-      fabric.browserShadowBlurConstant = 1;
-    }
-    if (process.env.launcher === 'Chrome') {
-      fabric.browserShadowBlurConstant = 1.5;
-    }
-    if (process.env.launcher === 'Edge') {
-      fabric.browserShadowBlurConstant = 1.75;
-    }
-    getFixture = global.getFixture;
-  }
-  else {
-    if (navigator.userAgent.indexOf('Firefox') !== -1) {
-      fabric.browserShadowBlurConstant = 0.9;
-    }
-    if (navigator.userAgent.indexOf('Chrome') !== -1) {
-      fabric.browserShadowBlurConstant = 1.5;
-    }
-    if (navigator.userAgent.indexOf('Edge') !== -1) {
-      fabric.browserShadowBlurConstant = 1.75;
-    }
-    getFixture = window.getFixture;
-  }
-  fabric.enableGLFiltering = false;
-  fabric.isWebglSupported = false;
-  fabric.Object.prototype.objectCaching = true;
-  let visualTestLoop;
-  if (fabric.isLikelyNode) {
-    visualTestLoop = global.visualTestLoop;
-  }
-  else {
-    visualTestLoop = window.visualTestLoop;
-  }
-
+QUnit.module('stroke projection', (hooks) => {
   const tests = [];
 
   const casesToTest = {
-    noMiterAfterMiterLimit2: [{x: 0, y: 0}, {x: 10, y: 30}, {x: 43, y: 0}],
-    acuteAngle: [{x: 3, y: -10}, {x: 18, y: 16}, {x: 123, y: -26}],
-    obtuseAngle: [ {x: 16, y: -29}, {x: 187, y: 50}, {x: 123, y: -26}],
-    rightAngle: [{x:0, y:0}, {x:0, y:-30}, {x:30, y:-30}],
-    straightAngle: [{x: 1, y:1}, {x: 6, y:6}, {x:36, y:36}],
-    twoPointsClose: [{x: 0, y: 0}, {x: 2, y: -0.98}, {x: 100, y: 0}],
-    convex: [{x: 20, y: 4}, {x: 20, y: 27}, {x: 60, y: 27}, {x: 60, y: 4}, {x: 40, y: -10}],
-    concave: [{x: 23.23027292144134, y: -20.710253258520993}, {x: 20, y: 27}, {x: 68, y: 37}, {x: 90, y: -4}, {x: 48, y: 12}],
-    complex: [{x: 20, y: 4}, {x: 10, y: 27}, {x: 60, y: 27}, {x: 70, y: 4}, {x: 40, y: 37}],
-    star: [{x: 0, y: 0}, {x: 72, y: 36}, {x: 36, y: -18}, {x: 0, y: 36}, {x: 72, y: 0}],
+    noMiterAfterMiterLimit2: [
+      { x: 0, y: 0 },
+      { x: 10, y: 30 },
+      { x: 43, y: 0 },
+    ],
+    acuteAngle: [
+      { x: 3, y: -10 },
+      { x: 18, y: 16 },
+      { x: 123, y: -26 },
+    ],
+    obtuseAngle: [
+      { x: 16, y: -29 },
+      { x: 187, y: 50 },
+      { x: 123, y: -26 },
+    ],
+    rightAngle: [
+      { x: 0, y: 0 },
+      { x: 0, y: -30 },
+      { x: 30, y: -30 },
+    ],
+    straightAngle: [
+      { x: 1, y: 1 },
+      { x: 6, y: 6 },
+      { x: 36, y: 36 },
+    ],
+    twoPointsClose: [
+      { x: 0, y: 0 },
+      { x: 2, y: -0.98 },
+      { x: 100, y: 0 },
+    ],
+    convex: [
+      { x: 20, y: 4 },
+      { x: 20, y: 27 },
+      { x: 60, y: 27 },
+      { x: 60, y: 4 },
+      { x: 40, y: -10 },
+    ],
+    concave: [
+      { x: 23.23027292144134, y: -20.710253258520993 },
+      { x: 20, y: 27 },
+      { x: 68, y: 37 },
+      { x: 90, y: -4 },
+      { x: 48, y: 12 },
+    ],
+    complex: [
+      { x: 20, y: 4 },
+      { x: 10, y: 27 },
+      { x: 60, y: 27 },
+      { x: 70, y: 4 },
+      { x: 40, y: 37 },
+    ],
+    star: [
+      { x: 0, y: 0 },
+      { x: 72, y: 36 },
+      { x: 36, y: -18 },
+      { x: 0, y: 36 },
+      { x: 72, y: 0 },
+    ],
     plus: [
-      {x: 0, y: 0},
-      {x: 0, y: 20},
-      {x: 20, y: 20},
-      {x: 20, y: 0},
-      {x: 40, y: 0},
-      {x: 40, y: -20},
-      {x: 20, y: -20},
-      {x: 20, y: -40},
-      {x: 0, y: -40},
-      {x: 0, y: -20},
-      {x: -20, y: -20},
-      {x: -20, y: 0}
+      { x: 0, y: 0 },
+      { x: 0, y: 20 },
+      { x: 20, y: 20 },
+      { x: 20, y: 0 },
+      { x: 40, y: 0 },
+      { x: 40, y: -20 },
+      { x: 20, y: -20 },
+      { x: 20, y: -40 },
+      { x: 0, y: -40 },
+      { x: 0, y: -20 },
+      { x: -20, y: -20 },
+      { x: -20, y: 0 },
     ],
     spikes: [
-      {x: 97.39086147549884, y: 204.072360524695},
-      {x: 52, y: 203},
-      {x: 87.75101187636338, y: 196.11948460540825},
-      {x: 32.56287292131242, y: 200.6984131649976},
-      {x: 108.71768475448265, y: 205.27734172458693},
-      {x: 60.03644427884856, y: 191.78155228579726},
-      {x: 76.18319235740043, y: 192.98653348568922},
-      {x: 70.64027883789757, y: 253.23559348028593},
-      {x: 109.92266595437465, y: 237.08884540173398},
-      {x: 26.53796692185277, y: 218.77313116337686},
-      {x: 70.15828635794081, y: 177.08078164711569},
-      {x: 21.236049642328275, y: 242.87275516121528},
-      {x: 97.63185771547685, y: 232.26892060216653},
-      {x: 105.82572987474202, y: 221.18309356316072},
-      {x: 78, y: 242},
-      {x: 98.35484643541207, y: 186.72063124625117},
-      {x: 113.77860579402882, y: 220.46010484322534},
-      {x: 84, y: 251},
-      {x: 42.68471500040471, y: 189.3715898860134},
-      {x: 88, y: 257}
-    ]
-  }
+      { x: 97.39086147549884, y: 204.072360524695 },
+      { x: 52, y: 203 },
+      { x: 87.75101187636338, y: 196.11948460540825 },
+      { x: 32.56287292131242, y: 200.6984131649976 },
+      { x: 108.71768475448265, y: 205.27734172458693 },
+      { x: 60.03644427884856, y: 191.78155228579726 },
+      { x: 76.18319235740043, y: 192.98653348568922 },
+      { x: 70.64027883789757, y: 253.23559348028593 },
+      { x: 109.92266595437465, y: 237.08884540173398 },
+      { x: 26.53796692185277, y: 218.77313116337686 },
+      { x: 70.15828635794081, y: 177.08078164711569 },
+      { x: 21.236049642328275, y: 242.87275516121528 },
+      { x: 97.63185771547685, y: 232.26892060216653 },
+      { x: 105.82572987474202, y: 221.18309356316072 },
+      { x: 78, y: 242 },
+      { x: 98.35484643541207, y: 186.72063124625117 },
+      { x: 113.77860579402882, y: 220.46010484322534 },
+      { x: 84, y: 251 },
+      { x: 42.68471500040471, y: 189.3715898860134 },
+      { x: 88, y: 257 },
+    ],
+  };
 
   function renderStrokeTest(canvas, testOptions, polyOptions) {
     const scale = new fabric.Point(2, 3),
@@ -100,7 +107,7 @@ const { fabric } = require("../../dist/fabric");
         cornerColor: 'white',
         objectCaching: false,
         exactBoundingBox: true,
-        ...polyOptions
+        ...polyOptions,
       });
     poly.scaleX = scale.x;
     poly.scaleY = scale.y;
@@ -113,7 +120,7 @@ const { fabric } = require("../../dist/fabric");
         top: poly.top,
         originX: poly.originX,
         originY: poly.originY,
-        fill: 'blue'
+        fill: 'blue',
       });
     canvas.add(bg, poly);
     canvas.setActiveObject(poly);
@@ -124,80 +131,97 @@ const { fabric } = require("../../dist/fabric");
     canvas.renderAll();
   }
 
-  let newModule = true;
-
   for (let [caseName, casePoints] of Object.entries(casesToTest)) {
     [fabric.Polyline, fabric.Polygon].forEach((builder) => {
       const type = builder.prototype.type,
         isPolygon = type === 'polygon',
-        strokes = isPolygon ? ['miter', 'round', 'bevel'] : ['butt', 'square', 'round'];
+        strokes = isPolygon
+          ? ['miter', 'round', 'bevel']
+          : ['butt', 'square', 'round'];
       strokes.forEach((strokeLineType) => {
         [true, false].forEach((strokeUniform) => {
-          [[0, 30], [20, 0], [25, 35]].forEach(([skewX, skewY]) => {
-              if (strokeLineType === 'round' && skewX !== 0 & skewY !== 0) return; // TODO: remove this line when fix strokeLineJoins equals `round` with `skewX`and `skewY` applied at sametime
-              tests.push({
-                test: `${caseName} of type ${type} with ${isPolygon ? 'strokeLineJoin' : 'strokeLineCap'}=${strokeLineType}, strokeUniform=${strokeUniform}, skewX=${skewX} and skewY=${skewY} values`,
-                code: function (canvas, callback) {
-                  renderStrokeTest(canvas,
-                    {
-                      builder,
-                      points: casePoints
-                    },
-                    {
-                      strokeLineJoin: strokeLineType,
-                      strokeUniform,
-                      skewX,
-                      skewY
-                    }
-                  );
-                  callback(canvas.lowerCanvasEl);
-                },
-                golden: `stroke-projection/${isPolygon ? 'strokeLineJoin' : 'strokeLineCap'}/${strokeLineType}/${caseName}-${strokeUniform ? 'uniform-' : ''}${type}-${skewX}skewX-${skewY}skewY.png`,
-                percentage: 0.001,
-                width: 600,
-                height: 900,
-                fabricClass: 'Canvas',
-                newModule: newModule ? 'stroke projection' : undefined,
-              });
-              newModule = false;
+          [
+            [0, 30],
+            [20, 0],
+            [25, 35],
+          ].forEach(([skewX, skewY]) => {
+            if (strokeLineType === 'round' && (skewX !== 0) & (skewY !== 0))
+              return; // TODO: remove this line when fix strokeLineJoins equals `round` with `skewX`and `skewY` applied at sametime
+            tests.push({
+              test: `${caseName} of type ${type} with ${
+                isPolygon ? 'strokeLineJoin' : 'strokeLineCap'
+              }=${strokeLineType}, strokeUniform=${strokeUniform}, skewX=${skewX} and skewY=${skewY} values`,
+              code: function (canvas, callback) {
+                renderStrokeTest(
+                  canvas,
+                  {
+                    builder,
+                    points: casePoints,
+                  },
+                  {
+                    strokeLineJoin: strokeLineType,
+                    strokeUniform,
+                    skewX,
+                    skewY,
+                  }
+                );
+                callback(canvas.lowerCanvasEl);
+              },
+              golden: `stroke-projection/${
+                isPolygon ? 'strokeLineJoin' : 'strokeLineCap'
+              }/${strokeLineType}/${caseName}-${
+                strokeUniform ? 'uniform-' : ''
+              }${type}-${skewX}skewX-${skewY}skewY.png`,
+              percentage: 0.001,
+              width: 600,
+              height: 900,
+              fabricClass: 'Canvas',
+            });
           });
         });
       });
     });
-  };
+  }
 
   // Test only miter limit
   for (let [caseName, casePoints] of Object.entries(casesToTest)) {
     const builder = fabric.Polyline;
     [5, 20, 120].forEach((strokeMiterLimit) => {
       [true, false].forEach((strokeUniform) => {
-        [[0, 30], [20, 0], [25, 35]].forEach(([skewX, skewY]) => {
-            tests.push({
-              test: `${caseName} with strokeMiterLimit=${strokeMiterLimit}, strokeUniform=${strokeUniform}, skewX=${skewX} and skewY=${skewY} values`,
-              code: function (canvas, callback) {
-                renderStrokeTest(canvas,
-                  {
-                    builder,
-                    points: casePoints
-                  },
-                  {
-                    strokeLineJoin: 'miter',
-                    strokeUniform,
-                    strokeMiterLimit
-                  }
-                );
-                callback(canvas.lowerCanvasEl);
-              },
-              golden: `stroke-projection/strokeLineJoin/miter-limit/${strokeUniform ? 'uniform-' : ''}${caseName}-${strokeMiterLimit}miterLimit-${skewX}skewX-${skewY}skewY.png`,
-              percentage: 0.001,
-              width: 600,
-              height: 900,
-              fabricClass: 'Canvas'
-            });
+        [
+          [0, 30],
+          [20, 0],
+          [25, 35],
+        ].forEach(([skewX, skewY]) => {
+          tests.push({
+            test: `${caseName} with strokeMiterLimit=${strokeMiterLimit}, strokeUniform=${strokeUniform}, skewX=${skewX} and skewY=${skewY} values`,
+            code: function (canvas, callback) {
+              renderStrokeTest(
+                canvas,
+                {
+                  builder,
+                  points: casePoints,
+                },
+                {
+                  strokeLineJoin: 'miter',
+                  strokeUniform,
+                  strokeMiterLimit,
+                }
+              );
+              callback(canvas.lowerCanvasEl);
+            },
+            golden: `stroke-projection/strokeLineJoin/miter-limit/${
+              strokeUniform ? 'uniform-' : ''
+            }${caseName}-${strokeMiterLimit}miterLimit-${skewX}skewX-${skewY}skewY.png`,
+            percentage: 0.001,
+            width: 600,
+            height: 900,
+            fabricClass: 'Canvas',
+          });
         });
-      })
-    })
-  };
+      });
+    });
+  }
 
   tests.forEach(visualTestLoop(QUnit));
-})();
+});
