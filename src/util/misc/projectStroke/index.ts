ef3cfef9d77f3bf5ef3a610bc4ca52843d7cc31d
ShaMan123 <shacharnen@gmail.com>
2022-10-20 09:48:37 +0300

    fix(): 1 point case

--- src/util/misc/projectStroke/index.ts
+++ src/util/misc/projectStroke/index.ts
@@ -18,47 +18,55 @@ export const projectStrokeOnPoints = (
 ): TProjection[] => {
   const projections: TProjection[] = [];
 
-  if (points.length <= 1) {
+  if (points.length === 0) {
     return projections;
   }
 
-  points
-    .reduce(
-      (reduced, point) => {
-        if (!reduced[reduced.length - 1].eq(point)) {
-          reduced.push(new Point(point));
-        }
-        return reduced;
-      },
-      [new Point(points[0])]
-    )
-    .forEach((A, index, points) => {
-      let B: IPoint, C: IPoint;
-      if (index === 0) {
-        C = points[1];
-        B = openPath ? A : findRight(points, (point) => !point.eq(A)) || A;
-      } else if (index === points.length - 1) {
-        B = points[index - 1];
-        C = openPath ? A : points[0];
-      } else {
-        B = points[index - 1];
-        C = points[index + 1];
+  const reduced = points.reduce(
+    (reduced, point) => {
+      if (!reduced[reduced.length - 1].eq(point)) {
+        reduced.push(new Point(point));
       }
+      return reduced;
+    },
+    [new Point(points[0])]
+  );
 
-      if (openPath && (index === 0 || index === points.length - 1)) {
-        projections.push(
-          ...new StrokeLineCapProjections(
-            A,
-            index === 0 ? C : B,
-            options
-          ).project()
-        );
-      } else {
-        projections.push(
-          ...new StrokeLineJoinProjections(A, B, C, options).project()
-        );
-      }
-    });
+  if (reduced.length === 1) {
+    openPath = true;
+  }
+
+  reduced.forEach((A, index, points) => {
+    let B: IPoint, C: IPoint;
+    if (index === 0) {
+      C = points[1];
+      B = openPath
+        ? A
+        : // we search for the last point that affects the projection
+          // this is important in case there are points at the end that are equal to the first point
+          findRight(points, (point) => !point.eq(A)) || A;
+    } else if (index === points.length - 1) {
+      B = points[index - 1];
+      C = openPath ? A : points[0];
+    } else {
+      B = points[index - 1];
+      C = points[index + 1];
+    }
+
+    if (openPath && (index === 0 || index === points.length - 1)) {
+      projections.push(
+        ...new StrokeLineCapProjections(
+          A,
+          index === 0 ? C : B,
+          options
+        ).project()
+      );
+    } else {
+      projections.push(
+        ...new StrokeLineJoinProjections(A, B, C, options).project()
+      );
+    }
+  });
 
   return projections;
 };
