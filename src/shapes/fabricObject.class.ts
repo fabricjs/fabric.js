c03ba7c45372c36c5551921019ff5c42ce5bdb97
ShaMan123 <shacharnen@gmail.com>
2022-11-01 00:35:34 +0200

    Update fabricObject.class.ts

--- src/shapes/fabricObject.class.ts
+++ src/shapes/fabricObject.class.ts
@@ -1,12 +1,11 @@
-import { FabricObjectAncestryMixin } from '../mixins/object_ancestry.mixin';
-import { FabricObjectObjectStackingMixin } from '../mixins/object_stacking.mixin';
-import { InteractiveFabricObject } from '../mixins/object_interactivity.mixin';
-import { FabricObjectObjectStraighteningMixin } from '../mixins/object_straightening.mixin';
-import { FabricObjectSVGExportMixin } from '../mixins/object.svg_export';
-import { applyMixins } from '../util/applyMixins';
 import { fabric } from '../../HEADER';
-import { FabricObject } from './object.class';
+import { FabricObjectSVGExportMixin } from '../mixins/object.svg_export';
+import { FabricObjectAncestryMixin } from '../mixins/object_ancestry.mixin';
 import { FabricObjectObjectAnimationMixin } from '../mixins/object_animation.mixin';
+import { InteractiveFabricObject } from '../mixins/object_interactivity.mixin';
+import { FabricObjectObjectStackingMixin } from '../mixins/object_stacking.mixin';
+import { FabricObjectObjectStraighteningMixin } from '../mixins/object_straightening.mixin';
+import { applyMixins } from '../util/applyMixins';
 
 // TODO somehow we have to make a tree-shakeable import
 
