0ae3ff3dc52a25c1c6868f3eb1061f3f6aa143ff
ShaMan123 <shacharnen@gmail.com>
2022-11-02 16:45:04 +0200

    Revert "Update index.mjs"
    This reverts commit 04eb6f917d26d9a4d29b977fb20d1ad87ae4f67b.

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -243,12 +243,9 @@ function logTestResults(dir, context, suite) {
       chalk.cyan(`${_.upperFirst(context)} ${_.upperFirst(suite)} Test Results`)
     )}\n`
   );
-  console.dir({
-    [context]: fs
-      .readFileSync(path.resolve(dir, suite, `${context}.txt`))
-      .toString()
-      .split('\n'),
-  });
+  console.log(
+    fs.readFileSync(path.resolve(dir, suite, `${context}.txt`)).toString()
+  );
 }
 
 /**
