cc29c9ba2cb55141208ac81410e8d47d4be1c62e
ShaMan123 <shacharnen@gmail.com>
2022-11-06 12:42:51 +0200

    revert rename for diff

--- src/controls/actions.ts
+++ src/controls/actions.ts
@@ -1,5 +1,5 @@
 import { fabric } from '../../HEADER';
-import { changeWidth } from './changeSize';
+import { changeWidth } from './changeWidth';
 import { renderCircleControl, renderSquareControl } from './controls.render';
 import { dragHandler } from './drag';
 import { rotationStyleHandler, rotationWithSnapping } from './rotate';
