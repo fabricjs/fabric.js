919b90d4014bfbcaa658f39f4a594d9286928409
ShaMan123 <shacharnen@gmail.com>
2022-11-06 21:05:53 +0200

    Revert "Update planeChange.ts"
    This reverts commit 4b4851638c19fd309cbf5b8f541e2f0d76f650f8.

--- src/util/misc/planeChange.ts
+++ src/util/misc/planeChange.ts
@@ -124,40 +124,15 @@ export const transformPointRelativeToCanvas = (
  * @param {TMat2D} [to] destination plane matrix to contain object. Passing `undefined` means `object` should be sent to the canvas coordinate plane.
  * @returns {TMat2D} the transform matrix that was applied to `object`
  */
-export function sendObjectToPlane(
+export const sendObjectToPlane = (
   object: FabricObject,
-  to?: FabricObject
-): { transform: TMat2D; angle: TDegree };
-/**
- * Use this signature for clip path
- * @deprecated clip path's awareness to ancestors is planned to be fixed
- * @todo remove signature + update examples once fixed
- */
-export function sendObjectToPlane(
-  object: FabricObject,
-  from: FabricObject | undefined,
-  to: FabricObject | undefined
-): { transform: TMat2D; angle: TDegree };
-export function sendObjectToPlane(
-  object: FabricObject,
-  arg1?: FabricObject,
-  arg2?: FabricObject
-) {
-  const { from, to } =
-    arguments.length === 3
-      ? { from: arg1, to: arg2 }
-      : { from: object.group, to: arg1 };
-  const t = calcPlaneChangeMatrix(
-    from?.calcTransformMatrix(),
-    to?.calcTransformMatrix()
-  );
-  const angle = (object.angle -
-    (from?.getTotalAngle() || 0) +
-    (to?.getTotalAngle() || 0)) as TDegree;
+  from?: TMat2D,
+  to?: TMat2D
+): TMat2D => {
+  const t = calcPlaneChangeMatrix(from, to);
   applyTransformToObject(
     object,
-    multiplyTransformMatrices(t, object.calcOwnMatrix()),
-    angle
+    multiplyTransformMatrices(t, object.calcOwnMatrix())
   );
-  return { transform: t, angle };
-}
+  return t;
+};
