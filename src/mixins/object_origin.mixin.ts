9af645e150ca942466c6c2761b47f04330a7122f
ShaMan123 <shacharnen@gmail.com>
2022-11-09 15:29:23 +0200

    d

--- src/mixins/object_origin.mixin.ts
+++ src/mixins/object_origin.mixin.ts
@@ -5,6 +5,7 @@ import { CommonMethods } from './shared_methods.mixin';
 import { TDegree, TOriginX, TOriginY } from '../typedefs';
 import { Group } from '../shapes/group.class';
 import { sizeAfterTransform } from '../util/misc/objectTransforms';
+import { TObjectEvents } from './EventTypeDefs';
 
 const originOffset = {
   left: -0.5,
@@ -27,7 +28,7 @@ export const resolveOrigin = (
     ? originOffset[originValue]
     : originValue - 0.5;
 
-export class ObjectOrigin extends CommonMethods {
+export class ObjectOrigin extends CommonMethods<TObjectEvents> {
   /**
    * Top position of an object. Note that by default it's relative to object top. You can change this by setting originY={top/center/bottom}
    * @type Number
--- src/mixins/shared_methods.mixin.ts
+++ src/mixins/shared_methods.mixin.ts
@@ -1,7 +1,7 @@
 //@ts-nocheck
 import { Observable } from './observable.mixin';
 
-export class CommonMethods extends Observable {
+export class CommonMethods<T> extends Observable<T> {
   /**
    * Sets object's properties from options
    * @param {Object} [options] Options object
