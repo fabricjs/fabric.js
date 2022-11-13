8aae9d6efe6b0207ec6e3c84c23f51498b4cd66d
ShaMan123 <shacharnen@gmail.com>
2022-11-13 10:36:03 +0200

    checkout

--- src/util/applymixins.ts
+++ src/util/applymixins.ts
@@ -1,22 +1,15 @@
-type TClass = { new (...args: any[]): any };
-
-/***
+/**
  * https://www.typescriptlang.org/docs/handbook/mixins.html#alternative-pattern
  */
-export function applyMixins<T extends TClass, S extends TClass>(
-  derivedCtor: T,
-  constructors: S[]
-) {
+export function applyMixins(derivedCtor: any, constructors: any[]) {
   constructors.forEach((baseCtor) => {
     Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
-      name !== 'constructor' &&
-        Object.defineProperty(
-          derivedCtor.prototype,
-          name,
-          Object.getOwnPropertyDescriptor(baseCtor.prototype, name) ||
-            Object.create(null)
-        );
+      Object.defineProperty(
+        derivedCtor.prototype,
+        name,
+        Object.getOwnPropertyDescriptor(baseCtor.prototype, name) ||
+          Object.create(null)
+      );
     });
   });
-  return derivedCtor as T & S;
 }
