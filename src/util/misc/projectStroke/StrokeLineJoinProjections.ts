3d2e063affc21c5d7c347bb2cf5559d8f20b7fbc
Luiz Zappa <luiz.zappa@hotmail.com>
2022-11-14 11:04:06 -0300

    refactor(StrokeLineCapProjections): safe guard with Math.max

--- src/util/misc/projectStroke/StrokeLineJoinProjections.ts
+++ src/util/misc/projectStroke/StrokeLineJoinProjections.ts
@@ -219,7 +219,7 @@ export class StrokeLineJoinProjections extends StrokeProjectionsBase {
       furthestY = new Point(
         // Safe guard due to floating point precision. In some situations the square root
         // was returning NaN because of a negative number close to zero.
-        Math.sqrt(circleRadius ** 2 - newY ** 2) || 0,
+        Math.sqrt(Math.max(circleRadius ** 2 - newY ** 2, 0)),
         newY
       ),
       newX = strokeUniform
@@ -229,7 +229,7 @@ export class StrokeLineJoinProjections extends StrokeProjectionsBase {
         : circleRadius / Math.sqrt(1 + shearing.x ** 2),
       furthestX = new Point(
         newX,
-        Math.sqrt(circleRadius ** 2 - newX ** 2) || 0
+        Math.sqrt(Math.max(circleRadius ** 2 - newX ** 2, 0))
       );
 
     [
