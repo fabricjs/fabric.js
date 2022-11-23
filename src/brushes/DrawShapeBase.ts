6062754061fce6df689d0acc3c7875d44cb2cedd
ShaMan123 <shacharnen@gmail.com>
2022-11-23 15:42:10 +0200

    shadow

--- src/brushes/DrawShapeBase.ts
+++ src/brushes/DrawShapeBase.ts
@@ -1,6 +1,10 @@
+import { fabric } from '../../HEADER';
 import { Point } from '../point.class';
 import { FabricObject } from '../shapes/fabricObject.class';
 import { BaseBrush } from './base_brush.class';
+
+// TODO: remove transient
+const { Shadow } = fabric;
 
 /**
  * Declarative shape drawing using pointer events
@@ -27,6 +31,7 @@ export abstract class DrawShapeBase<T extends FabricObject> extends BaseBrush {
       strokeMiterLimit: this.strokeMiterLimit,
       strokeLineJoin: this.strokeLineJoin,
       strokeDashArray: this.strokeDashArray,
+      shadow: this.shadow ? new Shadow(this.shadow) : undefined,
     });
   }
 
