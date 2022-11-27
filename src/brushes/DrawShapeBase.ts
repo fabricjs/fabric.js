c2676406ef84fd0107f8de90bd3c6342c828c6bf
ShaMan123 <shacharnen@gmail.com>
2022-11-27 13:51:50 +0200

    Update DrawShapeBase.ts

--- src/brushes/DrawShapeBase.ts
+++ src/brushes/DrawShapeBase.ts
@@ -36,9 +36,9 @@ export abstract class DrawShapeBase<T extends FabricObject> extends BaseBrush {
     const shape = this.shape;
     if (!shape) return;
     shape.setCoords();
-    this.canvas.fire('before:path:created', { path: this.shape });
+    this.canvas.fire('before:path:created', { path: shape });
     this.canvas.add(this.shape);
-    this.canvas.fire('path:created', { path: this.shape });
+    this.canvas.fire('path:created', { path: shape });
     this.canvas.clearContext(this.canvas.contextTop);
     this.shape = undefined;
   }
