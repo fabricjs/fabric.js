27f95bc9d0a49432e1228ba3912367e22acd4f48
ShaMan123 <shacharnen@gmail.com>
2022-11-27 21:49:18 +0200

    transform

--- src/filters/composed_filter.class.ts
+++ src/filters/composed_filter.class.ts
@@ -1,84 +1,87 @@
 //@ts-nocheck
-(function (global) {
-  'use strict';
+'use strict';
 
-  var fabric = global.fabric || (global.fabric = {}),
-    filters = fabric.Image.filters,
-    createClass = fabric.util.createClass;
+var fabric = global.fabric || (global.fabric = {}),
+  filters = fabric.Image.filters,
+  createClass = createClass;
 
-  /**
-   * A container class that knows how to apply a sequence of filters to an input image.
-   */
-  filters.Composed = createClass(
-    filters.BaseFilter,
-    /** @lends fabric.Image.filters.Composed.prototype */ {
-      type: 'Composed',
-
-      /**
-       * A non sparse array of filters to apply
-       */
-      subFilters: [],
-
-      /**
-       * Constructor
-       * @param {Object} [options] Options object
-       */
-      initialize: function (options) {
-        this.callSuper('initialize', options);
-        // create a new array instead mutating the prototype with push
-        this.subFilters = this.subFilters.slice(0);
-      },
-
-      /**
-       * Apply this container's filters to the input image provided.
-       *
-       * @param {Object} options
-       * @param {Number} options.passes The number of filters remaining to be applied.
-       */
-      applyTo: function (options) {
-        options.passes += this.subFilters.length - 1;
-        this.subFilters.forEach(function (filter) {
-          filter.applyTo(options);
-        });
-      },
-
-      /**
-       * Serialize this filter into JSON.
-       *
-       * @returns {Object} A JSON representation of this filter.
-       */
-      toObject: function () {
-        return fabric.util.object.extend(this.callSuper('toObject'), {
-          subFilters: this.subFilters.map(function (filter) {
-            return filter.toObject();
-          }),
-        });
-      },
-
-      isNeutralState: function () {
-        return !this.subFilters.some(function (filter) {
-          return !filter.isNeutralState();
-        });
-      },
-    }
-  );
+/**
+ * A container class that knows how to apply a sequence of filters to an input image.
+ */
+export class Composed extends filters.BaseFilter {
+  /** @lends fabric.Image.Composed.prototype */
+  type: string;
 
   /**
-   * Deserialize a JSON definition of a ComposedFilter into a concrete instance.
-   * @static
-   * @param {oject} object Object to create an instance from
-   * @param {object} [options]
-   * @param {AbortSignal} [options.signal] handle aborting `BlendImage` filter loading, see https://developer.mozilla.org/en-US/docs/Web/API/AbortController/signal
-   * @returns {Promise<fabric.Image.filters.Composed>}
+   * A non sparse array of filters to apply
    */
-  fabric.Image.filters.Composed.fromObject = function (object, options) {
-    var filters = object.subFilters || [];
-    return Promise.all(
-      filters.map(function (filter) {
-        return fabric.Image.filters[filter.type].fromObject(filter, options);
-      })
-    ).then(function (enlivedFilters) {
-      return new fabric.Image.filters.Composed({ subFilters: enlivedFilters });
+  subFilters;
+
+  /**
+   * Constructor
+   * @param {Object} [options] Options object
+   */
+  constructor(options) {
+    super(options);
+    // create a new array instead mutating the prototype with push
+    this.subFilters = this.subFilters.slice(0);
+  }
+
+  /**
+   * Apply this container's filters to the input image provided.
+   *
+   * @param {Object} options
+   * @param {Number} options.passes The number of filters remaining to be applied.
+   */
+  applyTo(options) {
+    options.passes += this.subFilters.length - 1;
+    this.subFilters.forEach(function (filter) {
+      filter.applyTo(options);
     });
-  };
-})(typeof exports !== 'undefined' ? exports : window);
+  }
+
+  /**
+   * Serialize this filter into JSON.
+   *
+   * @returns {Object} A JSON representation of this filter.
+   */
+  toObject() {
+    return object.extend(super.toObject(), {
+      subFilters: this.subFilters.map(function (filter) {
+        return filter.toObject();
+      }),
+    });
+  }
+
+  isNeutralState() {
+    return !this.subFilters.some(function (filter) {
+      return !filter.isNeutralState();
+    });
+  }
+}
+
+export const composedDefaultValues: Partial<TClassProperties<Composed>> = {
+  type: 'Composed',
+  subFilters: [],
+};
+
+Object.assign(Composed.prototype, composedDefaultValues);
+
+/**
+ * Deserialize a JSON definition of a ComposedFilter into a concrete instance.
+ * @static
+ * @param {oject} object Object to create an instance from
+ * @param {object} [options]
+ * @param {AbortSignal} [options.signal] handle aborting `BlendImage` filter loading, see https://developer.mozilla.org/en-US/docs/Web/API/AbortController/signal
+ * @returns {Promise<fabric.Image.Composed>}
+ */
+fabric.Image.Composed.fromObject = function (object, options) {
+  var filters = object.subFilters || [];
+  return Promise.all(
+    filters.map(function (filter) {
+      return fabric.Image.filters[filter.type].fromObject(filter, options);
+    })
+  ).then(function (enlivedFilters) {
+    return new fabric.Image.Composed({ subFilters: enlivedFilters });
+  });
+};
