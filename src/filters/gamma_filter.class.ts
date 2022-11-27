65d5d962af2ae4fe94f54d98582a435bc47cc7d4
ShaMan123 <shacharnen@gmail.com>
2022-11-28 00:12:44 +0200

    Update gamma_filter.class.ts

--- src/filters/gamma_filter.class.ts
+++ src/filters/gamma_filter.class.ts
@@ -1,55 +1,37 @@
-//@ts-nocheck
-'use strict';
+import { TClassProperties } from '../typedefs';
+import { BaseFilter, BaseFilterOptions } from './base_filter.class';
+import { T2DPipelineState, TWebGLUniformLocationMap } from './typedefs';
 
-var fabric = global.fabric || (global.fabric = {}),
-  filters = fabric.Image.filters,
-  createClass = createClass;
+export type GammaInput = [number, number, number];
 
 /**
  * Gamma filter class
- * @class fabric.Image.Gamma
- * @memberOf fabric.Image.filters
- * @extends fabric.Image.filters.BaseFilter
- * @see {@link fabric.Image.Gamma#initialize} for constructor definition
- * @see {@link http://fabricjs.com/image-filters|ImageFilters demo}
  * @example
- * var filter = new fabric.Image.Gamma({
+ * const filter = new Gamma({
  *   gamma: [1, 0.5, 2.1]
  * });
  * object.filters.push(filter);
  * object.applyFilters();
  */
-export class Gamma extends filters.BaseFilter {
-  /**
-   * Filter type
-   * @param {String} type
-   * @default
-   */
-  type: string;
-
-  fragmentSource;
-
+export class Gamma extends BaseFilter {
   /**
    * Gamma array value, from 0.01 to 2.2.
    * @param {Array} gamma
    * @default
    */
-  gamma;
+  gamma: GammaInput;
+  rgbValues?: {
+    r: Uint8Array;
+    g: Uint8Array;
+    b: Uint8Array;
+  };
 
-  /**
-   * Describe the property that is the filter parameter
-   * @param {String} m
-   * @default
-   */
-  mainParameter: string;
-
-  /**
-   * Constructor
-   * @param {Object} [options] Options object
-   */
-  constructor(options) {
-    this.gamma = [1, 1, 1];
-    filters.BaseFilter.prototype.initialize.call(this, options);
+  constructor({
+    gamma,
+    ...options
+  }: Partial<BaseFilterOptions> & { gamma?: GammaInput } = {}) {
+    super(options);
+    this.gamma = gamma || [1, 1, 1];
   }
 
   /**
@@ -58,36 +40,31 @@ export class Gamma extends filters.BaseFilter {
    * @param {Object} options
    * @param {ImageData} options.imageData The Uint8Array to be filtered.
    */
-  applyTo2d(options) {
-    var imageData = options.imageData,
-      data = imageData.data,
-      gamma = this.gamma,
-      len = data.length,
+  applyTo2d({ imageData: { data } }: T2DPipelineState) {
+    const gamma = this.gamma,
       rInv = 1 / gamma[0],
       gInv = 1 / gamma[1],
-      bInv = 1 / gamma[2],
-      i;
+      bInv = 1 / gamma[2];
 
-    if (!this.rVals) {
-      // eslint-disable-next-line
-      this.rVals = new Uint8Array(256);
-      // eslint-disable-next-line
-      this.gVals = new Uint8Array(256);
-      // eslint-disable-next-line
-      this.bVals = new Uint8Array(256);
+    if (!this.rgbValues) {
+      this.rgbValues = {
+        r: new Uint8Array(256),
+        g: new Uint8Array(256),
+        b: new Uint8Array(256),
+      };
     }
 
     // This is an optimization - pre-compute a look-up table for each color channel
     // instead of performing these pow calls for each pixel in the image.
-    for (i = 0, len = 256; i < len; i++) {
-      this.rVals[i] = Math.pow(i / 255, rInv) * 255;
-      this.gVals[i] = Math.pow(i / 255, gInv) * 255;
-      this.bVals[i] = Math.pow(i / 255, bInv) * 255;
+    for (let i = 0; i < 256; i++) {
+      this.rgbValues.r[i] = Math.pow(i / 255, rInv) * 255;
+      this.rgbValues.g[i] = Math.pow(i / 255, gInv) * 255;
+      this.rgbValues.b[i] = Math.pow(i / 255, bInv) * 255;
     }
-    for (i = 0, len = data.length; i < len; i += 4) {
-      data[i] = this.rVals[data[i]];
-      data[i + 1] = this.gVals[data[i + 1]];
-      data[i + 2] = this.bVals[data[i + 2]];
+    for (let i = 0; i < data.length; i += 4) {
+      data[i] = this.rgbValues.r[data[i]];
+      data[i + 1] = this.rgbValues.g[data[i + 1]];
+      data[i + 2] = this.rgbValues.b[data[i + 2]];
     }
   }
 
@@ -97,7 +74,10 @@ export class Gamma extends filters.BaseFilter {
    * @param {WebGLRenderingContext} gl The GL canvas context used to compile this filter's shader.
    * @param {WebGLShaderProgram} program This filter's compiled shader program.
    */
-  getUniformLocations(gl, program) {
+  getUniformLocations(
+    gl: WebGLRenderingContext,
+    program: WebGLProgram
+  ): TWebGLUniformLocationMap {
     return {
       uGamma: gl.getUniformLocation(program, 'uGamma'),
     };
@@ -109,37 +89,32 @@ export class Gamma extends filters.BaseFilter {
    * @param {WebGLRenderingContext} gl The GL canvas context used to compile this filter's shader.
    * @param {Object} uniformLocations A map of string uniform names to WebGLUniformLocation objects
    */
-  sendUniformData(gl, uniformLocations) {
+  sendUniformData(
+    gl: WebGLRenderingContext,
+    uniformLocations: TWebGLUniformLocationMap
+  ) {
     gl.uniform3fv(uniformLocations.uGamma, this.gamma);
   }
 }
 
 export const gammaDefaultValues: Partial<TClassProperties<Gamma>> = {
   type: 'Gamma',
-  fragmentSource:
-    'precision highp float;\n' +
-    'uniform sampler2D uTexture;\n' +
-    'uniform vec3 uGamma;\n' +
-    'varying vec2 vTexCoord;\n' +
-    'void main() {\n' +
-    'vec4 color = texture2D(uTexture, vTexCoord);\n' +
-    'vec3 correction = (1.0 / uGamma);\n' +
-    'color.r = pow(color.r, correction.r);\n' +
-    'color.g = pow(color.g, correction.g);\n' +
-    'color.b = pow(color.b, correction.b);\n' +
-    'gl_FragColor = color;\n' +
-    'gl_FragColor.rgb *= color.a;\n' +
-    '}',
-  gamma: [1, 1, 1],
+  fragmentSource: `
+    precision highp float;
+    uniform sampler2D uTexture;
+    uniform vec3 uGamma;
+    varying vec2 vTexCoord;
+    void main() {
+      vec4 color = texture2D(uTexture, vTexCoord);
+      vec3 correction = (1.0 / uGamma);
+      color.r = pow(color.r, correction.r);
+      color.g = pow(color.g, correction.g);
+      color.b = pow(color.b, correction.b);
+      gl_FragColor = color;
+      gl_FragColor.rgb *= color.a;
+    }
+  `,
   mainParameter: 'gamma',
 };
 
 Object.assign(Gamma.prototype, gammaDefaultValues);
-
-/**
- * Create filter instance from an object representation
- * @static
- * @param {Object} object Object to create an instance from
- * @returns {Promise<fabric.Image.Gamma>}
- */
-fabric.Image.Gamma.fromObject = fabric.Image.filters.BaseFilter.fromObject;
