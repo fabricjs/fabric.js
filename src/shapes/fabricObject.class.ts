ef6c29dfcaa7a9ed5b01139044ac73883387e047
ShaMan123 <shacharnen@gmail.com>
2022-11-27 12:41:27 +0200

    Update fabricObject.class.ts

--- src/shapes/fabricObject.class.ts
+++ src/shapes/fabricObject.class.ts
@@ -1,4 +1,5 @@
 import { fabric } from '../../HEADER';
+import { ObjectEvents } from '../EventTypeDefs';
 import { FabricObjectSVGExportMixin } from '../mixins/object.svg_export';
 import { FabricObjectObjectAnimationMixin } from '../mixins/object_animation.mixin';
 import { InteractiveFabricObject } from '../mixins/object_interactivity.mixin';
@@ -8,21 +9,21 @@ import { applyMixins } from '../util/applyMixins';
 
 // TODO somehow we have to make a tree-shakeable import
 
-const FabricObject = applyMixins(InteractiveFabricObject, [
+export class FabricObject extends applyMixins(InteractiveFabricObject, [
   FabricObjectObjectStackingMixin,
   FabricObjectObjectStraighteningMixin,
   FabricObjectSVGExportMixin,
   FabricObjectObjectAnimationMixin,
-]);
+]) {}
 
-export interface FabricObject
-  extends InteractiveFabricObject,
+// @ts-expect-error applyMixins is not generic so EventSpec is different
+export interface FabricObject<EventSpec extends ObjectEvents = ObjectEvents>
+  extends InteractiveFabricObject<EventSpec>,
     FabricObjectObjectStackingMixin,
     FabricObjectObjectStraighteningMixin,
     FabricObjectSVGExportMixin,
     FabricObjectObjectAnimationMixin {}
 
 export { fabricObjectDefaultValues } from './object.class';
-export { FabricObject };
 
 fabric.Object = FabricObject;
