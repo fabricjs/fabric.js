7042aa60e227e8d8c01028daa1a375424562fc92
ShaMan123 <shacharnen@gmail.com>
2022-11-27 23:23:18 +0200

    Update blur_filter.class.ts

--- src/filters/blur_filter.class.ts
+++ src/filters/blur_filter.class.ts
@@ -23,7 +23,8 @@ export class Blur extends BaseFilter {
    */
   blur: number;
 
-  mainParameter: string;
+  horizontal: boolean;
+  aspectRatio: number;
 
   applyTo(options: TWebGLPipelineState | T2DPipelineState) {
     if (options.webgl) {
@@ -163,29 +164,30 @@ export class Blur extends BaseFilter {
 
 export const blurDefaultValues: Partial<TClassProperties<Blur>> = {
   type: 'Blur',
-  fragmentSource:
-    'precision highp float;\n' +
-    'uniform sampler2D uTexture;\n' +
-    'uniform vec2 uDelta;\n' +
-    'varying vec2 vTexCoord;\n' +
-    'const float nSamples = 15.0;\n' +
-    'vec3 v3offset = vec3(12.9898, 78.233, 151.7182);\n' +
-    'float random(vec3 scale) {\n' +
-    /* use the fragment position for a different seed per-pixel */
-    'return fract(sin(dot(gl_FragCoord.xyz, scale)) * 43758.5453);\n' +
-    '}\n' +
-    'void main() {\n' +
-    'vec4 color = vec4(0.0);\n' +
-    'float total = 0.0;\n' +
-    'float offset = random(v3offset);\n' +
-    'for (float t = -nSamples; t <= nSamples; t++) {\n' +
-    'float percent = (t + offset - 0.5) / nSamples;\n' +
-    'float weight = 1.0 - abs(percent);\n' +
-    'color += texture2D(uTexture, vTexCoord + uDelta * percent) * weight;\n' +
-    'total += weight;\n' +
-    '}\n' +
-    'gl_FragColor = color / total;\n' +
-    '}',
+  fragmentSource: `
+    precision highp float;
+    uniform sampler2D uTexture;
+    uniform vec2 uDelta;
+    varying vec2 vTexCoord;
+    const float nSamples = 15.0;
+    vec3 v3offset = vec3(12.9898, 78.233, 151.7182);
+    float random(vec3 scale) {
+      /* use the fragment position for a different seed per-pixel */
+      return fract(sin(dot(gl_FragCoord.xyz, scale)) * 43758.5453);
+    }
+    void main() {
+      vec4 color = vec4(0.0);
+      float total = 0.0;
+      float offset = random(v3offset);
+      for (float t = -nSamples; t <= nSamples; t++) {
+        float percent = (t + offset - 0.5) / nSamples;
+        float weight = 1.0 - abs(percent);
+        color += texture2D(uTexture, vTexCoord + uDelta * percent) * weight;
+        total += weight;
+      }
+      gl_FragColor = color / total;
+    }
+  `,
   blur: 0,
   mainParameter: 'blur',
 };
