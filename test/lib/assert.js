b307abfc2706dd26da899ebaf1f7a0f584aaf3b5
ShaMan123 <shacharnen@gmail.com>
2022-12-01 12:10:52 +0200

    Update assert.js

--- test/lib/assert.js
+++ test/lib/assert.js
@@ -1,10 +1,13 @@
 const SVG_RE = /(SVGID|CLIPPATH|imageCrop)_[0-9]+/gm;
 const SVG_XLINK_HREF_RE = /xlink:href="([^"]*)"/gm;
 
+function basename(link) {
+    return link.split(/\\|\//).pop().replaceAll('"', '');
+}
 
 function replaceLinks(value) {
     return (value.match(SVG_XLINK_HREF_RE) || []).reduce((final, curr) => {
-        return final.replace(curr, `xlink:href="assets/${curr.split(/\\|\//).pop().replaceAll('"', '')}"`);
+        return final.replace(curr, `xlink:href="assets/${basename(curr)}"`);
     }, value)
 }
 
@@ -22,3 +25,14 @@ QUnit.assert.equalSVG = function (actual, expected, message) {
     QUnit.assert.equal(sanitizeSVG(actual), sanitizeSVG(expected), message);   
 }
 
+QUnit.assert.sameImageObject = function (actual, expected, message = 'image object should equal to ref') {
+    var a = {}, b = {};
+    Object.assign(a, actual, { src: basename(actual.src) });
+    Object.assign(b, expected, { src: basename(expected.src) });
+    this.pushResult({
+        result: QUnit.equiv(a, b),
+        actual,
+        expected,
+        message
+    });
+}
