58b09e3f0e280beea27f586c32b2066304bbe5c4
ShaMan123 <shacharnen@gmail.com>
2022-12-12 14:35:00 +0200

    add test

--- test/unit/class_registry.js
+++ test/unit/class_registry.js
@@ -1,26 +1,26 @@
 (function () {
   const classRegistry = new fabric.util.classRegistry.constructor();
   QUnit.module('fabric.util.classRegistry');
-  QUnit.test('getClass throw when no class is registered', function (assert) {
+  QUnit.test('getJSONClass throw when no class is registered', function (assert) {
     assert.ok(fabric.util.classRegistry, 'classRegistry is available');
-    assert.throws(() => classRegistry.getClass('rect'), new Error(`No class registered for rect`), 'initially Rect is undefined');
+    assert.throws(() => classRegistry.getJSONClass('rect'), new Error(`No class registered for rect`), 'initially Rect is undefined');
   });
-  QUnit.test('getClass will return specific class from the prototype type', function (assert) {
+  QUnit.test('getJSONClass will return specific class from the prototype type', function (assert) {
     class TestClass {
 
     }
     TestClass.prototype.type = 'anyType';
-    classRegistry.setClass(TestClass);
-    const resolved = classRegistry.getClass('anyType');
+    classRegistry.setJSONClass(TestClass);
+    const resolved = classRegistry.getJSONClass('anyType');
     assert.equal(resolved, TestClass, 'resolves class correctly');
   });
-  QUnit.test('getClass will return specific class from custom type', function (assert) {
+  QUnit.test('getJSONClass will return specific class from custom type', function (assert) {
     class TestClass2 {
 
     }
     TestClass2.prototype.type = 'anyType';
-    classRegistry.setClass(TestClass2, 'myCustomType');
-    const resolved = classRegistry.getClass('myCustomType');
+    classRegistry.setJSONClass(TestClass2, 'myCustomType');
+    const resolved = classRegistry.getJSONClass('myCustomType');
     assert.equal(resolved, TestClass2, 'resolves class correctly with custom type');
   });
   QUnit.test('can resolve different class for SVG and JSON', function (assert) {
@@ -32,10 +32,44 @@
 
     }
     TestClass4.prototype.type = 'typeC';
-    classRegistry.setClass(TestClass3, 'myCustomType');
+    classRegistry.setJSONClass(TestClass3, 'myCustomType');
     classRegistry.setSVGClass(TestClass4, 'myCustomType');
-    const resolved = classRegistry.getClass('myCustomType');
+    const resolved = classRegistry.getJSONClass('myCustomType');
     const resolvedSvg = classRegistry.getSVGClass('myCustomType');
     assert.notEqual(resolved, resolvedSvg, 'resolved different classes');
+  });
+  QUnit.test('can set both SVG and JSON', function (assert) {
+    class JSONClass {
+      static fromObject() {
+        return new JSONClass();
+      }
+    }
+    JSONClass.prototype.type = 'json';
+    class SVGClass {
+      static fromElement() {
+        return new SVGClass();
+      }
+    }
+    SVGClass.prototype.type = 'svg';
+    class ExportableClass {
+      static fromObject() {
+        return new ExportableClass();
+      }
+      static fromElement() {
+        return new ExportableClass();
+      }
+    }
+    ExportableClass.prototype.type = 'X';
+    classRegistry.setClass(JSONClass);
+    classRegistry.setClass(SVGClass);
+    classRegistry.setClass(ExportableClass, 'Y');
+    assert.equal(classRegistry.getJSONClass('json'), JSONClass, 'resolved class');
+    assert.throws(() => classRegistry.getSVGClass('json'), 'should not have registered class');
+    assert.equal(classRegistry.getSVGClass('svg'), SVGClass, 'resolved class');
+    assert.throws(() => classRegistry.getJSONClass('svg'), 'should not have registered class');
+    assert.equal(classRegistry.getJSONClass('Y'), ExportableClass, 'resolved class');
+    assert.equal(classRegistry.getSVGClass('Y'), ExportableClass, 'resolved class');
+    assert.throws(() => classRegistry.getJSONClass('X'), 'should not have registered class');
+    assert.throws(() => classRegistry.getSVGClass('X'), 'should not have registered class');
   });
 })()
