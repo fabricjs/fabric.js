7899a25877c33de4260c31bf0cc2ade4aaa947f6
ShaMan123 <shacharnen@gmail.com>
2022-12-22 13:36:57 +0200

    drag image visual tests

--- test/visual/text.js
+++ test/visual/text.js
@@ -491,5 +491,64 @@
     percentage: 0.092,
   });
 
+  function dragImage({ viewportTransform = fabric.iMatrix, retinaScaling = 1 }, canvas, callback) {
+    const text = new fabric.Textbox('lorem ipsum\ndolor\nsit Amet2\nconsectgetur',
+      { objectCaching: false, fontFamily: 'Arial', styles:
+        {0: {0: {fill: 'red',fontSize: 20},1: {fill: 'red',fontSize: 30},2: {fill: 'red',fontSize: 40},3: {fill: 'red',fontSize: 50},4: {fill: 'red',fontSize: 60},6: {textBackgroundColor: 'yellow'},7: {textBackgroundColor: 'yellow',textDecoration: ' line-through',linethrough: true},8: {textBackgroundColor: 'yellow',textDecoration: ' line-through',linethrough: true},9: {textBackgroundColor: 'yellow'}},1: {0: {textDecoration: 'underline'},1: {textDecoration: 'underline'},2: {fill: 'green',fontStyle: 'italic',textDecoration: 'underline'},3: {fill: 'green',fontStyle: 'italic',textDecoration: 'underline'},4: {fill: 'green',fontStyle: 'italic',textDecoration: 'underline'}},2: {0: {fill: 'blue',fontWeight: 'bold'},1: {fill: 'blue',fontWeight: 'bold'},2: {fill: 'blue',fontWeight: 'bold',fontSize: 63},4: {fontFamily: 'Courier',textDecoration: ' underline',underline: true},5: {fontFamily: 'Courier',textDecoration: ' underline',underline: true},6: {fontFamily: 'Courier',textDecoration: ' overline',overline: true},7: {fontFamily: 'Courier',textDecoration: ' overline',overline: true},8: {fontFamily: 'Courier',textDecoration: ' overline',overline: true}},3: {0: {fill: '#666',textDecoration: 'line-through'},1: {fill: '#666',textDecoration: 'line-through'},2: {fill: '#666',textDecoration: 'line-through'},3: {fill: '#666',textDecoration: 'line-through'},4: {fill: '#666',textDecoration: 'line-through'},7: {textDecoration: ' underline',underline: true},8: {stroke: '#ff1e15',strokeWidth: 2},9: {stroke: '#ff1e15',strokeWidth: 2}}}
+      }
+    );
+    canvas.add(text);
+    canvas.viewportTransform = viewportTransform;
+    const dragEventStub = {
+      clientX: 0,
+      clientY: 0,
+      dataTransfer: {
+        setDragImage(imageSource, x, y) {
+          callback(imageSource);
+        }
+      }
+    };
+    text.setDragImage(dragEventStub, {
+      selectionStart: 3,
+      selectionEnd: 20
+    });
+  }
+
+  tests.push({
+    test: 'Draggable text drag image',
+    code: dragImage.bind(null, {}),
+    disabled: fabric.isLikelyNode,
+    golden: 'drag_image.png',
+    percentage: 0.03,
+    fabricClass: 'Canvas'
+  });
+
+  tests.push({
+    test: 'Draggable text drag image + retina scaling',
+    code: dragImage.bind(null, { retinaScaling: 3 }),
+    disabled: fabric.isLikelyNode,
+    golden: 'drag_image.png',
+    percentage: 0.03,
+    fabricClass: 'Canvas'
+  });
+
+  tests.push({
+    test: 'Draggable text drag image + vpt',
+    code: dragImage.bind(null, { viewportTransform: [2, 0, 0, 1, 250, -250] }),
+    disabled: fabric.isLikelyNode,
+    golden: 'drag_image_vpt.png',
+    percentage: 0.03,
+    fabricClass: 'Canvas'
+  });
+
+  tests.push({
+    test: 'Draggable text drag image + vpt + retina',
+    code: dragImage.bind(null, { viewportTransform: [2, 0, 0, 1, 250, -250], retinaScaling: 1.25 }),
+    disabled: fabric.isLikelyNode,
+    golden: 'drag_image_vpt.png',
+    percentage: 0.03,
+    fabricClass: 'Canvas'
+  });
+
   tests.forEach(visualTestLoop(QUnit));
 })();
