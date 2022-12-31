f48c1d002e487bd4febd2550c6c7d65fc67b2b8c
Andrea Bogazzi <andreabogazzi79@gmail.com>
2022-12-31 01:16:57 +0100

    ok

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -11,14 +11,14 @@ export default {
   input: process.env.BUILD_INPUT?.split(splitter) || ['./src/index.ts'],
   output: [
     {
-      file: process.env.BUILD_OUTPUT || './bundle/fabric.js',
+      file: process.env.BUILD_OUTPUT || './dist/fabric.js',
       name: 'fabric',
       format: 'cjs',
       sourcemap: true,
     },
     Number(process.env.MINIFY)
       ? {
-          file: process.env.BUILD_MIN_OUTPUT || './bundle/fabric.min.js',
+          file: process.env.BUILD_MIN_OUTPUT || './dist/fabric.min.js',
           name: 'fabric',
           format: 'cjs',
           plugins: [
