6137e5e910e6ce42520b1b91f26482963319d956
ShaMan123 <shacharnen@gmail.com>
2023-01-01 11:18:34 +0200

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -14,14 +14,14 @@ export default {
     {
       file: process.env.BUILD_OUTPUT || './dist/fabric.js',
       name: 'fabric',
-      format: 'cjs',
+      format: 'umd',
       sourcemap: true,
     },
     Number(process.env.MINIFY)
       ? {
           file: process.env.BUILD_MIN_OUTPUT || './dist/fabric.min.js',
           name: 'fabric',
-          format: 'cjs',
+          format: 'umd',
           plugins: [
             runStats &&
               sizeSnapshot({
@@ -33,8 +33,5 @@ export default {
       : null,
   ],
   // see list of plugins (not comprehensive): https://github.com/rollup/awesome
-  plugins: [
-    json(),
-    ts({ tsconfig: './tsconfig.json' }),
-  ],
+  plugins: [json(), ts({ tsconfig: './tsconfig.json' })],
 };
