7d1b1999ecfc9936de23bfeb793706c65305ef7e
ShaMan123 <shacharnen@gmail.com>
2023-01-06 11:30:57 +0200

    Update index.ts

--- src/brushes/Eraser/index.ts
+++ src/brushes/Eraser/index.ts
@@ -1,11 +1,4 @@
-import { fabric } from '../../../HEADER';
-import { Eraser } from './Eraser';
-import { EraserBrush } from './EraserBrush';
-
 export * from './Eraser';
 export * from './EraserBrush';
 export * from './types';
 export * from './util';
-
-fabric.Eraser = Eraser;
-fabric.EraserBrush = EraserBrush;
