921b5f2734ec81e9620d61947d01b06eb0fd161b
ShaMan123 <shacharnen@gmail.com>
2023-01-09 14:18:03 +0200

    Update EventTypeDefs.ts

--- src/EventTypeDefs.ts
+++ src/EventTypeDefs.ts
@@ -134,10 +134,10 @@ export type TPointerEventInfo<E extends TPointerEvent = TPointerEvent> =
     currentTarget?: FabricObject | null;
   };
 
-type SimpleEventHandler<T extends Event = TPointerEvent> =
-  TEventWithTarget<T> & {
-    subTargets: FabricObject[];
-  };
+type SimpleEventHandler<T extends Event = TPointerEvent> = TEvent<T> & {
+  target?: FabricObject;
+  subTargets: FabricObject[];
+};
 
 type InEvent = {
   previousTarget?: FabricObject;
