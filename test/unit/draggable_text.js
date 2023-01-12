da6fc7ddb1b74f4193ca361fb2530e352d9f9cd3
ShaMan123 <shacharnen@gmail.com>
2023-01-12 14:57:16 +0200

    init tests

--- test/unit/draggable_text.js
+++ test/unit/draggable_text.js
@@ -1,4 +1,4 @@
-QUnit.module('draggable text', function (hooks) {
+(fabric.getEnv().isLikelyNode ? QUnit.module.skip : QUnit.module)('draggable text', function (hooks) {
     let canvas;
     hooks.before(function () {
         canvas = new fabric.Canvas(null, {
@@ -20,7 +20,7 @@ QUnit.module('draggable text', function (hooks) {
 
     [true, false].forEach(enableRetinaScaling => {
         QUnit.module(`enableRetinaScaling = ${enableRetinaScaling}`, function (hooks) {
-            let canvas, eventData, iText, iText2;
+            let canvas, eventData, iText, iText2, eventStream, renderEffects;
             let count = 0, countCanvas = 0;
             hooks.before(() => {
                 fabric.config.configure({ devicePixelRatio: 2 });
@@ -43,6 +43,7 @@ QUnit.module('draggable text', function (hooks) {
                     },
                     dataTransfer: {
                         data: {},
+                        dropEffect: 'none',
                         setData(type, value) {
                             this.data[type] = value;
                         },
@@ -58,77 +59,22 @@ QUnit.module('draggable text', function (hooks) {
                         clientY: 15
                     })
                 };
-                count = 0;
-                countCanvas = 0;
                 iText = new fabric.IText('test test');
                 iText2 = new fabric.IText('test2 test2', { left: 200 });
                 canvas.add(iText, iText2);
+                canvas.setActiveObject(iText);
+                iText.enterEditing();
+                iText.selectionStart = 0;
+                iText.selectionEnd = 4;
+                count = 0;
+                countCanvas = 0;
                 canvas.on('text:selection:changed', () => {
                     countCanvas++;
                 });
                 iText.on('selection:changed', () => {
                     count++;
                 });
-            });
-            hooks.afterEach(() => canvas.dispose());
-          
-            function startDragging() {
-              
-            }
-
-            QUnit.test('draggable: click', function (assert) {
-                iText.enterEditing();
-                iText.selectionStart = 0;
-                iText.selectionEnd = 4;
-                assert.equal(count, 1, 'selection:changed fired');
-                assert.equal(countCanvas, 1, 'text:selection:changed fired');
-                let called = 0;
-                // sinon spy!!
-                // iText.setCursorByClick = () => called++;
-                iText.fire('mousedown:before', {
-                    e: eventData,
-                    pointer: canvas.getPointer(eventData, true)
-                });
-                assert.ok(iText.__isDragging);
-                iText.fire('mousedown', {
-                    e: eventData,
-                    pointer: canvas.getPointer(eventData, true)
-                });
-                assert.ok(iText.__isDragging, 'flagged as dragging');
-                // assert.equal(called, 0, 'should not set cursor on mouse up');
-                iText.fire('mouseup', {
-                    e: eventData,
-                    pointer: canvas.getPointer(eventData, true),
-                    isClick: true
-                });
-                assert.ok(!iText.__isDragging, 'unflagged as dragging');
-                // assert.equal(called, 1, 'should set cursor on mouse up');
-                assert.equal(iText.selectionStart, 2, 'Itext set the selectionStart');
-                assert.equal(iText.selectionEnd, 2, 'Itext set the selectionend');
-                assertCursorAnimation(assert, iText, true);
-                assert.equal(count, 2, 'selection:changed fired');
-                assert.equal(countCanvas, 2, 'text:selection:changed fired');
-            });
-
-            QUnit.test('drag start', function (assert) {
-                canvas.setActiveObject(iText);
-                const renderEffects = [];
-                canvas._renderDragEffects = (e, source, target) => renderEffects.push({ e, source, target });
-                iText.enterEditing();
-                iText.selectionStart = 0;
-                iText.selectionEnd = 4;
-                assert.equal(count, 1, 'selection:changed fired');
-                assert.equal(countCanvas, 1, 'text:selection:changed fired');
-                iText.fire('mousedown:before', {
-                    e: eventData,
-                    pointer: canvas.getPointer(eventData, true)
-                });
-                assert.ok(iText.__isDragging);
-                iText.fire('mousedown', {
-                    e: eventData,
-                    pointer: canvas.getPointer(eventData, true)
-                });
-                const eventStream = {
+                eventStream = {
                     canvas: [],
                     source: [],
                     target: [],
@@ -144,28 +90,74 @@ QUnit.module('draggable text', function (hooks) {
                         eventStream.target.push({ ...ev, type });
                     });
                 });
-          
-                canvas._onDragStart(eventData);
+                renderEffects = [];
+                canvas._renderDragEffects = (e, source, target) => renderEffects.push({ e, source, target });
+            });
+            hooks.afterEach(() => canvas.dispose());
+            
+            function startDragging(eventData) {
+                const e = { ...eventData };
+                canvas._onMouseDown({ ...eventData });
+                canvas._onDragStart(e);
+                return e;
+            }
+
+            QUnit.test('click sets cursor', function (assert) {
+                assert.equal(count, 0, 'selection:changed fired');
+                assert.equal(countCanvas, 0, 'text:selection:changed fired');
+                let called = 0;
+                // sinon spy!!
+                // iText.setCursorByClick = () => called++;
+                canvas._onMouseDown(eventData);
+                assert.ok(iText.__isDragging, 'flagged as dragging');
+                // assert.equal(called, 0, 'should not set cursor on mouse up');
+                canvas._onMouseUp(eventData);
+                assert.ok(!iText.__isDragging, 'unflagged as dragging');
+                // assert.equal(called, 1, 'should set cursor on mouse up');
+                assert.equal(iText.selectionStart, 2, 'Itext set the selectionStart');
+                assert.equal(iText.selectionEnd, 2, 'Itext set the selectionend');
+                assertCursorAnimation(assert, iText, true);
+                assert.equal(count, 1, 'selection:changed fired');
+                assert.equal(countCanvas, 1, 'text:selection:changed fired');
+            });
+
+            QUnit.test('drag start', function (assert) {
+                const e = startDragging(eventData);
                 const charStyle = { "stroke": null, "strokeWidth": 1, "fill": "rgb(0,0,0)", "fontFamily": "Times New Roman", "fontSize": 40, "fontWeight": "normal", "fontStyle": "normal", "underline": false, "overline": false, "linethrough": false, "deltaY": 0, "textBackgroundColor": "" };
-                assert.deepEqual(eventData.dataTransfer.data, {
+                assert.deepEqual(e.dataTransfer.data, {
                     'application/fabric': JSON.stringify({
                         value: 'test',
                         styles: [charStyle, charStyle, charStyle, charStyle]
                     }),
                     'text/plain': "test"
                 }, 'should set dataTransfer');
-                assert.equal(eventData.dataTransfer.effectAllowed, 'copyMove', 'should set effectAllowed');
+                assert.equal(e.dataTransfer.effectAllowed, 'copyMove', 'should set effectAllowed');
                 // assert.equal(eventData.defaultPrevented, true, 'drag event default prevented');
-                assert.ok(eventData.dataTransfer.dragImageData.img instanceof HTMLCanvasElement, 'drag image was set');
-                assert.equal(eventData.dataTransfer.dragImageData.x, 30, 'drag image position');
-                assert.equal(eventData.dataTransfer.dragImageData.y, 15, 'drag image position');
-                assert.deepEqual(eventStream, {
-                    canvas: [{ target: iText, e: eventData }],
-                    source: [{ target: iText, e: eventData }],
-                    target: [],
-                }, 'drag start fired');
+                assert.ok(e.dataTransfer.dragImageData.img instanceof HTMLCanvasElement, 'drag image was set');
+                assert.equal(e.dataTransfer.dragImageData.x, 30, 'drag image position');
+                assert.equal(e.dataTransfer.dragImageData.y, 15, 'drag image position');
                 assert.deepEqual(renderEffects, [], 'not rendered effects yet');
-                const dragEvent = { ...eventData, defaultPrevented: false };
+                canvas._onDragEnd(eventData);
+                assert.deepEqual(eventStream.source, [
+                    {
+                        e,
+                        target: iText,
+                        type: 'dragstart'
+                    }, {
+                        e: eventData,
+                        target: iText,
+                        type: 'dragend',
+                        subTargets: [],
+                        dragSource: iText,
+                        dropTarget: undefined,
+                        didDrop: false
+                    }
+                ], 'events should match');
+                assert.deepEqual(eventStream.canvas, eventStream.source, 'events should match');
+            });
+
+            QUnit.test.skip('drag over', function (assert) {
+                const e = startDragging(eventData);
                 for (let index = 0; index < 350; index++) {
                     canvas._onDragOver({
                         ...eventData,
@@ -174,10 +166,10 @@ QUnit.module('draggable text', function (hooks) {
                     });
                 }
                 // canvas._onDrop(dragEvent);
-                canvas._onDragEnd(dragEvent);
+                // canvas._onDragEnd(dragEvent);
                 console.log(eventStream)
                 // assert.deepEqual(renderEffects, [{ e: eventData, source: iText, target: iText }]);
-            });
+            })
         });
     });
 });
