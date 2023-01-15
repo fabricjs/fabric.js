0456ecb18bda6775c3766d0e334e20cf0effb4ae
ShaMan123 <shacharnen@gmail.com>
2023-01-15 08:35:55 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -88,7 +88,6 @@ import {
   stylesToArray,
   hasStyleChanged,
 } from './src/util/misc/textStyles';
-import { clone, extend } from './src/util/lang_object';
 import {
   createCanvasElement,
   createImage,
@@ -194,10 +193,6 @@ const util = {
   stylesToArray,
   hasStyleChanged,
   getElementOffset,
-  object: {
-    clone,
-    extend,
-  },
   createCanvasElement,
   createImage,
   copyCanvasElement,
