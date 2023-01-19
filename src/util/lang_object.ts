70404a81b7661aa7f0b66fd51ceecd105f7a9656
Andrea Bogazzi <andreabogazzi79@gmail.com>
2023-01-19 13:17:16 +0100

    pause

--- src/util/lang_object.ts
+++ src/util/lang_object.ts
@@ -1,7 +1,3 @@
-//@ts-nocheck
-
-import { getEnv } from '../env';
-
 /**
  * Copies all enumerable properties of one js object to another
  * this does not and cannot compete with generic utils.
@@ -12,40 +8,15 @@ import { getEnv } from '../env';
  * @param {Object} source Where to copy from
  * @param {Boolean} [deep] Whether to extend nested objects
  * @return {Object}
+ * @deprecated
  */
-
-export const extend = (destination, source, deep) => {
+export const extend = (destination: Record<string, unknown>, source: Record<string, unknown>, deep: boolean) => {
   // the deep clone is for internal use, is not meant to avoid
   // javascript traps or cloning html element or self referenced objects.
   if (deep) {
-    if (!getEnv().isLikelyNode && source instanceof Element) {
-      // avoid cloning deep images, canvases,
-      destination = source;
-    } else if (Array.isArray(source)) {
-      destination = [];
-      for (let i = 0, len = source.length; i < len; i++) {
-        destination[i] = extend({}, source[i], deep);
-      }
-    } else if (source && typeof source === 'object') {
-      for (const property in source) {
-        if (property === 'canvas' || property === 'group') {
-          // we do not want to clone this props at all.
-          // we want to keep the keys in the copy
-          destination[property] = null;
-        } else if (Object.prototype.hasOwnProperty.call(source, property)) {
-          destination[property] = extend({}, source[property], deep);
-        }
-      }
-    } else {
-      // this sounds odd for an extend but is ok for recursive use
-      destination = source;
-    }
-  } else {
-    for (const property in source) {
-      destination[property] = source[property];
-    }
+    console.error('merge/extend deep is not supported');
   }
-  return destination;
+  return Object.assign(destination, source);
 };
 
 /**
@@ -54,7 +25,7 @@ export const extend = (destination, source, deep) => {
  * @param {Object} object Object to clone
  * @param {Boolean} [deep] Whether to clone nested objects
  * @return {Object}
+ * @deprecated
  */
-//TODO: this function return an empty object if you try to clone null
 export const clone = <T>(object: T, deep: boolean): T =>
-  deep ? extend({}, object, deep) : { ...object };
+  deep ? JSON.parse(JSON.stringify(object)) : { ...object };
