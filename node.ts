c18651c1de0ad89ca37fe96256d22d1986fd3792
ShaMan123 <shacharnen@gmail.com>
2023-01-31 11:47:43 +0200

    node stream methods

--- node.ts
+++ node.ts
@@ -1,3 +1,19 @@
 import './src/env/node';
 
+import type { Canvas as NodeCanvas, JpegConfig, PngConfig } from 'canvas';
+import { getEnv, StaticCanvas as Canvas } from './fabric';
+
 export * from './fabric';
+
+export class StaticCanvas extends Canvas {
+  getNodeCanvas() {
+    const impl = getEnv().jsdomImplForWrapper(this.lowerCanvasEl);
+    return (impl._canvas || impl._image) as NodeCanvas;
+  }
+  createPNGStream(opts?: PngConfig) {
+    return this.getNodeCanvas().createPNGStream(opts);
+  }
+  createJPEGStream(opts?: JpegConfig) {
+    return this.getNodeCanvas().createJPEGStream(opts);
+  }
+}
