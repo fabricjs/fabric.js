d2af7c73bdcea2a8d292c361e9ac83f2e97c90ee
ShaMan123 <shacharnen@gmail.com>
2023-02-01 00:10:28 +0200

    Update env.js

--- test/unit/env.js
+++ test/unit/env.js
@@ -1,18 +1,34 @@
 QUnit.module('env', (hooks) => {
-    QUnit[fabric.getEnv().isLikelyNode ? 'test' : 'skip']('SSR: importing fabric before window/document are defined', async assert => {
-        const done = assert.async();
-        const fabric = require('../..');
-        global.window = { devicePixelRatio: 1.25 };
-        global.document = { foo: 'bar' };
-        assert.deepEqual(fabric.getEnv(), {
-            document: { foo: 'bar' },
-            window: { devicePixelRatio: 1.25 },
-            isTouchSupported: undefined,
-            isLikelyNode: false,
-            copyPasteData: {}
-        }, 'env should match');
-        delete global.window;
-        delete global.document;
-        done();
+    (fabric.getEnv().isLikelyNode ? QUnit.module : QUnit.module.skip)('node', (hooks) => {
+        hooks.afterEach(() => {
+            delete global.window;
+            delete global.document;
+        })
+
+        QUnit.test('require sets env', async assert => {
+            const done = assert.async();
+            global.window = { devicePixelRatio: 1.25 };
+            global.document = { foo: 'bar' };
+            const fabric = require('../..');
+            assert.equal(fabric.getEnv().isLikelyNode, true, 'should be node env');
+            done();
+        });
+
+        QUnit.test('SSR: importing fabric before window/document are defined', async assert => {
+            const done = assert.async();
+            const fabric = require('../../dist/fabric.cjs');
+            assert.notOk(global.window, 'no window');
+            assert.notOk(global.document, 'no document');
+            global.window = { devicePixelRatio: 1.25 };
+            global.document = { foo: 'bar' };
+            assert.deepEqual(fabric.getEnv(), {
+                document: { foo: 'bar' },
+                window: { devicePixelRatio: 1.25 },
+                isTouchSupported: undefined,
+                isLikelyNode: false,
+                copyPasteData: {}
+            }, 'env should match');
+            done();
+        });
     });
-})
+});
