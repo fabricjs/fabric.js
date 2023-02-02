b9c81a6cece5316a4b666471bcf33f8c53ae1ed6
ShaMan123 <shacharnen@gmail.com>
2023-02-02 10:50:08 +0200

    enhance env tests

--- test/unit/env.js
+++ test/unit/env.js
@@ -5,30 +5,42 @@ QUnit.module('env', (hooks) => {
             delete global.document;
         })
 
-        QUnit.test('require sets env', async assert => {
+        QUnit.test('import/require sets env', async assert => {
             const done = assert.async();
             global.window = { devicePixelRatio: 1.25 };
             global.document = { foo: 'bar' };
-            const fabric = require('../..');
-            assert.equal(fabric.getEnv().isLikelyNode, true, 'should be node env');
+            const imported = await import('../../dist/fabric.node.cjs');
+            const required = require('../..');
+            assert.equal(imported.getEnv().isLikelyNode, true, 'should be node env');
+            assert.equal(required.getEnv().isLikelyNode, true, 'should be node env');
             done();
         });
 
         QUnit.test('SSR: importing fabric before window/document are defined', async assert => {
             const done = assert.async();
-            const fabric = require('../../dist/fabric.cjs');
+            const imported = await import('../../dist/fabric.cjs');
+            const required = require('../../dist/fabric.cjs');
             assert.notOk(global.window, 'no window');
             assert.notOk(global.document, 'no document');
             global.window = { devicePixelRatio: 1.25 };
             global.document = { foo: 'bar' };
-            assert.deepEqual(fabric.getEnv(), {
+            const expected = {
                 document: { foo: 'bar' },
                 window: { devicePixelRatio: 1.25 },
                 isTouchSupported: undefined,
                 isLikelyNode: false,
                 copyPasteData: {}
-            }, 'env should match');
+            };
+            assert.deepEqual(imported.getEnv(), expected, 'env should match');
+            assert.deepEqual(required.getEnv(), expected, 'env should match');
             done();
         });
+    });
+
+    (!fabric.getEnv().isLikelyNode ? QUnit.module : QUnit.module.skip)('browser', (hooks) => { 
+        QUnit.test('env', assert => {
+            assert.equal(fabric.getWindow(), window, 'window should be set');
+            assert.equal(fabric.getDocument(), document, 'window should be set');
+        })
     });
 });
