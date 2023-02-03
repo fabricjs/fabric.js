3918cdcc5c49dc5b1af29a8271b215c3ecdf440c
ShaMan123 <shacharnen@gmail.com>
2023-02-03 23:30:44 +0200

    Update index.ts

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,3 +1,11 @@
+/**
+ * This file is consumed by fabric.
+ * The `./node` and `./browser` files define the environment object that is used by this module.
+ * The `./node` module sets the env at import time.
+ * The `./browser` module doesn't set the env and is defined to be the default env.
+ * This is done in order to support SSR, where at time of import setting env will be wrong since window and document aren't defined.
+ */
+
 import { TFabricEnv } from './types';
 import { getEnv as getBrowserEnv } from './browser';
 
