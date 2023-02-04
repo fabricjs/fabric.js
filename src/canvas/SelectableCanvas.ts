9d3b47ba5d1e53fd9ca637cdce1df90c4a8e61e3
ShaMan123 <shacharnen@gmail.com>
2023-02-05 01:01:15 +0200

    setVpt + brush.clipPath

--- src/canvas/SelectableCanvas.ts
+++ src/canvas/SelectableCanvas.ts
@@ -1480,6 +1480,7 @@ export class SelectableCanvas<
     if (this.isCurrentlyDrawing()) {
       // force brush to redraw
       this.shouldClearContextTop = true;
+      this.freeDrawingBrush!.clipPath?.setCoords();
     }
     super.setViewportTransform(vpt);
     const activeObject = this._activeObject;
