b5805e9307eccc3b8ee71299fd8cb08cd93a07ee
ShaMan123 <shacharnen@gmail.com>
2023-02-04 02:55:20 +0200

    Update dom_misc.ts

--- src/util/dom_misc.ts
+++ src/util/dom_misc.ts
@@ -1,6 +1,4 @@
-//@ts-nocheck
-
-import { getDocument, getEnv } from '../env';
+import { getDocument } from '../env';
 
 /**
  * Wraps element with another element
@@ -9,7 +7,7 @@ import { getDocument, getEnv } from '../env';
  * @param {Object} [attributes] Attributes to set on a wrapper
  * @return {HTMLElement} wrapper
  */
-export function wrapElement(element, wrapper) {
+export function wrapElement(element: HTMLElement, wrapper: HTMLElement) {
   if (element.parentNode) {
     element.parentNode.replaceChild(wrapper, element);
   }
@@ -22,7 +20,7 @@ export function wrapElement(element, wrapper) {
  * @param {HTMLElement} element Element to operate on
  * @return {Object} Object with left/top values
  */
-export function getScrollLeftTop(element) {
+export function getScrollLeftTop(element: HTMLElement) {
   let left = 0,
     top = 0;
 
@@ -60,7 +58,7 @@ export function getScrollLeftTop(element) {
  * @param {HTMLElement} element Element to get offset for
  * @return {Object} Object with "left" and "top" properties
  */
-export function getElementOffset(element) {
+export function getElementOffset(element: HTMLElement) {
   let box = { left: 0, top: 0 };
   const doc = element && element.ownerDocument,
     offset = { left: 0, top: 0 },
@@ -69,13 +67,14 @@ export function getElementOffset(element) {
       borderTopWidth: 'top',
       paddingLeft: 'left',
       paddingTop: 'top',
-    };
+    } as const;
 
   if (!doc) {
     return offset;
   }
-  const elemStyle = getDocument().defaultView.getComputedStyle(element, null);
+  const elemStyle = getDocument().defaultView!.getComputedStyle(element, null);
   for (const attr in offsetAttributes) {
+    // @ts-expect-error TS learn to iterate!
     offset[offsetAttributes[attr]] += parseInt(elemStyle[attr], 10) || 0;
   }
 
@@ -98,7 +97,7 @@ export function getElementOffset(element) {
  * @param {HTMLElement} element Element to make unselectable
  * @return {HTMLElement} Element that was passed in
  */
-export function makeElementUnselectable(element) {
+export function makeElementUnselectable(element: HTMLElement) {
   if (typeof element.onselectstart !== 'undefined') {
     element.onselectstart = () => false;
   }
@@ -111,7 +110,7 @@ export function makeElementUnselectable(element) {
  * @param {HTMLElement} element Element to make selectable
  * @return {HTMLElement} Element that was passed in
  */
-export function makeElementSelectable(element) {
+export function makeElementSelectable(element: HTMLElement) {
   if (typeof element.onselectstart !== 'undefined') {
     element.onselectstart = null;
   }
