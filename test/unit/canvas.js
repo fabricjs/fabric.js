1f84c90f995d56fa1b9ba390ebac980f1a623272
ShaMan123 <shacharnen@gmail.com>
2023-02-06 01:38:44 +0200

    Update canvas.js

--- test/unit/canvas.js
+++ test/unit/canvas.js
@@ -1938,31 +1938,6 @@
     assert.equal(svg, svgWithActiveGroup);
   });
 
-  QUnit.test('active group objects reordering', function(assert) {
-    var rect1 = new fabric.Rect({ width: 30, height: 30, left: 130, top: 130 });
-    var rect2 = new fabric.Rect({ width: 50, height: 50, left: 100, top: 100 });
-    var circle1 = new fabric.Circle({ radius: 10, left: 60, top: 60 });
-    var circle2 = new fabric.Circle({ radius: 50, left: 50, top: 50 });
-    canvas.add(rect1, rect2, circle1, circle2);
-    assert.equal(canvas._objects[0], rect1);
-    assert.equal(canvas._objects[1], rect2);
-    assert.equal(canvas._objects[2], circle1);
-    assert.equal(canvas._objects[3], circle2);
-    var aGroup = new fabric.ActiveSelection([rect2, circle2, rect1, circle1], { canvas: canvas });
-    // before rendering objects are ordered in insert order
-    assert.equal(aGroup._objects[0], rect2);
-    assert.equal(aGroup._objects[1], circle2);
-    assert.equal(aGroup._objects[2], rect1);
-    assert.equal(aGroup._objects[3], circle1);
-    canvas.setActiveObject(aGroup)
-    canvas.renderAll();
-    // after rendering objects are ordered in canvas stack order
-    assert.equal(aGroup._objects[0], rect1);
-    assert.equal(aGroup._objects[1], rect2);
-    assert.equal(aGroup._objects[2], circle1);
-    assert.equal(aGroup._objects[3], circle2);
-  });
-
   [true, false].forEach(enableRetinaScaling => {
     QUnit.test(`set dimensions, enableRetinaScaling ${enableRetinaScaling}`, async function (assert) {
       var el = fabric.getDocument().createElement('canvas'),
