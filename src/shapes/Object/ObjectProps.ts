17a0396b44ac2f178ce34d02c246db1032cf5bbf
ShaMan123 <shacharnen@gmail.com>
2023-02-08 14:43:41 +0200

    extract the rest

--- src/shapes/Object/ObjectProps.ts
+++ src/shapes/Object/ObjectProps.ts
@@ -1,8 +1,130 @@
 import { Shadow } from '../../Shadow';
-import { TDegree, TFiller } from '../../typedefs';
+import { TDegree, TFiller, TOriginX, TOriginY } from '../../typedefs';
 import { FabricObject } from './Object';
 
-export interface ObjectProps {
+export interface ObjectBaseProps {
+  /**
+   * Top position of an object. Note that by default it's relative to object top. You can change this by setting originY={top/center/bottom}
+   * @type Number
+   * @default 0
+   */
+  top: number;
+
+  /**
+   * Left position of an object. Note that by default it's relative to object left. You can change this by setting originX={left/center/right}
+   * @type Number
+   * @default 0
+   */
+  left: number;
+
+  /**
+   * Object width
+   * @type Number
+   * @default
+   */
+  width: number;
+
+  /**
+   * Object height
+   * @type Number
+   * @default
+   */
+  height: number;
+
+  /**
+   * When true, an object is rendered as flipped horizontally
+   * @type Boolean
+   * @default false
+   */
+  flipX: boolean;
+
+  /**
+   * When true, an object is rendered as flipped vertically
+   * @type Boolean
+   * @default false
+   */
+  flipY: boolean;
+
+  /**
+   * Object scale factor (horizontal)
+   * @type Number
+   * @default 1
+   */
+  scaleX: number;
+
+  /**
+   * Object scale factor (vertical)
+   * @type Number
+   * @default 1
+   */
+  scaleY: number;
+
+  /**
+   * Angle of skew on x axes of an object (in degrees)
+   * @type Number
+   * @default 0
+   */
+  skewX: number;
+
+  /**
+   * Angle of skew on y axes of an object (in degrees)
+   * @type Number
+   * @default 0
+   */
+  skewY: number;
+
+  /**
+   * Horizontal origin of transformation of an object (one of "left", "right", "center")
+   * See http://jsfiddle.net/1ow02gea/244/ on how originX/originY affect objects in groups
+   * @type String
+   * @default 'left'
+   */
+  originX: TOriginX;
+
+  /**
+   * Vertical origin of transformation of an object (one of "top", "bottom", "center")
+   * See http://jsfiddle.net/1ow02gea/244/ on how originX/originY affect objects in groups
+   * @type String
+   * @default 'top'
+   */
+  originY: TOriginY;
+
+  /**
+   * Angle of rotation of an object (in degrees)
+   * @type Number
+   * @default 0
+   */
+  angle: TDegree;
+
+  /**
+   * Width of a stroke used to render this object
+   * @type Number
+   * @default 1
+   */
+  strokeWidth: number;
+
+  /**
+   * When `false`, the stoke width will scale with the object.
+   * When `true`, the stroke will always match the exact pixel size entered for stroke width.
+   * this Property does not work on Text classes or drawing call that uses strokeText,fillText methods
+   * default to false
+   * @since 2.6.0
+   * @type Boolean
+   * @default false
+   * @type Boolean
+   * @default false
+   */
+  strokeUniform: boolean;
+
+  /**
+   * Padding between object and its controlling borders (in pixels)
+   * @type Number
+   * @default 0
+   */
+  padding: number;
+}
+
+export interface ObjectProps extends ObjectBaseProps {
   /**
    * Opacity of an object
    * @type Number
