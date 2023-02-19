552f12ed7d464786b97cf56b3bbf2bc4a7661085
ShaMan123 <shacharnen@gmail.com>
2023-02-19 15:47:54 +0200

    Update constants.ts

--- src/shapes/IText/constants.ts
+++ src/shapes/IText/constants.ts
@@ -1,38 +1,45 @@
 import type { IText } from './IText';
-export type TKeyMapIText = Record<KeyboardEvent['keyCode'], keyof IText>;
+export type TKeyMapIText = Record<
+  KeyboardEvent['key'],
+  keyof IText | ((this: IText, e: KeyboardEvent) => any)
+>;
 
 // @TODO look into import { Key } from 'ts-key-enum';
-// and transition from keyCode to Key
-// also reduce string duplication
+/**
+ * For functionalities on keyDown when `direction === 'ltr'`
+ */
 export const keysMap: TKeyMapIText = {
-  9: 'exitEditing',
-  27: 'exitEditing',
-  33: 'moveCursorUp',
-  34: 'moveCursorDown',
-  35: 'moveCursorRight',
-  36: 'moveCursorLeft',
-  37: 'moveCursorLeft',
-  38: 'moveCursorUp',
-  39: 'moveCursorRight',
-  40: 'moveCursorDown',
+  Tab: 'exitEditing',
+  Escape: 'exitEditing',
+  PageUp: 'moveCursorUp',
+  PageDown: 'moveCursorDown',
+  End: 'moveCursorRight',
+  Home: 'moveCursorLeft',
+  ArrowLeft: 'moveCursorLeft',
+  ArrowUp: 'moveCursorUp',
+  ArrowRight: 'moveCursorRight',
+  ArrowDown: 'moveCursorDown',
 };
 
+/**
+ * For functionalities on keyDown when `direction === 'rtl'`
+ */
 export const keysMapRtl: TKeyMapIText = {
-  9: 'exitEditing',
-  27: 'exitEditing',
-  33: 'moveCursorUp',
-  34: 'moveCursorDown',
-  35: 'moveCursorLeft',
-  36: 'moveCursorRight',
-  37: 'moveCursorRight',
-  38: 'moveCursorUp',
-  39: 'moveCursorLeft',
-  40: 'moveCursorDown',
+  Tab: 'exitEditing',
+  Escape: 'exitEditing',
+  PageUp: 'moveCursorUp',
+  PageDown: 'moveCursorDown',
+  End: 'moveCursorLeft',
+  Home: 'moveCursorRight',
+  ArrowLeft: 'moveCursorRight',
+  ArrowUp: 'moveCursorUp',
+  ArrowRight: 'moveCursorLeft',
+  ArrowDown: 'moveCursorDown',
 };
 
 /**
  * For functionalities on keyDown + ctrl || cmd
  */
 export const ctrlKeysMapDown: TKeyMapIText = {
-  65: 'selectAll',
+  KeyA: 'selectAll',
 };
