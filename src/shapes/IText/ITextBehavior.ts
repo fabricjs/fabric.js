fabricjs/fabric.js

commit 9258628b239bfefbfe39afec9a2962b87328c81a

chore(TS): Remove stateful mixin (#8663)

src/shapes/Text/Text.ts
@@ -79,6 +79,22 @@ const additionalProps = [
   'pathAlign',
 ] as const;
 
+const textLayoutProperties: string[] = [
+  'fontSize',
+  'fontWeight',
+  'fontFamily',
+  'fontStyle',
+  'lineHeight',
+  'text',
+  'charSpacing',
+  'textAlign',
+  'styles',
+  'path',
+  'pathStartOffset',
+  'pathSide',
+  'pathAlign',
+];
+
 /**
  * Text class
  * @tutorial {@link http://fabricjs.com/fabric-intro-part-2#text}
@@ -87,11 +103,11 @@ export class Text<
   EventSpec extends ObjectEvents = ObjectEvents
 > extends StyledText<EventSpec> {
   /**
-   * Properties which when set cause object to change dimensions
-   * @type Array
-   * @private
+   * Properties that requires a text layout recalculation when changed
+   * @type string[]
+   * @protected
    */
-  declare _dimensionAffectingProps: string[];
+  static textLayoutProperties: string[] = textLayoutProperties;
 
   /**
    * @private
@@ -376,7 +392,6 @@ export class Text<
     }
     this.initDimensions();
     this.setCoords();
-    this.saveState({ propertySet: '_dimensionAffectingProps' });
   }
 
   /**
@@ -423,7 +438,6 @@ export class Text<
       // once text is measured we need to make space fatter to make justified text.
       this.enlargeSpaces();
     }
-    this.saveState({ propertySet: '_dimensionAffectingProps' });
   }
 
   /**
@@ -1431,8 +1445,7 @@ export class Text<
    * @private
    */
   _shouldClearDimensionCache() {
-    const shouldClear =
-      this._forceClearCache || this.hasStateChanged('_dimensionAffectingProps');
+    const shouldClear = this._forceClearCache;
     if (shouldClear) {
       this.dirty = true;
       this._forceClearCache = false;
@@ -1690,11 +1703,11 @@ export class Text<
           this.setPathInfo();
         }
         needsDims =
-          needsDims || this._dimensionAffectingProps.indexOf(_key) !== -1;
+          needsDims || this.constructor.textLayoutProperties.includes(_key);
         isAddingPath = isAddingPath || _key === 'path';
       }
     } else {
-      needsDims = this._dimensionAffectingProps.indexOf(key) !== -1;
+      needsDims = this.constructor.textLayoutProperties.includes(key);
       isAddingPath = key === 'path';
     }
     if (isAddingPath) {
@@ -1867,21 +1880,6 @@ export class Text<
 // regexes, list of properties that are not suppose to change by instances, magic consts.
 // this will be a separated effort
 export const textDefaultValues: Partial<TClassProperties<Text>> = {
-  _dimensionAffectingProps: [
-    'fontSize',
-    'fontWeight',
-    'fontFamily',
-    'fontStyle',
-    'lineHeight',
-    'text',
-    'charSpacing',
-    'textAlign',
-    'styles',
-    'path',
-    'pathStartOffset',
-    'pathSide',
-    'pathAlign',
-  ],
   _styleProperties: [
     'stroke',
     'strokeWidth',

.prettierignore
@@ -14,3 +14,4 @@ before_commit
 /dist/
 /lib/
 /test/
+/docs/

src/mixins/stateful.mixin.ts
@@ -1,104 +1,0 @@
-// @ts-nocheck
-import type { FabricObject } from '../shapes/Object/Object';
-import { cloneDeep } from '../util/internals/cloneDeep';
-
-const originalSet = 'stateProperties';
-
-/*
- * This function was taking care of running isEqual over statePropertie.
- * State properties included things that could reference a canvas or a group.
- * FabricJS does not support stateful saving anymore apart for a very small number of props for
- * Text cache invalidation temporarly.
- * So you shouldn't use saveState or hasStateChanged for your own application
- * @depreacted
- */
-function _isEqual(origValue: any, currentValue: any, firstPass = false) {
-  if (origValue === currentValue) {
-    // if the objects are identical, return
-    return true;
-  } else if (Array.isArray(origValue)) {
-    if (
-      !Array.isArray(currentValue) ||
-      origValue.length !== currentValue.length
-    ) {
-      return false;
-    }
-    for (let i = 0, len = origValue.length; i < len; i++) {
-      if (!_isEqual(origValue[i], currentValue[i])) {
-        return false;
-      }
-    }
-    return true;
-  } else if (origValue && typeof origValue === 'object') {
-    const keys = Object.keys(origValue);
-    if (
-      !currentValue ||
-      typeof currentValue !== 'object' ||
-      (!firstPass && keys.length !== Object.keys(currentValue).length)
-    ) {
-      return false;
-    }
-    for (let i = 0, len = keys.length; i < len; i++) {
-      const key = keys[i];
-      // since clipPath is in the statefull cache list and the clipPath objects
-      // would be iterated as an object, this would lead to possible infinite recursion
-      // we do not want to compare those.
-      if (key === 'canvas' || key === 'group') {
-        continue;
-      }
-      if (!_isEqual(origValue[key], currentValue[key])) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
-
-type TSaveStateOptions = {
-  propertySet: string;
-  stateProperties: string[];
-};
-
-export class StatefulMixin {
-  /**
-   * Returns true if object state (one of its state properties) was changed
-   * @param {String} [propertySet] optional name for the set of property we want to save
-   * @return {Boolean} true if instance' state has changed since `{@link fabric.Object#saveState}` was called
-   */
-  private hasStateChanged(propertySet: string = originalSet): boolean {
-    const dashedPropertySet = `_${propertySet}`;
-    if (
-      Object.keys(this[dashedPropertySet] || {}).length <
-      this[propertySet].length
-    ) {
-      return true;
-    }
-    return !_isEqual(this[dashedPropertySet], this, true);
-  }
-
-  private saveProps(destination: string, props: (keyof FabricObject)[]) {
-    props.reduce((o, key) => {
-      o[key] = this[key] ? cloneDeep(this[key]) : this[key];
-      return o;
-    }, this[destination]);
-  }
-
-  /**
-   * Saves state of an object
-   * @param {Object} [options] Object with additional `stateProperties` array to include when saving state
-   * @param {string[]} [options.stateProperties] Object with additional `stateProperties` array to include when saving state
-   * @param {string} [options.propertySet] name for the property set to save
-   * @return {fabric.Object} thisArg
-   */
-  private saveState(
-    this: FabricObject,
-    { propertySet = originalSet }: TSaveStateOptions = {}
-  ): FabricObject {
-    const destination = `_${propertySet}`;
-    if (!this[destination]) {
-      this[destination] = {};
-    }
-    this.saveProps(destination, this[propertySet]);
-    return this;
-  }
-}

src/shapes/IText/IText.ts
@@ -9,6 +9,7 @@ import {
 } from './constants';
 import { AssertKeys, TFiller } from '../../typedefs';
 import { classRegistry } from '../../util/class_registry';
+import { Text } from '../Text/Text';
 
 type CursorBoundaries = {
   left: number;

src/shapes/Object/FabricObject.ts
@@ -2,7 +2,6 @@ import { ObjectEvents } from '../../EventTypeDefs';
 import { FabricObjectSVGExportMixin } from './FabricObjectSVGExportMixin';
 import { InteractiveFabricObject } from './InteractiveObject';
 import { applyMixins } from '../../util/applyMixins';
-import { StatefulMixin } from '../../mixins/stateful.mixin';
 
 // TODO somehow we have to make a tree-shakeable import
 
@@ -14,6 +13,6 @@ export class FabricObject<
   EventSpec extends ObjectEvents = ObjectEvents
 > extends InteractiveFabricObject<EventSpec> {}
 
-applyMixins(FabricObject, [FabricObjectSVGExportMixin, StatefulMixin]);
+applyMixins(FabricObject, [FabricObjectSVGExportMixin]);
 
 export { cacheProperties, stateProperties } from './defaultValues';

src/shapes/Object/Object.ts
@@ -995,7 +995,8 @@ export class FabricObject<
   }
 
   /**
-   * @private
+   * Handles setting values on the instance and handling internal side effects
+   * @protected
    * @param {String} key
    * @param {*} value
    */

src/shapes/Textbox.ts
@@ -1,7 +1,6 @@
 // @ts-nocheck
 import { TClassProperties } from '../typedefs';
 import { IText } from './IText/IText';
-import { textDefaultValues } from './Text/Text';
 import { classRegistry } from '../util/class_registry';
 
 /**
@@ -35,6 +34,8 @@ export class Textbox extends IText {
    */
   declare splitByGrapheme: boolean;
 
+  static textLayoutProperties = [...IText.textLayoutProperties, 'width'];
+
   /**
    * Unlike superclass's version of this function, Textbox does not update
    * its width.
@@ -61,7 +62,6 @@ export class Textbox extends IText {
     }
     // clear cache and re-calculate height
     this.height = this.calcTextHeight();
-    this.saveState({ propertySet: '_dimensionAffectingProps' });
   }
 
   /**
@@ -460,8 +460,6 @@ export const textboxDefaultValues: Partial<TClassProperties<Textbox>> = {
   dynamicMinWidth: 2,
   lockScalingFlip: true,
   noScaleCache: false,
-  _dimensionAffectingProps:
-    textDefaultValues._dimensionAffectingProps!.concat('width'),
   _wordJoiners: /[ \t\r]/,
   splitByGrapheme: false,
 };

test/unit/stateful.js
@@ -1,128 +1,0 @@
-(function(){
-
-  QUnit.module('fabric.stateful');
-  QUnit.test('hasStateChanged', function(assert) {
-    var cObj = new fabric.Object();
-    assert.ok(typeof cObj.hasStateChanged === 'function');
-    cObj.saveState();
-    assert.ok(!cObj.hasStateChanged(), 'state should not be changed');
-    cObj.saveState();
-    cObj.set('left', 123).set('top', 456);
-    assert.ok(cObj.hasStateChanged(), 'state should be changed now');
-  });
-
-  QUnit.test('saveState', function(assert) {
-    var cObj = new fabric.Object();
-    assert.ok(typeof cObj.saveState === 'function');
-    cObj.saveState();
-    assert.equal(cObj.saveState(), cObj, 'chainable');
-    cObj.set('left', 123).set('top', 456);
-    cObj.saveState();
-    cObj.set('left', 223).set('top', 556);
-    assert.equal(cObj._stateProperties.left, 123);
-    assert.equal(cObj._stateProperties.top, 456);
-  });
-
-  QUnit.test('saveState with array', function(assert) {
-    var cObj = new fabric.Text('Hello');
-    cObj.set('strokeDashArray', [0, 4]);
-    cObj.saveState({ propertySet: 'cacheProperties' });
-    //equal(cObj.underline, cObj._stateProperties.underline, 'textDecoration in state is deepEqual');
-    //notEqual(cObj.textDecoration, cObj._stateProperties.textDecoration, 'textDecoration in not same Object');
-    cObj.strokeDashArray[0] = 2;
-    assert.ok(cObj.hasStateChanged('cacheProperties'), 'hasStateChanged detects changes in nested props');
-
-    cObj.saveState();
-    cObj.strokeDashArray[2] = 2;
-    assert.ok(cObj.hasStateChanged('cacheProperties'), 'more properties added');
-  });
-
-  QUnit.test('saveState with array to null', function(assert) {
-    var cObj = new fabric.Text('Hello');
-    cObj.set('strokeDashArray', [0, 4]);
-    cObj.saveState({ propertySet: 'cacheProperties' });
-    //equal(cObj.underline, cObj._stateProperties.underline, 'textDecoration in state is deepEqual');
-    //notEqual(cObj.textDecoration, cObj._stateProperties.textDecoration, 'textDecoration in not same Object');
-    cObj.strokeDashArray = null;
-    assert.ok(cObj.hasStateChanged('cacheProperties'), 'hasStateChanged detects changes in array without throwing');
-
-    cObj.saveState({ propertySet: 'cacheProperties' });
-    cObj.strokeDashArray = [2, 3];
-    assert.ok(cObj.hasStateChanged('cacheProperties'), 'back to array');
-  });
-
-  QUnit.test('saveState with fabric class gradient', function(assert) {
-    var cObj = new fabric.Object();
-    var gradient = new fabric.Gradient({
-      type: 'linear',
-      coords: {
-        x1: 0,
-        y1: 10,
-        x2: 100,
-        y2: 200,
-      },
-      colorStops: [
-        { offset: 0, color: 'red', opacity: 0 },
-        { offset: 1, color: 'green' }
-      ]
-    });
-
-    cObj.set('fill', '#FF0000');
-    cObj.saveState({ propertySet: 'cacheProperties' });
-    cObj.set('fill', gradient);
-    assert.ok(cObj.hasStateChanged('cacheProperties'), 'hasStateChanged detects changes in nested props');
-    cObj.saveState();
-    gradient.type = 'radial';
-    assert.ok(cObj.hasStateChanged('cacheProperties'), 'hasStateChanged detects changes in nested props on first level of nesting');
-    cObj.saveState();
-    gradient.coords.x1 = 3;
-    assert.ok(cObj.hasStateChanged('cacheProperties'), 'hasStateChanged detects changes in nested props on second level of nesting');
-    cObj.saveState();
-    gradient.colorStops[0].color = 'blue';
-    assert.ok(cObj.hasStateChanged('cacheProperties'), 'hasStateChanged detects changes in nested props on third level of nesting');
-  });
-
-  QUnit.test('saveState with fabric class gradient to other types', function(assert) {
-    var cObj = new fabric.Object();
-    var gradient = new fabric.Gradient({
-      type: 'linear',
-      coords: {
-        x1: 0,
-        y1: 10,
-        x2: 100,
-        y2: 200,
-      },
-      colorStops: [
-        { offset: 0, color: 'red', opacity: 0 },
-        { offset: 1, color: 'green' }
-      ]
-    });
-
-    cObj.set('fill', gradient);
-    cObj.saveState({ propertySet: 'cacheProperties' });
-    cObj.set('fill', 'red');
-    assert.ok(cObj.hasStateChanged('cacheProperties'), 'hasStateChanged detects changes in object to string without throwing');
-    cObj.saveState({ propertySet: 'cacheProperties' });
-    cObj.set('fill', gradient);
-    assert.ok(cObj.hasStateChanged('cacheProperties'), 'back to gradient');
-    cObj.saveState({ propertySet: 'cacheProperties' });
-    cObj.set('fill', null);
-    assert.ok(cObj.hasStateChanged('cacheProperties'), 'hasStateChanged detects changes in object to null without throwing');
-  });
-
-  QUnit.test('savestate with custom property set', function(assert) {
-    var cObj = new fabric.Object();
-    cObj.myProperties = ['a', 'b'];
-    cObj.a = 1;
-    cObj.b = 3;
-    cObj.saveState();
-    assert.ok(!cObj._myProperties, 'custom properties set does not exist');
-    cObj.saveState({ propertySet: 'myProperties' });
-    assert.ok(cObj._myProperties.a, 'a has been added in the custom property set');
-    cObj.left = 33;
-    assert.ok(cObj.hasStateChanged(), 'state has changed');
-    assert.ok(!cObj.hasStateChanged('myProperties'), 'custom state has not changed');
-    cObj.a = 2;
-    assert.ok(cObj.hasStateChanged('myProperties'), 'custom state has changed');
-  });
-})();

commit b8a4ffceada4733b500cab75a635fbef843664e8

fix(IText): layout regression
caused by #8663

src/shapes/IText/ITextBehavior.ts
@@ -499,8 +499,9 @@ export abstract class ITextBehavior<
       return;
     }
     this.cursorOffsetCache = {};
+    const prevText = this.text;
     this.text = this.hiddenTextarea.value;
-    if (this._shouldClearDimensionCache()) {
+    if (this._forceClearCache || prevText !== this.text) {
       this.initDimensions();
       this.setCoords();
     }
@@ -657,7 +658,7 @@ export abstract class ITextBehavior<
     this.selectionEnd = this.selectionStart;
     this._exitEditing();
     this._restoreEditingProps();
-    if (this._shouldClearDimensionCache()) {
+    if (this._forceClearCache) {
       this.initDimensions();
       this.setCoords();
     }
@@ -974,7 +975,7 @@ export abstract class ITextBehavior<
     this._text.splice(start, end - start);
     this.text = this._text.join('');
     this.set('dirty', true);
-    if (this._shouldClearDimensionCache()) {
+    if (this._forceClearCache || start !== end) {
       this.initDimensions();
       this.setCoords();
     }
@@ -1011,7 +1012,7 @@ export abstract class ITextBehavior<
     ];
     this.text = this._text.join('');
     this.set('dirty', true);
-    if (this._shouldClearDimensionCache()) {
+    if (this._forceClearCache || start !== end || text.length > 0) {
       this.initDimensions();
       this.setCoords();
     }

src/shapes/Text/StyledText.ts
@@ -12,7 +12,7 @@ export abstract class StyledText<
 > extends FabricObject<EventSpec> {
   declare abstract styles: TextStyle;
   protected declare abstract _textLines: string[][];
-  protected declare abstract _forceClearCache: boolean;
+  protected declare _forceClearCache: boolean;
   protected declare abstract _styleProperties: string[];
   abstract get2DCursorLocation(
     selectionStart: number,

src/shapes/Text/Text.ts
@@ -380,7 +380,6 @@ export class Text<
   declare cursorWidth: number;
   declare __lineHeights: number[];
   declare __lineWidths: number[];
-  declare _forceClearCache: boolean;
 
   declare initialized?: true;
 
@@ -428,6 +427,7 @@ export class Text<
   initDimensions() {
     this._splitText();
     this._clearCache();
+    this.dirty = true;
     if (this.path) {
       this.width = this.path.width;
       this.height = this.path.height;
@@ -1438,21 +1438,10 @@ export class Text<
    * @private
    */
   _clearCache() {
+    this._forceClearCache = false;
     this.__lineWidths = [];
     this.__lineHeights = [];
     this.__charBounds = [];
-  }
-
-  /**
-   * @private
-   */
-  _shouldClearDimensionCache() {
-    const shouldClear = this._forceClearCache;
-    if (shouldClear) {
-      this.dirty = true;
-      this._forceClearCache = false;
-    }
-    return shouldClear;
   }
 
   /**
@@ -1641,7 +1630,7 @@ export class Text<
     ) {
       return;
     }
-    if (this._shouldClearDimensionCache()) {
+    if (this._forceClearCache) {
       this.initDimensions();
     }
     super.render(ctx);