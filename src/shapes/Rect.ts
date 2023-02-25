5bd8f01d289009c9819fdb13a596a3f8f7fddde3
Andrea Bogazzi <andreabogazzi79@gmail.com>
2023-02-25 17:41:17 +0100

    removed debug statement

--- src/shapes/Rect.ts
+++ src/shapes/Rect.ts
@@ -194,15 +194,6 @@ export class Rect extends FabricObject {
       height,
       visible: Boolean(visible && width && height),
     });
-    console.log({
-      ...options,
-      ...restOfparsedAttributes,
-      left,
-      top,
-      width,
-      height,
-      visible: Boolean(visible && width && height),
-    });
     callback(rect);
   }
 
