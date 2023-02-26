076959420613e1b020abe32ce700e9cd0771b9b1
ShaMan123 <shacharnen@gmail.com>
2023-02-26 13:27:37 +0200

    Update karma.conf.js

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,3 +1,4 @@
-module.exports = function(config) {
+// next major karma migrates to esm so this file will be redundant
+module.exports = function (config) {
     return import('./karma.config.mjs').then(mod => mod.default(config));
 }
