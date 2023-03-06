ea4436112d10b6ef5cc12f5a7cea7d134b389f7b
ShaMan123 <shacharnen@gmail.com>
2023-03-06 13:26:37 +0200

    lint(): fix errors

--- src/filters/BaseFilter.ts
+++ src/filters/BaseFilter.ts
@@ -259,7 +259,9 @@ export class BaseFilter {
     }
   }
 
-  applyTo2d(options: T2DPipelineState): void {}
+  applyTo2d(options: T2DPipelineState): void {
+    // override by subclass
+  }
 
   /**
    * Returns a string that represent the current selected shader code for the filter.
@@ -354,7 +356,9 @@ export class BaseFilter {
   sendUniformData(
     gl: WebGLRenderingContext,
     uniformLocations: TWebGLUniformLocationMap
-  ): void {}
+  ): void {
+    // override by subclass
+  }
 
   /**
    * If needed by a 2d filter, this functions can create an helper canvas to be used
