91e95167f7121dbdcfe1d723572af9056e0e19fc
Andrea Bogazzi <andreabogazzi79@gmail.com>
2023-03-12 00:29:28 +0100

    no animation default

--- .codesandbox/templates/next/pages/index.tsx
+++ .codesandbox/templates/next/pages/index.tsx
@@ -1,7 +1,7 @@
 import * as fabric from 'fabric';
-import { NextPage } from 'next';
-import { useCallback } from 'react';
-import { Canvas } from '../components/Canvas';
+import {NextPage} from 'next';
+import {useCallback} from 'react';
+import {Canvas} from '../components/Canvas';
 
 const IndexPage: NextPage = () => {
   const onLoad = useCallback(async (canvas: fabric.Canvas) => {
@@ -9,26 +9,12 @@ const IndexPage: NextPage = () => {
       width: window.innerWidth,
       height: 500,
     });
-    const text = new fabric.Text('fabric.js sandbox', {
+    const text = new fabric.Textbox('fabric.js sandbox', {
       originX: 'center',
       top: 20,
     });
     canvas.add(text);
     canvas.centerObjectH(text);
-    function animate(toState: 0 | 1) {
-      text.animate(
-        { scaleX: Math.max(toState, 0.1) * 2 },
-        {
-          onChange: () => canvas.renderAll(),
-          onComplete: () => animate(Number(!toState) as 0 | 1),
-          duration: 1000,
-          easing: toState
-            ? fabric.util.ease.easeInOutQuad
-            : fabric.util.ease.easeInOutSine,
-        }
-      );
-    }
-    animate(1);
   }, []);
 
   return (
