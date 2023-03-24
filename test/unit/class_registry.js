6cb5e3dac15db16e2835efd436cf40072afc1001
ShaMan123 <shacharnen@gmail.com>
2023-03-24 22:07:09 +0300

    Update class_registry.js

--- test/unit/class_registry.js
+++ test/unit/class_registry.js
@@ -1,24 +1,24 @@
 (function () {
   const classRegistry = new fabric.classRegistry.constructor();
   QUnit.module('classRegistry');
-  QUnit.test('getClass throw when no class is registered', function (assert) {
+  QUnit.test('getJSONClass throw when no class is registered', function (assert) {
     assert.ok(fabric.classRegistry, 'classRegistry is available');
-    assert.throws(() => classRegistry.getClass('rect'), new Error(`No class registered for rect`), 'initially Rect is undefined');
+    assert.throws(() => classRegistry.getJSONClass('rect'), new Error(`No class registered for rect`), 'initially Rect is undefined');
   });
-  QUnit.test('getClass will return specific class matched by name', function (assert) {
+  QUnit.test('getJSONClass will return specific class matched by name', function (assert) {
     class TestClass {
 
     }
     classRegistry.setJSONClass(TestClass);
-    assert.equal(classRegistry.getClass('TestClass'), TestClass, 'resolves class correctly');
-    assert.equal(classRegistry.getClass('testclass'), TestClass, 'resolves class correctly to lower case');
+    assert.equal(classRegistry.getJSONClass('TestClass'), TestClass, 'resolves class correctly');
+    assert.equal(classRegistry.getJSONClass('testclass'), TestClass, 'resolves class correctly to lower case');
   });
-  QUnit.test('getClass will return specific class from custom type', function (assert) {
+  QUnit.test('getJSONClass will return specific class from custom type', function (assert) {
     class TestClass2 {
 
     }
     classRegistry.setJSONClass(TestClass2, 'myCustomType');
-    const resolved = classRegistry.getClass('myCustomType');
+    const resolved = classRegistry.getJSONClass('myCustomType');
     assert.equal(resolved, TestClass2, 'resolves class correctly with custom type');
   });
   QUnit.test('can resolve different class for SVG and JSON', function (assert) {
@@ -30,15 +30,15 @@
     }
     classRegistry.setJSONClass(TestClass3, 'myCustomType');
     classRegistry.setSVGClass(TestClass4, 'myCustomType');
-    const resolved = classRegistry.getClass('myCustomType');
+    const resolved = classRegistry.getJSONClass('myCustomType');
     const resolvedSvg = classRegistry.getSVGClass('myCustomType');
     assert.notEqual(resolved, resolvedSvg, 'resolved different classes');
   });
   QUnit.test('legacy resolution', function (assert) {
-    assert.equal(fabric.classRegistry.getClass('rect'), fabric.Rect, 'resolves class correctly');
-    assert.equal(fabric.classRegistry.getClass('i-text'), fabric.IText, 'resolves class correctly');
-    assert.equal(fabric.classRegistry.getClass('activeSelection'), fabric.ActiveSelection, 'resolves class correctly');
-    assert.equal(fabric.classRegistry.getClass('object'), fabric.Object, 'resolves class correctly');
+    assert.equal(fabric.classRegistry.getJSONClass('rect'), fabric.Rect, 'resolves class correctly');
+    assert.equal(fabric.classRegistry.getJSONClass('i-text'), fabric.IText, 'resolves class correctly');
+    assert.equal(fabric.classRegistry.getJSONClass('activeSelection'), fabric.ActiveSelection, 'resolves class correctly');
+    assert.equal(fabric.classRegistry.getJSONClass('object'), fabric.Object, 'resolves class correctly');
     assert.equal(fabric.classRegistry.getJSONClass({
       blur: 1,
       offsetY: 2
