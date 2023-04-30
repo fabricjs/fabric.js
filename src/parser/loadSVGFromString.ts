b5436c939cc82021a922356a602b95523b3c67d3
Andrea Bogazzi <andreabogazzi79@gmail.com>
2023-04-30 02:21:54 +0200

    Update src/parser/loadSVGFromString.ts
    Co-authored-by: Shachar <34343793+ShaMan123@users.noreply.github.com>

--- src/parser/loadSVGFromString.ts
+++ src/parser/loadSVGFromString.ts
@@ -8,10 +8,10 @@ import type { TSvgParsedCallback, TSvgReviverCallback } from './typedefs';
  * @memberOf fabric
  * @param {String} string representing the svg
  * @param {TSvgParsedCallback} callback Invoked when the parsing is done, with null if parsing wasn't possible with the list of svg nodes.
- * TSvgParsedCallback receive also `allElements` array as last argument. This is the full array from the list of svg nodes available in the document.
+ * {@link TSvgParsedCallback} also receives `allElements` array as the last argument. This is the full list of svg nodes available in the document.
  * You may want to use it if you are trying to regroup the objects as they were originally grouped in the SVG. ( This was the reason why it was added )
- * @param {TSvgReviverCallback} [reviver] Extra callback for further parsing of SVG elements, called after each fabric object created.
- * Takes as input the original svg element and the fabricObject generated as arguments. Used to inspect extra properties eventually not parsed by fabricJS
+ * @param {TSvgReviverCallback} [reviver] Extra callback for further parsing of SVG elements, called after each fabric object has been created.
+ * Takes as input the original svg element and the generated `FabricObject` as arguments. Used to inspect extra properties not parsed by fabric.
  * @param {Object} [options] Object containing options for parsing
  * @param {String} [options.crossOrigin] crossOrigin setting to use for external resources
  * @param {AbortSignal} [options.signal] handle aborting, see https://developer.mozilla.org/en-US/docs/Web/API/AbortController/signal
