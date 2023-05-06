0820436d345c9d118ef86bafd207023a58fd87bc
Andrea Bogazzi <andreabogazzi79@gmail.com>
2023-05-07 01:18:07 +0200

    opps

--- src/shapes/Line.ts
+++ src/shapes/Line.ts
@@ -241,12 +241,12 @@ export class Line<
    */
   static async fromElement(element: SVGElement) {
     const {
-        x1 = 0,
-        y1 = 0,
-        x2 = 0,
-        y2 = 0,
-        ...parsedAttributes
-      } = parseAttributes(element, this.ATTRIBUTE_NAMES),
+      x1 = 0,
+      y1 = 0,
+      x2 = 0,
+      y2 = 0,
+      ...parsedAttributes
+    } = parseAttributes(element, this.ATTRIBUTE_NAMES);
     return new this([x1, y1, x2, y2], parsedAttributes);
   }
 
