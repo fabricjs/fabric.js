fabricjs/fabric.js

commit 3ebe51a5d8753095756c04f05b2672622732cb09

chore(): move canvas click handler to TextManager (#8939)

src/canvas/TextEditingManager.ts
@@ -1,6 +1,7 @@
 import type { TPointerEvent } from '../EventTypeDefs';
 import type { ITextBehavior } from '../shapes/IText/ITextBehavior';
 import { removeFromArray } from '../util/internals';
+import type { Canvas } from './Canvas';
 
 /**
  * In charge of synchronizing all interactive text instances of a canvas
@@ -8,6 +9,14 @@ import { removeFromArray } from '../util/internals';
 export class TextEditingManager {
   private targets: ITextBehavior[] = [];
   private declare target?: ITextBehavior;
+  private __disposer: VoidFunction;
+
+  constructor(canvas: Canvas) {
+    const cb = () => this.target?.hiddenTextarea?.focus();
+    const el = canvas.upperCanvasEl;
+    el.addEventListener('click', cb);
+    this.__disposer = () => el.removeEventListener('click', cb);
+  }
 
   exitTextEditing() {
     this.target = undefined;
@@ -41,8 +50,15 @@ export class TextEditingManager {
     this.target?.isEditing && this.target.updateSelectionOnMouseMove(e);
   }
 
-  dispose() {
+  clear() {
     this.targets = [];
     this.target = undefined;
+  }
+
+  dispose() {
+    this.clear();
+    this.__disposer();
+    // @ts-expect-error disposing
+    delete this.__disposer;
   }
 }

src/canvas/Canvas.ts
@@ -116,7 +116,7 @@ export class Canvas extends SelectableCanvas {
 
   private _isClick: boolean;
 
-  textEditingManager = new TextEditingManager();
+  textEditingManager = new TextEditingManager(this);
 
   constructor(el: string | HTMLCanvasElement, options = {}) {
     super(el, options);
@@ -1593,7 +1593,7 @@ export class Canvas extends SelectableCanvas {
    * @override clear {@link textEditingManager}
    */
   clear() {
-    this.textEditingManager.dispose();
+    this.textEditingManager.clear();
     super.clear();
   }
 
@@ -1602,7 +1602,7 @@ export class Canvas extends SelectableCanvas {
    */
   destroy() {
     this.removeListeners();
-    super.destroy();
     this.textEditingManager.dispose();
+    super.destroy();
   }
 }

src/shapes/IText/ITextKeyBehavior.ts
@@ -101,18 +101,6 @@ export abstract class ITextKeyBehavior<
       'compositionend',
       this.onCompositionEnd.bind(this)
     );
-
-    if (!this._clickHandlerInitialized && this.canvas) {
-      this.canvas.upperCanvasEl.addEventListener(
-        'click',
-        this.onClick.bind(this)
-      );
-      this._clickHandlerInitialized = true;
-    }
-  }
-
-  onClick() {
-    this.hiddenTextarea && this.hiddenTextarea.focus();
   }
 
   /**

test/unit/canvas_events.js
@@ -1182,7 +1182,7 @@
     assert.ok(manager.target === a, 'should register a');
     canvas.remove(a);
     assert.ok(!manager.target, 'should unregister a');
-    manager.dispose();
+    manager.clear();
     assert.ok(!manager.target, 'should have disposed ref');
     assert.deepEqual(manager.targets, [], 'should have disposed refs');
     const g = new fabric.Group([a]);

commit a09ed9333bd963b9b6c200c4c318a7d9dd5d4920

fix(): regression to itext focusing from #8939

src/canvas/TextEditingManager.ts
@@ -12,7 +12,9 @@ export class TextEditingManager {
   private __disposer: VoidFunction;
 
   constructor(canvas: Canvas) {
-    const cb = () => this.target?.hiddenTextarea?.focus();
+    const cb = () => {
+      (canvas.getActiveObject() as ITextBehavior)?.hiddenTextarea?.focus();
+    };
     const el = canvas.upperCanvasEl;
     el.addEventListener('click', cb);
     this.__disposer = () => el.removeEventListener('click', cb);