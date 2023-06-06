277e3dab4934163a07baa7bdde8c5c82a5be0c1a
ShaMan123 <shacharnen@gmail.com>
2023-06-06 13:42:48 +0900

    Revert "fix(): splitByGrapheme"
    This reverts commit 19dd2e47b32bd66f1e135164c28c85764b485aa3.

--- src/shapes/Textbox.ts
+++ src/shapes/Textbox.ts
@@ -347,7 +347,7 @@ export class Textbox extends IText {
       word = splitByGrapheme ? word : this.graphemeSplit(word);
       const width = this._measureWord(word, lineIndex, offset);
       largestWordWidth = Math.max(width, largestWordWidth);
-      offset += word.length + Number(!splitByGrapheme);
+      offset += word.length + 1;
       return { word: word, width: width };
     });
 
