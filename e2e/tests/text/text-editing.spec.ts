31316cac24bd64d577b856fcbd4bb9a3c2470340
ShaMan123 <shacharnen@gmail.com>
2023-07-09 00:10:58 +0900

    cute

--- e2e/tests/text/text-editing.spec.ts
+++ e2e/tests/text/text-editing.spec.ts
@@ -1,31 +1,29 @@
-import { test, expect } from '@playwright/test';
-import {
-  addTextbox,
-  getObjectCenter,
-  clickCanvas,
-  getObjectControlPoint,
-  expectObjectToMatch,
-} from '../../utils/objects';
+import { test } from '@playwright/test';
+import { TestUtil } from '../../utils/objects';
 
 test('text typing itext vs textbox', async ({ page }) => {
+  const util = new TestUtil(page);
   await page.goto('http://127.0.0.1:8080/e2e/site');
-  const textboxID = 'text1';
-  await addTextbox(page, textboxID, 'initial text', {
+  const textboxID = await util.addTextbox('initial text', {
     width: 200,
     left: 50,
   });
-  const textCenter = await getObjectCenter(page, textboxID);
+  const textCenter = await util.getObjectCenter(textboxID);
+
   const state = {
     isEditing: false,
     text: 'initial text',
     width: 200,
   };
+  await util.expectObjectToMatch(textboxID, state);
 
-  await clickCanvas(page, {
+  await util.clickCanvas({
     position: textCenter,
     clickCount: 2,
     delay: 200,
   });
+  state.isEditing = true;
+  await util.expectObjectToMatch(textboxID, state);
 
   await page
     .locator('textarea')
@@ -35,39 +33,59 @@ test('text typing itext vs textbox', async ({ page }) => {
         delay: 160,
       }
     );
+  state.text =
+    'initial insert text in a textbox from the keyboard will wrap text on current textbox width';
+  await util.expectObjectToMatch(textboxID, state);
 
-  await expectObjectToMatch(page, textboxID, {
-    isEditing: true,
-    width: 200,
-    text: 'initial insert text in a textbox from the keyboard will wrap text on current textbox width',
-  });
-  expect(page).toMatchSnapshot();
-
-  const controlPoint = await getObjectControlPoint(page, textboxID, 'mr');
+  const mrControlPoint = await util.getObjectControlPoint(textboxID, 'mr');
 
   // click outside to stop editing
-  await clickCanvas(page, {
+  await util.clickCanvas({
     position: {
-      x: controlPoint.x + 20,
-      y: controlPoint.y,
+      x: mrControlPoint.x + 20,
+      y: mrControlPoint.y,
     },
     clickCount: 1,
     delay: 200,
   });
-  await expectObjectToMatch(page, textboxID, {
-    isEditing: false,
-    width: 200,
-    text: 'initial insert text in a textbox from the keyboard will wrap text on current textbox width',
-  });
+  state.isEditing = false;
+  await util.expectObjectToMatch(textboxID, state);
 
   // click the object to select it
-  await clickCanvas(page, {
+  await util.clickCanvas({
     position: textCenter,
   });
 
-  // drag the control
-  await page.mouse.move(controlPoint.x, controlPoint.y);
+  // drag the mr control
+  await page.mouse.move(mrControlPoint.x, mrControlPoint.y);
   await page.mouse.down();
-  await page.mouse.move(controlPoint.x + 300, controlPoint.y, { steps: 40 });
+  await page.mouse.move(mrControlPoint.x + 300, mrControlPoint.y, {
+    steps: 40,
+  });
+  state.width = 500;
+  await util.expectObjectToMatch(textboxID, state);
+  // drag in the opposite direction
+  await page.mouse.move(mrControlPoint.x - 300, mrControlPoint.y, {
+    steps: 60,
+  });
+  state.width = 148.828125;
+  await util.expectObjectToMatch(textboxID, state);
+  await page.mouse.up();
+
+  // drag the ml control
+  const mlControlPoint = await util.getObjectControlPoint(textboxID, 'ml');
+  await page.mouse.move(mlControlPoint.x, mlControlPoint.y);
+  await page.mouse.down();
+  await page.mouse.move(mlControlPoint.x - 300, mlControlPoint.y, {
+    steps: 40,
+  });
+  state.width = 450;
+  await util.expectObjectToMatch(textboxID, state);
+  // drag in the opposite direction
+  await page.mouse.move(mlControlPoint.x + 300, mlControlPoint.y, {
+    steps: 60,
+  });
+  state.width = 148.828125;
+  await util.expectObjectToMatch(textboxID, state);
   await page.mouse.up();
 });
--- e2e/utils/objects.ts
+++ e2e/utils/objects.ts
@@ -1,78 +1,64 @@
 import { Page, expect } from '@playwright/test';
 
-export async function accessObject<T extends Record<string, unknown>>(
-  page: Page,
-  objectId: string,
-  cb: object
-) {
-  const snapshot = await page.evaluate(
-    ({ objectId }) => {
-      return fabricCanvas.getObjects().find((obj) => obj.id === objectId);
-    },
-    { objectId }
-  );
-  expect(snapshot).toMatchObject(expected);
-}
+let ID = 0;
+const uuid = () => `test${ID++}`;
 
-export async function expectObjectToMatch<T extends Record<string, unknown>>(
-  page: Page,
-  objectId: string,
-  expected: T
-) {
-  const snapshot = await page.evaluate(
-    ({ objectId }) => {
-      return fabricCanvas.getObjects().find((obj) => obj.id === objectId);
-    },
-    { objectId }
-  );
-  expect(snapshot).toMatchObject(expected);
-}
+export class TestUtil {
+  constructor(readonly page: Page) {}
 
-export function addTextbox(
-  page: Page,
-  objectId: string,
-  text: string,
-  properties
-) {
-  return page.evaluate(
-    ({ objectId, text, properties }) => {
-      const textbox = new fabric.Textbox(text, {
-        ...properties,
-        id: objectId,
-      });
-      fabricCanvas.add(textbox);
-    },
-    { objectId, text, properties }
-  );
-}
+  async addTextbox(text: string, properties) {
+    const objectId = uuid();
+    await this.page.evaluate(
+      ({ objectId, text, properties }) => {
+        const textbox = new fabric.Textbox(text, {
+          ...properties,
+          id: objectId,
+        });
+        fabricCanvas.add(textbox);
+      },
+      { objectId, text, properties }
+    );
+    return objectId;
+  }
 
-export function getObjectCenter(page: Page, objectId: string) {
-  return page.evaluate(
-    ({ objectId }) => {
-      const obj = fabricCanvas.getObjects().find((obj) => obj.id === objectId);
-      return obj.getCenterPoint();
-    },
-    { objectId }
-  );
-}
+  getObjectCenter(objectId: string) {
+    return this.page.evaluate(
+      ({ objectId }) => {
+        const obj = fabricCanvas
+          .getObjects()
+          .find((obj) => obj.id === objectId);
+        return obj.getCenterPoint();
+      },
+      { objectId }
+    );
+  }
 
-export function clickCanvas(
-  page: Page,
-  clickProperties: Parameters<Page['click']>[1]
-) {
-  return page.click('canvas.upper-canvas', clickProperties);
-}
+  clickCanvas(clickProperties: Parameters<Page['click']>[1]) {
+    return this.page.click('canvas.upper-canvas', clickProperties);
+  }
 
-export function getObjectControlPoint(
-  page: Page,
-  objectId: string,
-  controlName: string
-) {
-  return page.evaluate(
-    ({ objectId, controlName }) => {
-      const obj = fabricCanvas.getObjects().find((obj) => obj.id === objectId);
-      return obj.oCoords[controlName];
-    },
-    { objectId, controlName }
-  );
+  getObjectControlPoint(objectId: string, controlName: string) {
+    return this.page.evaluate(
+      ({ objectId, controlName }) => {
+        const obj = fabricCanvas
+          .getObjects()
+          .find((obj) => obj.id === objectId);
+        return obj.oCoords[controlName];
+      },
+      { objectId, controlName }
+    );
+  }
+
+  async expectObjectToMatch<T extends Record<string, unknown>>(
+    objectId: string,
+    expected: T
+  ) {
+    const snapshot = await this.page.evaluate(
+      ({ objectId }) => {
+        return fabricCanvas.getObjects().find((obj) => obj.id === objectId);
+      },
+      { objectId }
+    );
+    expect(snapshot).toMatchObject(expected);
+  }
 }
