2240d8eb2e6aa6ee9f34f6a61047e1a43be09a98
Shachar <34343793+ShaMan123@users.noreply.github.com>
2023-07-10 17:58:34 +0900

    fix(util, Path): path distance measurement fix for M cmd (#9076)

--- src/shapes/Text/Text.ts
+++ src/shapes/Text/Text.ts
@@ -9,7 +9,6 @@ import type {
 import { StyledText } from './StyledText';
 import { SHARED_ATTRIBUTES } from '../../parser/attributes';
 import { parseAttributes } from '../../parser/parseAttributes';
-import type { XY } from '../../Point';
 import type {
   Abortable,
   TCacheCanvasDimensions,
@@ -898,9 +897,6 @@ export class Text<
       let positionInPath = 0;
       const totalPathLength =
         path.segmentsInfo[path.segmentsInfo.length - 1].length;
-      const startingPoint = getPointOnPath(path.path, 0, path.segmentsInfo)!;
-      startingPoint.x += path.pathOffset.x;
-      startingPoint.y += path.pathOffset.y;
       switch (this.textAlign) {
         case LEFT:
           positionInPath = reverse ? totalPathLength - width : 0;
@@ -927,7 +923,7 @@ export class Text<
         }
         // it would probably much faster to send all the grapheme position for a line
         // and calculate path position/angle at once.
-        this._setGraphemeOnPath(positionInPath, graphemeInfo, startingPoint!);
+        this._setGraphemeOnPath(positionInPath, graphemeInfo);
         positionInPath += graphemeInfo.kernedWidth;
       }
     }
@@ -942,18 +938,14 @@ export class Text<
    * @param {GraphemeBBox} graphemeInfo current grapheme box information
    * @param {Object} startingPoint position of the point
    */
-  _setGraphemeOnPath(
-    positionInPath: number,
-    graphemeInfo: GraphemeBBox,
-    startingPoint: XY
-  ) {
+  _setGraphemeOnPath(positionInPath: number, graphemeInfo: GraphemeBBox) {
     const centerPosition = positionInPath + graphemeInfo.kernedWidth / 2,
       path = this.path!;
 
     // we are at currentPositionOnPath. we want to know what point on the path is.
     const info = getPointOnPath(path.path, centerPosition, path.segmentsInfo)!;
-    graphemeInfo.renderLeft = info.x - startingPoint.x;
-    graphemeInfo.renderTop = info.y - startingPoint.y;
+    graphemeInfo.renderLeft = info.x - path.pathOffset.x;
+    graphemeInfo.renderTop = info.y - path.pathOffset.y;
     graphemeInfo.angle = info.angle + (this.pathSide === RIGHT ? Math.PI : 0);
   }
 
