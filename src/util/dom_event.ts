5adbebe3d57c4fc55f25d08320453c4c4fb07486
Andrea Bogazzi <andreabogazzi79@gmail.com>
2023-07-10 14:13:40 +0200

    more typings

--- src/util/dom_event.ts
+++ src/util/dom_event.ts
@@ -1,19 +1,18 @@
-//@ts-nocheck
 import { Point } from '../Point';
 import { getScrollLeftTop } from './dom_misc';
 
 const touchEvents = ['touchstart', 'touchmove', 'touchend'];
 
-function getTouchInfo(event) {
-  const touchProp = event.changedTouches;
+function getTouchInfo(event: TouchEvent | MouseEvent): MouseEvent | Touch {
+  const touchProp = (event as TouchEvent).changedTouches;
   if (touchProp && touchProp[0]) {
     return touchProp[0];
   }
-  return event;
+  return event as MouseEvent;
 }
 
-export const getPointer = (event) => {
-  const element = event.target,
+export const getPointer = (event: MouseEvent): Point => {
+  const element = event.target as HTMLElement,
     scroll = getScrollLeftTop(element),
     _evt = getTouchInfo(event);
   return new Point(_evt.clientX + scroll.left, _evt.clientY + scroll.top);
