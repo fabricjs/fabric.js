ce5d820d928890c6fab6f5cd4a21e37b5c3ed2a3
ShaMan123 <shacharnen@gmail.com>
2023-07-14 13:22:36 +0530

    fix coverage reporting

--- e2e/utils/setupTest.ts
+++ e2e/utils/setupTest.ts
@@ -5,8 +5,6 @@ import v8toIstanbul from 'v8-to-istanbul';
 
 // https://playwright.dev/docs/api/class-coverage
 
-const coverageStore: CoverageMapData[] = [];
-
 test.beforeEach(async ({ page }, testInfo) => {
   await page.coverage.startJSCoverage({ reportAnonymousScripts: false });
   // fix snapshot names so they are cross platform
@@ -30,20 +28,12 @@ test.afterEach(async ({ page }, { outputDir }) => {
       return converter.toIstanbul();
     })
   );
-  coverageStore.push(...nyc);
   writeFileSync(
     path.resolve(outputDir, 'coverage-v8.json'),
     JSON.stringify(coverage, null, 2)
   );
   writeFileSync(
     path.resolve(outputDir, 'coverage.json'),
-    JSON.stringify(nyc, null, 2)
-  );
-});
-
-test.afterAll(() => {
-  writeFileSync(
-    './e2e/test-results/coverage.json',
-    JSON.stringify(coverageStore, null, 2)
+    JSON.stringify({ [path.resolve('./dist/index.js')]: nyc }, null, 2)
   );
 });
