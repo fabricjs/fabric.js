52a0ee775654f8c71fc1afef110db5a9c7fe3738
ShaMan123 <shacharnen@gmail.com>
2023-07-15 19:11:29 +0530

    Update types.ts

--- src/env/types.ts
+++ src/env/types.ts
@@ -1,9 +1,10 @@
+import type { TextStyleDeclaration } from '../../fabric';
 import type { GLProbe } from '../filters/GLProbes/GLProbe';
 import type { DOMWindow } from 'jsdom';
 
 export type TCopyPasteData = {
   copiedText?: string;
-  copiedTextStyle?: Record<string, string>;
+  copiedTextStyle?: TextStyleDeclaration[];
 };
 export type TFabricEnv = {
   document: Document;
