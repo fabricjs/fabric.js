056170a3ed1567d5660e75c7027721fde43c02a4
ShaMan123 <shacharnen@gmail.com>
2023-07-16 17:51:51 +0530

    progress

--- e2e/tests/text/index.ts
+++ e2e/tests/text/index.ts
@@ -1,7 +1,5 @@
-import { Canvas, Textbox } from 'fabric';
-
-const el = document.getElementById('canvas');
-const canvas = (window.canvas = new Canvas(el));
+import { Textbox } from 'fabric';
+import { canvas } from 'init';
 
 const textbox = new Textbox('initial text', { width: 200, left: 50 });
 canvas.add(textbox);
