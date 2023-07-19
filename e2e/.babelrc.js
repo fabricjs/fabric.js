ced84513a5d50dd118758db6f858f2f7048ffb1d
ShaMan123 <shacharnen@gmail.com>
2023-07-19 17:21:54 +0530

    abs resolution + default imports
    abs resolution + default imports

--- e2e/.babelrc.js
+++ e2e/.babelrc.js
@@ -1,9 +1,24 @@
 // https://github.com/viruscamp/babel-plugin-transform-imports#using-a-function-as-the-transformer
 
 const path = require('path');
-const siteDir = path.resolve('./e2e/site');
 const testsDir = path.resolve('./e2e/tests');
 const testsBuiltDir = path.resolve('./e2e/dist');
+
+function resolve(file) {
+  const found = ['', '.js', '/index.js']
+    .map((resolution) => `${file}${resolution}`)
+    .find((file) => {
+      try {
+        return require.resolve(file);
+      } catch (error) {
+        return false;
+      }
+    });
+  if (!found) {
+    throw new Error(`Failed to resolve ${file}`);
+  }
+  return require.resolve(file);
+}
 
 module.exports = {
   extends: '../.babelrcAlt',
@@ -14,18 +29,17 @@ module.exports = {
         '\\..*': {
           skipDefaultConversion: true,
           transform: function (importName, matches, filename) {
-            const file = path.resolve(
-              path.dirname(filename),
-              `${matches[0]}.js`
-            );
-            return path
+            const file = path.resolve(path.dirname(filename), `${matches[0]}`);
+            return `/${path
               .relative(
-                siteDir,
-                file.startsWith(testsDir)
-                  ? path.resolve(testsBuiltDir, path.relative(testsDir, file))
-                  : file
+                process.cwd(),
+                resolve(
+                  file.startsWith(testsDir)
+                    ? path.resolve(testsBuiltDir, path.relative(testsDir, file))
+                    : file
+                )
               )
-              .replaceAll('\\', '/');
+              .replaceAll('\\', '/')}`;
           },
         },
       },
