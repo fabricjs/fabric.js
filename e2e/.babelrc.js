c90cfbc89a2baf60724a82c278491b9230f671d8
ShaMan123 <shacharnen@gmail.com>
2023-07-19 17:22:36 +0530

    Squashed commit of the following:
    commit 15e834a21a624e11bf0ea778704ffb627bd813aa
    Author: ShaMan123 <shacharnen@gmail.com>
    Date:   Wed Jul 19 17:21:54 2023 +0530
    
        abs resolution + default imports
    
    commit 4f7bda287b80b47cd738320c2f2c65520aebc316
    Author: ShaMan123 <shacharnen@gmail.com>
    Date:   Wed Jul 19 15:08:58 2023 +0530
    
        cleanup
    
    commit 47b954f5bd5d3fdeccc9f30c0eb32bfd44bcf4eb
    Author: ShaMan123 <shacharnen@gmail.com>
    Date:   Wed Jul 19 14:38:09 2023 +0530
    
        node version - this was supposed to throw
    
    commit 6ccd23cc830f76881a3440f1a552db24388a7386
    Author: ShaMan123 <shacharnen@gmail.com>
    Date:   Wed Jul 19 14:33:35 2023 +0530
    
        try to fix node14 ci error
    
        https://github.com/actions/setup-node/issues/214#issuecomment-842844274
    
        https://github.com/npm/npm/issues/19788
    
    commit cb31f22c874cfa5f8c2bfc710b60c044d8a8a5a1
    Author: ShaMan123 <shacharnen@gmail.com>
    Date:   Wed Jul 19 14:09:57 2023 +0530
    
        Update package-lock.json
    
    commit 248d0be7aaf3f0e3d07ded9f006a1ad96404c978
    Author: ShaMan123 <shacharnen@gmail.com>
    Date:   Wed Jul 19 13:59:48 2023 +0530
    
        forgot the setup
    
    commit 19e20d33648b54287f4320c7ba3e169916244e4a
    Merge: b9c0da688 f09a52e62
    Author: ShaMan123 <shacharnen@gmail.com>
    Date:   Wed Jul 19 13:54:49 2023 +0530
    
        Merge branch 'ci-e2e-rel-imports' of https://github.com/fabricjs/fabric.js into ci-e2e-rel-imports
    
    commit b9c0da688430b6b94b4ef8dffe13cd7e8af6d250
    Author: ShaMan123 <shacharnen@gmail.com>
    Date:   Wed Jul 19 13:54:25 2023 +0530
    
        extends
    
    commit f09a52e62ef4708dfbaf04370684b0cc52e53d92
    Author: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Date:   Wed Jul 19 08:22:08 2023 +0000
    
        update CHANGELOG.md
    
    commit 20b9eaac823393261a3803f58162d43024444bb6
    Author: ShaMan123 <shacharnen@gmail.com>
    Date:   Wed Jul 19 13:49:28 2023 +0530
    
        ci(e2e): support relative imports

--- e2e/.babelrc.js
+++ e2e/.babelrc.js
@@ -1,25 +1,8 @@
 // https://github.com/viruscamp/babel-plugin-transform-imports#using-a-function-as-the-transformer
 
 const path = require('path');
-const siteDir = path.resolve('./e2e/site');
 const testsDir = path.resolve('./e2e/tests');
 const testsBuiltDir = path.resolve('./e2e/dist');
-
-function resolve(file) {
-  const found = ['', '.js', '/index.js']
-    .map((resolution) => `${file}${resolution}`)
-    .find((file) => {
-      try {
-        return require.resolve(file);
-      } catch (error) {
-        return false;
-      }
-    });
-  if (!found) {
-    throw new Error(`Failed to resolve ${file}`);
-  }
-  return require.resolve(file);
-}
 
 module.exports = {
   extends: '../.babelrcAlt',
@@ -31,16 +14,16 @@ module.exports = {
           skipDefaultConversion: true,
           transform: function (importName, matches, filename) {
             const file = path.resolve(path.dirname(filename), `${matches[0]}`);
-            return path
+            return `/${path
               .relative(
-                siteDir,
+                process.cwd(),
                 resolve(
                   file.startsWith(testsDir)
                     ? path.resolve(testsBuiltDir, path.relative(testsDir, file))
                     : file
                 )
               )
-              .replaceAll('\\', '/');
+              .replaceAll('\\', '/')}`;
           },
         },
       },
