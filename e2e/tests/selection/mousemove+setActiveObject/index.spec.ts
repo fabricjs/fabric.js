dd634211d1d3313f01c310704857d033a0840d60
ShaMan123 <shacharnen@gmail.com>
2023-07-20 01:41:42 +0530

    Update index.spec.ts

--- e2e/tests/selection/mousemove+setActiveObject/index.spec.ts
+++ e2e/tests/selection/mousemove+setActiveObject/index.spec.ts
@@ -1,20 +1,21 @@
 import { expect, test } from '@playwright/test';
-import '../../../utils/setupTest';
-import { TestUtil } from '../../../utils/TestUtil';
+import { CanvasUtil } from '../../../utils/CanvasUtil';
+
+import '../../../setup';
 
 test('startImmediatePropagation: mousemove + setActiveObject', async ({
   page,
 }) => {
-  const util = new TestUtil(page);
+  const canvasUtil = new CanvasUtil(page);
   await page.mouse.move(300, 360);
   await page.mouse.down();
   await page.mouse.move(300, 100, { steps: 40 });
-  expect(await util.screenshot()).toMatchSnapshot({
+  expect(await canvasUtil.screenshot()).toMatchSnapshot({
     name: 'moved.png',
   });
   await page.mouse.move(300, 360, { steps: 40 });
   await page.mouse.up();
-  expect(await util.screenshot()).toMatchSnapshot({
+  expect(await canvasUtil.screenshot()).toMatchSnapshot({
     name: 'up.png',
   });
 });
