02799ca20f3f97b0cb4764b84ddca698da02027f
ShaMan123 <shacharnen@gmail.com>
2023-07-19 15:55:50 +0530

    resolve index files

--- e2e/.babelrc.js
+++ e2e/.babelrc.js
@@ -5,6 +5,22 @@ const siteDir = path.resolve('./e2e/site');
 const testsDir = path.resolve('./e2e/tests');
 const testsBuiltDir = path.resolve('./e2e/dist');
 
+function resolve(file) {
+  const found = ['', '.js', '/index.js']
+    .map((resolution) => `${file}${resolution}`)
+    .find((file) => {
+      try {
+        return require.resolve(file);
+      } catch (error) {
+        return false;
+      }
+    });
+  if (!found) {
+    throw new Error(`Failed to resolve ${file}`);
+  }
+  return require.resolve(file);
+}
+
 module.exports = {
   extends: '../.babelrcAlt',
   plugins: [
@@ -14,16 +30,15 @@ module.exports = {
         '\\..*': {
           skipDefaultConversion: true,
           transform: function (importName, matches, filename) {
-            const file = path.resolve(
-              path.dirname(filename),
-              `${matches[0]}.js`
-            );
+            const file = path.resolve(path.dirname(filename), `${matches[0]}`);
             return path
               .relative(
                 siteDir,
-                file.startsWith(testsDir)
-                  ? path.resolve(testsBuiltDir, path.relative(testsDir, file))
-                  : file
+                resolve(
+                  file.startsWith(testsDir)
+                    ? path.resolve(testsBuiltDir, path.relative(testsDir, file))
+                    : file
+                )
               )
               .replaceAll('\\', '/');
           },
