c30ebeb0ebd93d1ea7ae060d5b3cf51b0bcaac85
ShaMan123 <shacharnen@gmail.com>
2023-07-19 22:15:59 +0530

    Update index.spec.ts

--- e2e/tests/stroke_projection/index.spec.ts
+++ e2e/tests/stroke_projection/index.spec.ts
@@ -89,7 +89,7 @@ function runStep(page: Page, testInfo: TestInfo, spec: TestSpec) {
 
 test.describe('Stroke Projection', () => {
   test('BBox is correct', async ({ page }, testInfo) => {
-    testInfo.setTimeout(60 * 1000);
+    testInfo.setTimeout(120 * 1000);
     await Promise.all(
       [...common, ...miterLimit, ...singlePoint].map((spec) =>
         runStep(page, testInfo, spec)
