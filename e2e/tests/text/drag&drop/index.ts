3899f8c58225b14b230f2ee9373cac43e21d0b50
ShaMan123 <shacharnen@gmail.com>
2023-07-20 14:20:45 +0530

    Update index.ts

--- e2e/tests/text/drag&drop/index.ts
+++ e2e/tests/text/drag&drop/index.ts
@@ -1,29 +1,31 @@
 import * as fabric from 'fabric';
-import { before } from '../../test';
+import { beforeAll } from '../../test';
 
-before('#canvas', (el) => {
-  const canvas = new fabric.Canvas(el, { width: 300, height: 200 });
-  const textValue = 'fabric.js sandbox';
-  const text = new fabric.Textbox(textValue, {
-    originX: 'center',
-    width: 200,
-    left: 150,
-    top: 20,
-    splitByGrapheme: true,
-    styles: fabric.util.stylesFromArray(
-      [
-        {
-          style: {
-            fontWeight: 'bold',
-            fontSize: 64,
+beforeAll(
+  (canvas) => {
+    const textValue = 'fabric.js sandbox';
+    const text = new fabric.Textbox(textValue, {
+      originX: 'center',
+      width: 200,
+      left: 150,
+      top: 20,
+      splitByGrapheme: true,
+      styles: fabric.util.stylesFromArray(
+        [
+          {
+            style: {
+              fontWeight: 'bold',
+              fontSize: 64,
+            },
+            start: 0,
+            end: 9,
           },
-          start: 0,
-          end: 9,
-        },
-      ],
-      textValue
-    ),
-  });
-  canvas.add(text);
-  return { canvas, objects: { text } };
-});
+        ],
+        textValue
+      ),
+    });
+    canvas.add(text);
+    return { canvas, objects: { text } };
+  },
+  { width: 300, height: 200 }
+);
