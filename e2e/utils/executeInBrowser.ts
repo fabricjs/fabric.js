7ae2e6c18bce1edbc239f4ba80801b023a27b5b6
ShaMan123 <shacharnen@gmail.com>
2023-07-20 16:55:26 +0530

    Update executeInBrowser.ts

--- e2e/utils/executeInBrowser.ts
+++ e2e/utils/executeInBrowser.ts
@@ -1,19 +1,21 @@
+import type { Page } from '@playwright/test';
 import type { Canvas, Object as FabricObject } from 'fabric';
 
 export function executeInBrowser<C, R>(
+  page: Page,
   runInBrowser: (
     context: C,
     testContext: {
-      getCanvas: (selector: string) => Canvas;
+      getCanvas: (selector?: string) => Canvas;
       getObject: (key: string) => FabricObject;
     }
   ) => R,
-  context: C
+  context?: C
 ): Promise<R> {
-  return this.page.evaluate(
+  return page.evaluate(
     ([runInBrowser, context]) => {
       return eval(runInBrowser)(context, {
-        getCanvas: (selector) =>
+        getCanvas: (selector = '#canvas') =>
           canvasMap.get(document.querySelector(selector)),
         getObject: (key) => objectMap.get(key),
       });
