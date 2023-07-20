b58e3869fb352dad30927c5ae7887a419f3c1c1f
ShaMan123 <shacharnen@gmail.com>
2023-07-20 14:04:03 +0530

    GRAET!

--- e2e/tests/text/drag&drop/index.spec.ts
+++ e2e/tests/text/drag&drop/index.spec.ts
@@ -4,24 +4,76 @@ import '../../../setup';
 
 test('Drag & Drop', async ({ page }) => {
   const canvas = page.locator('canvas').nth(1);
-  console.log(await canvas.getAttribute('style'));
   await canvas.click({
     position: {
-      x: 127,
+      x: 130,
       y: 50,
     },
   });
-  await page.mouse.dblclick(127, 50);
+  await page.mouse.dblclick(130, 50);
   await canvas.hover({
     position: {
-      x: 127,
+      x: 130,
       y: 40,
     },
   });
   await page.mouse.down();
   await page.mouse.move(0, 140, { steps: 40 });
   await page.mouse.move(240, 140, { steps: 40 });
-  expect(await canvas.screenshot()).toMatchSnapshot({ name: 'drop:before' });
+  expect(await canvas.screenshot()).toMatchSnapshot({
+    name: 'drop:before.png',
+  });
   await page.mouse.up();
-  expect(await canvas.screenshot()).toMatchSnapshot({ name: 'drop' });
+  expect(await canvas.screenshot()).toMatchSnapshot({ name: 'drop.png' });
+  await page.waitForTimeout(50000);
+});
+
+test.only('Drag Image', async ({ page }) => {
+  const canvas = page.locator('canvas').nth(1);
+  const trigger = page.evaluate(
+    () =>
+      new Promise<{ image: string; x: number; y: number }>((resolve) =>
+        window.addEventListener(
+          'drag:image',
+          ({ detail: { image, x, y } }) =>
+            resolve({
+              image: image.toDataURL(`image/png`, 1),
+              x,
+              y,
+            }),
+          { once: true }
+        )
+      )
+  );
+  const dataTransfer = await page.evaluateHandle(() =>
+    Object.defineProperty(new DataTransfer(), 'setDragImage', {
+      value: (image, x, y) =>
+        window.dispatchEvent(
+          new CustomEvent('drag:image', { detail: { image, x, y } })
+        ),
+    })
+  );
+
+  await canvas.click({
+    position: {
+      x: 130,
+      y: 50,
+    },
+  });
+  await page.mouse.dblclick(130, 50);
+  await canvas.hover({
+    position: {
+      x: 130,
+      y: 40,
+    },
+  });
+  await page.mouse.down();
+  await canvas.dispatchEvent('dragstart', { dataTransfer });
+  const { x, y, image } = await trigger;
+  expect(image).toMatchImageSnapshot({
+    name: 'drag_image.png',
+  });
+  expect({ x, y }).toMatchDataSnapshot({
+    name: 'drag_image.json',
+  });
 });
--- e2e/tests/text/drag&drop/index.ts
+++ e2e/tests/text/drag&drop/index.ts
@@ -1,15 +1,8 @@
 import * as fabric from 'fabric';
 import { before } from '../../test';
 
-class Canvas extends fabric.Canvas {
-  clearContext(ctx: CanvasRenderingContext2D): void {
-    if (ctx === this.contextTop) return;
-    super.clearContext(ctx);
-  }
-}
-
 before('#canvas', (el) => {
-  const canvas = new Canvas(el, { width: 300, height: 200 });
+  const canvas = new fabric.Canvas(el, { width: 300, height: 200 });
   const textValue = 'fabric.js sandbox';
   const text = new fabric.Textbox(textValue, {
     originX: 'center',
