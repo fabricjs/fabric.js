0f996caf2799fb051aed9fcc5338ba46576af922
ShaMan123 <shacharnen@gmail.com>
2023-07-20 15:00:59 +0530

    Revert "matchers"
    This reverts commit cca2de58d26e3344b4786e03e3ab3f5440280a6e.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,6 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
 
-// extend `expect`
-import './e2e/matchers';
-
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
