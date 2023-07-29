0652cf3e786805a8e8481750b3ca8e0b8de08d75
ShaMan123 <shacharnen@gmail.com>
2023-07-29 21:31:55 +0530

    Update binaryToBuffer.ts

--- e2e/utils/binaryToBuffer.ts
+++ e2e/utils/binaryToBuffer.ts
@@ -2,5 +2,5 @@ export const binaryToBuffer = (binary: string) => {
   const [type, content] = binary
     .replace(/^data:image\/([^;]+);([^,]+),(.+)/, '$2 $3')
     .split(' ') as [BufferEncoding, string];
-  return new Uint8Array(Buffer.from(content, type).buffer);
+  return Buffer.from(content, type);
 };
