73add3cdba3397ea0f6cc14429e59938aeb0e72b
ShaMan123 <shacharnen@gmail.com>
2023-08-22 09:46:28 +0530

    Update Collection.ts

--- src/Collection.ts
+++ src/Collection.ts
@@ -131,7 +131,7 @@ export function createCollectionMixin<TBase extends Constructor>(Base: TBase) {
 
     /**
      * Returns true if collection contains an object.\
-     * **Prefer using {@link `FabricObject#isDescendantOf`} for performance reasons**
+     * **Prefer using {@link FabricObject#isDescendantOf} for performance reasons**
      * instead of `a.contains(b)` use `b.isDescendantOf(a)`
      * @param {Object} object Object to check against
      * @param {Boolean} [deep=false] `true` to check all descendants, `false` to check only `_objects`
