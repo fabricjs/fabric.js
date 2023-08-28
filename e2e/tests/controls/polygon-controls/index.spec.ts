5cca9cf98661dc2f70b09c2f908579a6b6339249
Andrea Bogazzi <andreabogazzi79@gmail.com>
2023-08-29 00:37:10 +0200

    completed test

--- e2e/tests/controls/polygon-controls/index.spec.ts
+++ e2e/tests/controls/polygon-controls/index.spec.ts
@@ -7,7 +7,6 @@ import '../../../setup';
 test('polygon controls can modify polygon - exact false', async ({ page }) => {
   const canvasUtil = new CanvasUtil(page);
   const starUtil = new ObjectUtil(page, 'star');
-  const starCenter = await starUtil.getObjectCenter();
 
   expect(await canvasUtil.screenshot()).toMatchSnapshot({
     name: 'initial.png',
@@ -36,5 +35,48 @@ test('polygon controls can modify polygon - exact false', async ({ page }) => {
   await page.mouse.up();
   expect(await canvasUtil.screenshot()).toMatchSnapshot({
     name: 'moved_controls_p4.png',
+  });
+});
+
+test('polygon controls can modify polygon - exact true', async ({ page }) => {
+  const canvasUtil = new CanvasUtil(page);
+  const starUtil = new ObjectUtil(page, 'star');
+
+  await starUtil.executeInBrowser((object) => {
+    object.strokeLineJoin = 'miter';
+    object.strokeWidth = 30;
+    object.exactBoundingBox = true;
+    object.setDimensions();
+    object.set('dirty', true);
+    object.canvas.renderAll();
+  });
+
+  expect(await canvasUtil.screenshot()).toMatchSnapshot({
+    name: 'initial-exact.png',
+  });
+
+  const p2ControlPoint = await starUtil.getObjectControlPoint('p2');
+
+  // drag the p2 control
+  await page.mouse.move(p2ControlPoint.x, p2ControlPoint.y);
+  await page.mouse.down();
+  await page.mouse.move(p2ControlPoint.x + 300, p2ControlPoint.y + 100, {
+    steps: 40,
+  });
+  await page.mouse.up();
+  expect(await canvasUtil.screenshot()).toMatchSnapshot({
+    name: 'moved_controls_p2_exact.png',
+  });
+
+  // drag in the opposite direction
+  const p4ControlPoint = await starUtil.getObjectControlPoint('p4');
+  await page.mouse.move(p4ControlPoint.x, p4ControlPoint.y);
+  await page.mouse.down();
+  await page.mouse.move(p4ControlPoint.x - 300, p4ControlPoint.y - 100, {
+    steps: 40,
+  });
+  await page.mouse.up();
+  expect(await canvasUtil.screenshot()).toMatchSnapshot({
+    name: 'moved_controls_p4_exact.png',
   });
 });
