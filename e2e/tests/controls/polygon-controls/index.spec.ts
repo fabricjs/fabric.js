ed387aaac7fb9b983e4a57b67bdad6a36afa5bc8
Andrea Bogazzi <andreabogazzi79@gmail.com>
2023-08-28 22:28:02 +0200

    test complete

--- e2e/tests/controls/polygon-controls/index.spec.ts
+++ e2e/tests/controls/polygon-controls/index.spec.ts
@@ -18,37 +18,23 @@ test('polygon controls can modify polygon - exact false', async ({ page }) => {
   // drag the p2 control
   await page.mouse.move(p2ControlPoint.x, p2ControlPoint.y);
   await page.mouse.down();
-  await page.mouse.move(p2ControlPoint.x + 300, p2ControlPoint.y, {
+  await page.mouse.move(p2ControlPoint.x + 300, p2ControlPoint.y + 100, {
     steps: 40,
   });
+  await page.mouse.up();
   expect(await canvasUtil.screenshot()).toMatchSnapshot({
     name: 'moved_controls_p2.png',
   });
-  // // drag in the opposite direction
-  // await page.mouse.move(mrControlPoint.x - 300, mrControlPoint.y, {
-  //   steps: 60,
-  // });
-  // await page.mouse.up();
-  // expect(await canvasUtil.screenshot()).toMatchSnapshot({
-  //   name: 'decrease_width_mr.png',
-  // });
 
-  // // drag the ml control
-  // const mlControlPoint = await textboxUtil.getObjectControlPoint('ml');
-  // await page.mouse.move(mlControlPoint.x, mlControlPoint.y);
-  // await page.mouse.down();
-  // await page.mouse.move(mlControlPoint.x - 300, mlControlPoint.y, {
-  //   steps: 40,
-  // });
-  // expect(await canvasUtil.screenshot()).toMatchSnapshot({
-  //   name: 'increase_width_ml.png',
-  // });
-  // // drag in the opposite direction
-  // await page.mouse.move(mlControlPoint.x + 300, mlControlPoint.y, {
-  //   steps: 60,
-  // });
-  // await page.mouse.up();
-  // expect(await canvasUtil.screenshot()).toMatchSnapshot({
-  //   name: 'decrease_width_ml.png',
-  // });
+  // drag in the opposite direction
+  const p4ControlPoint = await starUtil.getObjectControlPoint('p4');
+  await page.mouse.move(p4ControlPoint.x, p4ControlPoint.y);
+  await page.mouse.down();
+  await page.mouse.move(p4ControlPoint.x - 300, p4ControlPoint.y - 100, {
+    steps: 40,
+  });
+  await page.mouse.up();
+  expect(await canvasUtil.screenshot()).toMatchSnapshot({
+    name: 'moved_controls_p4.png',
+  });
 });
