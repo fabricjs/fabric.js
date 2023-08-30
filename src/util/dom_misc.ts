971f6dde26b32ee74f226dbbda241d2abcce133a
Andrea Bogazzi <andreabogazzi79@gmail.com>
2023-08-30 18:44:15 +0200

    Update dom_misc.ts

--- src/util/dom_misc.ts
+++ src/util/dom_misc.ts
@@ -17,8 +17,7 @@ export function getScrollLeftTop(element: HTMLElement | null) {
       scrollLeft: 0,
       scrollTop: 0,
     };
-  let left = 0,
-    top = 0;
+
   // While loop checks (and then sets element to) .parentNode OR .host
   //  to account for ShadowDOM. We still want to traverse up out of ShadowDOM,
   //  but the .parentNode of a root ShadowDOM node will always be null, instead
