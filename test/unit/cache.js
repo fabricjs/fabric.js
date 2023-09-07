a42bd9facba1ac281da490e9ca97dbeaa2854192
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-09-07 19:51:46 +0530

    Update cache.js

--- test/unit/cache.js
+++ test/unit/cache.js
@@ -11,19 +11,19 @@
 
   QUnit.test('Cache.limitDimsByArea', function(assert) {
     assert.ok(typeof fabric.cache.limitDimsByArea === 'function');
-    var [x, y] = fabric.cache.limitDimsByArea(1);
+    let [x, y] = fabric.cache.limitDimsByArea(1);
     assert.equal(x, 100);
     assert.equal(y, 100);
   });
 
   QUnit.test('Cache.limitDimsByArea ar > 1', function(assert) {
-    var [x , y] = fabric.cache.limitDimsByArea(3);
+    let [x , y] = fabric.cache.limitDimsByArea(3);
     assert.equal(x, 173);
     assert.equal(y, 57);
   });
 
   QUnit.test('Cache.limitDimsByArea ar < 1', function(assert) {
-    var [x, y] = fabric.cache.limitDimsByArea(1 / 3);
+    let [x, y] = fabric.cache.limitDimsByArea(1 / 3);
     assert.equal(x, 57);
     assert.equal(y, 173);
   });
