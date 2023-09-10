1ef6e6f87b1b0947cba0dd3c4dbf557a463c9b16
ShaMan123 <shacharnen@gmail.com>
2023-09-10 11:56:23 +0530

    BETTER
    This reverts commit e0196893f4d9d9a665be1785c3bb48b46a2e9136.
    
    better
    
    better

--- e2e/tests/template-with-node/index.spec.ts
+++ e2e/tests/template-with-node/index.spec.ts
@@ -5,33 +5,23 @@ import { render } from './common';
 
 import '../../setup';
 
-/**
- * **CAUTION**:
- * When updating snapshots we want the browser snapshot of a test to be committed and not the node snapshot
- * so we disable the node test.
- * This means that you should run tests once again after updating snapshots to ensure node snapshots pass.
- * In order to enforce that the test will fail.
- */
-test('TEST NAME', async ({ page }, { config: { updateSnapshots } }) => {
+test('TEST NAME', async ({ page }, config) => {
   await test.step('browser', async () => {
     expect(
       await new CanvasUtil(page).screenshot(),
       'browser snapshot'
     ).toMatchSnapshot({
       name: 'textbox.png',
+      maxDiffPixelRatio: 0.05,
     });
   });
 
   await test.step('node', async () => {
-    if (updateSnapshots === 'none') {
-      expect(
-        await createNodeSnapshot(render),
-        'node snapshot should match browser snapshot'
-      ).toMatchSnapshot({ name: 'textbox.png' });
-    } else {
-      test.step('Run the test again after updating snapshots to ensure node snapshots pass', () => {
-        test.fail();
-      });
-    }
+    // we want the browser snapshot of a test to be committed and not the node snapshot
+    config.config.updateSnapshots = 'none';
+    expect(
+      await createNodeSnapshot(render),
+      'node snapshot should match browser snapshot'
+    ).toMatchSnapshot({ name: 'textbox.png', maxDiffPixelRatio: 0.05 });
   });
 });
