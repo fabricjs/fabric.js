fd2c69b6a2783dc19d9b2868a514b904488960dd
ShaMan123 <shacharnen@gmail.com>
2023-09-10 13:15:37 +0530

    Update index.spec.ts

--- e2e/tests/controls/polygon-controls/index.spec.ts
+++ e2e/tests/controls/polygon-controls/index.spec.ts
@@ -1,8 +1,7 @@
 import { expect, test } from '@playwright/test';
+import setup from '../../../setup';
 import { CanvasUtil } from '../../../utils/CanvasUtil';
 import { ObjectUtil } from '../../../utils/ObjectUtil';
-
-import setup from '../../../setup';
 
 setup();
 
