8199148ac2cfd25fe86243e34f22e825bd4e298f
Andrea Bogazzi <andreabogazzi79@gmail.com>
2023-09-11 11:35:53 +0200

    more clear test

--- src/controls/wrapWithFixedAnchor.spec.ts
+++ src/controls/wrapWithFixedAnchor.spec.ts
@@ -1,6 +1,7 @@
 import type { TransformActionHandler, Transform } from '../EventTypeDefs';
 import { FabricObject } from '../shapes/Object/FabricObject';
 import { wrapWithFixedAnchor } from './wrapWithFixedAnchor';
+import { Point } from '../Point';
 
 const createTransformData = (): Transform => {
   return {
@@ -28,12 +29,35 @@ describe('wrapWithFixedAnchor', () => {
     };
     const wrappedAction = wrapWithFixedAnchor(actionToWrap);
     const transformData = createTransformData();
-    expect(transformData.target.top).toBe(100);
-    expect(transformData.target.left).toBe(100);
+    const { target } = transformData;
+    const targetAnchorPosition = target.translateToGivenOrigin(
+      new Point(target.left, target.top),
+      target.originX,
+      target.originY,
+      transformData.originX,
+      transformData.originY
+    );
+    // target position is 100,
+    expect(target.top).toBe(100);
+    expect(target.left).toBe(100);
+    // target anchor corner is different
+    expect(targetAnchorPosition.y).toBe(-203);
+    expect(targetAnchorPosition.x).toBe(-152.5);
     const event = new MouseEvent('move');
     wrappedAction(event, transformData as Transform, 0, 0);
-    expect(transformData.target.top).toBe(150.5);
-    expect(transformData.target.left).toBe(201);
+    const newTargetAnchorPosition = target.translateToGivenOrigin(
+      new Point(target.left, target.top),
+      target.originX,
+      target.originY,
+      transformData.originX,
+      transformData.originY
+    );
+    // after transform the position of the object moved.
+    expect(target.top).toBe(150.5);
+    expect(target.left).toBe(201);
+    // but the target anchor position didn't move
+    expect(newTargetAnchorPosition.y).toBe(-203);
+    expect(newTargetAnchorPosition.x).toBe(-152.5);
   });
   it('the fixed anchor point works with an origin swap', () => {
     const actionToWrap: TransformActionHandler = (eventData, transform) => {
@@ -44,12 +68,47 @@ describe('wrapWithFixedAnchor', () => {
       return true;
     };
     const wrappedAction = wrapWithFixedAnchor(actionToWrap);
+
     const transformData = createTransformData();
-    expect(transformData.target.top).toBe(100);
-    expect(transformData.target.left).toBe(100);
+    const { target } = transformData;
+    const targetAnchorPosition = target.translateToGivenOrigin(
+      new Point(target.left, target.top),
+      target.originX,
+      target.originY,
+      transformData.originX,
+      transformData.originY
+    );
+
+    // target position is 100,
+    expect(target.top).toBe(100);
+    expect(target.left).toBe(100);
+    // target anchor corner is different
+    expect(targetAnchorPosition.y).toBe(-203);
+    expect(targetAnchorPosition.x).toBe(-152.5);
     const event = new MouseEvent('move');
     wrappedAction(event, transformData as Transform, 0, 0);
-    expect(transformData.target.top).toBe(-556.5);
-    expect(transformData.target.left).toBe(-506);
+    const newTargetAnchorPosition = target.translateToGivenOrigin(
+      new Point(target.left, target.top),
+      target.originX,
+      target.originY,
+      transformData.originX,
+      transformData.originY
+    );
+    // after transform the position of the object moved.
+    expect(target.top).toBe(-556.5);
+    expect(target.left).toBe(-506);
+    // but the target anchor position didn't move
+    expect(newTargetAnchorPosition.y).toBe(-203);
+    expect(newTargetAnchorPosition.x).toBe(-152.5);
+    // while the top left corner of the object moved away
+    const topLeftCorner = target.translateToGivenOrigin(
+      new Point(target.left, target.top),
+      target.originX,
+      target.originY,
+      'left',
+      'top'
+    );
+    expect(topLeftCorner.y).toBe(-910);
+    expect(topLeftCorner.x).toBe(-859.5);
   });
 });
