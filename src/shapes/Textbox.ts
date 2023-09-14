09016605dc1da8c3a047aa701f4ff24f18c71e0a
Andrea Bogazzi <andreabogazzi79@gmail.com>
2023-09-14 09:40:57 +0200

    Update Textbox.ts

--- src/shapes/Textbox.ts
+++ src/shapes/Textbox.ts
@@ -499,7 +499,7 @@ export class Textbox<
    * Detect if a line has a linebreak and so we need to account for it when moving
    * and counting style.
    * This is important only for splitByGrapheme at the end of wrapping.
-   * If we are now wrapping the offset is always 1
+   * If we are not wrapping the offset is always 1
    * @return Number
    */
   missingNewlineOffset(lineIndex: number, skipWrapping?: boolean): 0 | 1 {
