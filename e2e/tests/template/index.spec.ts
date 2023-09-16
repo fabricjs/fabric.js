649de30b69a43dce3ab8049ee59986a4b633fd56
ShaMan123 <shacharnen@gmail.com>
2023-09-16 11:59:34 +0530

    comment

--- e2e/tests/template/index.spec.ts
+++ e2e/tests/template/index.spec.ts
@@ -12,7 +12,24 @@ test('TEST NAME', async ({ page }) => {
   // write the test
 });
 
-if (!process.env.CI) {
+/**
+ * ## Codegen
+ *
+ * This test supports code generation of tests
+ *
+ * ### Usage
+ *
+ * - Start the template
+ *   `npm run test:e2e -- e2e/tests/template/index.spec.ts`
+ * - Execute in chrome devtools:
+ *   `startRecording()`
+ * - Play with the canvas
+ * - Press the play button in playwright devtools  to execute the test
+ * - The generated code will be logged to the console and attached to the test results
+ *
+ * To edit an existing test simply add `await page.pause();` where you want to start recording.
+ */
+!process.env.CI &&
   test.only('Codegen', async ({ page, browser }) => {
     // unfortunately move events are not captured by codegen so it is not very useful for testing fabric
     // see https://github.com/microsoft/playwright/issues/21504
@@ -21,4 +38,3 @@ if (!process.env.CI) {
     // opens playwright codegen devtools
     await page.pause();
   });
-}
