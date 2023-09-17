51ca3a5e77031dddd2b5165c6635f91afcfa1301
Andrea Bogazzi <andreabogazzi79@gmail.com>
2023-09-17 23:18:59 +0200

    removed

--- .codesandbox/templates/vanilla/src/index.ts
+++ .codesandbox/templates/vanilla/src/index.ts
@@ -29,30 +29,8 @@ const text = new fabric.Textbox(textValue, {
     textValue
   ),
 });
-
-const rect = new fabric.Rect({
-  top: 65,
-  width: 150,
-  height: 150,
-  angle: 45,
-  fill: 'rgba(255,0,0,0.5)',
-  strokeWidth: 0,
-});
-
-const rect2 = new fabric.Rect({
-  left: 130,
-  width: 150,
-  height: 150,
-  angle: 45,
-  stroke: 'blue',
-  strokeWidth: 10,
-  fill: null,
-});
-
-const grp = new fabric.Group([rect, rect2, text]);
-
-canvas.add(grp);
-
+canvas.add(text);
+canvas.centerObjectH(text);
 function animate(toState) {
   text.animate(
     { scaleX: Math.max(toState, 0.1) * 2 },
