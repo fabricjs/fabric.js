dcb8982f2ce1ecdea1cc8ce5c61b76bcc6345c14
ShaMan123 <shacharnen@gmail.com>
2023-09-24 21:05:14 +0530

    benchmark

--- src/shapes/Object/Object.spec.ts
+++ src/shapes/Object/Object.spec.ts
@@ -1,3 +1,6 @@
+import { Intersection } from '../../Intersection';
+import { Point } from '../../Point';
+import { cornerPointContainsPoint } from '../../util/intersection/findCrossPoint';
 import { FabricObject } from './Object';
 
 describe('Object', () => {
@@ -57,5 +60,71 @@ describe('Object', () => {
     // test that left is unchanged because of origin being center
     expect(fObj.top).toBe(0);
     expect(fObj.left).toBe(0);
+  });
+
+  test('benchmark containsPoint, raw casting a rect', () => {
+    class Test1 extends FabricObject {
+      containsPoint(
+        point: Point,
+        absolute?: boolean,
+        calculate?: boolean
+      ): boolean {
+        return cornerPointContainsPoint(
+          point,
+          this._getCoords(absolute, calculate)
+        );
+      }
+    }
+
+    class Test2 extends FabricObject {
+      containsPoint(
+        point: Point,
+        absolute?: boolean,
+        calculate?: boolean
+      ): boolean {
+        return Intersection.isPointInPolygon(
+          point,
+          this.getCoords(absolute, calculate)
+        );
+      }
+    }
+
+    const rect1 = new Test1({
+      left: 10,
+      top: 10,
+      width: 10,
+      height: 10,
+      angle: 15.5,
+    });
+
+    const rect2 = new Test2({
+      left: 10,
+      top: 10,
+      width: 10,
+      height: 10,
+      angle: 15.5,
+    });
+
+    const points = Array(10_000)
+      .fill(null)
+      .map(
+        (_, index) => new Point(Math.random() * index, Math.random() * index)
+      );
+
+    const benchmark = (callback: VoidFunction) => {
+      const start = Date.now();
+      callback();
+      return Date.now() - start;
+    };
+
+    const benchmark1 = benchmark(() =>
+      points.forEach((point) => rect1.containsPoint(point))
+    );
+
+    const benchmark2 = benchmark(() =>
+      points.forEach((point) => rect2.containsPoint(point))
+    );
+
+    expect(benchmark2).toBeLessThan(benchmark1);
   });
 });
