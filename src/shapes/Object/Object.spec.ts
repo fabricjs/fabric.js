4d917303ab1bd2bacb89aeb04590d954652ab58c
ShaMan123 <shacharnen@gmail.com>
2023-09-24 15:19:15 +0530

    restore test

--- src/shapes/Object/Object.spec.ts
+++ src/shapes/Object/Object.spec.ts
@@ -1,3 +1,6 @@
+import { Point } from '../../Point';
+import { iMatrix } from '../../constants';
+import type { TMat2D } from '../../typedefs';
 import { FabricObject } from './Object';
 
 describe('Object', () => {
@@ -57,5 +60,93 @@ describe('Object', () => {
     // test that left is unchanged because of origin being center
     expect(fObj.top).toBe(0);
     expect(fObj.left).toBe(0);
+  });
+
+  describe('geometry', () => {
+    test.each(
+      (
+        [
+          [
+            {},
+            [
+              new Point(40, 30),
+              new Point(52, 30),
+              new Point(52, 47),
+              new Point(40, 47),
+            ],
+          ],
+          [
+            { angle: 20 },
+            [
+              new Point(40, 30),
+              new Point(51.2763114494309, 34.104241719908025),
+              new Point(45.46196901289453, 50.079016273268465),
+              new Point(34.18565756346363, 45.97477455336044),
+            ],
+          ],
+          [
+            { skewX: 45 },
+            [
+              new Point(40, 30),
+              new Point(69, 30),
+              new Point(69, 47),
+              new Point(40, 47),
+            ],
+          ],
+          [
+            { skewY: 45 },
+            [
+              new Point(40, 30),
+              new Point(52, 30),
+              new Point(52, 59),
+              new Point(40, 59),
+            ],
+          ],
+          [
+            { skewY: 45, skewX: 30, angle: 90 },
+            [
+              new Point(40, 30),
+              new Point(40, 58.74315780649914),
+              new Point(11, 58.74315780649914),
+              new Point(11, 30),
+            ],
+          ],
+        ] as const
+      )
+        .map(
+          ([options, expected]) =>
+            [
+              [options, undefined, expected],
+              [options, [2, 0, 0, 2, 35, 35] as TMat2D, expected],
+            ] as const
+        )
+        .flat()
+    )(
+      'getCoords %p and viewportTransform of %p',
+      (options, viewportTransform = iMatrix, expected) => {
+        const object = new FabricObject({
+          width: 10,
+          height: 15,
+          strokeWidth: 2,
+          top: 30,
+          left: 40,
+          ...options,
+        });
+        jest
+          .spyOn(object, 'getViewportTransform')
+          .mockReturnValue(viewportTransform);
+
+        const coords = object.getCoords();
+        expect(coords).toEqual(expected);
+
+        object.left += 5;
+        expect(object.getCoords()).toEqual(coords);
+
+        object.setCoords();
+        expect(object.getCoords()).toEqual(
+          expected.map((point) => point.add(new Point(5, 0)))
+        );
+      }
+    );
   });
 });
