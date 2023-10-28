2e2c8a1608b1b6c365f66c45f5a6c45310072857
Andrea Bogazzi <andreabogazzi79@gmail.com>
2023-10-29 00:03:06 +0200

    unused const

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,6 @@ export const VERSION = version;
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export function noop() {}
 
-export const quarterPI = Math.PI / 4;
 export const halfPI = Math.PI / 2;
 export const twoMathPi = Math.PI * 2;
 export const PiBy180 = Math.PI / 180;
