743058674c70aac8133a0266c922f189707e9c03
Andrea Bogazzi <andreabogazzi79@gmail.com>
2023-10-30 08:44:38 +0100

    comment

--- e2e/tests/text/adding-text/index.spec.ts
+++ e2e/tests/text/adding-text/index.spec.ts
@@ -99,6 +99,8 @@ for (const splitByGrapheme of [false, true]) {
       delay: 200,
     });
     await canvasUtil.press('Enter');
+    // this part of test is valid if the new line is after a styled char,
+    // and there is no style on the part of text that follows, but there is visible text.
     await expect(await canvasUtil.screenshot()).toMatchSnapshot({
       name: `6-after-adding-a-newline-splitByGrapheme-${splitByGrapheme}.png`,
       maxDiffPixelRatio: 0.03,
