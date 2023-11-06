a9df2b3c2de3608d4c26b1dc10e4d3de990bf308
Andrea Bogazzi <andreabogazzi79@gmail.com>
2023-11-06 14:25:51 +0100

    no-inheritance-in-docs

--- src/shapes/Polyline.ts
+++ src/shapes/Polyline.ts
@@ -27,6 +27,9 @@ export interface SerializedPolylineProps extends SerializedObjectProps {
   points: XY[];
 }
 
+/**
+ * @noInheritDoc
+ */
 export class Polyline<
   Props extends TOptions<FabricObjectProps> = Partial<FabricObjectProps>,
   SProps extends SerializedPolylineProps = SerializedPolylineProps,
