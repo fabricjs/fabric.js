0a966455aa9c623e5239ac9d0fe3026ca5007eae
ShaMan123 <shacharnen@gmail.com>
2023-11-10 15:23:36 +0200

    cleanup

--- .codesandbox/templates/next/instrumentation.ts
+++ .codesandbox/templates/next/instrumentation.ts
@@ -4,6 +4,7 @@ const PATH = './.instrumentation';
 
 /**
  * Used to hook into the server lifecycle
+ * An application can watch the directory for changes to {@link PATH} to listen to when the server and app are ready
  * https://nextjs.org/docs/pages/building-your-application/optimizing/instrumentation
  */
 export function register() {
