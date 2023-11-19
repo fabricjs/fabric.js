d98e53dc6f35d224d89ec585dad4ce6446a7d00a
ShaMan123 <shacharnen@gmail.com>
2023-11-19 17:50:02 +0200

    vue
    Update favicon.ico
    
    cleanup
    
    cleanup

---
+++ .codesandbox/templates/vue/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <canvas ref="canvas"></canvas>
+</template>
+
+<script>
+import * as fabric from 'fabric';
+import { markRaw } from 'vue';
+
+export default {
+  name: 'Canvas',
+  mounted() {
+    const canvas = markRaw(
+      new fabric.Canvas(this.$refs.canvas, {
+        width: 500,
+        height: 500,
+      })
+    );
+
+    const textValue = 'fabric.js sandbox';
+    const text = new fabric.Textbox(textValue, {
+      originX: 'center',
+      splitByGrapheme: true,
+      width: 200,
+      top: 20,
+      styles: fabric.util.stylesFromArray(
+        [
+          {
+            style: {
+              fontWeight: 'bold',
+              fontSize: 64,
+            },
+            start: 0,
+            end: 9,
+          },
+        ],
+        textValue
+      ),
+    });
+    canvas.add(text);
+    canvas.centerObjectH(text);
+  },
+};
+</script>
---
+++ .codesandbox/templates/vue/vite.config.ts
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
