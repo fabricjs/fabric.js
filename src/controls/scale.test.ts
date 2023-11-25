d00becf5b6c75ef76a366115c6ee3a955ab437a5
Jobby Johns <gloriousjob@gmail.com>
2023-11-25 16:34:59 -0500

    Update scale test to be simpler

--- src/controls/scale.test.ts
+++ src/controls/scale.test.ts
@@ -1,6 +1,4 @@
-import { Canvas, Point, Rect, type RectProps, type Transform } from 'fabric';
-import { degreesToRadians, saveObjectTransform } from '../util';
-import { CENTER } from '../constants';
+import { Canvas, Point, Rect, type RectProps } from 'fabric';
 import { scalingX, scalingY } from './scale';
 
 //
@@ -14,49 +12,26 @@ const createZeroThickRectangleScalingItems = (
   const target = new Rect({
     strokeWidth: 0,
     stroke: 'black',
+    ...rectOptions,
   });
-  // create mouse pointer that moves the second point slightly
-  const pointer = new Point(
-    rectOptions.height / 2,
-    rectOptions.width / 2
-  ).subtract(pointDiff);
   // add rect to canvas or we get undefined issues
   const canvas = new Canvas('canvas', { width: 100, height: 100 });
   canvas.add(target);
 
-  // create transform event based on polyline
-  const corner = 'mt';
-  const transform: Transform = {
-    target: target,
-    action: target.controls[corner].actionName,
-    actionHandler: target.controls[corner].actionHandler,
-    actionPerformed: false,
-    corner,
-    scaleX: target.scaleX,
-    scaleY: target.scaleY,
-    skewX: target.skewX,
-    skewY: target.skewY,
-    offsetX: pointer.x - target.left,
-    offsetY: pointer.y - target.top,
-    originX: CENTER,
-    originY: CENTER,
-    ex: pointer.x,
-    ey: pointer.y,
-    lastX: pointer.x,
-    lastY: pointer.y,
-    theta: degreesToRadians(target.angle),
-    width: target.width,
-    height: target.height,
-    shiftKey: false,
-    altKey: false,
-    original: {
-      ...saveObjectTransform(target),
-      originX: CENTER,
-      originY: CENTER,
-    },
-  };
-  // create empty mouse event as our tests don't care about it
-  const moveEvent = new MouseEvent('mousemove', { clientX: 0, clientY: 0 });
+  // create mouse event near center of rect, as the 0 size will put it on the middle scaler
+  const canvasOffset = canvas.calcOffset();
+  const moveEvent = new MouseEvent('mousemove', {
+    clientX: canvasOffset.left + target.left + target.width / 2 - pointDiff.x,
+    clientY: canvasOffset.top + target.top + target.height / 2 - pointDiff.y,
+  });
+  canvas.setActiveObject(target);
+  target.__corner = target._findTargetCorner(
+    canvas.getViewportPoint(moveEvent)
+  );
+  canvas._setupCurrentTransform(moveEvent, target, false);
+  const transform = canvas._currentTransform!;
+  const pointer = canvas.getScenePoint(moveEvent);
+
   // return items used by our action handler
   return { moveEvent, transform, pointer };
 };
@@ -64,7 +39,7 @@ const createZeroThickRectangleScalingItems = (
 //
 // actual tests
 //
-describe('Controls', () => {
+describe('Scale', () => {
   it('adjusts a 0 width rect for polyActionhandler without it returning Infinity/NaN', () => {
     // when NaN wasn't handled correctly, the polygon would disappear. Now, it stays
     const { moveEvent, transform, pointer } =
@@ -73,12 +48,13 @@ describe('Controls', () => {
         new Point(1, 0)
       );
 
-    scalingX(moveEvent, transform, pointer.x, pointer.y);
+    const didScale = scalingX(moveEvent, transform, pointer.x, pointer.y);
 
     expect((transform.target as Rect).scaleX).not.toBeNaN();
     expect((transform.target as Rect).scaleX).not.toBe(
       Number.POSITIVE_INFINITY
     );
+    expect(didScale).toBe(false);
   });
   it('adjusts a 0 height rect for polyActionhandler without it returning Infinity/NaN', () => {
     // when NaN wasn't handled correctly, the polygon would disappear. Now, it stays
@@ -88,11 +64,12 @@ describe('Controls', () => {
         new Point(0, 1)
       );
 
-    scalingY(moveEvent, transform, pointer.x, pointer.y);
+    const didScale = scalingY(moveEvent, transform, pointer.x, pointer.y);
 
     expect((transform.target as Rect).scaleY).not.toBeNaN();
     expect((transform.target as Rect).scaleY).not.toBe(
       Number.POSITIVE_INFINITY
     );
+    expect(didScale).toBe(false);
   });
 });
