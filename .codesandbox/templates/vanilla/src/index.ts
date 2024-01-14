e21d18065545282cd98d7f09df1a447f7fe6406e
Andrea Bogazzi <andreabogazzi79@gmail.com>
2024-01-14 17:47:41 +0100

    leave default alone

--- .codesandbox/templates/vanilla/src/index.ts
+++ .codesandbox/templates/vanilla/src/index.ts
@@ -1,6 +1,6 @@
 import * as fabric from 'fabric';
 import './styles.css';
-import { testCase } from './testcases/nestedSelections';
+import { testCase } from './testcases/simpleTextbox';
 
 const el = document.getElementById('canvas');
 const canvas = (window.canvas = new fabric.Canvas(el));
@@ -11,4 +11,4 @@ canvas.setDimensions({
   height: 500,
 });
 
-testCase(canvas, false);
+testCase(canvas);
