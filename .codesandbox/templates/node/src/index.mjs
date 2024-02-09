686f180735d4d3d5445b6b9106e166cb3edb2489
ShaMan123 <shacharnen@gmail.com>
2024-02-09 11:53:12 +0200

    demo

--- .codesandbox/templates/node/src/index.mjs
+++ .codesandbox/templates/node/src/index.mjs
@@ -1,25 +1,59 @@
-import http from 'http';
+import { Canvas } from 'canvas';
 import * as fabric from 'fabric/node';
+import http from 'http';
 
 const port = Number(process.argv[2]);
 
+fabric.FabricObject.ownDefaults.objectCaching = false;
+
 http
   .createServer((req, res) => {
-    const canvas = new fabric.StaticCanvas(null, { width: 100, height: 100 });
-    const rect = new fabric.Rect({ width: 20, height: 50, fill: '#ff0000' });
-    const text = new fabric.Text('fabric.js', { fill: 'blue', fontSize: 24 });
+    const canvas = new fabric.StaticCanvas(null, { width: 1000, height: 1000 });
+    const rect = new fabric.Rect({ width: 50, height: 50, fill: 'red' });
+    const path = fabric.util.getSmoothPathFromPoints([
+      new fabric.Point(50, 50),
+      new fabric.Point(100, 100),
+      new fabric.Point(50, 200),
+      new fabric.Point(400, 150),
+      new fabric.Point(500, 500),
+    ]);
+    const text = new fabric.FabricText(
+      new Array(9).fill('fabric.js').join(' '),
+      {
+        fill: 'blue',
+        fontSize: 24,
+        path: new fabric.Path(path),
+      }
+    );
     canvas.add(rect, text);
     canvas.renderAll();
+    const pdf = canvas.toCanvasElement(
+      1,
+      {
+        width: 460,
+        height: 450,
+      },
+      new Canvas(0, 0, 'pdf').getContext('2d')
+    );
+    const svg = canvas.toCanvasElement(
+      1,
+      {
+        width: 460,
+        height: 450,
+      },
+      new Canvas(0, 0, 'svg').getContext('2d')
+    );
+    const svg2 = text.toCanvasElement({
+      canvasElement: new Canvas(1000, 1000, 'svg').getContext('2d'),
+    });
     if (req.url === '/download') {
-      res.setHeader('Content-Type', 'image/png');
-      res.setHeader('Content-Disposition', 'attachment; filename="fabric.png"');
-      canvas.createPNGStream().pipe(res);
+      res.setHeader('Content-Disposition', 'attachment; filename="fabric.pdf"');
+      pdf.createPDFStream().pipe(res);
     } else if (req.url === '/view') {
-      canvas.createPNGStream().pipe(res);
+      pdf.createPDFStream().pipe(res);
     } else {
-      const imageData = canvas.toDataURL();
-      res.writeHead(200, '', { 'Content-Type': 'text/html' });
-      res.write(`<img src="${imageData}" />`);
+      res.writeHead(200, '', { 'Content-Type': 'application/pdf' });
+      res.write(pdf.toBuffer());
       res.end();
     }
   })
