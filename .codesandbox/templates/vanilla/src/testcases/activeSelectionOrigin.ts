b2af37ff8e4977272eceffafa86b48cc17bf0798
Andrea Bogazzi <andreabogazzi79@gmail.com>
2024-02-26 14:45:32 +0100

    fixed example

--- .codesandbox/templates/vanilla/src/testcases/activeSelectionOrigin.ts
+++ .codesandbox/templates/vanilla/src/testcases/activeSelectionOrigin.ts
@@ -1,12 +1,12 @@
 import * as fabric from 'fabric';
 
-fabric.FabricObject.ownDefaults.originX = 'right';
-fabric.FabricObject.ownDefaults.originY = 'bottom';
+fabric.FabricObject.ownDefaults.originX = 'center';
+fabric.FabricObject.ownDefaults.originY = 'center';
 
-fabric.ActiveSelection.ownDefaults.originX = 'left';
-fabric.ActiveSelection.ownDefaults.originY = 'top';
+fabric.ActiveSelection.ownDefaults.originX = 'center';
+fabric.ActiveSelection.ownDefaults.originY = 'center';
 
-export function testCase(canvas: fabric.Canvas, objectCaching = true) {
+export async function testCase(canvas: fabric.Canvas, objectCaching = true) {
   var rect1 = new fabric.Rect({
     left: 150,
     top: 150,
@@ -69,12 +69,10 @@ export function testCase(canvas: fabric.Canvas, objectCaching = true) {
   });
   canvas.add(rect3);
 
-  Promise.all([rect1, rect2, rect3].map((obj) => obj.clone())).then((objs) =>
-    canvas.add(...objs)
-  );
-
-  const as = new fabric.ActiveSelection([rect1, rect3, rect2], { canvas });
-  canvas.setActiveObject(as);
-  //   as.add(rect1);
-  //   as.add(rect3);
+  Promise.all([rect1, rect2, rect3].map((obj) => obj.clone()))
+    .then((objs) => canvas.add(...objs))
+    .then(() => {
+      const as = new fabric.ActiveSelection([rect1, rect3, rect2], { canvas });
+      canvas.setActiveObject(as);
+    });
 }
