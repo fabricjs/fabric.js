7dc7397867134ac9e4f27163a6f6bb3e0c64c3cf
zhe-he <zhe-he@qq.com>
2024-03-06 17:22:49 +0800

    update(shadow): add jest

--- src/Shadow.spec.ts
+++ src/Shadow.spec.ts
@@ -2,10 +2,14 @@ import '../jest.extend';
 import { Shadow } from './Shadow';
 
 describe('Shadow', () => {
-    it('fromObject', async () => {
-        const shadow = await Shadow.fromObject({});
-        expect(shadow).toMatchObjectSnapshot();
-        expect(shadow).toMatchObjectSnapshot({ includeDefaultValues: false });
-    });
+  it('fromObject', async () => {
+    const shadow = await Shadow.fromObject({});
+    expect(shadow).toMatchObjectSnapshot();
+    expect(shadow).toMatchObjectSnapshot({ includeDefaultValues: false });
+  });
 
+  it('fromObject is a promise', async () => {
+    const promise = Shadow.fromObject({});
+    expect(promise.then).toBeDefined();
+  });
 });
