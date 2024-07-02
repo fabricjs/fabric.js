4a40bce60498045c134c68ed58905f1d46c8f786
Andrea Bogazzi <andreabogazzi79@gmail.com>
2024-07-02 17:00:16 +0200

    remove unwanted changes

--- .codesandbox/templates/vanilla/src/testcases/filters.ts
+++ .codesandbox/templates/vanilla/src/testcases/filters.ts
@@ -35,53 +35,14 @@ export async function testCase(canvas: fabric.Canvas) {
       left: 100,
     })
   );
-
-  // 1. create & add image using custom class so we can log signal value
-
-  const imgSrc =
-    'https://images.unsplash.com/photo-1714997219940-6d9cd56c3609?dpr=2&h=200&w=120&auto=format&fit=crop&q=60';
-
-  const customImg = await fabric.FabricImage.fromURL(
-    imgSrc,
-    { crossOrigin: 'anonymous' },
-    { top: 150 }
-  );
-  const blurFilter = new fabric.filters.Blur({
-    blur: 0.9,
-  });
-  customImg.filters.push(blurFilter);
-  customImg.applyFilters();
-
-  // 2. create 2nd custom image and add it to group & on canvas
-  const inGroupImageSrc =
-    'https://images.unsplash.com/photo-1715145208397-c36448202991?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3';
-
-  const customImg2 = await fabric.FabricImage.fromURL(
-    inGroupImageSrc,
-    {
-      crossOrigin: 'anonymous',
-    },
-    {
-      left: 200,
-      top: 200,
-      width: 200,
-      height: 200,
-    }
-  );
-
-  const pixelateFilter = new fabric.filters.Pixelate({
-    blocksize: 2,
-  });
-  customImg2.filters.push(pixelateFilter);
-  customImg2.applyFilters();
   canvasImage.renderAll();
-  // const image = new fabric.FabricImage(canvasImage.lowerCanvasEl);
-  // image.filters = [
-  //   new fabric.filters.Noise({ noise: 0.2 }),
-  //   new fabric.filters.Vibrance({ vibrance: 0.3 }),
-  //   new fabric.filters.Vintage(),
-  //   new fabric.filters.Grayscale(),
-  // ];
-  // image.applyFilters();
-  canvas.add(customImg, customImg2);
+  const image = new fabric.Image(canvasImage.lowerCanvasEl);
+  image.filters = [
+    new fabric.filters.Noise({ noise: 0.2 }),
+    new fabric.filters.Vibrance({ vibrance: 0.3 }),
+    new fabric.filters.Vintage(),
+    new fabric.filters.Grayscale(),
+  ];
+  image.applyFilters();
+  canvas.add(image);
 }
