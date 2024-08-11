92e2d8cfebd9fad05cb454c2ceb8357b9cb9a449
Andrea Bogazzi <andreabogazzi79@gmail.com>
2024-08-11 23:22:32 +0200

    a test running

--- .codesandbox/templates/vanilla/src/testcases/guidelines.ts
+++ .codesandbox/templates/vanilla/src/testcases/guidelines.ts
@@ -2,7 +2,10 @@ import * as fabric from 'fabric';
 import { initAligningGuidelines } from 'fabric/extensions';
 
 export async function testCase(canvas: fabric.Canvas) {
-  initAligningGuidelines(canvas);
+  initAligningGuidelines(canvas, {
+    width: 2,
+    margin: 60,
+  });
 
   const rect = new fabric.Rect({ width: 50, height: 50, fill: 'blue' });
   const rect2 = new fabric.Rect({ width: 50, height: 50, fill: 'blue' });
