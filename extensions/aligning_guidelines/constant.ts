0536a527608215dcbb2540391bf61d75f31c47f6
zhe he <zhe-he@qq.com>
2024-08-12 16:42:37 +0800

    fix(align-guidelines): Remove unused code and parameters (#10056)

--- extensions/aligning_guidelines/constant.ts
+++ extensions/aligning_guidelines/constant.ts
@@ -1,7 +1,6 @@
 import type { AligningLineConfig } from './typedefs';
 
 export const aligningLineConfig: AligningLineConfig = {
-  offset: 0,
   /** At what distance from the shape does alignment begin? */
   margin: 4,
   /** Aligning line dimensions */
