2df59e4f69ffafbd0e660fe8d7f40c0eea93f309
Andrea Bogazzi <andreabogazzi79@gmail.com>
2024-08-15 22:46:03 +0200

    just to be sure

--- src/util/internals/cloneStyles.ts
+++ src/util/internals/cloneStyles.ts
@@ -1,14 +1,12 @@
 import type { TextStyle } from '../../shapes/Text/StyledText';
 
-export const cloneStyles = (style: TextStyle): TextStyle =>
-  Object.fromEntries(
-    Object.entries(style).map(([key, value]) => [
-      key,
-      Object.fromEntries(
-        Object.entries(value).map(([keyInner, valueInner]) => [
-          keyInner,
-          { ...valueInner },
-        ]),
-      ),
-    ]),
-  );
+export const cloneStyles = (style: TextStyle): TextStyle => {
+  const newObj: TextStyle = {};
+  Object.keys(style).forEach((key) => {
+    newObj[key] = {};
+    Object.keys(style[key]).forEach((keyInner) => {
+      newObj[key][keyInner] = { ...style[key][keyInner] };
+    });
+  });
+  return newObj;
+};
