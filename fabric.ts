5c008af61686ac5ede5160e9276cecf7b1ad5ab8
Andrea Bogazzi <andreabogazzi79@gmail.com>
2024-11-16 10:45:59 +0100

    add missing export

--- fabric.ts
+++ fabric.ts
@@ -64,7 +64,10 @@ export {
 /**
  * Exported so we can tweak default values
  */
-export { FabricObject as BaseFabricObject } from './src/shapes/Object/Object';
+export {
+  FabricObject as BaseFabricObject,
+  type DrawContext,
+} from './src/shapes/Object/Object';
 /**
  * Exported so we can tweak default values
  */
