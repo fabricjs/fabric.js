60cc8bfa3d9cf8655c8a99d18f73415ba841dc77
Andrea Bogazzi <andreabogazzi79@gmail.com>
2024-11-17 00:12:55 +0100

    redone control pictures

--- dist-extensions/fabric.d.ts
+++ dist-extensions/fabric.d.ts
@@ -47,7 +47,7 @@ FabricObject as Object, } from './src/shapes/Object/FabricObject';
 /**
  * Exported so we can tweak default values
  */
-export { FabricObject as BaseFabricObject } from './src/shapes/Object/Object';
+export { FabricObject as BaseFabricObject, type DrawContext, } from './src/shapes/Object/Object';
 /**
  * Exported so we can tweak default values
  */
