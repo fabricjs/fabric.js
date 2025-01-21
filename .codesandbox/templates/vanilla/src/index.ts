3b2742e40c014cf0225cb94eb45d2ffd4fe44288
Skye Giordano <skyebot@google.com>
2025-01-21 09:58:35 -0700

    Adds preserve object stacking to testcase

--- .codesandbox/templates/vanilla/src/index.ts
+++ .codesandbox/templates/vanilla/src/index.ts
@@ -3,12 +3,10 @@ import './styles.css';
 import { testCase } from './testcases/clipPathSelection';
 
 const el = document.getElementById('canvas');
-const canvas = (window.canvas = new fabric.Canvas(el));
-
-//  edit from here
-canvas.setDimensions({
+const canvas = (window.canvas = new fabric.Canvas(el, {
   width: 500,
   height: 500,
-});
+  preserveObjectStacking: true,
+}));
 
 testCase(canvas);
