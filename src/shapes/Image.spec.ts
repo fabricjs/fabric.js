41c352bd9fab56b5ec354b88c167e04773d26d6b
Andrea Bogazzi <andreabogazzi79@gmail.com>
2025-02-15 08:08:58 +0100

    update test

--- src/shapes/Image.spec.ts
+++ src/shapes/Image.spec.ts
@@ -1,5 +1,14 @@
 import { FabricImage } from './Image';
 import { Shadow } from '../Shadow';
+import { Brightness } from '../filters/Brightness';
+
+const mockApplyFilter = jest.fn();
+
+jest.mock('../filters/FilterBackend', () => ({
+  getFilterBackend: () => ({
+    applyFilters: mockApplyFilter,
+  }),
+}));
 
 describe('FabricImage', () => {
   describe('Svg export', () => {
@@ -23,5 +32,19 @@ describe('FabricImage', () => {
       });
       expect(img.toSVG()).toMatchSnapshot();
     });
+  });
+  describe('ApplyFilter use cacheKey', () => {
+    const imgElement = new Image(200, 200);
+    const img = new FabricImage(imgElement);
+    img.filters = [new Brightness({ brightness: 0.2 })];
+    img.applyFilters();
+    expect(mockApplyFilter).toHaveBeenCalledWith(
+      img.filters,
+      img._originalElement,
+      200,
+      200,
+      img.getElement(),
+      'texture0',
+    );
   });
 });
