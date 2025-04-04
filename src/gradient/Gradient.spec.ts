6c6f0ecead571e8e287511fb290201201a1685c0
Andrea Bogazzi <andreabogazzi79@gmail.com>
2025-04-05 00:28:20 +0200

    unit tests restored

--- src/gradient/Gradient.spec.ts
+++ src/gradient/Gradient.spec.ts
@@ -5,14 +5,16 @@ import type { GradientUnits, SVGOptions } from './typedefs';
 import { classRegistry } from '../ClassRegistry';
 
 import { describe, expect, it, test, vitest } from 'vitest';
+import { StaticCanvas } from '../canvas/StaticCanvas';
 
 vitest.mock('../util/internals/uid', () => ({
   uid: () => 0,
 }));
 
-function createLinearGradient(units: GradientUnits = 'pixels') {
+function createLinearGradient(units: GradientUnits = 'pixels', id?: string) {
   return new Gradient({
     type: 'linear',
+    id,
     gradientUnits: units,
     coords: {
       x1: 0,
@@ -46,18 +48,54 @@ function createRadialGradient(units: GradientUnits = 'pixels') {
   });
 }
 
+function createRadialGradientWithInternalRadius() {
+  return new Gradient({
+    type: 'radial',
+    coords: {
+      x1: 0,
+      y1: 10,
+      x2: 100,
+      y2: 200,
+      r1: 10,
+      r2: 50,
+    },
+    colorStops: [
+      { offset: 0, color: 'red' },
+      { offset: 1, color: 'rgba(0,255,0,0)' },
+    ],
+  });
+}
+
+function createRadialGradientSwapped() {
+  return new Gradient({
+    type: 'radial',
+    coords: {
+      x1: 0,
+      y1: 10,
+      x2: 100,
+      y2: 200,
+      r1: 50,
+      r2: 10,
+    },
+    colorStops: [
+      { offset: 0, color: 'red' },
+      { offset: 1, color: 'rgba(0,255,0,0)' },
+    ],
+  });
+}
+
 const SVG_LINEAR =
   '<linearGradient id="SVGID_0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(1 0 0 1 -50 -50)"  x1="0" y1="10" x2="100" y2="200">\n<stop offset="0%" style="stop-color:rgba(255,0,0,0);"/>\n<stop offset="100%" style="stop-color:green;"/>\n</linearGradient>\n';
 const SVG_RADIAL =
   '<radialGradient id="SVGID_0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(1 0 0 1 -50 -50)"  cx="100" cy="200" r="50" fx="0" fy="10">\n<stop offset="0%" style="stop-color:red;"/>\n<stop offset="100%" style="stop-color:rgba(0,255,0,0);"/>\n</radialGradient>\n';
 const SVG_INTERNALRADIUS =
-  '<radialGradient id="SVGID_0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(1 0 0 1 -50 -50)"  cx="100" cy="200" r="50" fx="0" fy="10">\n<stop offset="20%" style="stop-color:red;"/>\n<stop offset="100%" style="stop-color:green;stop-opacity: 0"/>\n</radialGradient>\n';
+  '<radialGradient id="SVGID_0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(1 0 0 1 -50 -50)"  cx="100" cy="200" r="50" fx="0" fy="10">\n<stop offset="20%" style="stop-color:red;"/>\n<stop offset="100%" style="stop-color:rgba(0,255,0,0);"/>\n</radialGradient>\n';
 const SVG_SWAPPED =
-  '<radialGradient id="SVGID_0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(1 0 0 1 -50 -50)"  cx="0" cy="10" r="50" fx="100" fy="200">\n<stop offset="20%" style="stop-color:green;stop-opacity: 0"/>\n<stop offset="100%" style="stop-color:red;"/>\n</radialGradient>\n';
+  '<radialGradient id="SVGID_0" gradientUnits="userSpaceOnUse" gradientTransform="matrix(1 0 0 1 -50 -50)"  cx="0" cy="10" r="50" fx="100" fy="200">\n<stop offset="20%" style="stop-color:rgba(0,255,0,0);"/>\n<stop offset="100%" style="stop-color:red;"/>\n</radialGradient>\n';
 const SVG_LINEAR_PERCENTAGE =
-  '<linearGradient id="SVGID_0" gradientUnits="objectBoundingBox" gradientTransform="matrix(1 0 0 1 0 0)"  x1="0" y1="10" x2="100" y2="200">\n<stop offset="0%" style="stop-color:red;stop-opacity: 0"/>\n<stop offset="100%" style="stop-color:green;"/>\n</linearGradient>\n';
+  '<linearGradient id="SVGID_0" gradientUnits="objectBoundingBox" gradientTransform="matrix(1 0 0 1 0 0)"  x1="0" y1="10" x2="100" y2="200">\n<stop offset="0%" style="stop-color:rgba(255,0,0,0);"/>\n<stop offset="100%" style="stop-color:green;"/>\n</linearGradient>\n';
 const SVG_RADIAL_PERCENTAGE =
-  '<radialGradient id="SVGID_0" gradientUnits="objectBoundingBox" gradientTransform="matrix(1 0 0 1 0 0)"  cx="100" cy="200" r="50" fx="0" fy="10">\n<stop offset="0%" style="stop-color:red;"/>\n<stop offset="100%" style="stop-color:green;stop-opacity: 0"/>\n</radialGradient>\n';
+  '<radialGradient id="SVGID_0" gradientUnits="objectBoundingBox" gradientTransform="matrix(1 0 0 1 0 0)"  cx="100" cy="200" r="50" fx="0" fy="10">\n<stop offset="0%" style="stop-color:red;"/>\n<stop offset="100%" style="stop-color:rgba(0,255,0,0);"/>\n</radialGradient>\n';
 
 describe('Gradient', () => {
   function fromElement(
@@ -120,45 +158,132 @@ describe('Gradient', () => {
       expect(gradient.toSVG(baseObj)).toEqual(SVG_RADIAL);
     });
 
-    // QUnit.test('toSVG radial with r1 > 0', function (assert) {
-    //   var gradient = createRadialGradientWithInternalRadius();
-    //   var obj = new fabric.Object({ width: 100, height: 100 });
-    //   assert.equalSVG(gradient.toSVG(obj), SVG_INTERNALRADIUS);
-    // });
+    test('toSVG radial with r1 > 0', () => {
+      const gradient = createRadialGradientWithInternalRadius();
+      const obj = new FabricObject({ width: 100, height: 100 });
+      expect(gradient.toSVG(obj)).toEqual(SVG_INTERNALRADIUS);
+    });
 
-    // QUnit.test('toSVG radial with r1 > 0 swapped', function (assert) {
-    //   var gradient = createRadialGradientSwapped();
-    //   var obj = new fabric.Object({ width: 100, height: 100 });
-    //   const gradientColorStops = JSON.stringify(gradient.colorStops);
-    //   assert.equalSVG(
-    //     gradient.toSVG(obj),
-    //     SVG_SWAPPED,
-    //     'it exports as expected',
-    //   );
-    //   const gradientColorStopsAfterExport = JSON.stringify(gradient.colorStops);
-    //   assert.equalSVG(
-    //     gradient.toSVG(obj),
-    //     SVG_SWAPPED,
-    //     'it exports as expected a second time',
-    //   );
-    //   assert.equalSVG(
-    //     gradientColorStops,
-    //     gradientColorStopsAfterExport,
-    //     'colorstops do not change',
-    //   );
-    // });
+    test('toSVG radial with r1 > 0 swapped', () => {
+      const gradient = createRadialGradientSwapped();
+      const obj = new FabricObject({ width: 100, height: 100 });
+      const gradientColorStops = JSON.stringify(gradient.colorStops);
+      expect(gradient.toSVG(obj), 'it exports as expected').toBe(SVG_SWAPPED);
+      const gradientColorStopsAfterExport = JSON.stringify(gradient.colorStops);
+      expect(gradient.toSVG(obj), 'it exports as expected a second time').toBe(
+        SVG_SWAPPED,
+      );
+      expect(gradientColorStops, 'colorstops do not change').toBe(
+        gradientColorStopsAfterExport,
+      );
+    });
 
-    // QUnit.test('toSVG linear objectBoundingBox', function (assert) {
-    //   var gradient = createLinearGradient('percentage');
-    //   var obj = new fabric.Object({ width: 100, height: 100 });
-    //   assert.equalSVG(gradient.toSVG(obj), SVG_LINEAR_PERCENTAGE);
-    // });
+    test('toSVG linear objectBoundingBox', () => {
+      const gradient = createLinearGradient('percentage');
+      const obj = new FabricObject({ width: 100, height: 100 });
+      expect(gradient.toSVG(obj)).toBe(SVG_LINEAR_PERCENTAGE);
+    });
 
-    // QUnit.test('toSVG radial objectBoundingBox', function (assert) {
-    //   var gradient = createRadialGradient('percentage');
-    //   var obj = new fabric.Object({ width: 100, height: 100 });
-    //   assert.equalSVG(gradient.toSVG(obj), SVG_RADIAL_PERCENTAGE);
-    // });
+    test('toSVG radial objectBoundingBox', () => {
+      const gradient = createRadialGradient('percentage');
+      const obj = new FabricObject({ width: 100, height: 100 });
+      expect(gradient.toSVG(obj)).toBe(SVG_RADIAL_PERCENTAGE);
+    });
+  });
+
+  test('properties radialGradient', () => {
+    const gradient = createRadialGradient();
+
+    expect(gradient.coords.x1).toBe(0);
+    expect(gradient.coords.y1).toBe(10);
+    expect(gradient.coords.x2).toBe(100);
+    expect(gradient.coords.y2).toBe(200);
+    expect(gradient.coords.r1).toBe(0);
+    expect(gradient.coords.r2).toBe(50);
+
+    expect(gradient.type, 'radial');
+
+    expect(gradient.colorStops[0].offset).toBe(0);
+    expect(gradient.colorStops[0].color).toBe('red');
+
+    expect(gradient.colorStops[1].offset).toBe(1);
+    expect(gradient.colorStops[1].color).toBe('rgba(0,255,0,0)');
+  });
+
+  test('toObject linearGradient', () => {
+    const gradient = createLinearGradient();
+    gradient.gradientTransform = [1, 0, 0, 1, 50, 50];
+    expect(typeof gradient.toObject === 'function');
+
+    const object = gradient.toObject();
+
+    expect(object.coords).toEqual(gradient.coords);
+    expect(object.coords, 'coords are not referenced').not.toBe(
+      gradient.coords,
+    );
+    expect(object.gradientUnits).toBe(gradient.gradientUnits);
+    expect(object.type).toBe(gradient.type);
+    expect(object.gradientTransform).toEqual(gradient.gradientTransform);
+    expect(object.gradientTransform, 'matrix is not referenced').not.toBe(
+      gradient.gradientTransform,
+    );
+    expect(object.colorStops).toEqual(gradient.colorStops);
+    expect(object.colorStops, 'colorStops are not referenced').not.toBe(
+      gradient.colorStops,
+    );
+  });
+
+  test('toObject with custom props', () => {
+    const gradient = createLinearGradient('pixels', 'myId');
+    const object = gradient.toObject(['id']);
+    expect(object.id).toBe('myId_0');
+  });
+
+  test('toObject radialGradient', () => {
+    const gradient = createRadialGradient();
+    const object = gradient.toObject();
+    expect(object.coords).toEqual(gradient.coords);
+    expect(object.coords).not.toBe(gradient.coords);
+    expect(object.type).toBe(gradient.type);
+    expect(object.colorStops).not.toBe(gradient.colorStops);
+    expect(object.colorStops).toEqual(gradient.colorStops);
+  });
+
+  test('toLive linearGradient', () => {
+    const canvas = new StaticCanvas(undefined, {
+      enableRetinaScaling: false,
+    });
+    const gradient = createLinearGradient();
+    const gradientHTML = canvas.contextContainer.createLinearGradient(
+      0,
+      0,
+      1,
+      1,
+    );
+    expect(gradient.toLive).toBeTruthy();
+    const gradientCtx = gradient.toLive(canvas.contextContainer);
+    expect(gradientCtx.toString(), 'The type match').toEqual(
+      gradientHTML.toString(),
+    );
+  });
+
+  test('toLive radialGradient', () => {
+    const canvas = new StaticCanvas(undefined, {
+      enableRetinaScaling: false,
+    });
+    const gradient = createRadialGradient();
+    const gradientHTML = canvas.contextContainer.createRadialGradient(
+      0,
+      0,
+      1,
+      1,
+      2,
+      2,
+    );
+    const gradientCtx = gradient.toLive(canvas.contextContainer);
+    expect(gradientCtx.toString(), 'is a gradient for canvas radial').toEqual(
+      gradientHTML.toString(),
+    );
   });
 
   test('registered in class registry', () => {
@@ -694,121 +819,3 @@ describe('Gradient', () => {
     expect(gradient.colorStops[3].color).toEqual('rgba(0,0,0,1)');
   });
 });
-
-/**
- * 
- * (function() {
-
-  function createRadialGradientWithInternalRadius() {
-    return new fabric.Gradient({
-      type: 'radial',
-      coords: {
-        x1: 0,
-        y1: 10,
-        x2: 100,
-        y2: 200,
-        r1: 10,
-        r2: 50
-      },
-      colorStops: [
-        { offset: 0, color: 'red' },
-        { offset: 1, color: 'green', opacity: 0 }
-      ]
-    });
-  }
-
-  function createRadialGradientSwapped() {
-    return new fabric.Gradient({
-      type: 'radial',
-      coords: {
-        x1: 0,
-        y1: 10,
-        x2: 100,
-        y2: 200,
-        r1: 50,
-        r2: 10
-      },
-      colorStops: [
-        { offset: 0, color: 'red' },
-        { offset: 1, color: 'green', opacity: 0 }
-      ]
-    });
-  }
-  
-
-
-  QUnit.test('properties radialGradient', function(assert) {
-    var gradient = createRadialGradient();
-
-    assert.equal(gradient.coords.x1, 0);
-    assert.equal(gradient.coords.y1, 10);
-    assert.equal(gradient.coords.x2, 100);
-    assert.equal(gradient.coords.y2, 200);
-    assert.equal(gradient.coords.r1, 0);
-    assert.equal(gradient.coords.r2, 50);
-
-    assert.equal(gradient.type, 'radial');
-
-    assert.equal(gradient.colorStops[0].offset, 0);
-    assert.equal(gradient.colorStops[0].color, 'red');
-    assert.ok(!('opacity' in gradient.colorStops[0]));
-
-    assert.equal(gradient.colorStops[1].offset, 1);
-    assert.equal(gradient.colorStops[1].color, 'green');
-    assert.equal(gradient.colorStops[1].opacity, 0);
-  });
-
-  QUnit.test('toObject linearGradient', function(assert) {
-    var gradient = createLinearGradient();
-    gradient.gradientTransform = [1, 0, 0, 1, 50, 50];
-    assert.ok(typeof gradient.toObject === 'function');
-
-    var object = gradient.toObject();
-
-    assert.deepEqual(object.coords, gradient.coords);
-    assert.equal(object.gradientUnits, gradient.gradientUnits);
-    assert.equal(object.type, gradient.type);
-    assert.deepEqual(object.gradientTransform, gradient.gradientTransform);
-    assert.deepEqual(object.colorStops, gradient.colorStops);
-  });
-
-  QUnit.test('toObject with custom props', function(assert) {
-    var gradient = createLinearGradient();
-    gradient.id = 'myId';
-    var object = gradient.toObject(['id']);
-    assert.equal(object.id, 'myId');
-  });
-
-  QUnit.test('toObject radialGradient', function(assert) {
-    var gradient = createRadialGradient();
-
-    assert.ok(typeof gradient.toObject === 'function');
-
-    var object = gradient.toObject();
-
-    assert.deepEqual(object.coords, gradient.coords);
-    assert.equal(object.type, gradient.type);
-    assert.deepEqual(object.colorStops, gradient.colorStops);
-  });
-
-  QUnit.test('toLive linearGradient', function(assert) {
-    var canvas = new fabric.StaticCanvas(null, {enableRetinaScaling: false});
-    var gradient = createLinearGradient();
-    var gradientHTML = canvas.contextContainer.createLinearGradient(0, 0, 1, 1);
-    assert.ok(typeof gradient.toLive === 'function');
-    var gradientCtx = gradient.toLive(canvas.contextContainer);
-    assert.equal(gradientCtx.toString(), gradientHTML.toString(), 'is a gradient for canvas radial');
-  });
-
-  QUnit.test('toLive radialGradient', function(assert) {
-    var canvas = new fabric.StaticCanvas(null, {enableRetinaScaling: false });
-    var gradient = createRadialGradient();
-    var gradientHTML = canvas.contextContainer.createRadialGradient(0, 0, 1, 1, 2, 2);
-    assert.ok(typeof gradient.toLive === 'function');
-    var gradientCtx = gradient.toLive(canvas.contextContainer);
-    assert.equal(gradientCtx.toString(), gradientHTML.toString(), 'is a gradient for canvas radial');
-  });
-
-})();
-
- */
