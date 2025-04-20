b588364cc5fb39094c0b24d2902973df01ce0057
Andrea Bogazzi <andreabogazzi79@gmail.com>
2025-04-20 11:32:42 +0200

    broke everything

--- src/env/env.spec.ts
+++ src/env/env.spec.ts
@@ -2,8 +2,8 @@ import { describe, expect, it, afterEach } from 'vitest';
 
 describe('env', () => {
   afterEach(() => {
-    delete global.window;
-    delete global.document;
+    delete globalThis.window;
+    delete globalThis.document;
   });
   it('import/require of `main` field of package.json throws', async () => {
     expect(import('../..'), 'should not resolve main').rejects.toBe(1);
@@ -11,8 +11,8 @@ describe('env', () => {
   });
 
   it('import/require sets env', async () => {
-    global.window = { devicePixelRatio: 1.25 };
-    global.document = { foo: 'bar' };
+    globalThis.window = { devicePixelRatio: 1.25 };
+    globalThis.document = { foo: 'bar' };
     const imported = await import('../../dist/index.node.cjs');
     const required = require('../../dist/index.node.cjs');
     expect(imported.getEnv().document.foo, 'should be node env').toBe(
@@ -26,12 +26,12 @@ describe('env', () => {
   it('SSR: importing fabric before window/document are defined', async () => {
     const imported = await import('../../dist/index.js');
     const required = require('../../dist/index.js');
-    expect(global.window, 'no window').toBeFalsy();
-    expect(global.document, 'no document').toBeFalsy();
+    expect(globalThis.window, 'no window').toBeFalsy();
+    expect(globalThis.document, 'no document').toBeFalsy();
     const win = { devicePixelRatio: 1.25 };
     const doc = { foo: 'bar' };
-    global.window = win;
-    global.document = doc;
+    globalThis.window = win;
+    globalThis.document = doc;
     [imported, required].forEach((fabric) => {
       expect(fabric.getEnv().window, 'window should match').toBe(win);
       expect(fabric.getEnv().document, 'document should match').toBe(doc);
