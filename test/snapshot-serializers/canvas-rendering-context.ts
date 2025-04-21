51b74cdd82214edce03a7f5c9e41685948b8a0f3
Smrtnyk <milankeser@outlook.de>
2025-04-21 21:32:05 +0200

    adjust snapshot serializer for canvas ctx

--- test/snapshot-serializers/canvas-rendering-context.ts
+++ test/snapshot-serializers/canvas-rendering-context.ts
@@ -1,5 +1,3 @@
-import { Printer, Config, Refs } from 'pretty-format';
-
 const isCanvasCtx = (val: unknown): val is CanvasRenderingContext2D =>
   // native browser
   (typeof CanvasRenderingContext2D !== 'undefined' &&
@@ -18,41 +16,8 @@ const isCanvasCtx = (val: unknown): val is CanvasRenderingContext2D =>
  */
 export default {
   test: isCanvasCtx,
-
-  serialize(val: any, config: Config, indentation: string, depth: number, refs: Refs, printer: Printer) {
-    const canvasClone = val.canvas.cloneNode(true);
-    const styleString = canvasClone.getAttribute('style') || '';
-    const normalizedStyle = styleString
-      .replace('touch-action: none; ', '')
-      .replace('touch-action: none;', '')
-
-      .replace('user-select: none; ', '')
-      .replace('user-select: none;', '');
-
-    canvasClone.setAttribute('style', normalizedStyle);
-
-    const contextProps = {
-      canvas: canvasClone
-    };
-
-    if ('createPattern' in val) {
-      Object.assign(contextProps, {
-        createPattern: '[Function]'
-      });
-    }
-
-    if ('drawImage' in val) {
-      Object.assign(contextProps, {
-        drawImage: '[Function]'
-      });
-    }
-
-    const customConfig = {
-      ...config,
-      printFunctionName: false
-    };
-
-    return printer(contextProps, customConfig, indentation, depth, refs);
+  serialize() {
+    return 'CanvasRenderingContext2d'
   }
 }
 ;
