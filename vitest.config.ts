1ba93b885431d814d0cfff30212c95e4065fa17c
Smrtnyk <milankeser@outlook.de>
2025-04-22 22:06:46 +0200

    feat(tests): add firefox to vitest browser tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -48,6 +48,25 @@ export default defineConfig({
           name: 'unit-chromium',
         },
       },
+      {
+        extends: true,
+        test: {
+          browser: {
+            provider: 'playwright',
+            enabled: true,
+            headless: true,
+            instances: [
+              {
+                browser: 'firefox',
+                context: {
+                  hasTouch: true,
+                },
+              },
+            ],
+          },
+          name: 'unit-firefox',
+        },
+      },
     ],
     include: [
       'src/**/*.test.{ts,tsx}',
