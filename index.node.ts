9d6ca4058d124568fdf23e49bb1b845a86024b70
Andrea Bogazzi <andreabogazzi79@gmail.com>
2025-11-09 16:13:51 +0100

    tests are failing

--- index.node.ts
+++ index.node.ts
@@ -2,23 +2,43 @@
 
 import { setEnv } from './src/env';
 import { getEnv, getNodeCanvas } from './src/env/node';
+import type { FabricObject } from './src/shapes/Object/FabricObject';
 
 setEnv(getEnv());
 
 // After the env is set we can export everything and expose specific node functionality
 
-import type { JpegConfig, PngConfig } from 'canvas';
+import type { CanvasRenderingContext2D, JpegConfig, PngConfig } from 'canvas';
+import type { StaticCanvasOptions, TOptions } from './fabric';
 import {
   Canvas as CanvasBase,
   StaticCanvas as StaticCanvasBase,
 } from './fabric';
-import { FabricObject } from './src/shapes/Object/Object';
+import { FabricObject as FabricObjectBase } from './src/shapes/Object/Object';
 
-FabricObject.ownDefaults.objectCaching = false;
+FabricObjectBase.ownDefaults.objectCaching = false;
+
+type PatternQuality = 'fast' | 'good' | 'best' | 'nearest' | 'bilinear';
 
 export * from './fabric';
 
+type NodeStaticCanvasOptions = {
+  patternQuality: PatternQuality;
+};
+
+const staticCanvasDefaults: TOptions<
+  StaticCanvasOptions & NodeStaticCanvasOptions
+> = {
+  ...StaticCanvasBase.ownDefaults,
+  patternQuality: 'best',
+};
+
 export class StaticCanvas extends StaticCanvasBase {
+  declare patternQuality: PatternQuality;
+
+  static ownDefaults: TOptions<StaticCanvasOptions & NodeStaticCanvasOptions> =
+    staticCanvasDefaults;
+
   getNodeCanvas() {
     return getNodeCanvas(this.getElement());
   }
@@ -27,6 +47,18 @@ export class StaticCanvas extends StaticCanvasBase {
   }
   createJPEGStream(opts?: JpegConfig) {
     return this.getNodeCanvas().createJPEGStream(opts);
+  }
+  // @ts-expect-error ctx needs to be parametric but is a huge change
+  renderCanvas(ctx: CanvasRenderingContext2D, objects: FabricObject[]) {
+    if (this.destroyed) {
+      return;
+    }
+    console.log(this.patternQuality);
+    ctx.patternQuality = this.patternQuality;
+    return super.renderCanvas(
+      ctx as unknown as globalThis.CanvasRenderingContext2D,
+      objects,
+    );
   }
 }
 
@@ -37,6 +69,11 @@ export class StaticCanvas extends StaticCanvasBase {
  * Use {@link StaticCanvas} instead.
  */
 export class Canvas extends CanvasBase {
+  declare patternQuality: PatternQuality;
+
+  static ownDefaults: TOptions<StaticCanvasOptions & NodeStaticCanvasOptions> =
+    staticCanvasDefaults;
+
   getNodeCanvas() {
     return getNodeCanvas(this.getElement());
   }
@@ -45,5 +82,16 @@ export class Canvas extends CanvasBase {
   }
   createJPEGStream(opts?: JpegConfig) {
     return this.getNodeCanvas().createJPEGStream(opts);
+  }
+  // @ts-expect-error ctx needs to be parametric but is a huge change
+  renderCanvas(ctx: CanvasRenderingContext2D, objects: FabricObject[]) {
+    if (this.destroyed) {
+      return;
+    }
+    ctx.patternQuality = this.patternQuality;
+    return super.renderCanvas(
+      ctx as unknown as globalThis.CanvasRenderingContext2D,
+      objects,
+    );
   }
 }
