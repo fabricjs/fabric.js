d757424a6f718241dfef7137bb36727a2e37262a
Andrea Bogazzi <andreabogazzi79@gmail.com>
2025-12-24 23:28:59 +0000

    test passing, drag handler in progress

--- src/controls/changeWidth.ts
+++ src/controls/changeWidth.ts
@@ -50,6 +50,42 @@ export const changeObjectWidth: TransformActionHandler = (
   return false;
 };
 
+export const changeObjectDimensionGen = (origin: 'originX' | 'originY', xorY: 'x' | 'y', scale: 'scaleX' | 'scaleY'):TransformActionHandler => (
+  eventData,
+  transform,
+  x,
+  y,
+) => {
+  const localPoint = getLocalPoint(
+    transform,
+    transform.originX,
+    transform.originY,
+    x,
+    y,
+  );
+  //  make sure the control changes width ONLY from it's side of target
+  const originValue = resolveOrigin(transform[origin]);
+  if (
+    originValue === 0 ||
+    (originValue > 0 &&
+      localPoint[xorY] < 0) ||
+    (originValue < 0 &&
+      localPoint[xorY] > 0)
+  ) {
+    const { target } = transform,
+      strokePadding =
+        target.strokeWidth / (target.strokeUniform ? target[scale] : 1),
+      multiplier = isTransformCentered(transform) ? 2 : 1,
+      oldWidth = target.width,
+      newWidth =
+        Math.abs((localPoint.x * multiplier) / target[scale]) - strokePadding;
+    target.set('width', Math.max(newWidth, 1));
+    //  check against actual target width in case `newWidth` was rejected
+    return oldWidth !== target.width;
+  }
+  return false;
+};
+
 export const changeWidth = wrapWithFireEvent(
   RESIZING,
   wrapWithFixedAnchor(changeObjectWidth),
