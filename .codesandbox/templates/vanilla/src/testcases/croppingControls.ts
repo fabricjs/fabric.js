b55c54cdc494149d1dbdbd836166971864c9f0ae
maurice_kindermann <maurice@goldencarers.com>
2026-01-19 23:28:31 +1000

    test: Add flipped image to cropping controls testcase

--- .codesandbox/templates/vanilla/src/testcases/croppingControls.ts
+++ .codesandbox/templates/vanilla/src/testcases/croppingControls.ts
@@ -3,28 +3,25 @@ import * as extensions from 'fabric/extensions';
 
 export async function testCase(canvas: fabric.Canvas) {
   canvas.setDimensions({ width: 900, height: 700 });
+  canvas.backgroundColor = '#f5f5f5';
+
+  const edgeControls = extensions.createImageEdgeResizeControls();
 
   const image = await fabric.FabricImage.fromURL(
     'https://fabricjs.com/assets/dragon.jpg',
   );
-
-  // Remove original controls and apply cropping controls
   image.once('mousedblclick', extensions.enterCropMode);
-
-  // Apply edge resize controls to replace default ml, mr, mt, mb
-  // These resize within crop bounds, then switch to uniform scaling
-  const edgeControls = extensions.createImageEdgeResizeControls();
   Object.assign(image.controls, {
     ml: edgeControls.mle,
     mr: edgeControls.mre,
     mt: edgeControls.mte,
     mb: edgeControls.mbe,
   });
-
-  // Set some initial crop to demonstrate the controls
   image.set({
-    scaleX: 0.5,
-    scaleY: 0.5,
+    left: 220,
+    top: 200,
+    scaleX: 0.4,
+    scaleY: 0.4,
     angle: 10,
     cropX: 80,
     cropY: 80,
@@ -38,7 +35,34 @@ export async function testCase(canvas: fabric.Canvas) {
     cornerStyle: 'circle',
   });
 
-  canvas.add(image);
-  canvas.centerObject(image);
-  canvas.setActiveObject(image);
+  const flippedImage = await fabric.FabricImage.fromURL(
+    'https://fabricjs.com/assets/dragon.jpg',
+  );
+  flippedImage.once('mousedblclick', extensions.enterCropMode);
+  Object.assign(flippedImage.controls, {
+    ml: edgeControls.mle,
+    mr: edgeControls.mre,
+    mt: edgeControls.mte,
+    mb: edgeControls.mbe,
+  });
+  flippedImage.set({
+    left: 620,
+    top: 200,
+    scaleX: 0.4,
+    scaleY: 0.4,
+    angle: 10,
+    flipX: true,
+    cropX: 80,
+    cropY: 80,
+    width: 400,
+    height: 400,
+    cornerStrokeColor: 'red',
+    cornerColor: 'white',
+    borderColor: 'red',
+    borderScaleFactor: 2,
+    transparentCorners: false,
+    cornerStyle: 'circle',
+  });
+
+  canvas.add(image, flippedImage);
 }
