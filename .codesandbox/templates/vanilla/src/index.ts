ed8b8e8890c65eca2c2390cd64acec717d55f1f2
Andrea Bogazzi <andreabogazzi79@gmail.com>
2026-02-28 10:44:54 +0100

    fix(): Fire mouseover/mouseleave for objects that are multi selection targets (#10874)

--- .codesandbox/templates/vanilla/src/index.ts
+++ .codesandbox/templates/vanilla/src/index.ts
@@ -1,6 +1,6 @@
 import * as fabric from 'fabric';
 import './styles.css';
-import { testCase } from './testcases/croppingControls';
+import { testCase } from './testcases/hoveringOverlapped';
 
 const el = document.getElementById('canvas');
 const canvas = (window.canvas = new fabric.Canvas(el, {
