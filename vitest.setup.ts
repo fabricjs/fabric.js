0d9d9c8cb0a02b173bd16510811e1c7a2bb9c38f
Andrea Bogazzi <andreabogazzi79@gmail.com>
2026-07-20 00:26:07 +0200

    try to make it pass

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,16 +1,39 @@
 import './vitest.extend';
 import { setEnv, setEnvFactory } from './packages/core/src/env';
-import { getEnv } from './packages/browser/src/env';
+import type { TFabricEnv } from './packages/core/src/env/types';
+import { WebGLProbe } from './packages/core/src/filters/GLProbes/WebGLProbe';
 import { beforeAll } from 'vitest';
 import { isJSDOM } from './vitest.extend';
 
-setEnvFactory(getEnv);
+const getTestEnv = (): TFabricEnv => {
+  const copyPasteData = {};
+  const testWindow = globalThis.window;
+  const testDocument = globalThis.document;
+
+  return {
+    document: testDocument,
+    window: testWindow,
+    isTouchSupported:
+      'ontouchstart' in testWindow ||
+      'ontouchstart' in testDocument ||
+      (testWindow &&
+        testWindow.navigator &&
+        testWindow.navigator.maxTouchPoints > 0),
+    WebGLProbe: new WebGLProbe(),
+    dispose() {
+      // noop
+    },
+    copyPasteData,
+  };
+};
+
+setEnvFactory(getTestEnv);
 
 // set custom env
 beforeAll(() => {
   if (isJSDOM()) {
     setEnv({
-      ...getEnv(),
+      ...getTestEnv(),
       window: globalThis.window,
       document: globalThis.document,
     });
@@ -26,6 +49,6 @@ beforeAll(() => {
       constructor(init: Partial<Touch>) {
         Object.assign(this, init);
       }
-    } as any;
+    } as unknown as typeof Touch;
   }
 });
